3ca12017b19b4f1f729a016fac575f4aaff8a4e6
Geoffrey Borggaard <geoffreyb@google.com>
2016-09-02 13:58:47 -0400

    Apparently, telling AppEngine to include an empty lib directory doesn't work. Commenting out for now.

--- rest-api/appengine_config.py
+++ rest-api/appengine_config.py
@@ -1,4 +1,6 @@
 from google.appengine.ext import vendor
 
 # Add any libraries installed in the "lib" folder.
-vendor.add('lib')
+
+# Uncomment when we actually want to load libraries.
+# vendor.add('lib')
