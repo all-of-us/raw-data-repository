fd6b8ed22eb531fc2914f06f44a109353e6a8f50
Geoffrey Borggaard <geoffreyb@google.com>
2016-09-13 17:11:21 -0400

    Removes dependency on appengine, allowing tests to work easier.

--- rest-api/config.py
+++ rest-api/config.py
@@ -2,7 +2,6 @@
 
 Contains things such as the database to connect to.
 """
-import endpoints
 
 CLOUDSQL_INSTANCE = 'pmi-drc-api-test:us-central1:pmi-rdr'
 CLOUDSQL_SOCKET = '/cloudsql/' + CLOUDSQL_INSTANCE
@@ -11,5 +10,4 @@ CLOUDSQL_USER = 'api'
 
 WEB_CLIENT_ID = 'pmi-rdr-test'
 PYTHON_TEST_CLIENT_ID = '116540421226121250670'
-ALLOWED_CLIENT_IDS = [PYTHON_TEST_CLIENT_ID, WEB_CLIENT_ID,
-                      endpoints.API_EXPLORER_CLIENT_ID]
+ALLOWED_CLIENT_IDS = [PYTHON_TEST_CLIENT_ID, WEB_CLIENT_ID]
