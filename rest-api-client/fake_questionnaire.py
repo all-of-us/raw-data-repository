47e0662168653c158cd66f9f68c62387fe2b9784
Josh Mandel <jmandel@google.com>
2016-09-30 17:44:46 -0400

    Update race, ethnicity codes

--- rest-api-client/fake_questionnaire.py
+++ rest-api-client/fake_questionnaire.py
@@ -6,28 +6,60 @@ response_text = open("questionnaire_response_example.json").read()
 
 def random_race():
     return random.choice([
-        'american-indian-or-alaskan-native',
-        'asian',
-        'black-or-african-american',
-        'native-hawaiian-or-pacific-islander',
-        'white',
-        'other',
-        'prefer-not-to-answer'
+        ['http://hl7.org/fhir/v3/Race', '1002-5', 'American Indian or Alaska Native'],
+        ['http://hl7.org/fhir/v3/Race', '2054-5', 'Black or African American'],
+        ['http://hl7.org/fhir/v3/Race', '2028-9', 'Asian'],
+        ['http://hl7.org/fhir/v3/Race', '2076-8', 'Native Hawaiian or Other Pacific Islander'],
+        ['http://hl7.org/fhir/v3/Race', '2106-3', 'White'],
+        ['http://hl7.org/fhir/v3/Race', '2131-1', 'Other Race'],
+        ['http://hl7.org/fhir/v3/NullFlavor', 'ASKU', 'Asked but no answer'],
     ])
 
 def random_ethnicity():
     return random.choice([
-        'hispanic-latino-or-spanish-origin',
-        'not-hispanic-latino-or-spanish-origin',
-        'prefer-not-to-answer'
+        ['http://hl7.org/fhir/v3/Ethnicity', '2135-2', 'Hispanic or Latino'],
+        ['http://hl7.org/fhir/v3/Ethnicity', '2186-5', 'Not Hispanic or Latino'],
+        ['http://hl7.org/fhir/v3/NullFlavor', 'ASKU', 'Asked but no answer'],
+    ])
+
+def random_ethnicity():
+    return random.choice([
+        ['http://hl7.org/fhir/v3/Ethnicity', '2135-2', 'Hispanic or Latino'],
+        ['http://hl7.org/fhir/v3/Ethnicity', '2186-5', 'Not Hispanic or Latino'],
+        ['http://hl7.org/fhir/v3/Ethnicity', '2028-9', 'Asian']
     ])
 
 def random_questionnaire(participant, response_time):
     q = copy.copy(response_text)
+    race = random_race()
+    ethnicity = ethnicity_race()
     for k,v in {
         '$participant_id': participant['drc_internal_id'],
-        '$race_code': random_race(),
+        '$race_code': race[0],
+        '$race_system': race[1],
+        '$race_display': race[2],
+        '$ethnicity_code': ethnicity[0],
+        '$ethnicity_system': ethnicity[1],
+        '$ethnicity_display': ethnicity[2],
         '$ethnicity_code': random_ethnicity(),
+        '$authored': response_time
+        }.iteritems():
+        q = q.replace(k,v)
+    print q
+    return json.loads(q)
+
+def random_questionnaire(participant, response_time):
+    q = copy.copy(response_text)
+    race = random_race()
+    ethnicity = random_ethnicity()
+    for k,v in {
+        '$participant_id': participant['drc_internal_id'],
+        '$race_code': race[0],
+        '$race_system': race[1],
+        '$race_display': race[2],
+        '$ethnicity_code': ethnicity[0],
+        '$ethnicity_system': ethnicity[1],
+        '$ethnicity_display': ethnicity[2],
         '$authored': response_time
         }.iteritems():
         q = q.replace(k,v)
