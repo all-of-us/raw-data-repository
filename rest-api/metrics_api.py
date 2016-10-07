fd526ddbf35b207d056fc2e5d6f0ea6a19f2d5cb
Geoffrey Borggaard <geoffreyb@google.com>
2016-10-07 06:22:21 -0400

    Run metrics every hour.

--- rest-api/metrics_api.py
+++ rest-api/metrics_api.py
@@ -23,7 +23,7 @@ class MetricsApi(Resource):
       offline.metrics_pipeline.MetricsPipeline().start()
 
 
-  @api_util.auth_required
+  @api_util.auth_required_cron_or_admin
   def post(self):
     resource = request.get_data()
     metrics_request = protojson.decode_message(metrics.MetricsRequest, resource)
