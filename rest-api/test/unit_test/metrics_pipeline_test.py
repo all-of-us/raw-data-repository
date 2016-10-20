174864326b5c5cf1a4e5eddea49e24997307d5c7
Geoffrey Borggaard <geoffreyb@google.com>
2016-10-20 14:36:57 -0400

    adds an end to end test.

--- rest-api/test/unit_test/metrics_pipeline_test.py
+++ rest-api/test/unit_test/metrics_pipeline_test.py
@@ -6,24 +6,24 @@ import json
 import metrics
 import participant
 import unittest
+import os
 
 from offline import metrics_pipeline
 from collections import Counter
 from google.appengine.api import memcache
+from google.appengine.api import queueinfo
+from google.appengine.datastore import datastore_stub_util
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
+from mapreduce import test_support
+from testlib import testutil
 
 
-class MetricsPipelineTest(unittest.TestCase):
+class MetricsPipelineTest(testutil.HandlerTestBase):
   def setUp(self):
+    testutil.HandlerTestBase.setUp(self)
     self.maxDiff = None
     self.longMessage = True
-    self.testbed = testbed.Testbed()
-    self.testbed.activate()
-    self.testbed.init_datastore_v3_stub()
-    self.testbed.init_memcache_stub()
-    ndb.get_context().clear_cache()
-
     self.saved_configs = copy.deepcopy(metrics_pipeline.METRICS_CONFIGS)
 
   def tearDown(self):
@@ -31,47 +31,7 @@ class MetricsPipelineTest(unittest.TestCase):
 
   def test_map_key_to_summary(self):
     key = ndb.Key(participant.Participant, '1')
-    history_list = [
-        # One participant signs up on 9/1
-        (datetime.datetime(2016, 9, 1, 11, 0, 1),
-         participant.Participant(
-             key=key,
-             date_of_birth=datetime.datetime(1975, 8, 21),
-             gender_identity=participant.GenderIdentity.MALE,
-             participant_id='1',
-             membership_tier=participant.MembershipTier.REGISTERED,
-             hpo_id='HPO1')),
-        # Accidentally changes status to FULL_PARTICIPANT
-        (datetime.datetime(2016, 9, 1, 11, 0, 2),
-         participant.Participant(
-             key=key,
-             date_of_birth=datetime.datetime(1975, 8, 21),
-             gender_identity=participant.GenderIdentity.MALE,
-             participant_id='1',
-             membership_tier=participant.MembershipTier.FULL_PARTICIPANT,
-             hpo_id='HPO1')),
-        # Fixes it back to REGISTERED
-        (datetime.datetime(2016, 9, 1, 11, 0, 3),
-         participant.Participant(
-             key=key,
-             date_of_birth=datetime.datetime(1975, 8, 21),
-             gender_identity=participant.GenderIdentity.MALE,
-             participant_id='1',
-             membership_tier=participant.MembershipTier.REGISTERED,
-             hpo_id='HPO1')),
-        # On 9/10, participant 1 changes their tier.
-        (datetime.datetime(2016, 9, 10),
-         participant.Participant(
-             key=key,
-             date_of_birth=datetime.datetime(1975, 8, 21),
-             sign_up_time=datetime.datetime(2016, 9, 1, 11, 0, 2),
-             gender_identity=participant.GenderIdentity.MALE,
-             participant_id='1',
-             membership_tier=participant.MembershipTier.VOLUNTEER,
-             hpo_id='HPO1')),
-    ]
-    for fake_date, p in history_list:
-      participant.DAO.store(p, fake_date)
+    self._populate_sample_history(key)
     results = list(metrics_pipeline.map_key_to_summary(key.to_old_key()))
     expected = [
         ({'date': '2016-09-01', 'facets': [{'type': 'HPO_ID', 'value': 'HPO1'}]},
@@ -255,6 +215,29 @@ class MetricsPipelineTest(unittest.TestCase):
     self.assertEqual('86-', metrics_pipeline._bucket_age(100))
 
 
+  def test_end_to_end(self):
+    key = ndb.Key(participant.Participant, '1')
+    self._populate_sample_history(key)
+    metrics_pipeline.MetricsPipeline().start()
+    test_support.execute_until_empty(self.taskqueue)
+
+    serving_version = metrics.get_serving_version()
+    metrics_list = list(metrics.MetricsBucket.query(ancestor=serving_version).fetch())
+    metrics_list = sorted(metrics_list, key=lambda m: m.date)
+    self.assertEquals(datetime.date(2016, 9, 1), metrics_list[0].date)
+    self.assertEquals(datetime.date(2016, 9, 10), metrics_list[1].date)
+    self.assertEquals('[{"type": "HPO_ID", "value": "HPO1"}]', metrics_list[0].facets)
+    self.assertEquals('[{"type": "HPO_ID", "value": "HPO1"}]', metrics_list[1].facets)
+    metrics0 = json.loads(metrics_list[0].metrics)
+    self.assertEquals(1, metrics0['Participant'])
+    self.assertEquals(1, metrics0['Participant.gender_identity.MALE'])
+    self.assertEquals(1, metrics0['Participant.membership_tier.REGISTERED'])
+    self.assertEquals(1, metrics0['Participant.hpo_id.HPO1'])
+
+    metrics1 = json.loads(metrics_list[1].metrics)
+    self.assertEquals(1, metrics1['Participant.membership_tier.VOLUNTEER'])
+    self.assertEquals(-1, metrics1['Participant.membership_tier.REGISTERED'])
+
   def _compare_json(self, a, b, msg=None):
     if isinstance(a, str):
       a = json.loads(a)
@@ -272,6 +255,50 @@ class MetricsPipelineTest(unittest.TestCase):
     for i, (a, b) in enumerate(zip(a_list, b_list)):
       msg = 'Comparing element {}'.format(i)
       self._compare_json(a, b, msg)
+
+  def _populate_sample_history(self, key):
+    history_list = [
+        # One participant signs up on 9/1
+        (datetime.datetime(2016, 9, 1, 11, 0, 1),
+         participant.Participant(
+             key=key,
+             date_of_birth=datetime.datetime(1975, 8, 21),
+             gender_identity=participant.GenderIdentity.MALE,
+             participant_id='1',
+             membership_tier=participant.MembershipTier.REGISTERED,
+             hpo_id='HPO1')),
+        # Accidentally changes status to FULL_PARTICIPANT
+        (datetime.datetime(2016, 9, 1, 11, 0, 2),
+         participant.Participant(
+             key=key,
+             date_of_birth=datetime.datetime(1975, 8, 21),
+             gender_identity=participant.GenderIdentity.MALE,
+             participant_id='1',
+             membership_tier=participant.MembershipTier.FULL_PARTICIPANT,
+             hpo_id='HPO1')),
+        # Fixes it back to REGISTERED
+        (datetime.datetime(2016, 9, 1, 11, 0, 3),
+         participant.Participant(
+             key=key,
+             date_of_birth=datetime.datetime(1975, 8, 21),
+             gender_identity=participant.GenderIdentity.MALE,
+             participant_id='1',
+             membership_tier=participant.MembershipTier.REGISTERED,
+             hpo_id='HPO1')),
+        # On 9/10, participant 1 changes their tier.
+        (datetime.datetime(2016, 9, 10),
+         participant.Participant(
+             key=key,
+             date_of_birth=datetime.datetime(1975, 8, 21),
+             sign_up_time=datetime.datetime(2016, 9, 1, 11, 0, 2),
+             gender_identity=participant.GenderIdentity.MALE,
+             participant_id='1',
+             membership_tier=participant.MembershipTier.VOLUNTEER,
+             hpo_id='HPO1')),
+    ]
+    for fake_date, p in history_list:
+      participant.DAO.store(p, fake_date)
+
 
 
 if __name__ == '__main__':
