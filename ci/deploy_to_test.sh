6c82f644c92ea5dcb3a5a5bd087f825d19f35670
Will Brockman <brockman@google.com>
2016-11-02 15:27:27 -0400

    Comment on requirements for deploy_to_test script to work.

--- ci/deploy_to_test.sh
+++ ci/deploy_to_test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Deploys the application to the project $1.
+# Requires environment variables GCLOUD_CREDENTIALS and GCLOUD_CREDENTIALS_KEY
+# which combine to decrypt the keys for a service account.
+
+# The service account must have role permissions as described here:
+# https://cloud.google.com/appengine/docs/python/access-control
+# In particular it requires project editor permission: this is currently
+# required to push queues.yaml, cron.yaml, index.yaml.
+
 set -e
 
 trap 'kill $(jobs -p)' EXIT
