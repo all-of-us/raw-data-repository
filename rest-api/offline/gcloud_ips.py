b88af0196bc2607a2b3ac2aea204fb99500df49a
Josh Mandel <jmandel@google.com>
2016-11-03 11:48:21 -0400

    Mock out dependency on DNS services

--- rest-api/offline/gcloud_ips.py
+++ rest-api/offline/gcloud_ips.py
@@ -17,29 +17,32 @@ START = "_cloud-netblocks.googleusercontent.com"
 Response = namedtuple("Response", ["next_entries", "ip4", "ip6"])
 
 def get_ip_ranges(start):
-    resolved_blocks = {}
-    explore(Response([start], [], []), resolved_blocks)
-    return Response(
-        [],
-        [v for r in resolved_blocks.values() for v in r.ip4],
-        [v for r in resolved_blocks.values() for v in r.ip6])
+  resolved_blocks = {}
+  explore(Response([start], [], []), resolved_blocks)
+  return Response(
+    [],
+    [v for r in resolved_blocks.values() for v in r.ip4],
+    [v for r in resolved_blocks.values() for v in r.ip6])
+
+def lookup_txt(domain):
+  return dns.resolver.query(domain, "TXT").rrset.to_text()
 
 def resolve(source):
-    q = dns.resolver.query(source, "TXT").response.answer[0].to_text()
-    return Response(
+  q = lookup_txt(source)
+  return Response(
         list(re.findall("include:(.*?\.googleusercontent\.com)", q)),
         list(re.findall("ip4:([0-9\.\/:]+)", q)),
         list(re.findall("ip6:([0-9\.\/:]+)", q))
-    )
+  )
 
 def explore(to_visit, resolved_blocks):
-    for next_entry in to_visit.next_entries:
-        if next_entry not in resolved_blocks:
-            response = resolve(next_entry)
-            resolved_blocks[next_entry] = response
-            explore(response, resolved_blocks)
+  for next_entry in to_visit.next_entries:
+    if next_entry not in resolved_blocks:
+      response = resolve(next_entry)
+      resolved_blocks[next_entry] = response
+      explore(response, resolved_blocks)
 
 if __name__ == "__main__":
-    import json
-    ips = get_ip_ranges(START)
-    print(json.dumps({'ip4': ips.ip4, 'ip6': ips.ip6}, indent=2))
+  import json
+  ips = get_ip_ranges(START)
+  print(json.dumps({'ip4': ips.ip4, 'ip6': ips.ip6}, indent=2))
--- rest-api/test/unit_test/gcloud_ips_test.py
+++ rest-api/test/unit_test/gcloud_ips_test.py
@@ -1,13 +1,28 @@
 """Tests for gcloud_ips"""
 
+import dns
 import unittest
+from mock import Mock, MagicMock
 from netaddr import IPNetwork
 
+
 class GcloudIpsTest(unittest.TestCase):
+  RESPONSES = [
+    '_cloud-netblocks.googleusercontent.com. 2677 IN TXT "v=spf1 include:_cloud-netblocks1.googleusercontent.com include:_cloud-netblocks2.googleusercontent.com include:_cloud-netblocks3.googleusercontent.com include:_cloud-netblocks4.googleusercontent.com include:_cloud-netblocks5.googleusercontent.com ?all"',
+    '_cloud-netblocks1.googleusercontent.com. 3599 IN TXT "v=spf1 ip4:8.34.208.0/20 ip4:8.35.192.0/21 ip4:8.35.200.0/23 ip4:108.59.80.0/20 ip4:108.170.192.0/20 ip4:108.170.208.0/21 ip4:108.170.216.0/22 ip4:108.170.220.0/23 ip4:108.170.222.0/24 ?all"',
+    '_cloud-netblocks2.googleusercontent.com. 3599 IN TXT "v=spf1 ip4:162.216.148.0/22 ip4:162.222.176.0/21 ip4:173.255.112.0/20 ip4:192.158.28.0/22 ip4:199.192.112.0/22 ip4:199.223.232.0/22 ip4:199.223.236.0/23 ip4:23.236.48.0/20 ip4:23.251.128.0/19 ?all"',
+    '_cloud-netblocks3.googleusercontent.com. 3599 IN TXT "v=spf1 ip4:107.167.160.0/19 ip4:107.178.192.0/18 ip4:146.148.2.0/23 ip4:146.148.4.0/22 ip4:146.148.8.0/21 ip4:146.148.16.0/20 ip4:146.148.32.0/19 ip4:146.148.64.0/18 ip4:130.211.4.0/22 ?all"',
+    '_cloud-netblocks4.googleusercontent.com. 3599 IN TXT "v=spf1 ip4:130.211.8.0/21 ip4:130.211.16.0/20 ip4:130.211.32.0/19 ip4:130.211.64.0/18 ip4:130.211.128.0/17 ip4:104.154.0.0/15 ip4:104.196.0.0/14 ip4:208.68.108.0/23 ip4:35.184.0.0/15 ip4:35.186.0.0/16 ?all"',
+    '_cloud-netblocks5.googleusercontent.com. 3226 IN TXT "v=spf1 ip6:2600:1900::/35 ?all"'
+  ]
+
 
   def test_gcloud_ips(self):
+    import offline.gcloud_ips
     from offline.gcloud_ips import START, get_ip_ranges
+    offline.gcloud_ips.lookup_txt = MagicMock(side_effect=self.RESPONSES)
     response = get_ip_ranges(START)
+    self.assertEqual(6, offline.gcloud_ips.lookup_txt.call_count)
     self.assertEqual(response.next_entries, [])
     self.assertTrue(len(response.ip4) > 0)
     self.assertTrue(len(response.ip6) > 0)
