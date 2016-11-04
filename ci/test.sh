76d1fc11c69f43939e3c8868184f0d6d6c1ba3b5
Geoffrey Borggaard <geoffreyb@google.com>
2016-11-04 14:35:32 -0400

    Run a real command to init the config store.

--- ci/test.sh
+++ ci/test.sh
@@ -20,16 +20,24 @@ until $(curl -s --fail http://localhost:8000); do
     sleep .25
 done
 
+cd ../../rest-api-client
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+
 # The first call will often fail, as it will populate the config store.
 # And due to eventual consistency on the config indexes, it often can not be
 # used immediately.
 # Burn a request to populate the config store.
 set +e
-curl -s --fail http://localhost:8080/rdr/v1/Metrics
+python participant_client.py  --instance http://localhost:8080
 sleep 2 # Give the indices a chance to get updated.
 set -e
 
 cd ..
+# The config store needs to be initialized before running this.
 ./ci/run_command.sh ci/init_config.py
 
 cd rest-api/test
@@ -37,12 +45,6 @@ GCLOUD_PATH=$(which gcloud)
 SDK_PATH=${GCLOUD_PATH%/bin/gcloud}
 
 ./run_tests.sh -g $SDK_PATH
-
-cd ../../rest-api-client
-pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip install -r requirements.txt
 
 python participant_client.py  --instance http://localhost:8080
 python load_fake_participants.py --count 2 --instance http://localhost:8080
