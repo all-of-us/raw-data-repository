80980c230574e5bc0bb457bc73203e747f4f5b88
Geoffrey Borggaard <geoffreyb@google.com>
2016-11-04 13:19:12 -0400

    Allow insecure requests for circle.

--- ci/configure_example_user.py
+++ ci/configure_example_user.py
@@ -1,8 +1,7 @@
 from google.appengine.ext import ndb
-from config import Config, ALLOWED_USER
+from config import Config, ALLOWED_USER, ALLOW_INSECURE
 
 example_user = 'example@example.com'
-Config(config_key=ALLOWED_USER,
-       value=example_user).put()
-
+Config(config_key=ALLOWED_USER, value=example_user).put()
+Config(config_key=ALLOW_INSECURE, value='True').put()
 print("Added %s"%example_user)
