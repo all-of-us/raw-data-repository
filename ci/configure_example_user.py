3d9fd86669a738ac1db4dd19c249a4b85b99b34c
Geoffrey Borggaard <geoffreyb@google.com>
2016-11-04 14:05:21 -0400

    Delete the existing ALLOW_SECURE config entry before adding a new one.

--- ci/configure_example_user.py
+++ ci/configure_example_user.py
@@ -3,5 +3,8 @@ from config import Config, ALLOWED_USER, ALLOW_INSECURE
 
 example_user = 'example@example.com'
 Config(config_key=ALLOWED_USER, value=example_user).put()
+for c in Config.query(Config.config_key==ALLOW_INSECURE).fetch():
+  c.delete()
+
 Config(config_key=ALLOW_INSECURE, value='True').put()
 print("Added %s"%example_user)
