7ea83de66e0bfcc2c325ee55a55c568a304ee1dc
Josh Mandel <jmandel@gmail.com>
2017-01-04 17:24:47 -0500

    Give physical eval data to 40% of fake patients

--- rest-api/test/client_test/evaluation.py
+++ rest-api/test/client_test/evaluation.py
@@ -2,14 +2,17 @@
 
 import json
 import unittest
+import datetime
 
 import test_util
+
 
 class TestEvaluation(unittest.TestCase):
   def setUp(self):
     self.maxDiff = None
     self.client = test_util.get_client('rdr/v1')
     self.participant_id = test_util.create_participant(self.client)
+    self.when = datetime.datetime.now().isoformat()
 
   def test_insert_eval(self):
     evaluation_files = [
@@ -18,7 +21,11 @@ class TestEvaluation(unittest.TestCase):
 
     for json_file in evaluation_files:
       with open(json_file) as f:
-        evaluation = json.load(f)
+        evaluation = f.read() \
+          .replace('$authored_time', self.when) \
+          .replace('$participant_id', self.participant_id)
+
+        evaluation = json.loads(evaluation)
         path = 'Participant/{}/PhysicalEvaluation'.format(self.participant_id)
         test_util.round_trip(self, self.client, path, evaluation)
 
