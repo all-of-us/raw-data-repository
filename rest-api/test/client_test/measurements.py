8c517ef168ae9bc076ded0fe2c05a4b000bedc28
Dan Rodney <danqrodney>
2017-02-01 09:18:31 -0600

    Fixing client test to use absolute paths when following next links.

--- rest-api/test/client_test/measurements.py
+++ rest-api/test/client_test/measurements.py
@@ -53,19 +53,19 @@ class TestPhysicalMeasurements(unittest.TestCase):
     sync_response = self.client.request_json('PhysicalMeasurements/_history')
     self.assertEquals('Bundle', sync_response['resourceType'])
     self.assertEquals('history', sync_response['type'])
-    link = sync_response.get('link')
-    self.assertTrue(link)          
+    link = sync_response.get('link')    
+    print "Link: {}".format(link)          
     self.assertTrue(sync_response.get('entry'))    
     self.assertTrue(len(sync_response['entry']) > 1)
     
     sync_response = self.client.request_json('PhysicalMeasurements/_history?_count=1')
     self.assertTrue(sync_response.get('entry'))    
     link = sync_response.get('link')
-    self.assertTrue(link)
+    self.assertTrue(link)    
     self.assertEquals("next", link[0]['relation'])    
     self.assertEquals(1, len(sync_response['entry']))
     
-    sync_response_2 = self.client.request_json(link[0]['url'])
+    sync_response_2 = self.client.request_json(link[0]['url'], absolute_path=True)
     self.assertEquals(1, len(sync_response_2['entry']))
     self.assertNotEquals(sync_response['entry'][0], sync_response_2['entry'][0])
         
