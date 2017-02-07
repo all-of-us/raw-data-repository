99f5779d7cf9415a916560a5b4614b3ef89c26d7
Mark Fickett <mwf@google.com>
2017-02-07 10:36:15 -0500

    Remove unused code from biobank client test.

--- rest-api/test/client_test/biobank_samples.py
+++ rest-api/test/client_test/biobank_samples.py
@@ -1,10 +1,7 @@
-"""Test for the biobank samples endpoint."""
-
-import json
 import unittest
-from client.client import HttpException
 
 import test_util
+
 
 class TestBiobankSamples(unittest.TestCase):
   def setUp(self):
@@ -12,6 +9,7 @@ class TestBiobankSamples(unittest.TestCase):
 
   def test_reload_no_files(self):
     self.client.request_json('BiobankSamplesReload', 'GET', cron=True)
+
 
 if __name__ == '__main__':
   unittest.main()
