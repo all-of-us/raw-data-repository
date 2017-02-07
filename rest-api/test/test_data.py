8fbf74186d0cb7a57bb48e5d5e30470406863279
Mark Fickett <mwf@google.com>
2017-02-07 14:34:23 -0500

    Switch to %s substitutions in data, add failing amendment test.

--- rest-api/test/test_data.py
+++ rest-api/test/test_data.py
@@ -11,7 +11,18 @@ def data_path(filename):
 
 def load_measurement_json(participant_id, now=None):
   with open(data_path('measurements-as-fhir.json')) as measurements_file:
-    json_text = measurements_file.read()
-    json_text = json_text.replace('$participant_id', participant_id)
-    json_text = json_text.replace('$authored_time', now or datetime.datetime.now().isoformat())
+    json_text = measurements_file.read() % {
+      'participant_id': participant_id,
+      'authored_time': now or datetime.datetime.now().isoformat(),
+    }
     return json.loads(json_text)  # deserialize to validate
+
+
+def load_measurement_json_amendment(participant_id, amended_id, now=None):
+  with open(data_path('measurements-as-fhir-amendment.json')) as measurements_file:
+    json_text = measurements_file.read() % {
+      'participant_id': participant_id,
+      'physical_measurement_id': amended_id,
+      'authored_time': now or datetime.datetime.now().isoformat(),
+    }
+    return json.loads(json_text)
--- rest-api/test/unit_test/physical_measurements_api_test.py
+++ rest-api/test/unit_test/physical_measurements_api_test.py
@@ -9,7 +9,7 @@ import physical_measurements_api
 import pprint
 
 from test.unit_test.unit_test_util import NdbTestBase
-from test.test_data import data_path, load_measurement_json
+from test import test_data
 
 _AUTH_USER = 'authorized@gservices.act'
 _CONFIG_USER_INFO = {
@@ -28,29 +28,47 @@ class PhysicalMeasurementsAPITest(NdbTestBase):
     self.app = main.app.test_client()
     config.override_setting(config.USER_INFO, _CONFIG_USER_INFO)
 
+  def post_json(self, local_path, post_data, expected_status=httplib.OK):
+    response = self.app.post(
+        main.PREFIX + local_path,
+        data=json.dumps(post_data),
+        content_type='application/json')
+    self.assertEquals(response.status_code, expected_status)
+    return json.loads(response.data)
+
   def tearDown(self):
     super(PhysicalMeasurementsAPITest, self).tearDown()
     config.remove_override(config.USER_INFO)
 
   @mock.patch('api_util.get_oauth_id')
   def test_original_measurement(self, mock_get_oauth_id):
+    mock_get_oauth_id.return_value = _AUTH_USER
     # Sanity check: Verify that there is no PhysicalMeasurement yet.
     existing = measurements.DAO().list(_PARTICIPANT)
     self.assertItemsEqual(existing['items'], [])
     # Simulate a POST to create a novel PhysicalMeasurement.
-    post_data = load_measurement_json(_PARTICIPANT)
-    mock_get_oauth_id.return_value = _AUTH_USER
-    response = self.app.post(
-        main.PREFIX + 'Participant/P123/PhysicalMeasurements',
-        data=json.dumps(post_data),
-        content_type='application/json')
+    response_data = self.post_json(
+        'Participant/P123/PhysicalMeasurements',
+        test_data.load_measurement_json(_PARTICIPANT))
 
     # Verify that the request succeeded and 1 bundle was created.
-    self.assertEquals(response.status_code, httplib.OK)
-    response_data = json.loads(response.data)
     self.assertIn('id', response_data)
     self.assertIn('entry', response_data)
 
     stored_items = measurements.DAO().list(_PARTICIPANT)['items']
     self.assertEquals(len(stored_items), 1)
     self.assertEquals(response_data['id'], stored_items[0]['id'])
+
+  @mock.patch('api_util.get_oauth_id')
+  def test_missing_amended(self, mock_get_oauth_id):
+    mock_get_oauth_id.return_value = _AUTH_USER
+
+    # Set up: create a novel PhysicalMeasurement.
+    response_data = self.post_json(
+        'Participant/P123/PhysicalMeasurements',
+        test_data.load_measurement_json(_PARTICIPANT))
+    created_id = response_data['id']
+
+    response_data = self.post_json(
+        'Participant/P123/PhysicalMeasurements',
+        test_data.load_measurement_json_amendment(_PARTICIPANT, created_id))
