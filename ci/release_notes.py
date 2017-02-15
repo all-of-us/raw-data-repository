97ff67884d52931f871eeaf24e12195800add8a0
Mark Fickett <mwf@google.com>
2017-02-15 10:51:30 -0500

    Rewrite to fetch serving version.

--- ci/release_notes.py
+++ ci/release_notes.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
-"""Generates release notes."""
+"""Generates release notes for a given deployed AppEngine environment based on code at HEAD.
 
+We expect deployed versions to be named with a git tag like v0-1-rc14. This script gets a project's
+current version, then gets commit messages from that tag to HEAD, and formats the messages (with
+JIRA's style of markup) to make release notes. HEAD is assumed (but not required) to also be tagged
+for the release that's going out / the release for which we're generating notes.
+"""
+
+import httplib
+import json
+import logging
 import re
 import subprocess
+import sys
+import urllib2
 
 _REPO_BASE_URL = 'https://github.com/vanderbilt/pmi-data'
 
@@ -19,60 +30,75 @@ h2. since %(prev)s
 """
 
 
-def _get_tag_output_lines():
-  """Lists lines with git tags, ordered most recent first. May include non-release tags."""
-  process = subprocess.Popen(
-      ['git', 'log', '--tags', '--simplify-by-decoration', '--pretty=format:%ci %d'],
-      stdout=subprocess.PIPE)
-  if process.wait() != 0:
-    raise RuntimeError('Getting tags failed.')
-  stdout, _ = process.communicate()
-  return stdout.split('\n')
-
-
-def _get_current_and_prev_release_tags(tag_lines):
-  """Extracts the current and previous (or None) releases' git tags from ordered git tag lines."""
-  release_tags = []
-  for line in tag_lines:
-    line_tags = _RELEASE_TAG_RE.findall(line)
-    if line_tags:
-      if len(set(line_tags)) != 1:
-        raise RuntimeError('Multiple releases %s on same commit %r.' % (line_tags, line))
-      release_tags.append(line_tags[0])
-  if len(release_tags) >= 2:
-    return release_tags[:2]
-  elif len(release_tags) == 0:
-    raise RuntimeError('No valid release tags.')
-  else:
-    return release_tags[0], None
-
-
 def _linkify_pull_request_ids(text):
+  """Converts all substrings like "(#123)" to links to pull requests."""
   return re.sub(
       r'\(#([0-9]+)\)',
       r'([#\1|%s/pull/\1])' % _REPO_BASE_URL,
       text)
 
 
-def _get_release_notes(current_tag, prev_tag):
-  """Formats release notes from git commit messages between the given tags."""
-  if prev_tag is not None:
-    version_range = '%s..%s' % (prev_tag, current_tag)
-  else:
-    version_range = current_tag
+def _get_deployed_version(project_id):
+  """Queries the app's version API (root path) for the currently serving version ID.
+
+  This assumes app versions are of the form "<git tag>.<extra AppEngine identifier>", for example
+  "v0-1-rc14.399070594381287524".
+
+  Args:
+    project_id: AppEngine project ID, for example all-of-us-rdr-staging. The appspot URL is
+        constructed from this ID.
+
+  Returns:
+    A version ID / git tag of the currently serving code in the given environment.
+  """
+  version_url = 'https://%s.appspot.com/' % project_id
+  response = urllib2.urlopen(version_url)
+  if response.getcode() != httplib.OK:
+    raise RuntimeError('HTTP %d for %r' % (response.getcode(), version_url))
+  app_version = json.loads(response.read())['version_id']
+  return app_version.split('.')[0]
+
+
+def _get_release_notes_since_tag(deployed_tag):
+  """Formats release notes for JIRA from commit messages, from the given tag to HEAD."""
   process = subprocess.Popen(
-      ['git', 'log', version_range, _LOG_LINE_FORMAT],
+      ['git', 'log', deployed_tag + '..', _LOG_LINE_FORMAT],
       stdout=subprocess.PIPE)
   if process.wait() != 0:
     raise RuntimeError('Getting commit messages failed.')
   stdout, _ = process.communicate()
+  commit_messages = stdout.decode('utf8')  # Keep further text processing from choking on non-ASCII.
   return _RELEASE_NOTES_T % {
-    'current': current_tag,
-    'prev': prev_tag or 'beginning of history',
-    'history': _linkify_pull_request_ids(stdout),
+    'current': _find_current_commit_tag(),
+    'prev': deployed_tag,
+    'history': _linkify_pull_request_ids(commit_messages),
   }
 
 
+def _find_current_commit_tag():
+  """Returns the current git tag (or tag + short commit hash) of the current commit."""
+  process = subprocess.Popen(['git', 'describe', '--tags'], stdout=subprocess.PIPE)
+  if process.wait() != 0:
+    raise RuntimeError('Getting current tag.')
+  stdout, _ = process.communicate()
+  tag = stdout.strip()
+  if not _RELEASE_TAG_RE.match(tag):
+    logging.warning('Current tag %r does not look like a release tag.', tag)
+  return tag
+
+
+def main():
+  """Looks up version tags, gets commit logs, and prints release notes."""
+  if len(sys.argv) != 2:
+    logging.critical('Usage: %s appengine_project_id', sys.argv[0])
+    sys.exit(1)
+  appengine_id = sys.argv[1]
+  deployed_version = _get_deployed_version(appengine_id)
+  if not _RELEASE_TAG_RE.match(deployed_version):
+    logging.warning(
+        'Tag %r from %r does not look like a release tag.', deployed_version, appengine_id)
+  print _get_release_notes_since_tag(deployed_version)
+
+
 if __name__ == '__main__':
-  current, prev = _get_current_and_prev_release_tags(_get_tag_output_lines())
-  print _get_release_notes(current, prev)
+  main()
