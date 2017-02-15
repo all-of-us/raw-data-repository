cb5723e809bbcf9818f48e48cfcbdfa6bab1012d
Dan Rodney <danqrodney>
2017-02-15 17:17:51 -0600

    Changing setup_local_database.sh to allow for nopassword, db_user, db_name args.
    Changing test.sh to set up a local database in the Circle CI environment.
    
    Changing circle.yml to include MySQL.

--- rest-api/tools/setup_local_database.sh
+++ rest-api/tools/setup_local_database.sh
@@ -8,7 +8,6 @@
 # If you have an environment variable named "MYSQL_ROOT_PASSWORD" it will be used as the password
 # to connect to the database; by default, the password "root" will be used.
 
-CONNECTION_STRING=mysql+mysqldb://root:root@localhost/rdr
 PASSWORD=root
 DB_CONNECTION_NAME=
 DB_USER=root
@@ -16,10 +15,27 @@ DB_NAME=rdr
 DB_INFO_FILE=/tmp/db_info.json
 CREATE_DB_FILE=/tmp/create_db.sql
 
+PASSWORD_ARGS='-p"${PASSWORD}"'
+PASSWORD_STRING=":${PASSWORD}"
+while true; do
+  case "$1" in
+    --nopassword) PASSWORD=; PASSWORD_ARGS=; PASSWORD_STRING=; shift 1;;
+    --db_user) DB_USER=$2; shift 2;;
+    --db_name) DB_NAME=$2; shift 2;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
 if [ ! -z "${MYSQL_ROOT_PASSWORD}" ]
 then
   PASSWORD="${MYSQL_ROOT_PASSWORD}"
+  PASSWORD_ARGS='-p"${PASSWORD}"'
+  PASSWORD_STRING=":${PASSWORD}"
 fi
+
+# Export this so Alembic can find it.
+export DB_CONNECTION_STRING="mysql+mysqldb://${DB_USER}${PASSWORD_STRING}@localhost/${DB_NAME}"
 
 function finish {
   rm -f ${DB_INFO_FILE}
@@ -27,9 +43,9 @@ function finish {
 }
 trap finish EXIT
 
-echo '{"db_connection_string": "'$CONNECTION_STRING'", ' \
+echo '{"db_connection_string": "'$DB_CONNECTION_STRING'", ' \
      ' "db_password": "'$PASSWORD'", ' \
-     ' "db_connection_name": "'$INSTANCE_CONNECTION_NAME'", '\
+     ' "db_connection_name": "", '\
      ' "db_user": "'$DB_USER'", '\
      ' "db_name": "'$DB_NAME'" }' > $DB_INFO_FILE
 echo 'DROP DATABASE IF EXISTS '$DB_NAME'; CREATE DATABASE '$DB_NAME > $CREATE_DB_FILE
