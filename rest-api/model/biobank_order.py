05a7f618a5b16d824ee51508cc0a0257ac993abd
Mark Fickett <mwf@google.com>
2017-02-21 18:24:58 -0500

    Add model class docs for BiobankOrder(ID,Samle).

--- rest-api/model/biobank_order.py
+++ rest-api/model/biobank_order.py
@@ -4,13 +4,16 @@ from model.base import Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Index, Boolean, Text
 
+
 class BiobankOrder(Base):
-  """An order requesting samples. The order contains a list of samples stored in 
+  """An order requesting samples. The order contains a list of samples stored in
   BiobankOrderedSample; the actual delivered and stored samples are stored in BiobankStoredSample.
   Our reconciliation report compares the two.
-  """    
+  """
   __tablename__ = 'biobank_order'
-  biobankOrderId = Column('biobank_order_id', Integer, primary_key=True, autoincrement=False)
+  # We want autoincrement=False for the ID, but omit it to avoid warnings and enforce a
+  # client-specified ID in the DAO layer.
+  biobankOrderId = Column('biobank_order_id', Integer, primary_key=True)
   participantId = Column('participant_id', Integer, ForeignKey('participant.participant_id'), 
                          nullable=False)  
   created = Column('created', DateTime, default=clock.CLOCK.now, nullable=False)  
@@ -25,14 +28,25 @@ class BiobankOrder(Base):
                          nullable=False)
   logPosition = relationship('LogPosition')  
 
-class BiobankOrderIdentifier(Base):  
-  __tablename__ = 'biobank_order_identifier'  
+
+class BiobankOrderIdentifier(Base):
+  """Arbitrary IDs for a BiobankOrder in other systems.
+
+  Other clients may create these, but they must be unique within each system.
+  """
+  __tablename__ = 'biobank_order_identifier'
   system = Column('system', String(80), primary_key=True)
   value = Column('value', String(80), primary_key=True)
-  orderId = Column('order_id', Integer, ForeignKey('biobank_order.biobank_order_id'), 
+  orderId = Column('order_id', Integer, ForeignKey('biobank_order.biobank_order_id'),
                    nullable=False)
 
-class BiobankOrderedSample(Base):  
+
+class BiobankOrderedSample(Base):
+  """Samples listed by a Biobank order.
+
+  These are distinct from BiobankSamples, which tracks received samples. The two should eventually
+  match up, but we see BiobankOrderedSamples first and track them separately.
+  """
   __tablename__ = 'biobank_ordered_sample'
   orderId = Column('order_id', Integer, ForeignKey('biobank_order.biobank_order_id'), 
                    primary_key=True)  
@@ -42,4 +56,3 @@ class BiobankOrderedSample(Base):
   collected = Column('collected', DateTime)
   processed = Column('processed', DateTime)
   finalized = Column('finalized', DateTime)
-
