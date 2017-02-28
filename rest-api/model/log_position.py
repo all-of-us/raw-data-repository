71fd8bb7c3481013d7095e07082aef3eb84cf2f4
Mark Fickett <mwf@google.com>
2017-02-28 10:23:43 -0500

    Move docs to LogPosition class.

--- rest-api/model/log_position.py
+++ rest-api/model/log_position.py
@@ -8,6 +8,11 @@ class LogPosition(Base):
   are not guaranteed to be unique, and not guaranteed to be written in the order that transactions
   are committed; as a result, relying on them for ordering when doing a sync can lead to entities
   being missed.)
+
+  Models that contribute to LogPosition should have a logPosition (relationship) and logPositionId
+  (foreign key to log_position_id below). Whenever they are created or updated, the associated DAO
+  must overwrite the model's logPosition with a new LogPosition() which, when the object is
+  committed, will increment the global log.
   """
   __tablename__ = 'log_position'
   logPositionId = Column('log_position_id', Integer, primary_key=True)
