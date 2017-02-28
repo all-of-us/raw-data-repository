0495e84ef61f9ea2f5905e031d0c4392e23331d5
Dan Rodney <danqrodney>
2017-02-28 12:58:04 -0600

    Reverting change to CodeDao (which wasn't supposed to be a part of this PR)

--- rest-api/dao/code_dao.py
+++ rest-api/dao/code_dao.py
@@ -24,9 +24,6 @@ class CodeBookDao(BaseDao):
 
   def get_id(self, obj):
     return obj.codeBookId
-    
-  def import_codebook(self, codebook_json):
-    pass
 
 class CodeDao(UpdatableDao):
   def __init__(self):
