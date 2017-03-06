37c7dd51fc77f95df9bc15456adb3db7cfb71abd
Mark Fickett <mwf@google.com>
2017-03-06 10:00:49 -0500

    rm summary update

--- rest-api/dao/participant_summary_dao.py
+++ rest-api/dao/participant_summary_dao.py
@@ -1,4 +1,3 @@
-import config
 from dao.base_dao import UpdatableDao
 from model.participant_summary import ParticipantSummary
 
@@ -9,10 +8,3 @@ class ParticipantSummaryDao(UpdatableDao):
 
   def get_id(self, obj):
     return obj.participantId
-
-  def update_from_biobank_stored_samples(self, session, participant_id, all_samples):
-    """Updates the count of samples (for baseline tests only) received for a participant."""
-    summary = self.get_with_session(session, participant_id)
-    baseline_tests = set(config.getSettingList(config.BASELINE_SAMPLE_TEST_CODES, []))
-    summary.numBaselineSamplesArrived = sum(
-        [1 for s in all_samples if s.testCode in baseline_tests])
