e845e683d9616b51d172ef3e015fa2916c701651
Mark Fickett <mwf@google.com>
2017-03-14 16:59:55 -0400

    Remove batching, add comment about upserting in batch efficiently.

--- rest-api/dao/biobank_stored_sample_dao.py
+++ rest-api/dao/biobank_stored_sample_dao.py
@@ -8,8 +8,6 @@ from model.biobank_stored_sample import BiobankStoredSample
 
 class BiobankStoredSampleDao(BaseDao):
   """Batch operations for updating samples. Individual insert/get operations are testing only."""
-  _UPDATE_BATCH_SIZE = 1000
-
   def __init__(self):
     super(BiobankStoredSampleDao, self).__init__(BiobankStoredSample)
 
@@ -18,47 +16,30 @@ class BiobankStoredSampleDao(BaseDao):
 
   def upsert_batched(self, sample_generator):
     """Inserts/updates samples, skipping any invalid samples."""
-    with self.session() as session:
-      # It's OK for the valid ID set to be slightly stale relative to upcoming sessions used for
-      # insertion, since we don't delete participant IDs.
-      valid_biobank_ids = ParticipantDao().get_valid_biobank_id_set(session)
-    for samples_batch in _split_into_batches(sample_generator, self._UPDATE_BATCH_SIZE):
-      with self.session() as session:
-        written, skipped = self._upsert_batch(session, samples_batch, valid_biobank_ids)
-      logging.info('Wrote %d samples, skipped %d invalid samples.', written, skipped)
-
-  def _upsert_batch(self, session, samples, valid_biobank_ids):
+    # SQLAlchemy does not provide batch upserting; individual session.merge() calls as below may be
+    # expensive but cannot be effectively batched, see stackoverflow.com/questions/25955200. If this
+    # proves to be a bottleneck, we can switch to generating "INSERT .. ON DUPLICATE KEY UPDATE".
     written = 0
     skipped = 0
-    for sample in samples:
-      if sample.biobankId not in valid_biobank_ids:
-        logging.warning(
-            'Skipping sample %r: invalid participant Biobank ID %r (%d valid IDs).',
-            sample.biobankStoredSampleId, sample.biobankId, len(valid_biobank_ids))
-        skipped += 1
-        continue
-      if sample.confirmed is None:
-        logging.warning(
-            'Skipping sample %r: no "confirmed" timestmap.', sample.biobankStoredSampleId)
-        skipped += 1
-        continue
-      if sample.test not in VALID_TESTS:
-        logging.warning(
-            'Skipping sample %r: invalid test code %r.', sample.test, sample.biobankStoredSampleId)
-        skipped += 1
-        continue
-      # We could switch to add_all or bulk_save_objects if this is slow.
-      session.merge(sample)
-      written += 1
-    return written, skipped
-
-
-def _split_into_batches(iterable, batch_size):
-  batch = []
-  for i, v in enumerate(iterable, start=1):
-    batch.append(v)
-    if i % batch_size == 0:
-      yield batch
-      batch = []
-  if batch:
-    yield batch
+    with self.session() as session:
+      valid_biobank_ids = ParticipantDao().get_valid_biobank_id_set(session)
+      for sample in sample_generator:
+        if sample.biobankId not in valid_biobank_ids:
+          logging.warning(
+              'Skipping sample %r: invalid participant Biobank ID %r (%d valid IDs).',
+              sample.biobankStoredSampleId, sample.biobankId, len(valid_biobank_ids))
+          skipped += 1
+          continue
+        if sample.confirmed is None:
+          logging.warning(
+              'Skipping sample %r: no "confirmed" timestmap.', sample.biobankStoredSampleId)
+          skipped += 1
+          continue
+        if sample.test not in VALID_TESTS:
+          logging.warning(
+              'Skipping sample %r: invalid test code %r.', sample.test, sample.biobankStoredSampleId)
+          skipped += 1
+          continue
+        session.merge(sample)
+        written += 1
+    logging.info('Wrote %d samples, skipped %d invalid samples.', written, skipped)
--- rest-api/test/unit_test/dao_test/biobank_stored_sample_dao_test.py
+++ rest-api/test/unit_test/dao_test/biobank_stored_sample_dao_test.py
@@ -2,7 +2,7 @@ import itertools
 
 import clock
 from dao.biobank_order_dao import VALID_TESTS
-from dao.biobank_stored_sample_dao import BiobankStoredSampleDao, _split_into_batches
+from dao.biobank_stored_sample_dao import BiobankStoredSampleDao
 from model.biobank_stored_sample import BiobankStoredSample
 from model.participant import Participant
 from dao.participant_dao import ParticipantDao
@@ -30,22 +30,3 @@ class BiobankStoredSampleDaoTest(SqlTestBase):
     fetched = self.dao.get(sample_id)
     self.assertEquals(test_code, created.test)
     self.assertEquals(test_code, fetched.test)
-
-  def test_batched_write_multi_batch(self):
-    num_samples = int(2.5 * self.dao._UPDATE_BATCH_SIZE)
-    test_code = iter(VALID_TESTS).next()
-    self.dao.upsert_batched(BiobankStoredSample(
-        biobankStoredSampleId='W%d' % i,
-        biobankId=self.participant.biobankId,
-        test=test_code,
-        confirmed=clock.CLOCK.now()) for i in xrange(num_samples))
-    self.assertEquals(self.dao.count(), num_samples)
-
-  def test_batching(self):
-    zero_to_ten = range(10)
-    batches = list(_split_into_batches(zero_to_ten, 4))
-    self.assertItemsEqual(zero_to_ten, itertools.chain(*batches))
-    self.assertEquals(len(batches), 3)
-    self.assertEquals(batches[0], [0, 1, 2, 3])
-    self.assertEquals(batches[1], [4, 5, 6, 7])
-    self.assertEquals(batches[2], [8, 9])
