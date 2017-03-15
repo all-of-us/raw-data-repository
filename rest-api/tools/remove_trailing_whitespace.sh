96ec873244233ca5a87ee754ac38a1c3bf4e86fc
Mark Fickett <mwf@google.com>
2017-03-15 09:46:35 -0400

    Make remove_trailing_whitespace script auto-commit.

--- rest-api/tools/remove_trailing_whitespace.sh
+++ rest-api/tools/remove_trailing_whitespace.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# Removes trailing whitespace from all files and suggests a commit.
-MESSAGE="Remove trailing whitespace."
+# Removes trailing whitespace from all files and commits.
+MESSAGE="Automatically removed trailing whitespace."
 
-git ls-files | grep "\." | parallel sed -i "'s/[ \t]*$//'"
+git ls-files | grep "\." | grep -v "\.csv" | parallel sed -i "'s/[ \t]*$//'"
 
 UNCOMMITTED=`git diff --name-only`
 if [ "$UNCOMMITTED" ]
 then
   git diff
-  echo "I removed the trailing whitespace above. Suggestion: git commit -a -m \"$MESSAGE\""
-  exit 1
+  echo "I removed the trailing whitespace above, auto-committing."
+  git commit -a -m "$MESSAGE"
 fi
