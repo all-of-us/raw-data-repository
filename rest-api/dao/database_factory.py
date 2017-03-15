48b838ec7ec2fde6d3e816b442d08d4c9c37677f
Mark Fickett <mwf@google.com>
2017-03-15 09:25:55 -0400

    Move format_date from database_factory to database_utils, update references. (#240)

--- rest-api/dao/database_factory.py
+++ rest-api/dao/database_factory.py
@@ -1,5 +1,4 @@
 import config
-import re
 import singletons
 
 from model.database import Database
@@ -14,26 +13,3 @@ class _SqlDatabase(Database):
 def get_database():
   """Returns a singleton _SqlDatabase."""
   return singletons.get(_SqlDatabase)
-
-DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
-ISODATE_PATTERN = 'ISODATE\[([^\]]+)\]'
-IS_SQLITE = None
-
-def is_sqlite():
-  global IS_SQLITE
-  if IS_SQLITE is None:
-    IS_SQLITE = get_database().db_type == 'sqlite'
-  return IS_SQLITE
-
-def format_date(expression, date_format=DATE_FORMAT):
-  if is_sqlite():
-    return "strftime('{}', {})".format(date_format, expression)
-  else:
-    return "DATE_FORMAT({}, '{}')".format(expression, date_format)
-
-
-def replace_isodate(sql, date_format=DATE_FORMAT):
-  if is_sqlite():
-    return re.sub(ISODATE_PATTERN, r"strftime('{}', \1)".format(date_format), sql)
-  else:
-    return re.sub(ISODATE_PATTERN, r"DATE_FORMAT(\1, '{}')".format(date_format), sql)
