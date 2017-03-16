78f92060969b1ee67a18270fad0afea5831c956b
Mark Fickett <mwf@google.com>
2017-03-16 10:01:07 -0400

    Process --key arg in update_config wrapper script.

--- rest-api/tools/install_config.sh
+++ rest-api/tools/install_config.sh
@@ -12,6 +12,7 @@ while true; do
     --project) PROJECT=$2; shift 2;;
     --update) UPDATE=Y; shift 1;;
     --config) CONFIG=$2; shift 2;;
+    --key) KEY=$2; shift 2;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -36,6 +37,10 @@ fi
 if [ "${UPDATE}" ]
 then
   EXTRA_ARGS+=" --update"
+fi
+if [ "${KEY}" ]
+then
+  EXTRA_ARGS+=" --key $KEY"
 fi
 echo "ARGS = ${EXTRA_ARGS}"
 
