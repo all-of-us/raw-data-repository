69af09ed8f85013ff2f6561256bef0cdd5c5c195
Dan Rodney <danqrodney>
2017-03-21 15:06:38 -0500

    Again

--- rest-api/api/questionnaire_response_api.py
+++ rest-api/api/questionnaire_response_api.py
@@ -10,7 +10,7 @@ class QuestionnaireResponseApi(BaseApi):
 
   @api_util.auth_required(PTC)
   def get(self, p_id, id_):
-    #pylint: ignored=unused-argument
+    #pylint: disable=unused-argument
     return super(QuestionnaireResponseApi, self).get(id_)
 
   @api_util.auth_required(PTC)
