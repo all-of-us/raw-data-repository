4feebba250a37217479413c355462c70058b7a95
Mark Fickett <mwf@google.com>
2017-03-31 14:13:53 -0400

    Allow SqlExporter to take in a session, have an overrideable writer.

--- rest-api/offline/sql_exporter.py
+++ rest-api/offline/sql_exporter.py
@@ -1,3 +1,4 @@
+import contextlib
 import csv
 import logging
 
@@ -6,29 +7,37 @@ from cloudstorage import cloudstorage_api
 from sqlalchemy import text
 
 DELIMITER = ','
-BATCH_SIZE = 1000
+_BATCH_SIZE = 1000
+
 
 class SqlExporter(object):
-
   def __init__(self, bucket_name):
-    self.database = database_factory.get_database()
-    self.bucket_name = bucket_name
+    self._bucket_name = bucket_name
 
-  def run_export(self, file_name, sql, **kwargs):
+  def run_export(self, file_name, sql, query_params=None):
+    with database_factory.get_database().session() as session:
+      self.run_export_with_session(file_name, session, sql, query_params=query_params)
+
+  def run_export_with_session(self, file_name, session, sql, query_params=None):
     # Each query from AppEngine standard environment must finish in 60 seconds.
     # If we start running into trouble with that, we'll either
     # need to break the SQL up into pages, or (more likely) switch to cloud SQL export.
-    cursor = self.database.get_engine().execute(text(sql), **kwargs)
+    cursor = session.execute(text(sql), params=query_params)
     try:
-      filename = '/%s/%s' % (self.bucket_name, file_name)
-      logging.info('Exporting data to %s...', filename)
-      with cloudstorage_api.open(filename, mode='w') as dest:
-        writer = csv.writer(dest, delimiter=DELIMITER)
+      gcs_path = '/%s/%s' % (self._bucket_name, file_name)
+      logging.info('Exporting data to %s...', gcs_path)
+      with self._open_writer(gcs_path) as writer:
         writer.writerow(cursor.keys())
-        results = cursor.fetchmany(BATCH_SIZE)
+        results = cursor.fetchmany(_BATCH_SIZE)
         while results:
           writer.writerows(results)
-          results = cursor.fetchmany(BATCH_SIZE)
+          results = cursor.fetchmany(_BATCH_SIZE)
     finally:
       cursor.close()
-    logging.info('Export to %s complete.', filename)
+    logging.info('Export to %s complete.', gcs_path)
+
+  @contextlib.contextmanager
+  def _open_writer(self, gcs_path):
+    with cloudstorage_api.open(gcs_path, mode='w') as dest:
+      writer = csv.writer(dest, delimiter=DELIMITER)
+      yield writer
