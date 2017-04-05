3282270c5100d7c4602a9b9f4da0f21818c2c795
Mark Fickett <mwf@google.com>
2017-04-05 13:35:14 -0400

    Split slower MySQL tests out into separate file for easier filtering.

--- rest-api/test/unit_test/offline_test/biobank_samples_pipeline_test.py
+++ rest-api/test/unit_test/offline_test/biobank_samples_pipeline_test.py
@@ -6,19 +6,14 @@ import time
 
 from cloudstorage import cloudstorage_api  # stubbed by testbed
 
-import clock
 import config
 from code_constants import BIOBANK_TESTS
-from dao.biobank_order_dao import BiobankOrderDao
 from dao.biobank_stored_sample_dao import BiobankStoredSampleDao
 from dao.participant_dao import ParticipantDao
 from dao.participant_summary_dao import ParticipantSummaryDao
 from offline import biobank_samples_pipeline
-from test.unit_test.unit_test_util import CloudStorageSqlTestBase, NdbTestBase, InMemorySqlExporter
-from test.unit_test.unit_test_util import SqlTestBase, InMemorySqlExporter, TestBase
+from test.unit_test.unit_test_util import CloudStorageSqlTestBase, NdbTestBase, TestBase
 from test import test_data
-from model.biobank_order import BiobankOrder, BiobankOrderedSample
-from model.biobank_stored_sample import BiobankStoredSample
 from model.utils import to_client_biobank_id
 from model.participant import Participant
 from participant_enums import SampleStatus
@@ -127,123 +122,3 @@ class BiobankSamplesPipelineTest(CloudStorageSqlTestBase, NdbTestBase):
           path.startswith(expected_prefix),
           'Report path %r must start with %r.' % (expected_prefix, path))
       self.assertTrue(path.endswith('.csv'))
-
-
-# Expected names for the reconciliation_data columns in output CSVs.
-_CSV_COLUMN_NAMES = (
-  'biobank_id',
-
-  'sent_test',
-  'sent_count',
-  'sent_order_id',
-  'sent_collection_time',
-  'sent_finalized_time',
-  'site_id',
-
-  'received_test',
-  'received_count',
-  'received_sample_id',
-  'received_time',
-
-  'elapsed_hours',
-)
-
-
-class MySqlReconciliationTest(SqlTestBase):
-  def setUp(self):
-    super(MySqlReconciliationTest, self).setUp(use_mysql=True)
-    self.participant_dao = ParticipantDao()
-    self.summary_dao = ParticipantSummaryDao()
-    self.order_dao = BiobankOrderDao()
-    self.sample_dao = BiobankStoredSampleDao()
-
-  def _insert_participant(self):
-    participant = self.participant_dao.insert(Participant())
-    # satisfies the consent requirement
-    self.summary_dao.insert(self.participant_summary(participant))
-    return participant
-
-  def _insert_order(self, participant, order_id, tests, order_time):
-    order = BiobankOrder(
-        biobankOrderId=order_id,
-        participantId=participant.participantId,
-        sourceSiteValue='SiteValue-%s' % participant.participantId,
-        created=order_time,
-        samples=[])
-    for test_code in tests:
-      order.samples.append(BiobankOrderedSample(
-          biobankOrderId=order.biobankOrderId,
-          test=test_code,
-          description=u'test',
-          processingRequired=False,
-          collected=order_time,
-          finalized=order_time))
-    return self.order_dao.insert(order)
-
-  def _insert_samples(self, participant, tests, received_time):
-    for test_code in tests:
-      self.sample_dao.insert(BiobankStoredSample(
-          biobankStoredSampleId='StoredSample-%s-%s' % (participant.participantId, test_code),
-          biobankId=participant.biobankId,
-          test=test_code,
-          confirmed=received_time))
-
-  def test_reconciliation_query(self):
-    order_time = clock.CLOCK.now()
-    within_a_day = order_time + datetime.timedelta(hours=23)
-    late_time = order_time + datetime.timedelta(hours=25)
-    days_later = order_time + datetime.timedelta(days=5)
-
-    p_on_time = self._insert_participant()
-    self._insert_order(p_on_time, 'GoodOrder', _BASELINE_TESTS[:2], order_time)
-    self._insert_samples(p_on_time, _BASELINE_TESTS[:2], within_a_day)
-
-    p_late_and_missing = self._insert_participant()
-    o_late_and_missing = self._insert_order(
-        p_late_and_missing, 'SlowOrder', _BASELINE_TESTS[:2], order_time)
-    self._insert_samples(p_late_and_missing, [_BASELINE_TESTS[0]], late_time)
-
-    p_extra = self._insert_participant()
-    self._insert_samples(p_extra, [_BASELINE_TESTS[-1]], order_time)
-
-    p_repeated = self._insert_participant()
-    self._insert_order(p_repeated, 'OrigOrder', [_BASELINE_TESTS[0]], order_time)
-    self._insert_samples(p_repeated, [_BASELINE_TESTS[0]], within_a_day)
-    self._insert_order(p_repeated, 'RepeatedOrder', [_BASELINE_TESTS[0]], days_later)
-
-    received, late, missing = 'rx.csv', 'late.csv', 'missing.csv'
-    exporter = InMemorySqlExporter(self)
-    biobank_samples_pipeline._query_and_write_reports(exporter, received, late, missing)
-
-    exporter.assertFilesEqual((received, late, missing))
-
-    # sent-and-received: 2 on-time, 1 late, none of the missing/extra/repeated ones
-    exporter.assertRowCount(received, 3)
-    exporter.assertColumnNamesEqual(received, _CSV_COLUMN_NAMES)
-    exporter.assertHasRow(received, {
-        'biobank_id': to_client_biobank_id(p_on_time.biobankId), 'sent_test': _BASELINE_TESTS[0]})
-    exporter.assertHasRow(received, {
-        'biobank_id': to_client_biobank_id(p_on_time.biobankId), 'sent_test': _BASELINE_TESTS[1]})
-    exporter.assertHasRow(received, {
-        'biobank_id': to_client_biobank_id(p_late_and_missing.biobankId),
-        'sent_test': _BASELINE_TESTS[0]})
-
-    # sent-and-received: 1 late
-    exporter.assertRowCount(late, 1)
-    exporter.assertHasRow(late, {
-        'biobank_id': to_client_biobank_id(p_late_and_missing.biobankId),
-        'sent_order_id': o_late_and_missing.biobankOrderId,
-        'elapsed_hours': '25'})
-
-    # gone awry
-    exporter.assertRowCount(missing, 3)
-    exporter.assertHasRow(missing, {
-        'biobank_id': to_client_biobank_id(p_late_and_missing.biobankId),
-        'sent_order_id': o_late_and_missing.biobankOrderId,
-        'elapsed_hours': ''})
-    exporter.assertHasRow(missing, {
-        'biobank_id': to_client_biobank_id(p_repeated.biobankId),
-        'sent_count': '2',
-        'received_count': '1'})
-    exporter.assertHasRow(missing, {
-        'biobank_id': to_client_biobank_id(p_extra.biobankId), 'sent_order_id': ''})
