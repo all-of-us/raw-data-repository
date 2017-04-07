6687aa7abc27710fc69e1198a6ce1a8744d34135
Mark Fickett <mwf@google.com>
2017-04-07 16:34:55 -0400

    Automatically update the db schema when deploying new versions to test or staging. (#297)
    * Allow CREDS_FILE and INSTANCE to be specified for db upgrade (and auth setup).
    * Upgrade db after pusting to test or staging.

--- rest-api/tools/upgrade_database.sh
+++ rest-api/tools/upgrade_database.sh
@@ -1,18 +1,29 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Applies schema migrations found in alembic/versions to upgrade a database.
 # A specific revision level can be provided, or if none is, all revisions will be applied (i.e.
 # the schema of the database will be updated to the latest.)
-# By default, the database running on your local development box will be upgraded; if you specify
-# --project and --account, the database for the project will be upgraded.
+#
+USAGE="
+Examples:
+Upgrade the local development database:
+    tools/upgrade_database.sh [--revision <REVISION>]
+Upgrade a deployed db from your development box:
+    tools/upgrade_database.sh --project all-or-us-rdr-staging \
+        --account $USER@google.com [--creds_account $USER@pmi-ops.org]
+Upgrade a deployed db from CircleCI:
+    tools/upgrade_database.sh --instance https://all-of-us-x.appspot.com --creds_file ~/creds_file.key
+"
 
-USAGE="tools/upgrade_database.sh [--revision <REVISION>] [--account <ACCOUNT> --project <PROJECT> [--creds_account <ACCOUNT>]]"
 while true; do
   case "$1" in
     --revision) REVISION=$2; shift 2;;
     --account) ACCOUNT=$2; shift 2;;
     --creds_account) CREDS_ACCOUNT=$2; shift 2;;
     --project) PROJECT=$2; shift 2;;
+    --instance) INSTANCE=$2; shift 2;;
+    -i) INSTANCE=$2; shift 2;;
+    --creds_file) CREDS_FILE=$2; shift 2;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -22,22 +33,28 @@ if [ "${PROJECT}" ]
 then
   if [ -z "${ACCOUNT}" ]
   then
-    echo "Usage: $USAGE"
+    echo "--account must be specified with --project. $USAGE"
     exit 1
   fi
   if [ -z "${CREDS_ACCOUNT}" ]
   then
     CREDS_ACCOUNT="${ACCOUNT}"
   fi
+elif [ "${INSTANCE}" ]
+then
+  if [ -z "${CREDS_FILE}" ]
+  then
+    echo "--creds_file must be specified with --instance. $USAGE"
+    exit 1
+  fi
 fi
-
 
 if [ -z "${REVISION}" ]
 then
   REVISION=head
 fi
 
-if [ "${PROJECT}" ]
+if [ "${PROJECT}" -o "${INSTANCE}" ]
 then
   source tools/auth_setup.sh
   run_cloud_sql_proxy
@@ -50,5 +67,6 @@ else
   fi
 fi
 
+# alembic.env.get_url() picks up DB_CONNECTION_STRING to find the db to upgrade.
 (source tools/set_path.sh; alembic upgrade ${REVISION})
 
