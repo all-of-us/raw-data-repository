8dfc87d9913d33b88be52dad5f9c5cbb0782ed05
danqrodney <danrodney@google.com>
2017-04-10 15:09:51 -0500

    Fixing DB_INFO_FILE references (#304)

--- rest-api/tools/setup_database.sh
+++ rest-api/tools/setup_database.sh
@@ -94,7 +94,7 @@ echo '{"db_connection_string": "'$CONNECTION_STRING'", ' \
      ' "db_password": "'$PASSWORD'", ' \
      ' "db_connection_name": "'$INSTANCE_CONNECTION_NAME'", '\
      ' "db_user": "'$DB_USER'", '\
-     ' "db_name": "'$DB_NAME'" }' > $DB_INFO_FILE
+     ' "db_name": "'$DB_NAME'" }' > $TMP_DB_INFO_FILE
 
 echo 'CREATE DATABASE IF NOT EXISTS '$DB_NAME > $CREATE_DB_FILE
 
@@ -104,5 +104,5 @@ echo "Creating empty database..."
 mysql -u "$DB_USER" -p"$PASSWORD" --host 127.0.0.1 --port ${PORT} < ${CREATE_DB_FILE}
 
 echo "Setting database configuration"
-tools/install_config.sh --key db_config --config ${DB_INFO_FILE} --instance $INSTANCE --update --creds_file ${CREDS_FILE}
+tools/install_config.sh --key db_config --config ${TMP_DB_INFO_FILE} --instance $INSTANCE --update --creds_file ${CREDS_FILE}
 
