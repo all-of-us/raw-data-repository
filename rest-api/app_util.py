b68c15fb0d157fa7a5c7f55c8762536827e529ed
Mark Fickett <mwf@google.com>
2017-04-11 11:14:22 -0400

    pydoc style in app_util

--- rest-api/app_util.py
+++ rest-api/app_util.py
@@ -1,17 +1,19 @@
-"""Utility methods for setting up apps."""
-
 import logging
 
 from flask import request
 
-# All responses are json, so we tag them as such at the app level to
-# provide uniform protection against content-sniffing-based attacks.
+
 def add_headers(response):
+  """Add uniform headers to all API responses.
+
+  All responses are JSON, so we tag them as such at the app level to provide uniform protection
+  against content-sniffing-based attacks.
+  """
   response.headers['Content-Disposition'] = 'attachment'
   response.headers['X-Content-Type-Options'] = 'nosniff'
   response.headers['Content-Type'] = 'application/json'
   return response
 
-# Some uniform logging of request characteristics before any checks are applied.
 def request_logging():
+  """Some uniform logging of request characteristics before any checks are applied."""
   logging.info('Request protocol: HTTPS={}'.format(request.environ.get('HTTPS')))
