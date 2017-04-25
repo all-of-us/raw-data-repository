f5d4fb2ccb5f61f0fec1b063b51b6dd376648d9f
Mark Fickett <mwf@google.com>
2017-04-25 15:26:29 -0400

    Add doc: sample generation is CSV data only [no API call or offline processing trigger].

--- rest-api/data_gen/fake_biobank_samples_generator.py
+++ rest-api/data_gen/fake_biobank_samples_generator.py
@@ -45,6 +45,7 @@ class FakeBiobankSamplesGenerator(object):
   """Generates fake biobank samples for the participants in the database."""
 
   def generate_samples(self):
+    """Creates fake sample CSV data in GCS."""
     bucket_name = config.getSetting(config.BIOBANK_SAMPLES_BUCKET_NAME)
     now = clock.CLOCK.now()
     file_name = '/%s/fake_%s.csv' % (bucket_name, now.isoformat())
