f4f72a57476ff0a051151181ee9106a571c9bd11
Mark Fickett <mwf@google.com>
2017-04-27 17:18:13 -0400

    Add command-line project arg, some error checking.

--- rest-api/tools/load_test.sh
+++ rest-api/tools/load_test.sh
@@ -13,21 +13,47 @@
 # *   Click run, gather stats, click stop.
 #
 # Locust docs: http://docs.locust.io/en/latest/quickstart.html
-# Install: sudo pip install locustio
+
+if ! which locust
+then
+  echo "Install Locust with \"sudo pip install locustio\"."
+  exit 1
+fi
+function usage {
+  echo "Usage: load_test.sh --project all-of-us-rdr-test"
+  exit 1
+}
+while true
+do
+  case "$1" in
+    --project) PROJECT=$2; shift 2;;
+    *) break;;
+  esac
+done
+if [ "$@" ]
+then
+  echo "Unrecognized arguments: $@"
+  usage
+fi
+if [ -z "$PROJECT" ]
+then
+  usage
+fi
+
 ACCOUNT=$USER@google.com
 CREDS_ACCOUNT=$ACCOUNT
-PROJECT=all-of-us-rdr-dev
-
 . tools/auth_setup.sh
+
 # Some dependency data is loaded via DAOs before load testing via API.
 run_cloud_sql_proxy
 set_db_connection_string
 
+# Prefer system-wide installs of some packages (specifically "requests"). The
+# gcloud libraries added by set_path.sh include an older version.
 OLDPATH=$PYTHONPATH
 . tools/set_path.sh;
-# Prefer system-wide installs of some packages (specifically "requests").
 export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$OLDPATH:$PYTHONPATH
-cd "$BASE_DIR";
+
 LOCUST_CREDS_FILE="$CREDS_FILE" \
 LOCUST_TARGET_INSTANCE="https://$PROJECT.appspot.com" \
 locust -f "$BASE_DIR"/tools/load_test_locustfile.py
