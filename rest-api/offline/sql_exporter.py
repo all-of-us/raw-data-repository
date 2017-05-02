1d7f625a10b9321b1c19e969b66c9b62c74a02d7
Dan Rodney <danqrodney>
2017-05-02 09:38:42 -0500

    SQLExporter changes

--- rest-api/offline/sql_exporter.py
+++ rest-api/offline/sql_exporter.py
@@ -10,6 +10,13 @@ from sqlalchemy import text
 DELIMITER = ','
 _BATCH_SIZE = 1000
 
+class SqlExportFileWriter(object):
+  def __init__(self, dest):
+    self.writer = csv.writer(dest, delimiter=DELIMITER)
+
+  def writerows(self, results):
+    writer.writerows(results)
+
 
 class SqlExporter(object):
   """Executes a SQL query, fetches results in batches, and writes output to a CSV in GCS."""
@@ -17,21 +24,24 @@ class SqlExporter(object):
     self._bucket_name = bucket_name
 
   def run_export(self, file_name, sql, query_params=None):
-    with database_factory.get_database().session() as session:
-      self.run_export_with_session(file_name, session, sql, query_params=query_params)
+    with self._open_writer(file_name) as writer:
+      self.run_export_with_writer(writer)
 
-  def run_export_with_session(self, file_name, session, sql, query_params=None):
+  def run_export_with_writer(self, writer, sql, query_params):
+    with database_factory.get_database().session() as session:
+      self.run_export_with_session(writer, session, sql, query_params=query_params)
+
+  def run_export_with_session(self, writer, session, sql, query_params=None):
     # Each query from AppEngine standard environment must finish in 60 seconds.
     # If we start running into trouble with that, we'll either
     # need to break the SQL up into pages, or (more likely) switch to cloud SQL export.
     cursor = session.execute(text(sql), params=query_params)
     try:
-      with self._open_writer(file_name) as writer:
-        writer.writerow(cursor.keys())
+      writer.writerow(cursor.keys())
+      results = cursor.fetchmany(_BATCH_SIZE)
+      while results:
+        writer.writerows(results)
         results = cursor.fetchmany(_BATCH_SIZE)
-        while results:
-          writer.writerows(results)
-          results = cursor.fetchmany(_BATCH_SIZE)
     finally:
       cursor.close()
 
@@ -40,6 +50,6 @@ class SqlExporter(object):
     gcs_path = '/%s/%s' % (self._bucket_name, file_name)
     logging.info('Exporting data to %s...', gcs_path)
     with cloudstorage_api.open(gcs_path, mode='w') as dest:
-      writer = csv.writer(dest, delimiter=DELIMITER)
+      writer = SqlExportFileWriter(dest)
       yield writer
     logging.info('Export to %s complete.', gcs_path)
