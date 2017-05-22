5331a6cd3c6e6145e577a185f5d3112d2fa865e1
Dan Rodney <danqrodney>
2017-05-22 13:12:05 -0500

    Changing to error instead of warning.

--- rest-api/dao/code_dao.py
+++ rest-api/dao/code_dao.py
@@ -178,8 +178,8 @@ class CodeDao(CacheAllDao):
           display, code_type, parent_id = code_map[(system, value)]
           code = Code(system=system, value=value, display=display,
                       codeType=code_type, mapped=False, parentId=parent_id)
-          logging.warn("Adding unmapped code: system = %s, value = %s" % (code.system, code.value),
-                       stack_info=True)
+          logging.error("Adding unmapped code: system = %s, value = %s" % (code.system, code.value),
+                        stack_info=True)
           self.insert_with_session(session, code)
           session.flush()
           result_map[(system, value)] = code.codeId
