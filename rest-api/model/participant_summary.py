34c4f4dac8eef661056ef8296d9a7999c9bd101f
Dan Rodney <danqrodney>
2017-05-31 14:55:02 -0500

    Automatically removed trailing whitespace.

--- rest-api/model/participant_summary.py
+++ rest-api/model/participant_summary.py
@@ -61,10 +61,10 @@ class ParticipantSummary(Base):
   consentForElectronicHealthRecords = Column('consent_for_electronic_health_records',
       Enum(QuestionnaireStatus), default=QuestionnaireStatus.UNSET)
   consentForElectronicHealthRecordsTime = Column('consent_for_electronic_health_records_time',
-                                                 UTCDateTime)  
-  consentForCABoR = Column('consent_for_cabor', 
+                                                 UTCDateTime)
+  consentForCABoR = Column('consent_for_cabor',
                            Enum(QuestionnaireStatus), default=QuestionnaireStatus.UNSET)
-  consentForCABoRTime = Column('consent_for_cabor_time', UTCDateTime)                         
+  consentForCABoRTime = Column('consent_for_cabor_time', UTCDateTime)
   questionnaireOnOverallHealth = Column('questionnaire_on_overall_health',
       Enum(QuestionnaireStatus), default=QuestionnaireStatus.UNSET)
   questionnaireOnOverallHealthTime = Column('questionnaire_on_overall_health_time', UTCDateTime)
