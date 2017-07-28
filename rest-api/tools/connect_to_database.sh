dc4ea975a6fa38742578cc0604086047ee1d8f45
danqrodney <danrodney@google.com>
2017-07-27 22:33:12 -0400

    Changing connect_to_database.sh so it can run commands and generate C… (#434)
    * Changing connect_to_database.sh so it can run commands and generate CSV output.
    
    * Addressing PR comment
    
    Updating stable participants

--- rest-api/tools/connect_to_database.sh
+++ rest-api/tools/connect_to_database.sh
@@ -3,7 +3,8 @@
 # Starts Cloud SQL proxy, connected to the Cloud SQL instance for a particular environment,
 # and runs mysql to connect to it.
 
-USAGE="tools/connect_to_database.sh --account <ACCOUNT> --project <PROJECT> [--creds_account <ACCOUNT>] [--user <USER> --password <PASSWORD>]"
+USAGE="tools/connect_to_database.sh --account <ACCOUNT> --project <PROJECT> [--creds_account <ACCOUNT>] \
+  [--user <USER> --password <PASSWORD>] [--command <COMMAND> [--output <OUTPUT_FILE>]]"
 while true; do
   case "$1" in
     --account) ACCOUNT=$2; shift 2;;
@@ -11,6 +12,8 @@ while true; do
     --project) PROJECT=$2; shift 2;;
     --user) CONNECT_USER=$2; shift 2;;
     --password) CONNECT_PASSWORD=$2; shift 2;;
+    --command) COMMAND=$2; shift 2;;
+    --output_csv) OUTPUT=$2; shift 2;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -37,4 +40,20 @@ then
   CONNECT_PASSWORD="${PASSWORD}"
 fi
 
-mysql -u "$CONNECT_USER" -p"$CONNECT_PASSWORD" --host 127.0.0.1 --port ${PORT} "$DB_NAME"
+SUFFIX=
+if [ "${COMMAND}" ]
+then
+  SUFFIX="-e \"${COMMAND}\""
+fi
+
+SUFFIX2=
+if [ "${OUTPUT}" ]
+then
+  # Convert the output of MySQL tables to CSV format.
+  SUFFIX2=" | sed 's/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g' > ${OUTPUT}"
+fi
+
+MYSQL_COMMAND="mysql -u \"${CONNECT_USER}\" -p\"${CONNECT_PASSWORD}\" -h 127.0.0.1 --port ${PORT} -D \"${DB_NAME}\" ${SUFFIX} ${SUFFIX2}"
+eval ${MYSQL_COMMAND}
+
+
