42c34745c4171f41a84cb94300516cabe01a6ebe
Mark Fickett <mwf@google.com>
2017-08-03 15:07:28 -0400

    Incidental style cleanup in PS API.

--- rest-api/api/participant_summary_api.py
+++ rest-api/api/participant_summary_api.py
@@ -1,14 +1,15 @@
-import api_util
+import logging
 
 from api.base_api import BaseApi
-from api_util import PTC_AND_HEALTHPRO
+from api_util import auth_required, PTC_AND_HEALTHPRO
 from dao.participant_summary_dao import ParticipantSummaryDao
+
 
 class ParticipantSummaryApi(BaseApi):
   def __init__(self):
     super(ParticipantSummaryApi, self).__init__(ParticipantSummaryDao())
 
-  @api_util.auth_required(PTC_AND_HEALTHPRO)
+  @auth_required(PTC_AND_HEALTHPRO)
   def get(self, p_id=None):
     if p_id:
       return super(ParticipantSummaryApi, self).get(p_id)
