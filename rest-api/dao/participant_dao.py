13983bbc26402cd2edf66b22a3b9ff2e87555ba7
Mark Fickett <mwf@google.com>
2017-09-14 14:48:37 -0400

    Switch to wrapping helpers.

--- rest-api/dao/participant_dao.py
+++ rest-api/dao/participant_dao.py
@@ -248,7 +248,10 @@ def raise_if_withdrawn(obj):
 
 
 def make_primary_provider_link(hpo_name=None, hpo_id=None, hpo=None):
-  """Returns serialized FHIR JSON for a primary provider link based on HPO information."""
+  """Returns serialized FHIR JSON for a primary provider link based on HPO information.
+
+  Exactly one of hpo_name, hpo_id, or hpo (a model.HPO) must be provided.
+  """
   if len([v for v in hpo_name, hpo_id, hpo if v is not None]) != 1:
     raise ValueError(
         'Exactly one of hpo_name=%r hpo_id=%r or hpo=%r must be defined.'
