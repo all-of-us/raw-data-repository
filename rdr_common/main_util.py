eb6590a01310a15047a9cdeed2072c2ac18c1d91
Mark Fickett <mwf@google.com>
2017-09-28 11:42:36 -0400

    Fix auto-detection of caller __doc__.

--- rdr_common/main_util.py
+++ rdr_common/main_util.py
@@ -1,4 +1,5 @@
 import argparse
+import importlib
 import inspect
 import logging
 import sys
@@ -15,7 +16,19 @@ def configure_logging():
 def get_parser(description=None):
   """Gets an ArgumentParser, defaulting to the caller's __doc__ as the description."""
   if description is None:
-    doc = inspect.getmodule(inspect.stack()[1]).__doc__
+    caller_frame = inspect.stack()[1]
+    caller_path = caller_frame[1]
+    caller_module_name = inspect.getmodulename(caller_path)
+    try:
+      caller_module = importlib.import_module(caller_module_name)
+      doc = caller_module.__doc__
+    except ImportError:
+      logging.error(
+          'Could not auto-detect __doc__ for parser description for module %r, '
+          'derived from caller path %r.',
+          caller_module_name,
+          caller_path)
+      doc = None
   else:
     doc = description
   return argparse.ArgumentParser(
