f33916577f28d9118841096dae02a40d25f70a48
danqrodney <danrodney@google.com>
2017-10-02 13:28:54 -0500

    Generating SQL dumps in GCS for Cloud SQL import. (#485)
    * Changing setup_local_database_for_etl.sh to read source_to_concept_map.csv from GCS, like other CSV files;
    and to generate MySQL dumps that can be uploaded to GCS, and subsequently imported into Cloud SQL using
    setup_database_for_etl.sh.
    
    * More stuff
    
    * Responding to PR comment
    
    * Responding to moar PR comments

---
+++ rest-api/etl/setup_database_for_etl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ae
+
+# Prepares databases in Cloud SQL for running the RDR -> OMOP ETL. 
+# Imports "cdm" and "voc" databases located in GCS, which were produced by running 
+# setup_local_database_for_etl.
+# If the databases already exist, drop them prior to running this.
+# Note: this takes a while. Go get some coffee while it's running!
+
+USAGE="tools/setup_database_for_etl.sh --project <PROJECT> --account <ACCOUNT>"
+while true; do
+  case "$1" in
+    --account) ACCOUNT=$2; shift 2;;
+    --project) PROJECT=$2; shift 2;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+if [ -z "${ACCOUNT}" ]
+then
+  echo "Usage: $USAGE"
+  exit 1
+fi
+
+if [ -z "${PROJECT}" ]
+then
+  echo "Usage: $USAGE"
+  exit 1
+fi
+CREDS_ACCOUNT=${ACCOUNT}
+
+echo "Activating service account..."
+source tools/auth_setup.sh
+gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$CREDS_FILE
+
+SQL_SERVICE_ACCOUNT=`gcloud sql instances describe --project ${PROJECT} --account ${ACCOUNT} \
+rdrmaindb | grep serviceAccountEmailAddress | cut -d: -f2`
+
+echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT}..."
+gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:W gs://all-of-us-rdr-vocabulary
+gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:R gs://all-of-us-rdr-vocabulary/vocabularies-2017-09-18/*.sql
+
+echo "Importing CDM database..."
+gcloud sql instances import --quiet --project ${PROJECT} --account ${ACCOUNT} rdrmaindb gs://all-of-us-rdr-vocabulary/vocabularies-2017-09-18/cdm.sql
+
+echo "Importing VOC database..."
+gcloud sql instances import --quiet --project ${PROJECT} --account ${ACCOUNT} rdrmaindb gs://all-of-us-rdr-vocabulary/vocabularies-2017-09-18/voc.sql
+  
+echo "Done."
