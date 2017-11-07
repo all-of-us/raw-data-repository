c49f81951280281d886fcf721fba15b35b7c6892
Shyam Naru <shyamnaru@gmail.com>
2017-11-07 11:43:33 -0600

    [DA-423] Set limits on Metrics endpoint (#518)
    * Set limit between start and end date on metrics endpoint
    
    * Write test case for mertics limit
    
    * Fix error message for invalid end date
    
    * Metrics throw bad request if start and end dates are empty
    
    * Write test case for empty start and end dates
    
    * Update mertics_api_test to include start and end dates
    
    * Use assertRaises method to check exceptions in tests

--- rest-api/api/metrics_api.py
+++ rest-api/api/metrics_api.py
@@ -9,6 +9,7 @@ from flask.ext.restful import Resource
 from werkzeug.exceptions import BadRequest
 
 DATE_FORMAT = '%Y-%m-%d'
+DAYS_LIMIT = 7
 
 class MetricsApi(Resource):
 
@@ -16,23 +17,27 @@ class MetricsApi(Resource):
   def post(self):
     dao = MetricsBucketDao()
     resource = request.get_data()
-    start_date = None
-    end_date = None
     if resource:
       resource_json = json.loads(resource)
       start_date_str = resource_json.get('start_date')
       end_date_str = resource_json.get('end_date')
-      if start_date_str:
-        try:
-          start_date = datetime.datetime.strptime(start_date_str, DATE_FORMAT).date()
-        except ValueError:
-          raise BadRequest("Invalid start date: %s" % start_date_str)
-      if end_date_str:
-        try:
-          end_date = datetime.datetime.strptime(end_date_str, DATE_FORMAT).date()
-        except ValueError:
-          raise BadRequest("Invalid start date: %s" % end_date_str)
-    buckets = dao.get_active_buckets(start_date, end_date)
-    if buckets is None:
-      return []
-    return [dao.to_client_json(bucket) for bucket in buckets]
+      if not start_date_str or not end_date_str:
+        raise BadRequest("Start date and end date should not be empty")
+      try:
+        start_date = datetime.datetime.strptime(start_date_str, DATE_FORMAT).date()
+      except ValueError:
+        raise BadRequest("Invalid start date: %s" % start_date_str)
+      try:
+        end_date = datetime.datetime.strptime(end_date_str, DATE_FORMAT).date()
+      except ValueError:
+        raise BadRequest("Invalid end date: %s" % end_date_str)
+      date_diff = abs((end_date - start_date).days)
+      if date_diff > DAYS_LIMIT:
+        raise BadRequest("Difference between start date and end date "\
+          "should not be greater than %s days" % DAYS_LIMIT)
+      buckets = dao.get_active_buckets(start_date, end_date)
+      if buckets is None:
+        return []
+      return [dao.to_client_json(bucket) for bucket in buckets]
+    else:
+      raise BadRequest("Request data is empty")
