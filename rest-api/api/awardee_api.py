05a5d8cab013ae309876b3b09e68335c198d4d83
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-12-06 10:00:19 -0600

    Implementing get

--- rest-api/api/awardee_api.py
+++ rest-api/api/awardee_api.py
@@ -2,6 +2,7 @@ from api.base_api import BaseApi
 from app_util import auth_required
 from api_util import PTC_AND_HEALTHPRO
 from dao.hpo_dao import HPODao
+from werkzeug.exceptions import NotFound
 
 class AwardeeApi(BaseApi):
   def __init__(self):
@@ -10,6 +11,9 @@ class AwardeeApi(BaseApi):
   @auth_required(PTC_AND_HEALTHPRO)
   def get(self, p_id=None):
     if p_id:
-      return super(AwardeeApi, self).get(p_id)
+       hpo = self.dao.get_by_name(p_id)
+       if not hpo:
+         raise NotFound("Awardee with ID %s not found" % p_id)
+       return self._make_response(self.dao.get_with_children(hpo.hpoId))
     else:
       return super(AwardeeApi, self)._query('id', None)
