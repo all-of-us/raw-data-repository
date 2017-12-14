6ed1d8536cebc2e511d12327ceb935bfd4481037
CH Albach <calbach@users.noreply.github.com>
2017-12-14 11:24:22 -0800

    [RW-263] Persist aggregate metric computations (#552)

--- rest-api/dao/metric_set_dao.py
+++ rest-api/dao/metric_set_dao.py
@@ -1,17 +1,25 @@
-from dao.base_dao import BaseDao
+from dao.base_dao import UpsertableDao
 from model.metric_set import AggregateMetrics, MetricSet
 from dao import database_factory
 
 
-class MetricSetDao(BaseDao):
+class MetricSetDao(UpsertableDao):
 
   def __init__(self):
     super(MetricSetDao, self).__init__(MetricSet,
                                        db=database_factory.get_generic_database())
 
 
-class AggregateMetricsDao(BaseDao):
+class AggregateMetricsDao(UpsertableDao):
 
   def __init__(self):
     super(AggregateMetricsDao, self).__init__(
         AggregateMetrics, db=database_factory.get_generic_database())
+
+  def delete_all_for_metric_set_with_session(self, session, ms_id):
+    session.execute(AggregateMetrics.__table__.delete()
+                    .where(AggregateMetrics.metricSetId == ms_id))
+
+  def delete_all_for_metric_set(self, ms_id):
+    with self.session() as session:
+      self.delete_all_for_metric_set_with_session(session, ms_id)
