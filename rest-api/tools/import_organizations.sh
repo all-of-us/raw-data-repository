77de29b21f8e4029f1b13a919a0ba1197f2437ba
Michael Mead <michaelmead007@icloud.com>
2018-01-04 11:51:53 -0600

    Da 465 (#559)
    * DA-465 add lat/long function for Site update.
    
    * DA-465 start implementing google geocode api
    
    * DA-465 start implementing google geocode api
    
    * DA-465 start implementing google geocode api
    
    * DA-465 start implementing google geocode api
    
    * DA-465 fix linting
    
    * DA-465 fix linting
    
    * DA-465 remove unneeded zip
    
    * do something with var to prevent linting errors
    
    * DA-465 remove string
    
    * add key file to auth_setup and pass key to google maps client api
    
    * remove blank line to shut the linter up
    
    * trying to fix trailing whitespace
    
    * DA-465 remove erroneous lat/long that already exists in model.
    
    * DA-465 cleanup tmp geocode file
    
    * DA-465 call geocode key function from import organizations.
    
    * DA-465 remove tmp geocode file, pass in project and account to get geocode function
    
    * DA-465 add logging to import organizations
    
    * DA-465 change indent level
    
    * DA-465 only geocode if address exists, make pmi drc api test the default config for geocode key
    
    * DA-465 move get_geocode_key function to import_organizations, add exception handling to geocode function, add cleanup function to import_organizations
    
    * DA-465 fix whitespace
    
    * DA-465 change try/catch to a dict .get syntax and change logging level to warn
    
    * DA-465 add cleanup function to prevent auth_setup override
    
    * DA-465 add index error checking in case of bad address being sent to geocode api
    
    * DA-465 clear cleanup function so auth_setup cleanup is ran

--- rest-api/tools/import_organizations.sh
+++ rest-api/tools/import_organizations.sh
@@ -14,14 +14,30 @@ while true; do
   esac
 done
 
+if [ -z "${ACCOUNT}" ]
+then
+  echo "Usage: $USAGE"
+  exit 1
+fi
+
+TMP_GEOCODE_DIR=$(mktemp -d)
+TMP_GEOCODE_INFO_FILE=${TMP_GEOCODE_DIR}/geocode_key.json
+
+function cleanup {
+}
+
+function get_geocode_key {
+    echo "Getting geocode api key ..."
+    (tools/install_config.sh --key geocode_key --account "${ACCOUNT}" \
+	    --project "pmi-drc-api-test"  --config_output "$TMP_GEOCODE_INFO_FILE")                    
+    export API_KEY=$(cat $TMP_GEOCODE_INFO_FILE | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["'api_key'"]')
+}
+
+CREDS_ACCOUNT="${ACCOUNT}"
+get_geocode_key
+
 if [ "${PROJECT}" ]
 then
-  if [ -z "${ACCOUNT}" ]
-  then
-    echo "Usage: $USAGE"
-    exit 1
-  fi
-  CREDS_ACCOUNT="${ACCOUNT}"
   source tools/auth_setup.sh
   run_cloud_sql_proxy
   set_db_connection_string
@@ -36,3 +52,11 @@ fi
 source tools/set_path.sh
 python tools/import_organizations.py --awardee_file data/awardees.csv \
   --organization_file data/organizations.csv --site_file data/sites.csv $DRY_RUN
+
+function finish {
+  cleanup
+  rm -rf ${TMP_GEOCODE_DIR}
+  rm -f ${TMP_GEOCODE_INFO_FILE}
+}
+trap finish EXIT
+
