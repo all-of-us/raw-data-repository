dab38f897baf75d7a45ee268aeb5ec1231e2b02f
CH Albach <calbach@google.com>
2018-01-25 10:38:29 -0800

    mv SQL to new migration

--- rest-api/alembic/versions/04564bf1b5e8_deidentify_ppi_views.py
+++ rest-api/alembic/versions/04564bf1b5e8_deidentify_ppi_views.py
@@ -32,53 +32,9 @@ def downgrade(engine_name):
     globals()["downgrade_%s" % engine_name]()
 
 
-_MEASUREMENTS_VIEW_SQL = """
-CREATE OR REPLACE VIEW physical_measurements_view AS
- SELECT
-   p.participant_id participant_id,
-   YEAR(p.sign_up_time) participant_sign_up_year,
-   p.withdrawal_status participant_withdrawal_status,
-   YEAR(p.withdrawal_time) participant_withdrawal_year,
-   p.suspension_status participant_suspension_status,
-   YEAR(p.suspension_time) participant_suspension_year,
-   pm.physical_measurements_id physical_measurements_id,
-   YEAR(pm.created) created_year,
-   pm.amended_measurements_id amended_measurements_id,
-   pm.created_site_id created_site_id,
-   pm.created_username created_username,
-   pm.finalized_site_id finalized_site_id,
-   pm.finalized_username finalized_username,
-   m.measurement_id measurement_id,
-   m.code_system code_system,
-   m.code_value code_value,
-   YEAR(m.measurement_time) measurement_year,
-   m.body_site_code_system body_site_code_system,
-   m.body_site_code_value body_site_code_value,
-   IF(m.value_string IS NULL, 0, 1) value_string_present,
-   m.value_decimal value_decimal,
-   m.value_unit value_unit,
-   m.value_code_system value_code_system,
-   m.value_code_value value_code_value,
-   YEAR(m.value_datetime) value_datetime_year,
-   m.parent_id parent_id,
-   m.qualifier_id qualifier_id
- FROM
-   participant p
-    INNER JOIN physical_measurements pm
-       ON pm.participant_id = p.participant_id
-    INNER JOIN measurement m
-       ON pm.physical_measurements_id = m.physical_measurements_id
-    INNER JOIN participant_summary ps
-       ON p.participant_id = ps.participant_id
-    LEFT OUTER JOIN hpo
-       ON p.hpo_id = hpo.hpo_id
-  WHERE (ps.email IS NULL OR ps.email NOT LIKE '%@example.com') AND
-        (hpo.name IS NULL OR hpo.name != 'TEST') AND
-        pm.final = 1
-"""
-
 def upgrade_rdr():
-  op.execute(_MEASUREMENTS_VIEW_SQL)
+    pass
+
 
 def downgrade_rdr():
     pass
