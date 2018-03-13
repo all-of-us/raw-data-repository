1a5b7b00c794784ebe9a87337b36898de36dfc46
Seeker1911 <michaelmead007@icloud.com>
2018-03-13 09:22:28 -0500

    DA-484 make sync bundle link

--- rest-api/api/participant_summary_api.py
+++ rest-api/api/participant_summary_api.py
@@ -1,5 +1,5 @@
 from api.base_api import BaseApi, make_sync_results_for_request
-from api_util import PTC_HEALTHPRO_AWARDEE, AWARDEE
+from api_util import PTC_HEALTHPRO_AWARDEE, AWARDEE,  DEV_MAIL
 from app_util import auth_required, get_validated_user_info
 from dao.participant_summary_dao import ParticipantSummaryDao
 from flask import request
@@ -15,17 +15,20 @@ class ParticipantSummaryApi(BaseApi):
     user_awardee = None
     user_email, user_info = get_validated_user_info()
     if AWARDEE in user_info['roles']:
-      user_awardee = user_info['awardee']
-      print user_email
+      if user_email == DEV_MAIL:
+        user_awardee = request.args.get('awardee')
+      else:
+        user_awardee = user_info['awardee']
 
     # data only for user_awardee, assert that query has same awardee
     if p_id:
       if user_awardee:
-        raise Forbidden
+        if user_email != DEV_MAIL:
+          raise Forbidden
       return super(ParticipantSummaryApi, self).get(p_id)
     else:
       if user_awardee:
-        # make sure request ahs awardee
+        # make sure request has awardee
         requested_awardee = request.args.get('awardee')
         if requested_awardee != user_awardee:
           raise Forbidden
@@ -38,13 +41,13 @@ class ParticipantSummaryApi(BaseApi):
       for filters in query.field_filters:
         if filters.field_name == 'lastModified':
           # set time delta subtract
-          pass
+          print filters
       query.always_return_token = True
     return query
 
   def _make_bundle(self, results, id_field, participant_id):
     if self._is_last_modified_sync():
-      return make_sync_results_for_request(self, results)
+      return make_sync_results_for_request(self.dao, results)
 
     return super(ParticipantSummaryApi, self)._make_bundle(results, id_field,
                                                                    participant_id)
