2191ec58ed49e4161966108eb9b0acab86d91726
Seeker1911 <michaelmead007@icloud.com>
2018-03-21 15:42:37 -0500

    start getting next link

--- rdr_client/work_queue.py
+++ rdr_client/work_queue.py
@@ -6,20 +6,18 @@ import pprint
 
 from main_util import configure_logging
 from client import Client
-from oauth2client.service_account import ServiceAccountCredentials
+
 
 def main():
   client = Client()
   AWARDEE = 'PITT'
   response = client.request_json('ParticipantSummary?_sync=true&_sort=lastModified&awardee={}'
                                                                       .format(AWARDEE), 'GET')
-  logging.info(pprint.pformat(response))
-  print '--------------------------------------'
-  print dir(client._get_authorized_http().credentials)
-  print '--------------------------------------'
-  print client._get_authorized_http().__dict__
-  print client.creds_file
 
+  sync_url = response['link'][0]['url']
+  index = sync_url.find('ParticipantSummary')
+  #next_batch = client.request_json(sync_url[index:])
+  logging.info(pprint.pformat(response))
 
 if __name__ == '__main__':
   configure_logging()
