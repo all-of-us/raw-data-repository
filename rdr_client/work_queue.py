b46042418dc99c9e2dd8ebec3d8d8ee3f6bb9f65
Seeker1911 <michaelmead007@icloud.com>
2018-03-21 13:25:31 -0500

    trying different client methods

--- rdr_client/work_queue.py
+++ rdr_client/work_queue.py
@@ -3,16 +3,23 @@
 
 import logging
 import pprint
+
 from main_util import configure_logging
 from client import Client
-
+from oauth2client.service_account import ServiceAccountCredentials
 
 def main():
   client = Client()
-
-  response = client.request_json('ParticipantSummary?_sync=true&_sort=lastModified&awardee=PITT',
-                                 'GET')
+  AWARDEE = 'PITT'
+  response = client.request_json('ParticipantSummary?_sync=true&_sort=lastModified&awardee={}'
+                                                                      .format(AWARDEE), 'GET')
   logging.info(pprint.pformat(response))
+  print '--------------------------------------'
+  print dir(client._get_authorized_http().credentials)
+  print '--------------------------------------'
+  print client._get_authorized_http().__dict__
+  print client.creds_file
+
 
 if __name__ == '__main__':
   configure_logging()
