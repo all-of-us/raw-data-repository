e4940348930aca36b3d1e29927bf55a0a10e0bd0
Seeker1911 <michaelmead007@icloud.com>
2018-03-23 14:38:51 -0500

    DA-539 add flag

--- rest-api/tools/remove_service_account_keys.sh
+++ rest-api/tools/remove_service_account_keys.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
-DATE=`date '+%Y-%m-%dT%H:%M:%SZ'`
-EXPIRE_DATE=`date -v"-3d"  "+%Y-%m-%d %H:%M:%S"`
-EXPIRE_DATE_EPOCH=`date -j -f"%Y-%m-%d %H:%M:%S" "$EXPIRE_DATE" +"%s"`
-PROJECTS="pmi-drc-api-test"
+while true; do
+  case "$1" in
+    --project) PROJECT=$2; shift 2;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
 
-for project in $PROJECTS
-do
-   echo "Project: $project"
-   for account in $(gcloud iam service-accounts list --project $project --format="value(email)")
+EXPIRE_DATE=$(date -v"-3d"  "+%Y-%m-%d %H:%M:%S")
+EXPIRE_DATE_EPOCH=$(date -j -f"%Y-%m-%d %H:%M:%S" "$EXPIRE_DATE" +"%s")
+
+   echo "Project: $PROJECT"
+   for account in $(gcloud iam service-accounts list --project $PROJECT --format="value(email)")
    do
         # match any service account starting with awardee-
         if [[ $account == awardee-* ]]
         then
             echo " -> Account: $account"
-            for key in $(gcloud iam service-accounts keys list --project $project --iam-account $account --format="value(name.basename())")
+            for key in $(gcloud iam service-accounts keys list --project $PROJECT --iam-account $account --format="value(name.basename())")
             do
-                for createdon in $(gcloud iam service-accounts keys list --iam-account $account --project $project --format="value(CREATED_AT)" )
+                for createdon in $(gcloud iam service-accounts keys list --iam-account $account --project $PROJECT --format="value(CREATED_AT)" )
                    do
                        echo "        Key created on $createdon"
-                       KEY_EPOCH=`date -j -f"%Y-%m-%dT%H:%M:%SZ" "$createdon" +"%s"`
+                       KEY_EPOCH=$(date -j -f"%Y-%m-%dT%H:%M:%SZ" "$createdon" +"%s")
                        if [ "$KEY_EPOCH" -gt "$EXPIRE_DATE_EPOCH" ]
                        then
                            echo "       Deleting key older than 3 days."
@@ -30,4 +34,3 @@ do
             done
         fi
    done
-done
