55e8ba562420ae785fa641086e49ea0e7495d2f4
Seeker1911 <michaelmead007@icloud.com>
2018-03-26 15:27:53 -0500

    [DA-561] update sync results to call every n seconds

--- rdr_client/work_queue.py
+++ rdr_client/work_queue.py
@@ -1,27 +1,51 @@
-""" Uses the service account 'awardee-pitt' to get participant summaries for work queue api
-    with an awardee of PITT"""
+""" Uses a service account to get and sync participant summaries for work queue api
+    You can only return results for the same awardee that the service account is connected to.
+
+    Run this using ./run_client.sh --project all-of-us-rdr-prod --account [your account]
+    --service_account [service account]@all-of-us-rdr-prod.iam.gserviceaccount.com work_queue.py
+
+    You should call code like this with credentials for a service account that has been set up for
+    your awardee by AllOfUs.
+    Expect to get the same participant summaries back in multiple responses (anything modified
+    within 60 seconds of the last summary in the previous response).
+    you'll get back relation="next" on the link if there are more results available in the RDR
+    that weren't returned and relation="sync" if there were.
+    Use the _count parameter to adjust this number. The default is 100
+
+    README: https://github.com/all-of-us/raw-data-repository#participantsummary-api
+"""
 
 import logging
 import pprint
-
+import time
+import sys
 from main_util import configure_logging
 from client import Client
 
 
 def main():
   client = Client()
-  AWARDEE = 'PITT'
-  response = client.request_json('ParticipantSummary?_sync=true&_count=1&_sort=lastModified&awardee'
-                                 '={}'.format(AWARDEE), 'GET')
+  awardee = 'PITT'
+  sync_time = 300  # Number of seconds before next sync.
+  response = client.request_json('ParticipantSummary?_sync=true&_sort=lastModified&awardee'
+                                 '={}'.format(awardee), 'GET')
 
+  logging.info(pprint.pformat(response))
   sync_url = response['link'][0]['url']
   index = sync_url.find('ParticipantSummary')
+
+  while True:
+    time.sleep(sync_time)
+    sync_results = sync(client, sync_url, index)
+    sync_url = sync_results['link'][0]['url']
+
+
+def sync(client, sync_url, index):
+  print '--------------      Getting next batch from sync...        -----------------', '\n'
   sync_results = client.request_json(sync_url[index:], 'GET')
-  logging.info(pprint.pformat(response))
-  print
-  print '--------------      Getting next batch from sync...        -----------------'
-  print
   logging.info(pprint.pformat(sync_results))
+  return sync_results
+
 
 if __name__ == '__main__':
   configure_logging()
