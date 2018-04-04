2e401e340156b2adbc0fb5644b37ba9dd812f60f
Seeker1911 <michaelmead007@icloud.com>
2018-04-04 12:55:05 -0500

    DA-539 more attempts at getting service accounts

--- rest-api/offline/sa_key_remove.py
+++ rest-api/offline/sa_key_remove.py
@@ -1,15 +1,34 @@
-import dev_appserver
-import appengine_config
-import oauth2client
-import google.auth.iam
-from oauth2client.contrib import appengine
-from oauth2client.service_account import ServiceAccountCredentials
-from client import Client
+# import google.auth.iam as ga
+# from oauth2client.service_account import ServiceAccountCredentials
+# from google.auth.credentials import Credentials
+# from google.oauth2 import credentials, id_token, service_account
+# import httplib2
+# from oauth2client.contrib import gce
+# import sys
+# creds = 'pmi-drc-api-test@appspot.gserviceaccount.com'
+# SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
+# signer = ga.Signer(SCOPE, creds, creds)
+#
+# creds_file = sys.argv[1]
+# sa = ServiceAccountCredentials('michael.mead@pmi-ops.org', signer)
+# print sa.id_token
+# print sa.access_token
+# print sa.serialization_data
+# print sa.scopes
+# print sa.client_secret
+# print sa.client_id
+#
+# print signer.key_id
+
+token = '0d41bd307b8d0c04239062484739c0e001e2c4fd'
+# curl 'https://iam.googleapis.com/v1/projects/pmi-drc-api-test/serviceAccounts' \
+#   -H 'Authorization: Bearer [YOUR_BEARER_TOKEN]' \
+#   -H 'Accept: application/json' \
+#   --compressed
+
+headers = {"Authorization":"Bearer 0d41bd307b8d0c04239062484739c0e001e2c4fd" , "Accept":"application/json"}
+
 import requests
-client = Client(default_instance='localhost')
-
-list_keys = 'https://iam.googleapis.com/v1/projects/pmi-drc-api-test/serviceAccounts/awardee' \
-            '-pitt@pmi-drc-api-test.iam.gserviceaccount.com/keys'
-r = requests.post(list_keys)
+r = requests.get('https://iam.googleapis.com/v1/projects/pmi-drc-api-test/serviceAccounts',
+                 headers=headers)
 print r
-
