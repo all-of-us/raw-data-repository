d11af24c010ff4f396e2d06bc8bb93d345cd32e1
Eric Weitz <eweitz@broadinstitute.org>
2018-04-12 14:05:25 -0400

    Insert test HPO during automated test

--- rest-api/test/unit_test/api_test/participant_counts_over_time_api_test.py
+++ rest-api/test/unit_test/api_test/participant_counts_over_time_api_test.py
@@ -2,16 +2,22 @@ import datetime
 from clock import FakeClock
 
 from dao.participant_dao import ParticipantDao
+from dao.hpo_dao import HPODao
+from model.hpo import HPO
 from dao.participant_summary_dao import ParticipantSummaryDao
 from test.unit_test.unit_test_util import FlaskTestBase, random_ids
 from model.participant import Participant
+from participant_enums import OrganizationType, TEST_HPO_NAME, TEST_HPO_ID
 
 class ParticipantCountsOverTimeApiTest(FlaskTestBase):
 
   def setUp(self):
-    super(ParticipantCountsOverTimeApiTest, self).setUp()
+    super(ParticipantCountsOverTimeApiTest, self).setUp(use_mysql=True)
     self.dao = ParticipantDao()
     self.participant_summary_dao = ParticipantSummaryDao()
+    hpo_dao = HPODao()
+    hpo_dao.insert(HPO(hpoId=TEST_HPO_ID, name=TEST_HPO_NAME, displayName='Test',
+                     organizationType=OrganizationType.UNSET))
 
   def test_get_counts_with_default_parameters(self):
     p = Participant()
@@ -33,8 +39,5 @@ class ParticipantCountsOverTimeApiTest(FlaskTestBase):
     qs = ''.join(qs.split())  # Remove all whitespace
 
     response = self.send_get('ParticipantCountsOverTime', query_string=qs)
-
-    print('response')
-    print(response)
 
     self.assertEquals(1, 1)
