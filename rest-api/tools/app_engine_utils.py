f91d6831dc5296555f2d012db061bcb6f5424b1d
Seeker1911 <michaelmead007@icloud.com>
2018-04-19 12:35:54 -0500

    DA-539 add to utils

--- rest-api/tools/app_engine_utils.py
+++ rest-api/tools/app_engine_utils.py
@@ -4,13 +4,13 @@ import os
 """ Helper functions for using app_engine api's with Python SDK """
 
 def get_key():
-  storage_key = os.path.expanduser('~/.gcp/cloudsecurity-monitoring.json')
+ if os.path.isfile(get_key()):
+   os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_key()
+   storage_key = os.path.expanduser('~/.gcp/cloudsecurity-monitoring.json')
 
-  return storage_key
+ return storage_key
 
 
-if os.path.isfile(get_key()):
-  os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_key()
 
 
 def get_projects():
@@ -27,3 +27,22 @@ def get_projects():
     request = service.projects().list_next(previous_request=request, previous_response=response)
 
   return project_list
+
+def make_key(app_id):
+  service = discovery.build('iam', 'v1')
+  project_name = 'projects/' + app_id
+  request = service.projects().serviceAccounts().list(name=project_name)
+  response = request.execute()
+  account = response['accounts']
+  email = account['email']
+  serviceaccount = project_name + '/serviceAccounts/' + email
+  create_request = service.projects().serviceAccounts().keys().create(name=serviceaccount)
+  key = create_request.execute()
+  print key
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+ main()
