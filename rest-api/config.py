774ca601218aabf205c909267dcebb9e34526abc
Seeker1911 <michaelmead007@icloud.com>
2018-04-19 13:57:03 -0500

    DA-539 fix a stupid typo

--- rest-api/config.py
+++ rest-api/config.py
@@ -48,7 +48,7 @@ BIOBANK_STATUS_MAIL_RECIPIENTS = 'biobank_status_mail_recipients'
 ADD_QUESTIONNAIRE_CODES_IF_MISSING = 'add_questionnaire_codes_if_missing'
 
 REQUIRED_CONFIG_KEYS = [BIOBANK_SAMPLES_BUCKET_NAME]
-DAY_TO_DELETE = 3
+DAYS_TO_DELETE = 3
 
 
 # Overrides for testing scenarios
