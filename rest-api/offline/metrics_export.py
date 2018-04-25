26bd3ad6a6077fecfc2f1b63d6b3a957bb1765da
Seeker1911 <michaelmead007@icloud.com>
2018-04-25 10:29:21 -0500

    DA-596 fix line

--- rest-api/offline/metrics_export.py
+++ rest-api/offline/metrics_export.py
@@ -81,8 +81,8 @@ SELECT qr.participant_id participant_id, ISODATE[qr.created] start_time,
    AND NOT EXISTS
     (SELECT * FROM participant_summary ps
       WHERE ps.participant_id = p.participant_id
-        AND ps.email LIKE :test_email_pattern)
         AND ps.withdrawal_status != 2
+        AND ps.email LIKE :test_email_pattern)
  ORDER BY qr.participant_id, qr.created, qc.value
 """
 
