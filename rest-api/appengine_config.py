03aa6d518539855f3aed3e4a8affd230b0773047
Seeker1911 <michaelmead007@icloud.com>
2018-05-24 14:00:57 -0500

    add wsgi middleware

--- rest-api/appengine_config.py
+++ rest-api/appengine_config.py
@@ -5,7 +5,32 @@ cloud.google.com/appengine/docs/standard/python/tools/using-libraries-python-27
 
 from google.appengine.ext import vendor
 
+import cProfile
+import cStringIO
+import logging
+import pstats
+
 vendor.add('lib')
 vendor.add('lib-common')
 vendor.add('appengine-mapreduce/python/src')
 vendor.add('appengine-mapreduce/python/test')
+
+
+#def webapp_add_wsgi_middleware(app):
+#
+#  def profiling_wrapper(environ, start_response):
+#    profile = cProfile.Profile()
+#    response = profile.runcall(app, environ, start_response)
+#    stream = cStringIO.StringIO()
+#    stats = pstats.Stats(profile, stream=stream)
+#    stats.sort_stats('cumulative').print_stats()
+#    logging.info('Profile data:\n%s', stream.getvalue())
+#    return response
+#
+#  return profiling_wrapper
+
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
+
