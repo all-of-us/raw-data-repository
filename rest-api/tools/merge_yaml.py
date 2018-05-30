720c4d3edd2dd12c6d78fda4feb40a70c26b37d0
Seeker1911 <michaelmead007@icloud.com>
2018-05-30 08:34:12 -0500

    DA-649 introduce pyyaml.

--- rest-api/tools/merge_yaml.py
+++ rest-api/tools/merge_yaml.py
@@ -1,15 +1,21 @@
+import logging
+import yaml
 from main_util import configure_logging, get_parser
 
+
 def main(args):
-  print args.base_yaml, '< base yaml'
-  print args.env_yaml, '< env yaml'
   base_file = args.base_yaml
   env_file = args.env_yaml
-  with open(base_file, 'r') as base_reader, open(env_file, 'r') as env_reader:
-    base_lines = base_reader.readlines()
-    env_lines = env_reader.readlines()
-    print base_lines, '< base lines'
-    print env_lines, '< env lines'
+  try:
+    with open(base_file, 'r') as base_reader, open(env_file, 'r') as env_reader:
+      print yaml.load(base_reader)
+      base_lines = base_reader.readlines()
+      env_lines = env_reader.readlines()
+      # print base_lines, '< base lines'
+      # print env_lines, '< env lines'
+  except IOError:
+    logging.warning('Unable to open files for reading.')
+
 
 
 if __name__ == '__main__':
