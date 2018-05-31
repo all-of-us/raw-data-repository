32aeec408b7f9baf14e4db417aa6a30ce62ebd9a
Michael Mead <michaelmead007@icloud.com>
2018-05-31 09:38:28 -0500

    DA-649 add profiling (#714)
    * add wsgi middleware
    
    * change script
    
    * typo
    
    * change url handler
    
    * change path to stats
    
    * change path to stats
    
    * remove unused function
    
    * DA-649 testing multiple handler calls in yaml files
    
    * DA-649 add a merge util
    
    * clean git status for test
    
    * DA-649 fix typo
    
    * read files
    
    * DA-649 introduce pyyaml.
    
    * DA-649 write to file.
    
    * DA-649 write to file.
    
    * DA-649 write to file.
    
    * DA-649 write to file.
    
    * DA-649 write to file.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * remove app.yaml
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 testing parse yaml function.
    
    * DA-649 switch to handlers for each environment
    
    * DA-649 remove merge script
    
    * fix workqueue memory limit in sandbox
    
    * DA-649 add pyzmq to license expections
    
    * reset requirements
    
    * DA-649 moving builtins key to environment files to avoid using appstats in prod.
    
    * DA-649 remove pyzmq from license_exceptions.
    
    * DA-649 remove stats handler from test.yaml

--- rest-api/appengine_config.py
+++ rest-api/appengine_config.py
@@ -9,3 +9,10 @@ vendor.add('lib')
 vendor.add('lib-common')
 vendor.add('appengine-mapreduce/python/src')
 vendor.add('appengine-mapreduce/python/test')
+
+
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
+
