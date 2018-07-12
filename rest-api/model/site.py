d42a248e61e3c413ddbb67be61ee836cd7bc005c
Michael Mead <michaelmead007@icloud.com>
2018-07-12 14:00:15 -0500

    update site.py to accept longer form text fields. (#739)
    * update site.py to accept longer form text fields.
    
    * change to unicode
    
    * fix alembic
    
    * fix migrations
    
    * add missing commas
    
    * fix extra rows in sites.csv
    
    (cherry picked from commit 9c17999)

--- rest-api/model/site.py
+++ rest-api/model/site.py
@@ -1,6 +1,6 @@
 from model.base import Base
-from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey
-from site_enums import SiteStatus, EnrollingStatus, DigitalSchedulingStatus, ObsoleteStatus
+from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey, UnicodeText
+from site_enums import SiteStatus, EnrollingStatus, DigitalSchedulingStatus
 from model.utils import Enum
 
 class Site(Base):
@@ -21,12 +21,12 @@ class Site(Base):
   scheduleInstructions = Column('schedule_instructions', String(2048))
   scheduleInstructions_ES = Column('schedule_instructions_es', String(2048))
   launchDate = Column('launch_date', Date)
-  notes = Column('notes', String(1024))
-  notes_ES = Column('notes_es', String(1024))
+  notes = Column('notes', UnicodeText)
+  notes_ES = Column('notes_es', UnicodeText)
   latitude = Column('latitude', Float)
   longitude = Column('longitude', Float)
   timeZoneId = Column('time_zone_id', String(1024))
-  directions = Column('directions', String(1024))
+  directions = Column('directions', UnicodeText)
   physicalLocationName = Column('physical_location_name', String(1024))
   address1 = Column('address_1', String(1024))
   address2 = Column('address_2', String(1024))
@@ -36,4 +36,3 @@ class Site(Base):
   phoneNumber = Column('phone_number', String(80))
   adminEmails = Column('admin_emails', String(4096))
   link = Column('link', String(255))
-  isObsolete = Column('is_obsolete', Enum(ObsoleteStatus), default=0)
