6f160e486667036a354968603b3a7c44b116d0fd
Seeker1911 <michaelmead007@icloud.com>
2018-08-02 09:06:20 -0500

    DA-688 refactor to use subprocess.

--- rest-api/tools/ehr_upload.py
+++ rest-api/tools/ehr_upload.py
@@ -1,5 +1,9 @@
 """Get site/participant info for a given awardee to fetch consent forms from a bucket
-and upload to awardee bucket."""
+and upload to a new awardee bucket by the same name.
+eg: tools/ehr_upload.sh --awardee AZ_TUCSON --bucket ptc-uploads-pmi-drc-api-prod
+    --account me@pmi-ops.org --project all-of-us-rdr-prod
+"""
+import subprocess
 from dao import database_factory
 from main_util import get_parser, configure_logging
 from sqlalchemy import text
@@ -7,31 +11,11 @@ from sqlalchemy import text
 
 def main(args):
   with database_factory.get_database().session() as session:
-    if args.sites:
-      get_sites_for_awardee(session, args.awardee)
-    if args.participants:
-      get_participants_under_sites(session, args.awardee)
+    get_participants_under_sites(session, args.awardee, args.source_bucket, args.destination_bucket)
 
 
-def get_sites_for_awardee(session, awardee):
-  sql = """ select site_name from site
-      where  hpo_id in (select hpo_id from hpo where name = '{}')
-      order by site_name;
-      """.format(awardee)
-  cursor = session.execute(text(sql))
-
-  try:
-    results = cursor.fetchall()
-    results = results[:3]  # @TODO: For testing purposes
-    results = [r[0] for r in results]
-    results = [str(i).replace(" ", "_") for i in results]
-    print results
-  finally:
-    cursor.close()
-
-
-def get_participants_under_sites(session, awardee):
-  sql = """ select p.participant_id, s.site_name from participant p, site s
+def get_participants_under_sites(session, awardee, source_bucket, destination_bucket):
+  sql = """ select p.participant_id, s.google_group from participant p, site s
       where  p.site_id in (
         select site_id from site where hpo_id in (select hpo_id from hpo where name = '{}'))
       order by s.site_name;
@@ -40,9 +24,18 @@ def get_participants_under_sites(session, awardee):
   try:
     results = cursor.fetchall()
     results = results[:3]  # @TODO: For testing purposes
-    map(int, [r[0] for r in results])
+    results = [(int(i), str(k)) for i, k in results]
+    if results:
+      for participant, google_group in results:
+        gsutil = "gsutil -m cp -r gs://" + source_bucket + "/Participant/P" + str(
+          participant) + "/* " + "gs://" + destination_bucket + "/Participant/" + \
+          google_group + "/P" + str(participant)
 
-    print results
+        system_call = subprocess.call(gsutil, shell=True)
+        if system_call == 0:
+          print "Successfully moved folder " + google_group + '/' + str(participant)
+        elif system_call == 1:
+          print "There was an error moving folder " + google_group + '/' + str(participant)
   finally:
     cursor.close()
 
@@ -52,7 +45,9 @@ if __name__ == '__main__':
   parser = get_parser()
   parser.add_argument('--awardee', help='The awardee to find participants and sites for',
                       required=True)
-  parser.add_argument('--sites', help='command line flag to get sites')
-  parser.add_argument('--participants', help='command line flag to get participants')
+  parser.add_argument('--source_bucket', help='The bucket to read from in one env.'
+                      ' i.e. ptc-uploads-pmi-drc-api-prod', required=True)
+  parser.add_argument('--destination_bucket', help='The bucket to write to in one env.'
+                      'i.e. ptc-uploads-pmi-drc-api-prod', required=True)
 
   main(parser.parse_args())
--- rest-api/tools/ehr_upload.sh
+++ rest-api/tools/ehr_upload.sh
@@ -6,47 +6,36 @@
 # for each site, iterate id's and download associated bucket files
 # upload participant files to new bucket under a directory named after the site
 
-USAGE="tools/ehr_upload.sh --awardee <AWARDEE> --account <ACCOUNT> --project <PROJECT>"
+USAGE="tools/ehr_upload.sh --awardee <AWARDEE> --bucket <BUCKET> --account <ACCOUNT> --project <PROJECT>"
 while true; do
   case "$1" in
     --account) ACCOUNT=$2; shift 2;;
     --project) PROJECT=$2; shift 2;;
     --awardee) AWARDEE=$2; shift 2;;
+    --source_bucket)  SOURCE_BUCKET=$2; shift 2;;
+    --destination_bucket)  DESTINATION_BUCKET=$2; shift 2;;
     * ) break ;;
   esac
 done
 
-if [ -z "${AWARDEE}" ] || [ -z "${PROJECT}" ] || [ -z "${ACCOUNT}" ]
+if [ -z "${AWARDEE}" ] || [ -z "${PROJECT}" ] || [ -z "${ACCOUNT}" ] || [ -z "${SOURCE_BUCKET}" ] || [ -z "${DESTINATION_BUCKET}" ]
 then
   echo "Usage: ${USAGE}"
   exit 1
 fi
 
 CREDS_ACCOUNT="${ACCOUNT}"
-#SERVICE_ACCOUNT="exporter@${PROJECT}.iam.gserviceaccount.com"
+SERVICE_ACCOUNT="configurator@all-of-us-rdr-prod.iam.gserviceaccount.com"
 source tools/set_path.sh
 source tools/auth_setup.sh
 run_cloud_sql_proxy
 set_db_connection_string
-#gcloud auth activate-service-account --key-file ${CREDS_FILE}
+gcloud auth activate-service-account --key-file ${CREDS_FILE}
 
-gsutil ls -l gs://ptc-uploads-pmi-drc-api-sandbox/Participant
-
-GET_SITES_FOR_AWARDEE=$(python tools/ehr_upload.py --awardee ${AWARDEE} --sites 'true')
-GET_PARTICPANTS_WITH_SITES=$(python tools/ehr_upload.py --awardee ${AWARDEE} --participants 'true')
-echo '******************'
-echo $GET_SITES_FOR_AWARDEE
-echo '*******************'
-#echo $GET_PARTICPANTS_WITH_SITES
-#gsutil cp gs://ptc-uploads-pmi-drc-api-sandbox/Participant/P100/botw_emoji.jpg  gs://ptc-uploads-pmi-drc-api-sandbox/Participant/P200
-
-for site in $GET_SITES_FOR_AWARDEE;
+GET_SITES_FOR_AWARDEE=$(python tools/ehr_upload.py --awardee ${AWARDEE} --source_bucket ${SOURCE_BUCKET} --destination_bucket ${DESTINATION_BUCKET})
+IFS=$'\n';
+for message in $GET_SITES_FOR_AWARDEE;
 do
-IFS=",";
-set $site;
-echo $site;
-echo "***"
-#echo $1
+echo $message
 done
-### FOR EACH SITE, GET THE PID'S AND UPLOAD TO SITE FOLDER
-#gsutil -m cp -r gs://ptc-uploads-pmi-drc-api-sandbox/Participant/* gs://ptc-uploads-pmi-drc-api-sandbox/Participant/some_test_folder
+IFS=$OIFS;
