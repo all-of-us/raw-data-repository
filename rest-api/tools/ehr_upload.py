fe20053d11d8fd073db0c953c7523639010de5bf
Seeker1911 <michaelmead007@icloud.com>
2018-08-03 13:55:24 -0500

    DA-688 start adding a function to get participants without site affiliation

--- rest-api/tools/ehr_upload.py
+++ rest-api/tools/ehr_upload.py
@@ -36,9 +36,32 @@ def get_participants_under_sites(session, organization, source_bucket, destinati
           print "There was an error moving folder " + google_group + '/' + str(participant)
   finally:
     cursor.close()
+    get_participants_without_site_pairing()
 
-def get_participants_without_site_pairing():
-  pass
+
+def get_participants_without_site_pairing(session, organization, source_bucket, destination_bucket):
+  sql = """     
+    select participant_id, google_group from participant p left join site s on p.site_id = s.site_id
+    where p.organization_id in (select organization_id from organization where external_id = '{}');
+   """.format(organization)
+  cursor = session.execute(text(sql))
+  try:
+    results = cursor.fetchall()
+    results = [(int(i), str(k)) for i, k in results]
+    if results:
+      for participant, google_group in results:
+        gsutil = "gsutil -m cp -r -L -p gs://" + source_bucket + "/Participant/P" + str(
+          participant) + "/* " + "gs://" + destination_bucket + "/Participant/" + \
+                 google_group + "/P" + str(participant)
+
+        system_call = subprocess.call(gsutil, shell=True)
+        if system_call == 0:
+          print "Successfully moved folder " + google_group + '/' + str(participant)
+        elif system_call == 1:
+          print "There was an error moving folder " + google_group + '/' + str(participant)
+  finally:
+    cursor.close()
+
 
 if __name__ == '__main__':
   configure_logging()
