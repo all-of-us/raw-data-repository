049ffcd520e3df0e9b73b54c423c656ca45f6ed1
Seeker1911 <michaelmead007@icloud.com>
2018-08-08 08:55:17 -0500

    DA-688 refactor

--- rest-api/tools/ehr_upload.py
+++ rest-api/tools/ehr_upload.py
@@ -13,12 +13,17 @@ def main(args):
     get_participants_under_sites(
       session, args.organization, args.source_bucket, args.destination_bucket)
 
+    get_participants_without_site_pairing(
+        session, args.organization, args.source_bucket, args.destination_bucket)
 
 def get_participants_under_sites(session, organization, source_bucket, destination_bucket):
   sql = """
-  select p.participant_id, google_group from participant p left join site s on p.site_id = s.site_id left join participant_summary summary on p.participant_id = summary.participant_id
+  select p.participant_id, google_group from participant p left join site s on
+  p.site_id = s.site_id
+  left join participant_summary summary on p.participant_id = summary.participant_id
   where p.organization_id in (select organization_id from organization where external_id = '{}')
-  and summary.consent_for_electronic_health_records =1 and summary.consent_for_study_enrollment = 1;
+  and summary.consent_for_electronic_health_records = 1
+  and summary.consent_for_study_enrollment = 1;
    """.format(organization)
   cursor = session.execute(text(sql))
   try:
@@ -28,23 +33,24 @@ def get_participants_under_sites(session, organization, source_bucket, destinati
       for participant, google_group in results:
         gsutil = "gsutil -m cp gs://" + source_bucket + "/Participant/P" + str(
           participant) + "/* " + "gs://" + destination_bucket + "/Participant/" + \
-          google_group + "/P" + str(participant)
+          google_group + "/P" + str(participant) + "/"
 
         system_call = subprocess.call(gsutil, shell=True)
         if system_call == 0:
           print "Successfully moved folder " + google_group + '/' + str(participant)
         elif system_call == 1:
           print "There was an error moving folder " + google_group + '/' + str(participant)
+        else:
+            print "System error message: " + system_call
   finally:
     cursor.close()
-    get_participants_without_site_pairing(session, organization, source_bucket, destination_bucket)
 
 
 def get_participants_without_site_pairing(session, organization, source_bucket, destination_bucket):
   sql = """
     select participant_id from participant p
-    where p.site_id is NULL and p.organization_id in (
-    select organization_id from organization where external_id = '{}');
+    right join organization o on o.organization_id = p.organization_id
+    where p.site_id is NULL and o.external_id = '{}';
    """.format(organization)
   cursor = session.execute(text(sql))
   try:
@@ -54,13 +60,15 @@ def get_participants_without_site_pairing(session, organization, source_bucket, 
       for participant in results:
         gsutil = "gsutil -m cp gs://" + source_bucket + "/Participant/P" + str(
           participant) + "/* " + "gs://" + destination_bucket + "/Participant/" + \
-                 "no_site_pairing" + "/P" + str(participant)
+                 "no_site_pairing" + "/P" + str(participant) + "/"
 
         system_call = subprocess.call(gsutil, shell=True)
         if system_call == 0:
           print "Successfully moved folder " + "no_site_pairing" + '/' + str(participant)
         elif system_call == 1:
           print "There was an error moving folder " + "no_site_pairing" + '/' + str(participant)
+        else:
+            print "System error message: " + system_call
   finally:
     cursor.close()
 
