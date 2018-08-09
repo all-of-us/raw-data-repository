d4c9d1fbe85232d9a76dc551250f50cf22818e7c
Seeker1911 <michaelmead007@icloud.com>
2018-08-08 20:06:17 -0500

    fix insert participants logic error

--- rest-api/tools/import_organizations.py
+++ rest-api/tools/import_organizations.py
@@ -299,11 +299,11 @@ class SiteImporter(CsvImporter):
               except KeyError:
                 logging.warn('No running instance for %s', version['name'])
 
-                if response['done']:
-                  insert_participants = True
-                  logging.info('Reboot of instance: %s in stable complete.', instance['name'])
-                else:
-                  logging.warn('Not able to reboot instance on server, Error: %s', response)
+          if response['done']:
+            insert_participants = True
+            logging.info('Reboot of instance: %s in stable complete.', instance['name'])
+          else:
+            logging.warn('Not able to reboot instance on server, Error: %s', response)
 
           if insert_participants:
             logging.info('Starting import of test participants.')
