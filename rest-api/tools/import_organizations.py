08c7ce7660d4edd0ec1928cbca26207848a1cef8
Michael Mead <michaelmead007@icloud.com>
2018-08-10 08:17:55 -0500

    Mm/insert participants (#762)
    * fix insert participants logic error
    
    * refactor
    
    * ok, I moved it out to far, all better now

--- rest-api/tools/import_organizations.py
+++ rest-api/tools/import_organizations.py
@@ -296,19 +296,19 @@ class SiteImporter(CsvImporter):
                                                                         instancesId=sha)
 
                   response = delete_instance.execute()
+
               except KeyError:
                 logging.warn('No running instance for %s', version['name'])
 
-                if response['done']:
-                  insert_participants = True
-                  logging.info('Reboot of instance: %s in stable complete.', instance['name'])
-                else:
-                  logging.warn('Not able to reboot instance on server, Error: %s', response)
+              if response['done']:
+                insert_participants = True
+                logging.info('Reboot of instance: %s in stable complete.', instance['name'])
+              else:
+                logging.warn('Not able to reboot instance on server, Error: %s', response)
 
           if insert_participants:
             logging.info('Starting import of test participants.')
             self._insert_new_participants(self.new_sites_list)
-
 
   def delete_sql_statement(self, session, str_list):
     sql = """
