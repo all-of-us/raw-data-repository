8d82f6001c66a7f253e4019de9ad22ce42c4007b
Seeker1911 <michaelmead007@icloud.com>
2018-09-28 09:42:52 -0500

    DA-741 add test for canceling an ammended order.

--- rest-api/test/unit_test/api_test/physical_measurements_api_test.py
+++ rest-api/test/unit_test/api_test/physical_measurements_api_test.py
@@ -283,8 +283,10 @@ class PhysicalMeasurementsApiTest(FlaskTestBase):
     response = self.send_get(path)
     self.assertEqual(response['status'], 'restored')
     self.assertEqual(response['reason'], 'need to restore')
+    # Response should not contain cancelledInfo
     self.assertTrue('cancelledUsername' not in response)
     self.assertTrue('cancelledSiteId' not in response)
+    self.assertTrue('cancelledTime' not in response)
 
   def test_cannot_restore_a_valid_pm(self):
     self.send_consent(self.participant_id)
@@ -304,3 +306,20 @@ class PhysicalMeasurementsApiTest(FlaskTestBase):
     path = path + '/' + response['id']
     self.send_patch(path, get_restore_or_cancel_info())
     self.send_patch(path, get_restore_or_cancel_info(), expected_status=httplib.BAD_REQUEST)
+
+  def test_cancel_an_ammended_order(self):
+    self.send_consent(self.participant_id)
+    measurements_1 = load_measurement_json(self.participant_id)
+    path = 'Participant/%s/PhysicalMeasurements' % self.participant_id
+    response = self.send_post(path, measurements_1)
+    measurements_2 = load_measurement_json_amendment(self.participant_id, response['id'])
+    response_2 = self.send_post(path, measurements_2)
+
+    path = path + '/' + response_2['id']
+    cancel_info = get_restore_or_cancel_info()
+    self.send_patch(path, cancel_info)
+    response = self.send_get(path)
+    self.assertEqual(response['status'], 'cancelled')
+    self.assertEqual(response['reason'], 'a mistake was made.')
+    self.assertEqual(response['cancelledUsername'], 'mike@pmi-ops.org')
+    self.assertEqual(response['cancelledSiteId'], 1)
