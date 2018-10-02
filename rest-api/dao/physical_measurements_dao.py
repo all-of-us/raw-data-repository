a7b3d21f771a70b75ef5706bdefe209a4563cb65
Seeker1911 <michaelmead007@icloud.com>
2018-10-02 09:14:08 -0500

    DA-741 add logging to for physical measurement and participant summary.

--- rest-api/dao/physical_measurements_dao.py
+++ rest-api/dao/physical_measurements_dao.py
@@ -360,9 +360,12 @@ class PhysicalMeasurementsDao(UpdatableDao):
       measurement.cancelledTime = None
       measurement.status = PhysicalMeasurementsStatus.UNSET
 
+    logging.info('%s %s physical measuremnt %s.', author, resource['status'],
+                 measurement.physicalMeasurementsId)
     payload = self.add_root_fields_to_resource(measurement)
-    # @TODO: ADD LOGGING !!!
     super(PhysicalMeasurementsDao, self)._do_update(session, payload, payload)
+    logging.info('updating participant summary to %s for participant: %s', payload.status,
+                 payload.participantId)
     self._update_participant_summary(session, payload)
 
     return payload
