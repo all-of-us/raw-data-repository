d9d4c4418178a5ea5f791a23e25a48961ce032ed
Seeker1911 <michaelmead007@icloud.com>
2018-11-21 10:04:43 -0600

    Create tool script to sync consent files to awardee buckets. Gets info from internal Google Sheet All HPO's Report.

--- rest-api/tools/ehr_upload.py
+++ rest-api/tools/ehr_upload.py
@@ -1,24 +1,37 @@
-"""Get site/participant info for a given organization to fetch consent forms from a bucket
-and upload to a new organization bucket by the same name.
-eg: tools/ehr_upload.sh --organization AZ_TUCSON --bucket ptc-uploads-pmi-drc-api-prod
+"""Get site/participant info for a given organization to fetch consent forms from
+ptc-uploads-all-of-us-rdr-prod (the default location for new participant uploads from PTC.
+Syncs consent files to a new organization bucket by the same name in the Awardees consent bucket
+i.e.aou179/hpo-site-xxx/Participant/<consent files>
+To run: tools/ehr_upload.sh --account <pmi ops account> --project all-of-us-rdr-prod
 """
+import StringIO
 import csv
 import logging
-import subprocess
-import shlex
-import requests
 import os
-from sqlalchemy import text
+import shlex
+import subprocess
+import urllib2
+
 from dao import database_factory
 from main_util import configure_logging
+from sqlalchemy import text
 
 
-FILE_URL = os.getenv('GAE_HPO_REPORT_URL')
+# @TODO: This spreadsheet is confidential, cant put it in version control. Need something better
+# than an environment variable though.
+SPREADSHEET_ID = os.getenv('HPO_REPORT_SPREADSHEET_ID')
 SOURCE_BUCKET = 'ptc-uploads-all-of-us-rdr-prod'
 BUCKET_NAME = 'Bucket Name'
 AGGREGATING_ORG_ID = 'Aggregating Org ID'
 ORG_ID = 'Org ID'
 ORG_STATUS = 'Org Status'
+
+
+FILE_URL = 'https://docs.google.com/spreadsheets/d/%(id)s/export?format=csv&id=%(id)s&gid=%(' \
+      'gid)s' % {
+        'id': SPREADSHEET_ID,
+        'gid': "0",
+        }
 
 
 def get_sql(organization):
@@ -27,6 +40,7 @@ def get_sql(organization):
     p.site_id = s.site_id
     left join participant_summary summary on p.participant_id = summary.participant_id
     where p.organization_id in (select organization_id from organization where external_id = '{}')
+    and s.google_group is not null
     and summary.consent_for_electronic_health_records = 1
     and summary.consent_for_study_enrollment = 1;
      """.format(organization)
@@ -42,12 +56,9 @@ def get_sql(organization):
   return site_pairing_sql, no_site_pairing_sql
 
 
-def _fetch_csv_data(url):
-  logging.info('url is => {}'.format(url))
-  response = requests.get(url)
-  logging.info('response is => {}'.format(response.iter_lines()))
-  reader = csv.DictReader(response.iter_lines())
-  return reader
+def _fetch_csv_data():
+  response = urllib2.urlopen(FILE_URL)
+  return csv.DictReader(StringIO.StringIO(response.read()))
 
 
 def _ensure_buckets(hpo_data):
@@ -64,7 +75,6 @@ def read_hpo_report(csv_reader):
   hpo_data = {}
 
   for row in csv_reader:
-    print row
     bucket_name = row.get(BUCKET_NAME)
     aggregate_id = row.get(AGGREGATING_ORG_ID)
     org_id = row.get(ORG_ID)
@@ -73,18 +83,16 @@ def read_hpo_report(csv_reader):
     if org_status == 'Active':
       hpo_data[org_id] = {'aggregate_id': aggregate_id, 'bucket': bucket_name}
 
-  logging.info('hpo_data is => {}'.format(hpo_data))
   _ensure_buckets(hpo_data)
   return hpo_data
 
 
 def sync_ehr_consents():
-  csv_reader = _fetch_csv_data(FILE_URL)
+  csv_reader = _fetch_csv_data()
   csv_reader.next()
   hpo_data = read_hpo_report(csv_reader)
   logging.info('Reading data complete, beginning sync...')
-  logging.info(hpo_data.items())
-  for org, data in hpo_data.items():
+  for org, data in hpo_data.items()[:2]:
     site_paired_sql, no_paired_sql = get_sql(org)
     logging.info('syncing participants for {}'.format(org))
     run_sql(data['bucket'], site_paired_sql, no_paired_sql)
