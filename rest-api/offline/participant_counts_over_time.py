e7a23fa019c854b8474428349bd9a65b50f3656f
Michael Mead <michaelmead007@icloud.com>
2019-01-18 10:26:02 -0600

    Mm/metrics updates (#892)
    * create cron job for participant counts api
    
    * DA-829 setup basic cron job for ehr metrics.

---
+++ rest-api/offline/participant_counts_over_time.py
@@ -0,0 +1,7 @@
+from dao.participant_counts_over_time_service import ParticipantCountsOverTimeService
+
+
+def calculate_participant_metrics():
+  # call metrics functions
+  service = ParticipantCountsOverTimeService()
+  service.refresh_metrics_cache_data()
