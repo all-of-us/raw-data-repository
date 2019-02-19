8cef8c36c661ca8f05d969823d2a1c20df258c05
Robert Abram <rabram991@gmail.com>
2019-02-19 10:37:13 -0600

    [DA-859] Refactor participant summary token sync (#936)
    * Added '_backfill' parameter to disable 60 backfill when using sync.
    Added new api to return only participantId and lastModified.
    
    * Updated api documentation
    
    * Added unit test for new modified API.
    Updated sync test to check '_backfill' parameter.

--- rest-api/api/participant_summary_api.py
+++ rest-api/api/participant_summary_api.py
@@ -3,7 +3,9 @@ from api_util import PTC_HEALTHPRO_AWARDEE, AWARDEE, DEV_MAIL
 from app_util import auth_required, get_validated_user_info
 from dao.participant_summary_dao import ParticipantSummaryDao
 from flask import request
-from werkzeug.exceptions import Forbidden, InternalServerError
+from model.participant_summary import ParticipantSummary
+from model.hpo import HPO
+from werkzeug.exceptions import Forbidden, InternalServerError, BadRequest
 
 
 class ParticipantSummaryApi(BaseApi):
@@ -39,15 +41,57 @@ class ParticipantSummaryApi(BaseApi):
 
   def _make_query(self):
     query = super(ParticipantSummaryApi, self)._make_query()
-    if self._is_last_modified_sync():
-      query.always_return_token = True
-
+    query.always_return_token = self._get_request_arg_bool('_sync')
+    query.backfill_sync = self._get_request_arg_bool('_backfill', True)
     return query
 
   def _make_bundle(self, results, id_field, participant_id):
-    if self._is_last_modified_sync():
+    if self._get_request_arg_bool('_sync'):
       return make_sync_results_for_request(self.dao, results)
     return super(ParticipantSummaryApi, self)._make_bundle(results, id_field, participant_id)
 
-  def _is_last_modified_sync(self):
-    return request.args.get('_sync') == 'true'
+
+class ParticipantSummaryModifiedApi(BaseApi):
+  """
+  API to return participant_id and last_modified fields
+  """
+  def __init__(self):
+    super(ParticipantSummaryModifiedApi, self).__init__(ParticipantSummaryDao())
+
+  def get(self):
+    """
+    Return participant_id and last_modified for all records or a subset based
+    on the awardee parameter.
+    """
+    response = list()
+    user_email, user_info = get_validated_user_info()
+    request_awardee = None
+
+    with self.dao.session() as session:
+
+      # validate parameter when passed an awardee.
+      if 'awardee' in request.args:
+        request_awardee = request.args.get('awardee')
+        hpo = session.query(HPO.hpoId).filter(HPO.name == request_awardee).first()
+        if not hpo:
+          raise BadRequest('invalid awardee')
+
+      # verify user has access to the requested awardee.
+      if AWARDEE in user_info['roles'] and user_email != DEV_MAIL:
+        try:
+          if not request_awardee or user_info['awardee'] != request_awardee:
+            raise Forbidden
+        except KeyError:
+          raise InternalServerError("config error for awardee")
+
+      query = session.query(ParticipantSummary.participantId, ParticipantSummary.lastModified)
+      query = query.order_by(ParticipantSummary.participantId)
+      if request_awardee:
+        query = query.filter(ParticipantSummary.hpoId == hpo.hpoId)
+
+      items = query.all()
+      for item in items:
+        response.append({'participantId': 'P{0}'.format(item.participantId),
+                         'lastModified': item.lastModified.isoformat()})
+
+    return response
