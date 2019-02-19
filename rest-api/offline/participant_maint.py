c664020bdc52f30609d0372eca08158801b55217
Robert Abram <rabram991@gmail.com>
2019-02-19 10:38:24 -0600

    [DA-863] Cron job to skew last modified duplicates (#940)
    * Add microseconds to last_modified column in participant and participant_summary
    
    * Added cron api to skew duplicate last modified values
    
    * Added unit test to test date skew method
    
    * Add skew duplicates cron job to sandbox cron config
    
    * make sure random microseconds is in the correct range
    
    * Moved cron definition to avoid merge issue with Tanner's PR
    
    * Moved cron definition to avoid merge issue with Tanner's PR
    
    * Moved cron definition to avoid merge issue with Tanner's PR
    
    * Moved cron definition to avoid merge issue with Tanner's PR

---
+++ rest-api/offline/participant_maint.py
@@ -0,0 +1,35 @@
+
+from dao.participant_summary_dao import ParticipantSummaryDao
+
+def skew_duplicate_last_modified():
+  """
+  Query participant summary looking for duplicate lastModified dates.
+  If there are enough, randomly skew the milliseconds to eliminate
+  the duplicates.
+  """
+  dao = ParticipantSummaryDao()
+  min_dups = 6
+
+  with dao.session() as session:
+
+    # Find last modified dates from participant summary where there are
+    # at least 6 duplicates.
+    sql = """
+      select last_modified, count from (
+        select last_modified, count(1) as count 
+          from participant_summary group by last_modified order by count desc
+      ) a where a.count > :min_dups
+    """
+    results = session.execute(sql, {'min_dups': min_dups}).fetchall()
+
+    if results and len(results) > 0:
+      # loop over results and randomize only the microseconds value of the timestamp.
+      for rec in results:
+        sql = """
+          update participant_summary set last_modified =
+                date_add(date_format(last_modified, '%Y-%m-%d %H:%i:%S'), 
+                         INTERVAL (FLOOR(RAND() * 999998) + 1) MICROSECOND)
+             where last_modified = :ts
+          """
+
+        session.execute(sql, {'ts': rec['last_modified']})
