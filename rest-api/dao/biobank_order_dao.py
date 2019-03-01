838ad05a222fbc3120acca1a6ec34238243e3566
Seeker1911 <michaelmead007@icloud.com>
2019-03-01 11:06:00 -0600

    DA-879 fix indent for realz

--- rest-api/dao/biobank_order_dao.py
+++ rest-api/dao/biobank_order_dao.py
@@ -253,8 +253,8 @@ class BiobankOrderDao(UpdatableDao):
                                                 participant_summary.participantId, obj)
 
     if obj.orderStatus == BiobankOrderStatus.CANCELLED and \
-      participant_summary.numberDistinctVisits > 0 and is_distinct_visit:
-                          participant_summary.numberDistinctVisits -= 1
+       participant_summary.numberDistinctVisits > 0 and is_distinct_visit:
+      participant_summary.numberDistinctVisits -= 1
 
     participant_summary.lastModified = clock.CLOCK.now()
     for sample in obj.samples:
