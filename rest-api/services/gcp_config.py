bd76f4f72291901af67c08b61900145213aa78a5
Robert Abram <robert.abram@entpack.com>
2019-03-18 14:55:17 -0600

    Only point DB service connections to primary db.

--- rest-api/services/gcp_config.py
+++ rest-api/services/gcp_config.py
@@ -17,10 +17,10 @@ GCP_PROJECTS = [
 GCP_INSTANCES = {
   'all-of-us-rdr-prod': 'all-of-us-rdr-prod:us-central1:rdrmaindb',
   'all-of-us-rdr-stable': 'all-of-us-rdr-stable:us-central1:rdrmaindb',
-  'all-of-us-rdr-staging': 'all-of-us-rdr-staging:us-central1:rdrbackupdb',
+  'all-of-us-rdr-staging': 'all-of-us-rdr-staging:us-central1:rdrmaindb',
   'all-of-us-rdr-sandbox': 'all-of-us-rdr-sandbox:us-central1:rdrmaindb',
   'pmi-drc-api-test': 'pmi-drc-api-test:us-central1:rdrmaindb',
-  'pmi-drc-api-test-repl': 'pmi-drc-api-test:us-central1:rdrbackupdb',
+  'pmi-drc-api-test-repl': 'pmi-drc-api-test:us-central1:rdrmaindb',
 }
 
 
