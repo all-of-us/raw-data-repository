f0592b790bc77ffc33da380931c18c811058b8a3
Robert Abram <rabram991@gmail.com>
2019-03-26 09:07:10 -0500

    Add option to connect to replica instances. (#989)
    Change Staging replica instance name to 'rdrbackupdb1'.
    Code cleanup and organization.

--- rest-api/services/gcp_config.py
+++ rest-api/services/gcp_config.py
@@ -20,7 +20,14 @@ GCP_INSTANCES = {
   'all-of-us-rdr-staging': 'all-of-us-rdr-staging:us-central1:rdrmaindb',
   'all-of-us-rdr-sandbox': 'all-of-us-rdr-sandbox:us-central1:rdrmaindb',
   'pmi-drc-api-test': 'pmi-drc-api-test:us-central1:rdrmaindb',
-  'pmi-drc-api-test-repl': 'pmi-drc-api-test:us-central1:rdrmaindb',
+}
+
+GCP_REPLICA_INSTANCES = {
+  'all-of-us-rdr-prod': 'all-of-us-rdr-prod:us-central1:rdrbackupdb',
+  'all-of-us-rdr-stable': 'all-of-us-rdr-stable:us-central1:rdrbackupdb',
+  'all-of-us-rdr-staging': 'all-of-us-rdr-staging:us-central1:rdrbackupdb1',
+  'all-of-us-rdr-sandbox': 'all-of-us-rdr-sandbox:us-central1:rdrmaindb',
+  'pmi-drc-api-test': 'pmi-drc-api-test:us-central1:rdrbackupdb',
 }
 
 
