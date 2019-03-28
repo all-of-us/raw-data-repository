df88363f18b3ed43fdbb935d6ba031cdf9d0bca8
Tanner Netterville <tannern@gmail.com>
2019-03-28 14:31:54 -0500

    DA-986 EHR Metrics endpoints: unify language and behavior from SITES/AWARDEES/HPOS to ORGANIZATIONS

--- rest-api/dao/ehr_dao.py
+++ rest-api/dao/ehr_dao.py
@@ -12,15 +12,16 @@ class EhrReceiptDao(BaseDao):
   def get_id(self, obj):
     return obj.ehrReceiptId
 
-  def get_active_org_counts_in_interval(self, start_date, end_date, interval, org_ids=None):
+  def get_active_organization_counts_in_interval(self, start_date, end_date, interval,
+                                                 organization_ids=None):
     """
     Get number of receipts per organization received in specific time intervals
 
     :param start_date: query min date
     :param end_date: query max date
     :param interval: time interval (one of the INTERVAL constants)
-    :param org_ids: (optional) filter results to matching organizations
-    :return: dictionary of siteId:list(dict(date=X, count=Y), ...)
+    :param organization_ids: (optional) filter results to matching organizations
+    :return: dictionary of organizationId:list(dict(date=X, count=Y), ...)
     :rtype: dict
     """
     interval_query = CalendarDao.get_interval_query(
@@ -29,20 +30,20 @@ class EhrReceiptDao(BaseDao):
       interval_key=interval,
       include_end_date=True
     )
-    active_site_count_conditions = (
+    active_org_count_conditions = (
       (EhrReceipt.receiptTime >= interval_query.c.start_date)
       & (EhrReceipt.receiptTime < interval_query.c.end_date)
     )
-    if org_ids:
-      active_site_count_conditions &= EhrReceipt.organizationId.in_(org_ids)
-    active_site_count_query = (
-      select([func.count(EhrReceipt.hpoId.distinct())])
-        .where(active_site_count_conditions)
+    if organization_ids:
+      active_org_count_conditions &= EhrReceipt.organizationId.in_(organization_ids)
+    active_org_count_query = (
+      select([func.count(EhrReceipt.organizationId.distinct())])
+        .where(active_org_count_conditions)
     )
     query = select([
       interval_query.c.start_date,
       interval_query.c.end_date,
-      active_site_count_query.label('active_site_count'),
+      active_org_count_query.label('active_organization_count'),
     ])
     with self.session() as session:
       cursor = session.execute(query)
