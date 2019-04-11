59520bef02e3534a25790c881c32c488c1ff1296
wangy70 <yu.wang.3@vumc.org>
2019-04-11 13:21:54 -0500

    add_method_to_genomics_dao

--- rest-api/dao/genomics_dao.py
+++ rest-api/dao/genomics_dao.py
@@ -1,16 +1,40 @@
 from dao.base_dao import UpdatableDao
 from model.genomics import GenomicSet, GenomicSetMember
+from query import Query, Operator, FieldFilter, OrderBy
 
 class GenomicSetDao(UpdatableDao):
   """ Stub for GenomicSet model """
 
   def __init__(self):
-    super(GenomicSetDao, self).__init__(GenomicSet)
+    super(GenomicSetDao, self).__init__(GenomicSet, order_by_ending=['id'])
 
+  def get_one_by_file_name(self, filename):
+    return super(GenomicSetDao, self) \
+      .query(Query([FieldFilter('genomicSetFile', Operator.EQUALS, filename)], None, 1, None)).items
+
+  def get_new_version_number(self, genomic_set_name):
+    genomic_sets = super(GenomicSetDao, self)\
+      .query(Query([FieldFilter('genomicSetName', Operator.EQUALS, genomic_set_name)],
+                   OrderBy('genomicSetVersion', False), 1, None)).items
+    if genomic_sets:
+      return genomic_sets[0].genomicSetVersion + 1
+    else:
+      return 1
 
 class GenomicSetMemberDao(UpdatableDao):
   """ Stub for GenomicSetMember model """
 
   def __init__(self):
-    super(GenomicSetMemberDao, self).__init__(GenomicSetMember)
+    super(GenomicSetMemberDao, self).__init__(GenomicSetMember, order_by_ending=['id'])
 
+  def upsert_all(self, genomic_set_members):
+    """Inserts/updates members. """
+    members = list(genomic_set_members)
+
+    def upsert(session):
+      written = 0
+      for member in members:
+        session.merge(member)
+        written += 1
+      return written
+    return self._database.autoretry(upsert)
