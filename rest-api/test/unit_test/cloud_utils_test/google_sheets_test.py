7250503b17ae91164e81b1aed92dea4444e89a8f
Robert Abram <robert.abram@entpack.com>
2019-04-26 08:13:27 -0600

    unittesting Google SDK requires initializations

--- rest-api/test/unit_test/cloud_utils_test/google_sheets_test.py
+++ rest-api/test/unit_test/cloud_utils_test/google_sheets_test.py
@@ -3,10 +3,17 @@ import json
 from test.unit_test.unit_test_util import TestBase
 
 from cloud_utils.google_sheets import GoogleSheetCSVReader
-
+from google.appengine.api import urlfetch_stub
+from google.appengine.api import apiproxy_stub_map
 
 class GoogleSheetCSVReaderTest(TestBase):
   EXAMPLE_SHEET_ID = '1ZY6KMtnMZ_5-dv8cqRgVWHOH9j23GcMZTC61UMXisuE' # https://docs.google.com/spreadsheets/d/1ZY6KMtnMZ_5-dv8cqRgVWHOH9j23GcMZTC61UMXisuE/edit?usp=sharing
+
+  def setUp(self):
+    # unittesting Google SDK.
+    # https://cloud.google.com/appengine/docs/standard/python/tools/localunittesting
+    apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap()
+    apiproxy_stub_map.apiproxy.RegisterStub('urlfetch', urlfetch_stub.URLFetchServiceStub())
 
   def test_reads_google_sheet_by_id(self):
     """should behave like a csv.DictReader"""
