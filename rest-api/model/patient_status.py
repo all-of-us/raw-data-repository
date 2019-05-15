ab258262aa996468c9390feb30c82fc09cbada7e
Robert Abram <robert.abram@entpack.com>
2019-05-15 16:22:48 -0600

    Created Patient Status model

---
+++ rest-api/model/patient_status.py
@@ -0,0 +1,30 @@
+from model.base import Base, model_insert_listener, model_update_listener
+from model.utils import Enum
+from participant_enums import PatientStatusFlag
+from sqlalchemy import Column, DateTime, Integer, ForeignKey, UniqueConstraint, Text, event
+
+
+class PatientStatus(Base):
+  """
+  Site patient status
+  """
+  __tablename__ = 'patient_status'
+
+  # Primary Key
+  id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+  # have mysql set the creation data for each new order
+  created = Column('created', DateTime, nullable=True)
+  # have mysql always update the modified data when the record is changed
+  modified = Column('modified', DateTime, nullable=True)
+  participantId = Column('participant_id', Integer, ForeignKey('participant.participant_id'), nullable=False)
+  patientStatus = Column('patient_status', Enum(PatientStatusFlag), nullable=False)
+  hpoId = Column('hpo_id', Integer, ForeignKey('hpo.hpo_id'), nullable=False)
+  organizationId = Column('organization_id', Integer, ForeignKey('organization.organization_id'), nullable=False)
+  comment = Column('comment', Text, nullable=True)
+
+  __table_args__ = (
+    UniqueConstraint('participant_id', 'organization_id', name='uidx_patient_status'),
+  )
+
+event.listen(PatientStatus, 'before_insert', model_insert_listener)
+event.listen(PatientStatus, 'before_update', model_update_listener)
