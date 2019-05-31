d3c37b809ddbac951a1d7a4406b9788972fc2642
Robert Abram <robert.abram@entpack.com>
2019-05-31 16:41:32 -0600

    created table to store bigquery sync info

---
+++ rest-api/model/bigquery_sync.py
@@ -0,0 +1,28 @@
+from model.base import Base, model_insert_listener, model_update_listener
+from sqlalchemy import Column, DateTime, Integer, String, Index, event
+from sqlalchemy.dialects.mysql import JSON
+
+
+class BigQuerySync(Base):
+  """
+  BigQuery synchronization table
+  """
+  __tablename__ = 'bigquery_sync'
+
+  # Primary Key
+  id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+  # have mysql set the creation data for each new order
+  created = Column('created', DateTime, nullable=True, index=True)
+  # have mysql always update the modified data when the record is changed
+  modified = Column('modified', DateTime, nullable=True, index=True)
+  # BigQuery dataset name
+  dataSet = Column('dataset', String(80), nullable=False)
+  # BigQuery table name
+  table = Column('table', String(80), nullable=False)
+  # data resource hold the data to be transferred to BigQuery
+  resource = Column('resource', JSON, nullable=False)
+
+Index('ix_ds_table', BigQuerySync.dataSet, BigQuerySync.table)
+
+event.listen(BigQuerySync, 'before_insert', model_insert_listener)
+event.listen(BigQuerySync, 'before_update', model_update_listener)
