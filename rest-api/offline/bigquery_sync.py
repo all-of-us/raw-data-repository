9b478c6d4babbc0ee7532d436434b748e8e3df53
Robert Abram <robert.m.abram.1@vumc.org>
2019-07-12 10:23:42 -0600

    update bigquery cron job to add rebuild tasks

--- rest-api/offline/bigquery_sync.py
+++ rest-api/offline/bigquery_sync.py
@@ -1,11 +1,12 @@
+import httplib
 import json
 import logging
 import math
 from datetime import datetime
 
-from google.appengine.api import app_identity
+from google.appengine.api import app_identity, taskqueue
 from googleapiclient.discovery import build
-from sqlalchemy import or_
+from sqlalchemy import or_, func
 
 from cloud_utils.bigquery import BigQueryJob
 from dao.bigquery_sync_dao import BQParticipantSummaryGenerator, BigQuerySyncDao
@@ -14,12 +15,47 @@ from model.bigquery_sync import BigQuerySync
 from model.participant import Participant
 
 
-def rebuild_bigquery_data():
+def rebuild_bigquery_handler():
+  """
+  Cron job handler, setup queued tasks to rebuild bigquery data
+  """
+  timestamp = datetime.utcnow()
+  batch_size = 10000
+
+  dao = ParticipantDao()
+  with dao.session() as session:
+    total_rows = session.query(func.count(Participant.participantId)).first()[0]
+    count = int(math.ceil(float(total_rows) / float(batch_size)))
+    logging.info('Calculated {0} tasks from {1} records and a batch size of {2}.'.
+                          format(count, total_rows, batch_size))
+
+    while count > 0:
+      task = taskqueue.add(
+        queue_name='bigquery-rebuild',
+        url='/rdr/v1/BQRebuildTaskApi',
+        method='GET',
+        target='worker',
+        params={'timestamp': timestamp, 'limit': batch_size}
+      )
+
+      logging.info('Task {} enqueued, ETA {}.'.format(task.name, task.eta))
+      count -= 1
+
+
+def rebuild_bigquery_data(timestamp, limit=0):
   """
   Loop through all participants and generate the BQ participant summary data and
   store it in the biguqery_sync table.
   Warning: this will force a rebuild and eventually a re-sync for every participant record.
+  :param timestamp: datetime: to be used to rebuild any records old than this.
+  :param limit: integer: 0 = all, otherwise only process records until limit has been reached.
   """
+  if not limit or not isinstance(limit, int) or limit < 0:
+    raise ValueError('invalid limit value.')
+
+  if not timestamp:
+    timestamp = datetime.utcnow()
+
   try:
     app_id = app_identity.get_application_id()
   except AttributeError:
@@ -28,14 +64,22 @@ def rebuild_bigquery_data():
   bqgen = BQParticipantSummaryGenerator()
 
   with dao.session() as session:
-    results = session.query(Participant.participantId).all()
+    # Collect all participants who do not have a PS generated yet or the modified date is less than the timestamp.
+    sq = session.query(Participant.participantId, BigQuerySync.id, BigQuerySync.modified).\
+                          outerjoin(BigQuerySync).subquery()
+    query = session.query(sq.c.participant_id.label('participantId')).\
+                          filter(or_(sq.c.id == None, sq.c.modified < timestamp))
+    if limit:
+      query = query.limit(limit)
+    # sql = dao.query_to_text(query)
+    results = query.all()
     count = 0
     excluded = 0
-    # put a log entry in every 5,000 records. Should be approximately every 10 minutes.
+    # put a log entry in every 2,500 records. Should be approximately every 10 minutes.
     for row in results:
       count += 1
-      if count % 5000 == 0:
-        logging.info('processed {0} participants'.format(count))
+      if count % 1000 == 0:
+        logging.info('Processed {0} participants.'.format(count))
 
       bqr = bqgen.make_participant_summary(row.participantId)
       # filter test or ghost participants if production
@@ -46,7 +90,7 @@ def rebuild_bigquery_data():
 
       bqgen.save_participant_summary(row.participantId, bqr)
 
-    logging.info('rebuilt bigquery data for {0} participants, excluded {1} test or ghost participants.'.format(
+    logging.info('Rebuilt BigQuery data for {0} participants, excluded {1} test/ghost participants.'.format(
       count - excluded, excluded))
 
 
@@ -83,7 +127,7 @@ def insert_batch_into_bq(bq, dataset, table, batch, dryrun=False):
   return True, resp
 
 
-def sync_bigquery(dryrun=False):
+def sync_bigquery_handler(dryrun=False):
   """
   Cron entry point, Sync MySQL records to bigquery.
   :param dryrun: Don't send to bigquery if True
@@ -109,8 +153,13 @@ def sync_bigquery(dryrun=False):
     for table in tables:
       count = 0
       errors = ''
-      max_created, max_modified = \
-            _get_remote_max_timestamps(dataset, table) if dryrun is False else datetime.min
+      try:
+        max_created, max_modified = \
+              _get_remote_max_timestamps(dataset, table) if dryrun is False else datetime.min
+      except httplib.HTTPException:
+        logging.warning('Failed to retrieve max date values from bigquery, skipping this run.')
+        return 0
+
       # figure out how many records need to be sync'd and divide into slices.
       total_rows = session.query(BigQuerySync.id). \
                   filter(or_(BigQuerySync.created > max_created, BigQuerySync.modified > max_modified)).count()
@@ -147,14 +196,14 @@ def sync_bigquery(dryrun=False):
             errors = resp
         # Don't exceed our execution time limit.
         if (datetime.now() - start_ts).seconds > run_limit:
-          logging.info('hit {0} second time limit.'.format(run_limit))
+          logging.info('Hit {0} second time limit.'.format(run_limit))
           break
 
       if errors:
         logging.error(errors)
 
       total_inserts += count
-      logging.info('inserted {0} records into {1}.{2}.'.format(count, dataset, table))
+      logging.info('Inserted {0} records into {1}.{2}.'.format(count, dataset, table))
 
   return total_inserts
 
@@ -176,5 +225,5 @@ def _get_remote_max_timestamps(dataset, table):
       mm = row.max_modified if row.max_modified else datetime.min
       return mc, mm
 
-  raise LookupError('failed to get max created and modified values from bigquery table {0}.{1}'.
+  raise LookupError('Failed to get max created and modified values from bigquery table {0}.{1}'.
                     format(dataset, table))
