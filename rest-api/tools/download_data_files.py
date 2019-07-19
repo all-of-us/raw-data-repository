94903ca93467215274558dfb64f9c601f8f3d1c5
michael.mead <michael.mead@vanderbilt.edu>
2019-07-19 14:15:49 -0500

    DA-1194 add all hpos report files

--- rest-api/tools/download_data_files.py
+++ rest-api/tools/download_data_files.py
@@ -1,4 +1,6 @@
 import os
+""" Downloads csv files from Google buckets and places in data/ dir (gitignored).
+"""
 
 # @todo: change this to the new proper bucket once it's made
 SOURCE_BUCKET = 'all-of-us-rdr-sequestered-config-test/'
@@ -6,10 +8,14 @@ SOURCE_BUCKET = 'all-of-us-rdr-sequestered-config-test/'
 AWARDEES = SOURCE_BUCKET + 'awardees.csv'
 ORGS = SOURCE_BUCKET + 'organizations.csv'
 SITES = SOURCE_BUCKET + 'sites.csv'
+LIST_SERV = SOURCE_BUCKET + 'list-serv.csv'
+ALL_HPO_SITES = SOURCE_BUCKET + 'all-hpo-sites.csv'
+ROLLOUT = SOURCE_BUCKET + 'rollout-plan.csv'
 
+ALL_FILES = (AWARDEES, ORGS, SITES, LIST_SERV, ALL_HPO_SITES, ROLLOUT)
 
 def import_data_files():
-  for f in (AWARDEES, ORGS, SITES):
+  for f in ALL_FILES:
     command = 'gsutil -m cp -r gs://'+f+' '+ 'data/' + f.split('/')[-1]
     os.system(command)
 
