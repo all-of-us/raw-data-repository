39979e855b1967a544bf5fd357ece26d26b12c3a
Tanner Netterville <tannern@gmail.com>
2019-08-09 10:51:15 -0500

    add a placeholder for taskqueue

--- rdr_service/__init__.py
+++ rdr_service/__init__.py
@@ -1,3 +1,21 @@
+import inspect
+import logging
+
+
+LOG = logging.getLogger(__name__)
+
+
+def _log_need_to_fix(name):
+    def wrapped(*args, **kwargs):
+        stack = inspect.stack()
+        caller = inspect.getframeinfo(stack[1][0])
+        LOG.warning("NEED TO FIX: {}, {} called {}".format(
+            caller.filename,
+            caller.lineno,
+            name
+        ))
+    return staticmethod(wrapped)
+
 
 # TODO: Decide if these are the best places for these.
 
@@ -6,3 +24,7 @@ class deferred():
 
   def defer(self, *args):
     pass
+
+
+class taskqueue:
+    add = _log_need_to_fix('taskqueue.add')
