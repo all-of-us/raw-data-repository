f2b95fa4fd4146872338c0337a25f5935e02ff83
Robert Abram <robert.m.abram.1@vumc.org>
2019-08-09 16:36:19 -0500

    Get CircleCI testing working

--- ci/test.sh
+++ ci/test.sh
@@ -6,9 +6,8 @@ trap '[[ "$(jobs -p)" ]] && kill $(jobs -p)' EXIT
 
 ci/test_pre_push.sh
 
-python3 -m venv venv3
-source venv3/bin/activate
-pip install --upgrade pip
+python3 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 safety check  # checks current (API) venv
 
@@ -26,29 +25,29 @@ until $(curl -s --fail http://localhost:8000); do
     sleep .25
 done
 
-cd rdr_service
+#cd rdr_service
+#
+#./tools/install_config.sh --config=config/config_dev.json --update
+#./tools/setup_local_database.sh --nopassword --db_user ubuntu --db_name circle_test
+#
+#cd $PROJ_DIR
+#
+#./ci/check_licenses.sh
+#
+#cd rdr_service/rdr_client
+#
+## Verify that the simplest client script works.
+#./run_client.sh participant_create_and_get.py
+## Verify that data generation works.
+#./run_client.sh generate_fake_data.py \
+#    --num_participants 3 \
+#    --include_physical_measurements --include_biobank_orders --create_biobank_samples
+## Verify that we can retrieve awardees successfully.
+#./run_client.sh get_awardees.py
+#
+#cd $PROJ_DIR/rdr_service/test
+#
+#GCLOUD_PATH=$(which gcloud)
+#SDK_PATH=${GCLOUD_PATH%/bin/gcloud}
 
-./tools/install_config.sh --config=config/config_dev.json --update
-./tools/setup_local_database.sh --nopassword --db_user ubuntu --db_name circle_test
-
-cd $PROJ_DIR
-
-./ci/check_licenses.sh
-
-cd rdr_service/rdr_client
-
-# Verify that the simplest client script works.
-./run_client.sh participant_create_and_get.py
-# Verify that data generation works.
-./run_client.sh generate_fake_data.py \
-    --num_participants 3 \
-    --include_physical_measurements --include_biobank_orders --create_biobank_samples
-# Verify that we can retrieve awardees successfully.
-./run_client.sh get_awardees.py
-
-cd $PROJ_DIR/rdr_service/test
-
-GCLOUD_PATH=$(which gcloud)
-SDK_PATH=${GCLOUD_PATH%/bin/gcloud}
-
-./run_tests.sh -g $SDK_PATH
+python -m unittest discover -v -s tests
