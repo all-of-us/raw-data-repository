6dfe10593a4b824b97c147368a17d8259f5d8fe3
Robert Abram <robert.m.abram.1@vumc.org>
2019-08-09 12:45:51 -0500

    Fix tests so they run correctly.  Changed package to flask_restful.

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -11,7 +11,8 @@ class TestEnvironment(unittest.TestCase):
     """
 
     def setUp(self) -> None:
-        start_mysql_instance()
+        # start_mysql_instance()
+        pass
 
     def test_python_version(self):
         """ Make sure we are using Python 3.7 or higher """
@@ -24,9 +25,8 @@ class TestEnvironment(unittest.TestCase):
         self.assertTrue(os.path.exists(os.path.join(cwd, 'rdr_service')))
         self.assertTrue(os.path.exists(os.path.join(cwd, 'rdr_service/tools')))
         self.assertTrue(os.path.exists(os.path.join(cwd, 'rdr_service/main.py')))
-        print('crap')
 
-    # def test_flask_app(self):
-    #     """ Test that we can import the flask app object """
-    #     from rdr_service.main import app
-    #     self.assertTrue(isinstance(app, object))
+    def test_flask_app(self):
+        """ Test that we can import the flask app object """
+        from rdr_service.main import app
+        self.assertTrue(isinstance(app, object))
