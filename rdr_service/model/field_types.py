b3bdce0a7ea497498bd4bea3defcc21f2886e3d4
michael.mead <michael.mead@vanderbilt.edu>
2019-08-14 10:18:53 -0500

    add field_types blob to utf8 converter

---
+++ rdr_service/model/field_types.py
@@ -0,0 +1,17 @@
+import sqlalchemy.types as types
+
+class BlobUTF8(types.TypeDecorator):
+    '''Prefixes Unicode values with "PREFIX:" on the way in and
+    strips it off on the way out.
+    '''
+
+    impl = types.BLOB
+
+    def process_bind_param(self, value, dialect):
+	return bytes(value, 'utf-8')
+
+    def process_result_value(self, value, dialect):
+	return value.decode('utf-8')
+
+#    def copy(self, **kw):
+#	return BlobUTF8(self.impl.length)
