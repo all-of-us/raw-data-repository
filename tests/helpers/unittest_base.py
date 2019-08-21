f282ee757c88e7e3bb44e88fe28db64d97904725
Michael Mead <michaelmead007@icloud.com>
2019-08-21 11:20:28 -0500

    completed participant answers, includes additions to unit test base and mysql helpers, as well as adds test_data, should make future tests easier (#1221)

--- tests/helpers/unittest_base.py
+++ tests/helpers/unittest_base.py
@@ -1,11 +1,17 @@
+import json
+import logging
 import os
 import faker
 import unittest
+import http.client
+import importlib
 
+from tests.test_data import data_path
 from rdr_service.code_constants import PPI_SYSTEM
 from rdr_service.concepts import Concept
 from rdr_service.dao import questionnaire_dao, questionnaire_response_dao
 from rdr_service.dao.participant_dao import ParticipantDao
+from rdr_service import main
 from rdr_service.model.participant import Participant, ParticipantHistory
 from rdr_service.model.participant_summary import ParticipantSummary
 from rdr_service.participant_enums import (
@@ -16,8 +22,72 @@ from rdr_service.participant_enums import (
 )
 from tests.helpers.mysql_helper import reset_mysql_instance
 
+class QuestionnaireTestMixin:
 
-class BaseTestCase(unittest.TestCase):
+    @staticmethod
+    def questionnaire_response_url(participant_id):
+        return "Participant/%s/QuestionnaireResponse" % participant_id
+
+    def create_questionnaire(self, filename):
+        with open(data_path(filename)) as f:
+            questionnaire = json.load(f)
+            response = self.send_post("Questionnaire", questionnaire)
+            return response["id"]
+
+    @staticmethod
+    def make_questionnaire_response_json(
+        participant_id,
+        questionnaire_id,
+        code_answers=None,
+        string_answers=None,
+        date_answers=None,
+        uri_answers=None,
+        language=None,
+        authored=None,
+    ):
+        results = []
+        if code_answers:
+            for answer in code_answers:
+                results.append(
+                    {
+                        "linkId": answer[0],
+                        "answer": [{"valueCoding": {"code": answer[1].code, "system": answer[1].system}}],
+                    }
+                )
+        if string_answers:
+            for answer in string_answers:
+                results.append({"linkId": answer[0], "answer": [{"valueString": answer[1]}]})
+        if date_answers:
+            for answer in date_answers:
+                results.append({"linkId": answer[0], "answer": [{"valueDate": "%s" % answer[1].isoformat()}]})
+        if uri_answers:
+            for answer in uri_answers:
+                results.append({"linkId": answer[0], "answer": [{"valueUri": answer[1]}]})
+
+        response_json = {
+            "resourceType": "QuestionnaireResponse",
+            "status": "completed",
+            "subject": {"reference": "Patient/{}".format(participant_id)},
+            "questionnaire": {"reference": "Questionnaire/{}".format(questionnaire_id)},
+            "group": {"question": results},
+        }
+        if language is not None:
+            response_json.update(
+                {
+                    "extension": [
+                        {
+                            "url": "http://hl7.org/fhir/StructureDefinition/iso21090-ST-language",
+                            "valueCode": "{}".format(language),
+                        }
+                    ]
+                }
+            )
+        if authored is not None:
+            response_json.update({"authored": authored.isoformat()})
+        return response_json
+
+
+class BaseTestCase(unittest.TestCase, QuestionnaireTestMixin):
     """ Base class for unit tests."""
 
     def __init__(self, *args, **kwargs):
@@ -25,17 +95,20 @@ class BaseTestCase(unittest.TestCase):
         # Set this so the database factory knows to use the unittest connection string from the config.
         os.environ["UNITTEST_FLAG"] = "True"
         self.fake = faker.Faker()
+        logging.getLogger().setLevel(logging.CRITICAL)
 
-    def setUp(self) -> None:
+    def setUp(self, with_data=True, with_consent_codes=False) -> None:
         super(BaseTestCase, self).setUp()
+        self.app = main.app.test_client()
 
-        reset_mysql_instance()
+        reset_mysql_instance(with_data, with_consent_codes)
 
         # Allow printing the full diff report on errors.
         self.maxDiff = None
         # Always add codes if missing when handling questionnaire responses.
         questionnaire_dao._add_codes_if_missing = lambda: True
         questionnaire_response_dao._add_codes_if_missing = lambda email: True
+        self._consent_questionnaire_id = None
 
     @staticmethod
     def _participant_with_defaults(**kwargs):
@@ -107,54 +180,79 @@ class BaseTestCase(unittest.TestCase):
         summary.email = self.fake.email()
         return summary
 
+    def create_participant(self):
+        response = self.send_post("Participant", {})
+        return response["participantId"]
 
-def make_questionnaire_response_json(
-    participant_id,
-    questionnaire_id,
-    code_answers=None,
-    string_answers=None,
-    date_answers=None,
-    uri_answers=None,
-    language=None,
-    authored=None,
-):
-    results = []
-    if code_answers:
-        for answer in code_answers:
-            results.append(
-                {
-                    "linkId": answer[0],
-                    "answer": [{"valueCoding": {"code": answer[1].code, "system": answer[1].system}}],
-                }
-            )
-    if string_answers:
-        for answer in string_answers:
-            results.append({"linkId": answer[0], "answer": [{"valueString": answer[1]}]})
-    if date_answers:
-        for answer in date_answers:
-            results.append({"linkId": answer[0], "answer": [{"valueDate": "%s" % answer[1].isoformat()}]})
-    if uri_answers:
-        for answer in uri_answers:
-            results.append({"linkId": answer[0], "answer": [{"valueUri": answer[1]}]})
+    def send_post(self, *args, **kwargs):
+        return self.send_request("POST", *args, **kwargs)
 
-    response_json = {
-        "resourceType": "QuestionnaireResponse",
-        "status": "completed",
-        "subject": {"reference": "Patient/{}".format(participant_id)},
-        "questionnaire": {"reference": "Questionnaire/{}".format(questionnaire_id)},
-        "group": {"question": results},
-    }
-    if language is not None:
-        response_json.update(
-            {
-                "extension": [
-                    {
-                        "url": "http://hl7.org/fhir/StructureDefinition/iso21090-ST-language",
-                        "valueCode": "{}".format(language),
-                    }
-                ]
-            }
+    def send_put(self, *args, **kwargs):
+        return self.send_request("PUT", *args, **kwargs)
+
+    def send_patch(self, *args, **kwargs):
+        return self.send_request("PATCH", *args, **kwargs)
+
+    def send_get(self, *args, **kwargs):
+        return self.send_request("GET", *args, **kwargs)
+
+    def send_request(self, method, local_path, request_data=None, query_string=None,
+        expected_status=http.client.OK,
+        headers=None,
+        expected_response_headers=None,
+    ):
+        """Makes a JSON API call against the test client and returns its response data.
+
+    Args:
+      method: HTTP method, as a string.
+      local_path: The API endpoint's URL (excluding main.PREFIX).
+      request_data: Parsed JSON payload for the request.
+      expected_status: What HTTP status to assert, if not 200 (OK).
+    """
+        response = self.app.open(
+            main.PREFIX + local_path,
+            method=method,
+            data=json.dumps(request_data) if request_data is not None else None,
+            query_string=query_string,
+            content_type="application/json",
+            headers=headers,
         )
-    if authored is not None:
-        response_json.update({"authored": authored.isoformat()})
-    return response_json
+        self.assertEqual(response.status_code, expected_status, response.data)
+        if expected_response_headers:
+            self.assertTrue(
+                set(expected_response_headers.items()).issubset(set(response.headers.items())),
+                "Expected response headers: %s; actual: %s" % (expected_response_headers, response.headers),
+            )
+        if expected_status == http.client.OK:
+            return json.loads(response.data)
+        if expected_status == http.client.CREATED:
+            return response
+        return None
+
+
+    def send_consent(self, participant_id, email=None, language=None, code_values=None, authored=None):
+        if not self._consent_questionnaire_id:
+            self._consent_questionnaire_id = self.create_questionnaire("study_consent.json")
+        self.first_name = self.fake.first_name()
+        self.last_name = self.fake.last_name()
+        if not email:
+            self.email = self.fake.email()
+            email = self.email
+        self.streetAddress = "1234 Main Street"
+        self.streetAddress2 = "APT C"
+        qr_json = self.make_questionnaire_response_json(
+            participant_id,
+            self._consent_questionnaire_id,
+            string_answers=[
+                ("firstName", self.first_name),
+                ("lastName", self.last_name),
+                ("email", email),
+                ("streetAddress", self.streetAddress),
+                ("streetAddress2", self.streetAddress2),
+            ],
+            language=language,
+            code_answers=code_values,
+            authored=authored,
+        )
+        self.send_post(self.questionnaire_response_url(participant_id), qr_json)
+
