89f9b2fdd12bba08bd82599d64774c91064edbdd
michael.mead <michael.mead@vanderbilt.edu>
2019-09-12 17:24:23 -0500

    add gcloudignore and google cloud debugging import to main

--- main.py
+++ main.py
@@ -4,6 +4,12 @@
 #
 from rdr_service.main import app
 
+try:
+  import googleclouddebugger
+  googleclouddebugger.enable()
+except ImportError:
+  pass
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
