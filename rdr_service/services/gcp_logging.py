35d2e5de667d8d9062204d0bcf65dd4b6c6977b2
Robert Abram <robert.m.abram.1@vumc.org>
2019-09-19 07:40:02 -0500

    logging test

--- rdr_service/services/gcp_logging.py
+++ rdr_service/services/gcp_logging.py
@@ -3,11 +3,16 @@ import requests
 
 from google.cloud import logging
 from google.cloud.logging.resource import Resource
+# https://github.com/googleapis/google-cloud-python/issues/2572
+from rdr_service.services import gcp_request_log_pb2
+from google.logging.type import http_request_pb2
 
-
-
+# https://googleapis.dev/python/logging/latest/usage.html
+# https://any-api.com/googleapis_com/logging/docs/Definitions/LogEntry
 # https://cloud.google.com/appengine/docs/standard/python3/runtime
-
+# https://cloud.google.com/logging/docs/api/v2/resource-list
+# https://github.com/googleapis/googleapis/blob/master/google/appengine/logging/v1/request_log.proto
+# https://developers.google.com/protocol-buffers/docs/downloads
 
 def setup_logging_resource():
     """
@@ -15,12 +20,13 @@ def setup_logging_resource():
     :return: Resource object
     """
     zone = 'local-machine'
-    try:
-        resp = requests.get('http://metadata.google.internal/computeMetadata/v1/instance/zone', timeout=10.0)
-        if resp.status_code == 200:
-            zone = resp.text.strip()
-    except Exception:
-        pass
+    if 'GAE_SERVICE' in os.environ:
+        try:
+            resp = requests.get('http://metadata.google.internal/computeMetadata/v1/instance/zone', timeout=15.0)
+            if resp.status_code == 200:
+                zone = resp.text.strip()
+        except Exception:
+            zone = 'unknown'
 
     labels = {
         "project_id": os.environ.get('GAE_APPLICATION', 'localhost'),
@@ -33,6 +39,10 @@ def setup_logging_resource():
     return resource
 
 _res = setup_logging_resource()
+
+req = gcp_request_log_pb2.RequestLog()
+
+
 
 #
 # logger = c.logger('appengine.googleapis.com%2Frequest_log')
