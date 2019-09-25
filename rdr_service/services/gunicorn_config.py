873736a3d80c3a3d4134a212583001a7f4bab4c0
Robert Abram <robert.m.abram.1@vumc.org>
2019-09-25 13:19:27 -0500

    Environment variable fix for Gunicorn

--- rdr_service/services/gunicorn_config.py
+++ rdr_service/services/gunicorn_config.py
@@ -4,13 +4,14 @@ import os
 _port = 8080 # local dev/testing.
 
 if os.getenv('GAE_ENV', '').startswith('standard'):
-    _port = 8081  # default app engine port.
-    if 'PORT' in os.environ:
-        _port = os.environ['PORT']
+    _port = os.environ.get('PORT', 8081)
 
 bind = "0.0.0.0:{0}".format(_port)
 workers = 1
 threads = 1
-
+# raw_env = [
+#     "RDR_CONFIG_PROVIDER={0}".format(os.environ.get('RDR_CONFIG_PROVIDER', None)),
+#     "RDR_STORAGE_PROVIDER={0}".format(os.environ.get('RDR_STORAGE_PROVIDER', None)),
+# ]
 # workers = multiprocessing.cpu_count() * 2 + 1
 # threads = multiprocessing.cpu_count() * 2 + 1
