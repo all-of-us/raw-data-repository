578bf32626663ef19c3bcf7d1486e6140865c1b5
j-kanuch <joshua.d.kanuch@vumc.org>
2019-09-26 15:22:39 -0500

    Added raw-data-repository/ to sys.path in RTD config.

--- rdr_service/doc/conf.py
+++ rdr_service/doc/conf.py
@@ -10,11 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
 import sys
 from unittest.mock import MagicMock
+
+sys.path.insert(0, os.path.abspath('../../'))
+
+
 
 # -- Project information -----------------------------------------------------
 # pylint: disable=redefined-builtin
