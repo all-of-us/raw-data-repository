1618ea7f80306211a8e402993d0cd3242be1e528
wangy70 <yu.wang.3@vumc.org>
2019-10-24 10:41:29 -0500

    bq sycn from backup database for organization and site

--- rest-api/dao/bq_organization_dao.py
+++ rest-api/dao/bq_organization_dao.py
@@ -11,14 +11,15 @@ class BQOrganizationGenerator(BigQueryGenerator):
   Generate an Organization BQRecord object
   """
 
-  def make_bqrecord(self, organization_id, convert_to_enum=False):
+  def make_bqrecord(self, organization_id, convert_to_enum=False, backup=True):
     """
     Build a BQRecord object from the given organization id.
     :param organization_id: Primary key value from the organization table.
     :param convert_to_enum: If schema field description includes Enum class info, convert value to Enum.
+    :param backup: if True, get from backup database
     :return: BQRecord object
     """
-    ro_dao = BigQuerySyncDao()
+    ro_dao = BigQuerySyncDao(backup=backup)
     with ro_dao.session() as ro_session:
       row = ro_session.execute(
         text('select * from organization where organization_id = :id'), {'id': organization_id}).first()
@@ -47,7 +48,7 @@ def bq_organization_update(project_id=None):
 
 def bq_organization_update_by_id(org_id):
   gen = BQOrganizationGenerator()
-  bqr = gen.make_bqrecord(org_id)
+  bqr = gen.make_bqrecord(org_id, backup=False)
   w_dao = BigQuerySyncDao()
   with w_dao.session() as w_session:
     gen.save_bqrecord(org_id, bqr, bqtable=BQOrganization, w_dao=w_dao, w_session=w_session)
--- rest-api/dao/bq_site_dao.py
+++ rest-api/dao/bq_site_dao.py
@@ -12,14 +12,15 @@ class BQSiteGenerator(BigQueryGenerator):
   Generate a Site BQRecord object
   """
 
-  def make_bqrecord(self, site_id, convert_to_enum=False):
+  def make_bqrecord(self, site_id, convert_to_enum=False, backup=True):
     """
     Build a BQRecord object from the given site id.
     :param site_id: Primary key value from site table.
     :param convert_to_enum: If schema field description includes Enum class info, convert value to Enum.
+    :param backup: if True, get from backup database
     :return: BQRecord object
     """
-    ro_dao = BigQuerySyncDao()
+    ro_dao = BigQuerySyncDao(backup=backup)
     with ro_dao.session() as ro_session:
       row = ro_session.execute(text('select * from site where site_id = :id'), {'id': site_id}).first()
       data = ro_dao.to_dict(row)
@@ -31,7 +32,7 @@ def bq_site_update(project_id=None):
   Generate all new Site records for BQ. Since there is called from a tool, this is not deferred.
   :param project_id: Override the project_id
   """
-  ro_dao = BigQuerySyncDao()
+  ro_dao = BigQuerySyncDao(backup=True)
   with ro_dao.session() as ro_session:
     gen = BQSiteGenerator()
     results = ro_session.query(Site.siteId).all()
@@ -46,7 +47,7 @@ def bq_site_update(project_id=None):
 
 def bq_site_update_by_id(site_id):
   gen = BQSiteGenerator()
-  bqr = gen.make_bqrecord(site_id)
+  bqr = gen.make_bqrecord(site_id, backup=False)
   w_dao = BigQuerySyncDao()
   with w_dao.session() as w_session:
     gen.save_bqrecord(site_id, bqr, bqtable=BQSite, w_dao=w_dao, w_session=w_session)
