84f28fbf17669d4d7ff10373732201c79b08c760
j-kanuch <54945800+j-kanuch@users.noreply.github.com>
2019-10-31 11:22:25 -0500

    [DA-1350] Populate biobank_order.finalized_time from biobank_ordered_sample (#1389)
    * Populate biobank_order.finalized_time from biobank_ordered_sample
    
    * Implementing Codacy suggestion.
    
    * Cleaning up test_from_json to remove unnecessary loop.

--- tests/dao_tests/test_biobank_order_dao.py
+++ tests/dao_tests/test_biobank_order_dao.py
@@ -10,6 +10,8 @@ from rdr_service.dao.participant_summary_dao import ParticipantSummaryDao
 from rdr_service.model.biobank_order import BiobankOrder, BiobankOrderIdentifier, BiobankOrderedSample
 from rdr_service.model.participant import Participant
 from rdr_service.participant_enums import BiobankOrderStatus, OrderStatus, WithdrawalStatus
+from rdr_service.api_util import parse_date
+
 from tests.test_data import load_biobank_order_json
 from tests.helpers.unittest_base import BaseTestCase
 
@@ -113,6 +115,10 @@ class BiobankOrderDaoTest(BaseTestCase):
         self.assertEqual("sue@pmi-ops.org", order.processedUsername)
         self.assertEqual(2, order.finalizedSiteId)
         self.assertEqual("bob@pmi-ops.org", order.finalizedUsername)
+
+        # testing finalized_time
+        samples_finalized_time = parse_date(order_json['samples'][0]['finalized'])
+        self.assertEqual(samples_finalized_time, order.finalizedTime)
 
     def test_to_json(self):
         order = self._make_biobank_order()
