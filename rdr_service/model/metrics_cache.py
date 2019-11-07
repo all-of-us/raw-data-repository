10f8f38aeb210947ac1b1089fd7f6c6c221104c5
wangy70 <yu.wang.3@vumc.org>
2019-11-07 13:08:53 -0600

    dashboard_enrollmentstatus_filter_for_gender_race_age

--- rdr_service/model/metrics_cache.py
+++ rdr_service/model/metrics_cache.py
@@ -28,6 +28,7 @@ class MetricsRaceCache(Base):
     dateInserted = Column("date_inserted", UTCDateTime, default=clock.CLOCK.now, nullable=False, primary_key=True)
     type = Column("type", String(50), primary_key=True)
     registeredFlag = Column("registered_flag", Boolean, nullable=False, primary_key=True)
+    participantFlag = Column('participant_flag', Boolean, nullable=False, primary_key=True)
     consentedFlag = Column("consent_flag", Boolean, nullable=False, primary_key=True)
     coreFlag = Column("core_flag", Boolean, nullable=False, primary_key=True)
     hpoId = Column("hpo_id", String(20), primary_key=True)
