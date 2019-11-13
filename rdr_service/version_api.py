02ff80231d744e57749bf6b88d8f5241f1e41ed4
Robert Abram <robert.m.abram.1@vumc.org>
2019-11-13 10:52:20 -0600

    remove circular dependency.

--- rdr_service/version_api.py
+++ rdr_service/version_api.py
@@ -7,7 +7,6 @@ import logging
 from flask import request
 from flask_restful import Resource
 
-from api.base_api import log_api_request
 from rdr_service.config import GAE_VERSION_ID
 
 
@@ -16,5 +15,4 @@ class VersionApi(Resource):
 
     def get(self):
         logging.info(str(request.headers))
-        log_api_request()
         return {"version_id": GAE_VERSION_ID}
