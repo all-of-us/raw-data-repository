85d06649c552b88a643a086bf839df18563cf5af
Robert Abram <robert.m.abram.1@vumc.org>
2019-12-18 12:33:00 -0600

    Add depreciation notice around BQ EHR fields. (#1505)

--- rdr_service/model/bq_participant_summary.py
+++ rdr_service/model/bq_participant_summary.py
@@ -161,6 +161,7 @@ class BQParticipantSummarySchema(BQSchema):
     enrollment_core_ordered = BQField('enrollment_core_ordered', BQFieldTypeEnum.DATETIME, BQFieldModeEnum.NULLABLE)
     enrollment_core_stored = BQField('enrollment_core_stored', BQFieldTypeEnum.DATETIME, BQFieldModeEnum.NULLABLE)
 
+    # These EHR fields are being replaced by a direct query from Curation's BigQuery database and need to be removed.
     ehr_status = BQField('ehr_status', BQFieldTypeEnum.STRING, BQFieldModeEnum.NULLABLE)
     ehr_status_id = BQField('ehr_status_id', BQFieldTypeEnum.INTEGER, BQFieldModeEnum.NULLABLE)
     ehr_receipt = BQField('ehr_receipt', BQFieldTypeEnum.DATETIME, BQFieldModeEnum.NULLABLE)
