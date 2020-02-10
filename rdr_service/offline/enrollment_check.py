6e706cb9f2fadca8eb2c30603fa50f812e25c881
seeker1911 <michaelmead007@icloud.com>
2020-02-10 13:47:13 -0600

    DA-1421 add cron job to check for enrollment status accuracy for full participants.

---
+++ rdr_service/offline/enrollment_check.py
@@ -0,0 +1,95 @@
+from datetime import datetime, timedelta
+
+from rdr_service.dao.participant_summary_dao import ParticipantSummaryDao
+from rdr_service.model.participant_summary import ParticipantSummary
+from rdr_service.participant_enums import PhysicalMeasurementsStatus, QuestionnaireStatus, SampleStatus
+from rdr_service.services.jira_utils import JiraTicketHandler
+
+PS_DAO = ParticipantSummaryDao()
+THIRTYONE_DAYS = datetime.now() - timedelta(days=31)
+
+
+def check_enrollment(create_ticket=True):
+    """For members who are listed as full-member,
+     check that necessary requirements are valid"""
+    members = get_last_30_days()
+    results = None
+    if members:
+        results = check_requirements(members)
+    if results:
+        # make a roc ticket
+        if create_ticket:
+            jira_handler = JiraTicketHandler()
+            jira_handler.create_ticket('Participant Status offline job', str(results).strip('[]'), issue_type='Task',
+                                       board_id='ROC')
+        return False
+    else:
+        return True
+
+
+def get_last_30_days():
+    """Gets members with full-member (core) status from last 30 days.
+    return: members"""
+    with PS_DAO.session() as session:
+        return session.query(ParticipantSummary).filter(ParticipantSummary.enrollmentStatus == 3) \
+            .filter(ParticipantSummary.lastModified >= THIRTYONE_DAYS).all()
+
+
+def check_requirements(members):
+    """ A full participant (core) is defined as:
+    completed the primary informed consent process
+    HIPAA Authorization/EHR consent
+    required PPI modules (Basics, Overall Health, and Lifestyle modules)
+    provide physical measurements
+    at least one biosample suitable for genetic sequencing.
+    """
+    result_list = []
+    status_results = {}
+
+    for member in members:
+        mandatory = check_mandatory(member, status_results)
+        optional = check_optional(member, status_results)
+        if optional:
+            mandatory.update(optional)
+        if mandatory:
+            result_list.append(mandatory)
+
+    return result_list if result_list else False
+
+
+def check_mandatory(member, status_results):
+    required = {'consentForStudyEnrollment': QuestionnaireStatus.SUBMITTED,
+                'questionnaireOnLifestyle': QuestionnaireStatus.SUBMITTED,
+                'questionnaireOnOverallHealth': QuestionnaireStatus.SUBMITTED,
+                'questionnaireOnTheBasics': QuestionnaireStatus.SUBMITTED,
+                'physicalMeasurementsStatus': PhysicalMeasurementsStatus.COMPLETED,
+                'samplesToIsolateDNA': SampleStatus.RECEIVED}
+
+    for attribute, value in required.items():
+        if attribute not in member.__dict__:
+            status_results['participantId'] = member.particpantId
+            status_results[attribute] = 'missing'
+        if getattr(member, attribute) != value:
+            status_results['participantId'] = member.participantId
+            status_results[attribute] = getattr(member, attribute)
+
+    return status_results or None
+
+
+def check_optional(member, status_results):
+    comparable = {'consentForDvElectronicHealthRecordsSharing': QuestionnaireStatus.SUBMITTED,
+                  'consentForElectronicHealthRecords': QuestionnaireStatus.SUBMITTED}
+
+    truthy_list = [i in member.__dict__ for i in comparable]
+    if not any(truthy_list):
+        status_results['participantId'] = member.particpantId
+        status_results['consentForDvElectronicHealthRecordsSharing'] = getattr(
+            member, 'consentForDvElectronicHealthRecordsSharing')
+        status_results['consentForElectronicHealthRecords'] = getattr(member, 'consentForElectronicHealthRecords')
+
+    for attribute, value in comparable.items():
+        if getattr(member, attribute) != value and getattr(member, attribute) is not None:
+            status_results['participantId'] = member.participantId
+            status_results[attribute] = getattr(member, attribute)
+
+    return status_results or None
---
+++ tests/cron_job_tests/test_enrollment_status_checking.py
@@ -0,0 +1,73 @@
+from rdr_service.participant_enums import EnrollmentStatus, SampleStatus, QuestionnaireStatus, \
+    PhysicalMeasurementsStatus
+from tests.helpers.unittest_base import BaseTestCase
+from datetime import datetime, timedelta
+from datetime import datetime, timedelta
+
+from rdr_service.participant_enums import EnrollmentStatus, PhysicalMeasurementsStatus, QuestionnaireStatus, \
+    SampleStatus
+from tests.helpers.unittest_base import BaseTestCase
+
+
+class CheckEnrollmentStatusTest(BaseTestCase):
+    """Tests checking enrollment status of participants cron job"""
+
+    def setUp(self):
+        super().setUp()
+
+    # test core participant meets requirements
+    def test_core_meets_req(self):
+        from rdr_service.offline.enrollment_check import check_enrollment
+        person, ps_dao = self.setup_core_participant()
+        # missing questionnaires and pm status
+        self.assertEqual(check_enrollment(create_ticket=False), False)
+        # update required attributes
+        person.questionnaireOnLifestyle = QuestionnaireStatus.SUBMITTED
+        person.questionnaireOnOverallHealth = QuestionnaireStatus.SUBMITTED
+        person.questionnaireOnTheBasics = QuestionnaireStatus.SUBMITTED
+        person.physicalMeasurementsStatus = PhysicalMeasurementsStatus.COMPLETED
+        with ps_dao.session() as session:
+            session.add(person)
+        self.assertEqual(check_enrollment(create_ticket=False), True)
+
+    def setup_core_participant(self):
+        """ A full participant (core) is defined as:
+        completed the primary informed consent process
+        HIPAA Authorization/EHR consent
+        required PPI modules (Basics, Overall Health, and Lifestyle modules)
+        provide physical measurements
+        at least one biosample suitable for genetic sequencing.
+        """
+        twenty_nine = datetime.now() - timedelta(days=29)
+        p = self._participant_with_defaults(participantId=6666666, biobankId=9999999, version=1,
+                                            lastModified=twenty_nine, signUpTime=twenty_nine)
+        valid_kwargs = dict(
+            participantId=p.participantId,
+            biobankId=p.biobankId,
+            withdrawalStatus=p.withdrawalStatus,
+            dateOfBirth=datetime(2000, 1, 1),
+            firstName="foo",
+            lastName="bar",
+            zipCode="12345",
+            sampleStatus1ED04=SampleStatus.RECEIVED,
+            sampleStatus1SAL2=SampleStatus.RECEIVED,
+            samplesToIsolateDNA=SampleStatus.RECEIVED,
+            consentForStudyEnrollmentTime=datetime(2019, 1, 1),
+            numCompletedBaselinePPIModules=3,
+            consentForStudyEnrollment=1,
+            consentForElectronicHealthRecords=1,
+            enrollmentStatus=EnrollmentStatus.FULL_PARTICIPANT,
+            lastModified=twenty_nine)
+
+        # kwargs = dict(valid_kwargs, **override_kwargs)
+        person = self._participant_summary_with_defaults(**valid_kwargs)
+        from rdr_service.dao.participant_summary_dao import ParticipantSummaryDao
+        from rdr_service.dao.participant_dao import ParticipantDao
+        dao = ParticipantDao()
+        with dao.session() as session:
+            session.add(p)
+        ps_dao = ParticipantSummaryDao()
+        with ps_dao.session() as session:
+            session.add(person)
+
+        return person, ps_dao
