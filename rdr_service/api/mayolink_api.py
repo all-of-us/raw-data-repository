c9aa9cb474d04eef27195936b81b6f0c70355390
wangy70 <43476466+wangy70@users.noreply.github.com>
2020-03-06 12:02:03 -0600

    [DA-1415] quest_biobank_order_api (#1624)
    * quest_biobank_order_api
    
    * quest_biobank_order_api

--- rdr_service/api/mayolink_api.py
+++ rdr_service/api/mayolink_api.py
@@ -1,59 +1,15 @@
 import json
-import xml.etree.cElementTree as etree
-
+import xml.etree.ElementTree as ET
 import httplib2
 import xmltodict
 from werkzeug.exceptions import ServiceUnavailable
 
 from rdr_service import config
-from rdr_service.api.base_api import UpdatableApi
-from rdr_service.api_util import RDR_AND_PTC, format_json_enum, open_cloud_file
+from rdr_service.api_util import RDR_AND_PTC, open_cloud_file
 from rdr_service.app_util import auth_required
 
 
-class MayoLinkApi(UpdatableApi):
-
-    payload_template = """<?xml version="1.0" encoding="UTF-8"?>
-        <orders xmlns="http://orders.mayomedicallaboratories.com">
-            <order>
-                <collected></collected>
-                <account></account>
-                <number></number>
-                <patient>
-                    <medical_record_number></medical_record_number>
-                    <first_name></first_name>
-                    <last_name></last_name>
-                    <middle_name/>
-                    <birth_date></birth_date>
-                    <gender></gender>
-                    <address1/>
-                    <address2/>
-                    <city/>
-                    <state/>
-                    <postal_code/>
-                    <phone/>
-                    <account_number/>
-                    <race/>
-                    <ethnic_group/>
-                </patient>
-                <physician>
-                    <name></name>
-                    <phone/>
-                    <npi/>
-                </physician>
-                <report_notes/>
-                <tests>
-                    <test>
-                        <code></code>
-                        <name></name>
-                        <comments/>
-                    </test>
-                </tests>
-                <comments/>
-            </order>
-        </orders>
-    """
-
+class MayoLinkApi:
     def __init__(self):
         self.namespace = "http://orders.mayomedicallaboratories.com"
         self.config_bucket = config.CONFIG_BUCKET
@@ -93,40 +49,27 @@ class MayoLinkApi(UpdatableApi):
 
         raise ServiceUnavailable("Mayolink service unavailable, please re-try later")
 
-
     def __dict_to_mayo_xml__(self, order):
-        root = etree.fromstring(self.payload_template)
-        # A super lame way to do this, sorry ? :-)
-        root[0][0].text = order["order"]["collected"]
-        root[0][1].text = str(self.account)
-        root[0][2].text = order["order"]["number"]
-        root[0][3][0].text = order["order"]["patient"]["medical_record_number"]
-        root[0][3][1].text = order["order"]["patient"]["first_name"]
-        root[0][3][2].text = order["order"]["patient"]["last_name"]
-        root[0][3][3].text = order["order"]["patient"]["middle_name"]
-        root[0][3][4].text = order["order"]["patient"]["birth_date"]
-        root[0][3][5].text = order["order"]["patient"]["gender"]
-        root[0][3][6].text = order["order"]["patient"]["address1"]
-        root[0][3][7].text = order["order"]["patient"]["address2"]
-        root[0][3][8].text = order["order"]["patient"]["city"]
-        root[0][3][9].text = order["order"]["patient"]["state"]
-        root[0][3][10].text = order["order"]["patient"]["postal_code"]
-        root[0][3][11].text = order["order"]["patient"]["phone"]
-        root[0][3][12].text = order["order"]["patient"]["account_number"]
-        root[0][3][13].text = format_json_enum(order["order"]["patient"], "race")
-        root[0][3][14].text = order["order"]["patient"]["ethnic_group"]
-        root[0][4][0].text = order["order"]["physician"]["name"]
-        root[0][4][1].text = order["order"]["physician"]["phone"]
-        root[0][4][2].text = order["order"]["physician"]["npi"]
-        root[0][5].text = order["order"]["report_notes"]
-        root[0][6][0][0].text = order["order"]["tests"]["test"]["code"]
-        root[0][6][0][1].text = order["order"]["tests"]["test"]["name"]
-        root[0][6][0][2].text = order["order"]["tests"]["test"]["comments"]
-        root[0][7].text = order["order"]["comments"]
-
-        request = etree.tostring(root)
+        order['order']['account'] = self.account
+        orders_element = ET.Element("orders")
+        orders_element.set('xmlns', self.namespace)
+        tree_root = self.create_xml_tree_from_dict(orders_element, order)
+        request = ET.tostring(tree_root, encoding='UTF-8', method='xml')
         return request
 
     def _xml_to_dict(self, content):
         result = xmltodict.parse(content)
         return result
+
+    def create_xml_tree_from_dict(self, root, dict_tree):
+        if type(dict_tree) == dict:
+            for k, v in dict_tree.items():
+                if type(v) != list:
+                    self.create_xml_tree_from_dict(ET.SubElement(root, k), v)
+                else:
+                    sub_element = ET.SubElement(root, k)
+                    for item in v:
+                        self.create_xml_tree_from_dict(sub_element, item)
+            return root
+        else:
+            root.text = str(dict_tree)
