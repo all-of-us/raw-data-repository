89f80a96b2a44821487bb3ba274b81698c458044
j-kanuch <joshua.d.kanuch@vumc.org>
2020-03-27 14:23:08 -0500

    Added gem to auth role.

--- rdr_service/api/genomic_api.py
+++ rdr_service/api/genomic_api.py
@@ -1,7 +1,7 @@
 from werkzeug.exceptions import NotFound, BadRequest
 
 from rdr_service.api.base_api import BaseApi
-from rdr_service.api_util import PTC_AND_HEALTHPRO
+from rdr_service.api_util import GEM
 from rdr_service.app_util import auth_required
 from rdr_service.dao.genomics_dao import GemPiiDao
 
@@ -10,7 +10,7 @@ class GenomicGemPiiApi(BaseApi):
     def __init__(self):
         super(GenomicGemPiiApi, self).__init__(GemPiiDao())
 
-    @auth_required(PTC_AND_HEALTHPRO)
+    @auth_required(GEM)
     def get(self, p_id=None):
         if p_id is not None:
             pii = self.dao.get_by_pid(p_id)
