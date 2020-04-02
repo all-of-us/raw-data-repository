ea9860e628cfea05db67e03c6de5542577f9ecd0
j-kanuch <joshua.d.kanuch@vumc.org>
2020-04-02 15:14:50 -0500

    Implemented fix for biobank orders.

--- rdr_service/tools/tool_libs/dup_participant_fix.py
+++ rdr_service/tools/tool_libs/dup_participant_fix.py
@@ -12,7 +12,9 @@ import os
 import sys
 
 from rdr_service.dao.participant_dao import ParticipantDao
+from rdr_service.model.biobank_stored_sample import BiobankStoredSample
 from rdr_service.model.participant import Participant
+from rdr_service.model.biobank_order import BiobankOrder, BiobankOrderIdentifier
 
 from rdr_service.services.system_utils import setup_logging, setup_i18n
 from rdr_service.tools.tool_libs import GCPProcessContext, GCPEnvConfigObject
@@ -48,6 +50,50 @@ class ProgramTemplateClass(object):
 
         return p, p1, p2
 
+    def get_biobank_records_for_participants(self, dao, mappings):
+        """
+        Return the participant records for each pid in pids.
+        :param dao: DAO object to run queries with.
+        :param mappings: Tuple with 3 pid values.
+        :return: Original Participant record, New Participant 1 record, New Participant 2 record.
+        """
+        with dao.session() as session:
+            op = session.query(Participant).filter(Participant.participantId == mappings[0]).first()
+            np = session.query(Participant).filter(Participant.participantId == mappings[1]).first()
+            bbo = session.query(BiobankOrder).filter(BiobankOrder.biobankOrderId == mappings[2]).first()
+            ss = session.query(BiobankStoredSample).join(
+                BiobankOrderIdentifier,
+                BiobankOrderIdentifier.value == BiobankStoredSample.biobankOrderIdentifier
+            ).filter(
+                BiobankOrderIdentifier.biobankOrderId == mappings[2]
+            ).first()
+
+        return op, np, bbo, ss
+
+    def fix_biobank_order(self, dao, np, bbo):
+        """
+        Updates the Biobank Order object to the new PID
+        :param dao: the dao
+        :param np: new participant
+        :param bbo: biobank order object
+        :return: updated biobank order object
+        """
+        bbo.participantId = np.participantId
+        with dao.session() as session:
+            return session.merge(bbo)
+
+    def fix_biobank_stored_sample(self, dao, np, ss):
+        """
+        Updates the Biobank stored sample to the new biobank_id
+        :param dao:
+        :param np: new participant
+        :param ss: stored sample object
+        :return: updated sample object
+        """
+        ss.biobankId = np.biobankId
+        with dao.session() as session:
+            return session.merge(ss)
+
     def run(self):
         """
         Main program process
@@ -68,28 +114,46 @@ class ProgramTemplateClass(object):
         self.gcp_env.activate_sql_proxy()
         dao = ParticipantDao()
 
-        pids_list = list()
+        mappings_list = list()
 
         if self.args.participant:
-            pids_list.append(tuple(i.strip() for i in self.args.participant.split(',')))
+            mappings_list.append(tuple(i.strip() for i in self.args.participant.split(',')))
         else:
-            with open(self.args.csv) as h:
+            with open(self.args.csv, encoding='utf-8-sig') as h:
                 lines = h.readlines()
                 for line in lines:
-                    pids_list.append(tuple(i.strip() for i in line.split(',')))
+                    mappings_list.append(tuple(i.strip() for i in line.split(',')))
+                if self.args.fix_biobank_orders:
+                    headers = mappings_list.pop(0)
+                    if headers != ("old_pid", "new_pid", "biobank_order_id"):
+                        _logger.error("Invalid columns in CSV")
+                        _logger.error(f"   {headers}")
+                        return 1
 
-        for pids in pids_list:
-            p, p1, p2 = self.get_participant_records(dao, pids)
+        if self.args.fix_biobank_orders:
+            for mapping in mappings_list:
+                old_p, new_p, biobank_order, stored_sample = self.get_biobank_records_for_participants(dao, mapping)
 
-            if not p or not p1 or not p2:
-                _logger.error(f'  error {p.participantId}, {p1.participantId}, {p2.participantId}')
-                continue
+                if not old_p or not new_p or not biobank_order or not stored_sample:
+                    _logger.error(
+                        f'  error {old_p.participantId}, {new_p.participantId}, {biobank_order.biobankOrderId}')
+                    continue
 
-            _logger.warning(f'  splitting {p.participantId} -> {p1.participantId} | {p2.participantId}')
+                # Process Biobank Orders
+                _logger.warning(
+                    f'  reassigning Biobank Order {biobank_order.biobankOrderId} | '
+                    f'{old_p.participantId} -> {new_p.participantId}')
+                updated_bbo = self.fix_biobank_order(dao, new_p, biobank_order)
+                _logger.info(f'  update successful for {updated_bbo.biobankOrderId}: '
+                             f'{updated_bbo.participantId}')
 
-            # TODO: Call methods for each step in process here
-
-
+                # Process Biobank Stored Samples
+                _logger.warning(
+                    f'  reassigning Stored Sample {stored_sample.biobankStoredSampleId} | '
+                    f'{old_p.biobankId} -> {new_p.biobankId}')
+                updated_ss = self.fix_biobank_stored_sample(dao, new_p, stored_sample)
+                _logger.info(f'  update successful for {updated_ss.biobankStoredSampleId}: '
+                             f'{updated_ss.biobankId}')
         return 0
 
 
@@ -109,7 +173,18 @@ def run():
     parser.add_argument("--service-account", help="gcp iam service account", default=None)  # noqa
     parser.add_argument("--csv", help="csv file with participant ids", default=None)  # noqa
     parser.add_argument("--participant", help="old pid,new pid 1,new pid 2", default=None)  # noqa
+    parser.add_argument("--fix-biobank-orders", help="Fix Biobank Orders", default=False, action="store_true")  # noqa
+    parser.add_argument("--fix-physical-measurements", help="Fix Physical Measurements",
+                        default=False, action="store_true")  # noqa
     args = parser.parse_args()
+
+    if not args.fix_biobank_orders and not args.fix_physical_measurements:
+        _logger.error('Either --fix-biobank-orders or --fix-physical-measurements must be provided.')
+        return 1
+
+    if args.fix_biobank_orders and args.fix_physical_measurements:
+        _logger.error('Arguments --fix_biobank_orders and --fix_physical_measurements may not be used together.')
+        return 1
 
     if not args.participant and not args.csv:
         _logger.error('Either --csv or --participant argument must be provided.')
