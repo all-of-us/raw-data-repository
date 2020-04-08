19c3a400fd37c7c8a2e9d510d6f79ec3a11ab710
j-kanuch <joshua.d.kanuch@vumc.org>
2020-04-08 12:00:02 -0500

    Writing genomic set.

--- rdr_service/tools/tool_libs/genomic_utils.py
+++ rdr_service/tools/tool_libs/genomic_utils.py
@@ -11,10 +11,13 @@ import logging
 import sys
 import os
 
-from rdr_service.dao.genomics_dao import GenomicSetMemberDao
+from rdr_service import clock
+from rdr_service.dao.genomics_dao import GenomicSetMemberDao, GenomicSetDao
+from rdr_service.genomic.genomic_biobank_manifest_handler import OUTPUT_CSV_TIME_FORMAT
+from rdr_service.model.genomics import GenomicSetMember, GenomicSet
 from rdr_service.services.system_utils import setup_logging, setup_i18n
 from rdr_service.tools.tool_libs import GCPProcessContext, GCPEnvConfigObject
-from rdr_service.participant_enums import GenomicManifestTypes
+from rdr_service.participant_enums import GenomicManifestTypes, GenomicSetStatus
 
 _logger = logging.getLogger("rdr_logger")
 
@@ -33,9 +36,60 @@ class ResendSamplesClass(object):
         self.args = args
         self.gcp_env = gcp_env
         self.dao = None
+        self.nowf = clock.CLOCK.now().strftime(OUTPUT_CSV_TIME_FORMAT)
 
-    def resend_biobank_manifest(self):
+    def get_members_for_samples(self, samples, set_id):
+        """
+        returns the genomic set members' data for samples
+        calls upload_manifest()
+        :param samples: list of samples to resend
+        :return: the members' records for the samples
+        """
+        with self.dao.session() as session:
+            member_records = session.query(GenomicSetMember)\
+                .filter(GenomicSetMember.sampleId.in_(samples)).all()
+
+            updated_members = list()
+            for member in member_records:
+                member.genomicSetId = set_id
+                updated_members.append(session.merge(member))
+
+        return updated_members
+
+    def create_new_genomic_set(self):
+        """
+        inserts a new genomic set and returns the genomic set object
+        :return: genomic set object
+        """
+        set_dao = GenomicSetDao()
+        attributes = {
+            'genomicSetName': f'sample_resend_utility_{self.nowf}',
+            'genomicSetCriteria': '.',
+            'genomicSetVersion': 1,
+            'genomicSetStatus': GenomicSetStatus.VALID,
+        }
+        new_set_obj = GenomicSet(**attributes)
+        return set_dao.insert(new_set_obj)
+
+    def update_members_set_data(self):
         pass
+
+    def export_bb_manifest(self, set_id):
+        """
+        Runs the manifest handler to export the genomic set
+        :param set_id:
+        :return:
+        """
+        pass
+
+    def process_and_generate_bb_manifest(self, samples):
+        """
+        Executes the methods to get the members, create set, and export data
+        :return:
+        """
+        genset = self.create_new_genomic_set()
+        members = self.process_members_for_samples(samples, genset.id)
+
 
     def run(self):
         """
@@ -75,17 +129,17 @@ class ResendSamplesClass(object):
         # Parse samples to resend from CSV or List
         samples_list = list()
         if self.args.samples:
-            for i in self.args.samples.split(','):
-                samples_list.append(int(i.strip()))
+            for sample in self.args.samples.split(','):
+                samples_list.append(int(sample.strip()))
         else:
             with open(self.args.csv, encoding='utf-8-sig') as h:
                 lines = h.readlines()
                 for line in lines:
                     samples_list.append(int(line.strip()))
-        print(samples_list)
 
+        # Execute manifest resends
         if self.args.manifest == "DRC_BIOBANK":
-            self.resend_biobank_manifest()
+            self.process_and_generate_bb_manifest(samples_list)
 
         return 0
 
