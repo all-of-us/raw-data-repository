6a6b0085f39ca59437ab94e4c16b765e77b07d04
Josh Kanuch <54945800+j-kanuch@users.noreply.github.com>
2020-04-13 13:47:57 -0500

    Data Generators Stored Samples and Genomic Members (#1678)
    * Added stored samples to spec-gen
    
    * Added genomic set member to spec gen.

--- rdr_service/data_gen/generators/__init__.py
+++ rdr_service/data_gen/generators/__init__.py
@@ -4,3 +4,5 @@ from rdr_service.data_gen.generators.codebook import CodeBook
 from rdr_service.data_gen.generators.hpo import HPOGen, HPOSiteGen
 from rdr_service.data_gen.generators.physical_measurements import PhysicalMeasurementsGen
 from rdr_service.data_gen.generators.questionnaires import QuestionnaireGen
+from rdr_service.data_gen.generators.stored_samples import StoredSampleGen
+from rdr_service.data_gen.generators.genomics import GenomicSetMemberGen
