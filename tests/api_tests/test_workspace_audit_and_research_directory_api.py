e17ac03d84ffa7a0dcb0b95e02b925a9d232b86e
wangy70 <yu.wang.3@vumc.org>
2020-04-29 13:43:02 -0500

    delay_24hours_audit_work_flow

--- tests/api_tests/test_workspace_audit_and_research_directory_api.py
+++ tests/api_tests/test_workspace_audit_and_research_directory_api.py
@@ -1,4 +1,7 @@
 from tests.helpers.unittest_base import BaseTestCase
+from rdr_service.clock import FakeClock
+from rdr_service import clock
+from datetime import timedelta
 
 
 class ResearchProjectsDirectoryApiTest(BaseTestCase):
@@ -151,7 +154,10 @@ class ResearchProjectsDirectoryApiTest(BaseTestCase):
                 "findingsFromStudy": 'findingsFromStudy string2'
             }
         ]
-        self.send_post('workbench/directory/workspaces', request_data=request_json)
+        now = clock.CLOCK.now()
+        sequest_hours_ago = now - timedelta(hours=24)
+        with FakeClock(sequest_hours_ago):
+            self.send_post('workbench/directory/workspaces', request_data=request_json)
         # test get research projects directory before review
         result = self.send_get('researchHub/projectDirectory')
         self.assertEqual(len(result['data']), 2)
@@ -429,6 +435,92 @@ class ResearchProjectsDirectoryApiTest(BaseTestCase):
                        }
                        },
                       result['data'])
+
+    def test_get_research_projects_directory_less_than_23_hours(self):
+        # create researchers
+        researchers_json = [
+            {
+                "userId": 0,
+                "creationTime": "2019-11-26T21:21:13.056Z",
+                "modifiedTime": "2019-11-26T21:21:13.056Z",
+                "givenName": "given name 1",
+                "familyName": "family name 1",
+                "streetAddress1": "string",
+                "streetAddress2": "string",
+                "city": "string",
+                "state": "string",
+                "zipCode": "string",
+                "country": "string",
+                "ethnicity": "HISPANIC",
+                "gender": ["MAN"],
+                "race": ["AIAN"],
+                "sexAtBirth": ["FEMALE"],
+                "sexualOrientation": "BISEXUAL",
+                "affiliations": [
+                    {
+                        "institution": "institution1",
+                        "role": "institution role 1",
+                        "nonAcademicAffiliation": "INDUSTRY"
+                    }
+                ]
+            }
+        ]
+        self.send_post('workbench/directory/researchers', request_data=researchers_json)
+
+        # create workspace
+        request_json = [
+            {
+                "workspaceId": 0,
+                "name": "workspace name str",
+                "creationTime": "2019-11-25T17:43:41.085Z",
+                "modifiedTime": "2019-11-25T17:43:41.085Z",
+                "status": "ACTIVE",
+                "workspaceUsers": [
+                    {
+                        "userId": 0,
+                        "role": "READER",
+                        "status": "ACTIVE"
+                    }
+                ],
+                "excludeFromPublicDirectory": False,
+                "diseaseFocusedResearch": True,
+                "diseaseFocusedResearchName": "disease focused research name str",
+                "otherPurposeDetails": "other purpose details str",
+                "methodsDevelopment": True,
+                "controlSet": True,
+                "ancestry": True,
+                "socialBehavioral": True,
+                "populationHealth": True,
+                "drugDevelopment": True,
+                "commercialPurpose": True,
+                "educational": True,
+                "otherPurpose": True,
+                "scientificApproaches": 'reasonForInvestigation string',
+                "intendToStudy": 'intendToStudy string',
+                "findingsFromStudy": 'findingsFromStudy string',
+                "focusOnUnderrepresentedPopulations": True,
+                "workspaceDemographic": {
+                    "raceEthnicity": ['AIAN', 'MENA'],
+                    "age": ['AGE_0_11', 'AGE_65_74'],
+                    "sexAtBirth": "UNSET",
+                    "genderIdentity": "OTHER_THAN_MAN_WOMAN",
+                    "sexualOrientation": "OTHER_THAN_STRAIGHT",
+                    "geography": "RURAL",
+                    "disabilityStatus": "DISABILITY",
+                    "accessToCare": "NOT_EASILY_ACCESS_CARE",
+                    "educationLevel": "LESS_THAN_HIGH_SCHOOL",
+                    "incomeLevel": "BELOW_FEDERAL_POVERTY_LEVEL_200_PERCENT",
+                    "others": "string"
+                }
+            }
+        ]
+        now = clock.CLOCK.now()
+        sequest_hours_ago = now - timedelta(hours=22)
+        with FakeClock(sequest_hours_ago):
+            self.send_post('workbench/directory/workspaces', request_data=request_json)
+        # test get research projects directory before review
+        result = self.send_get('researchHub/projectDirectory')
+        self.assertEqual(len(result['data']), 0)
 
     def test_workspace_audit_sync_api(self):
         # create researchers
