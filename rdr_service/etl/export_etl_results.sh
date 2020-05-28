4bda1fc8a361e7be11198067cf67828dee14e6d1
Robert Abram <robert.m.abram.1@vumc.org>
2020-05-28 07:26:14 -0500

    Changed ETL SQL script to add row id field for easy exporting. (#1759)

--- rdr_service/etl/export_etl_results.sh
+++ rdr_service/etl/export_etl_results.sh
@@ -1,5 +1,5 @@
-
 #!/bin/bash -e
+
 # Exports ETL results to GCS.
 
 USAGE="etl/export_etl_results.sh [--project <PROJECT> --account <ACCOUNT> --instance_name <INSTANCE_NAME>] --directory <DIRECTORY>"
@@ -19,32 +19,18 @@ then
   echo "Usage: $USAGE"
   exit 1
 fi
-#PROJECT_AND_ACCOUNT=
-#if [ "${PROJECT}" ]
-#then
-#  PROJECT_AND_ACCOUNT="--project ${PROJECT} --account ${ACCOUNT} --service_account exporter@${PROJECT}.iam.gserviceaccount.com"
-#  if [ -n "${INSTANCE_NAME}" ]; then
-#    PROJECT_AND_ACCOUNT="${PROJECT_AND_ACCOUNT} --instance_name ${INSTANCE_NAME}"
-#  fi
-#fi
-
-if [ -z "${INSTANCE_NAME}"]; then
-  INSTANCE_NAME="rdrmaindb"
+PROJECT_AND_ACCOUNT=
+if [ "${PROJECT}" ]
+then
+  PROJECT_AND_ACCOUNT="--project ${PROJECT} --account ${ACCOUNT} --service_account exporter@${PROJECT}.iam.gserviceaccount.com"
+  if [ -n "${INSTANCE_NAME}" ]; then
+    PROJECT_AND_ACCOUNT="${PROJECT_AND_ACCOUNT} --instance_name ${INSTANCE_NAME}"
+  fi
 fi
 
 #pushd ../rdr_client
 echo "Exporting tables from the cdm database..."
-#./rdr_client/run_client.sh ${PROJECT_AND_ACCOUNT} export_tables.py \
-#   --directory ${DIRECTORY} --database cdm \
-#   --tables care_site,condition_era,condition_occurrence,cost,death,device_exposure,dose_era,drug_era,drug_exposure,fact_relationship,location,measurement,observation,observation_period,payer_plan_period,person,procedure_occurrence,provider,visit_occurrence
-##popd
-
-gcloud config set ${PROJECT}
-
-for TABLE in care_site condition_era condition_occurrence cost death device_exposure dose_era drug_era drug_exposure fact_relationship location measurement observation observation_period payer_plan_period person procedure_occurrence provider visit_occurrence
-do
-  echo -n "  exporting: ${TABLE}..."
-  SQL="SELECT * FROM cdm.${TABLE}"
-  gcloud sql export csv ${INSTANCE_NAME} gs://all-of-us-rdr-prod-cdm/${DIRECTORY}/${TABLE}.csv --database="cdm" --query="${SQL}"
-  echo " done."
-done
+./rdr_client/run_client.sh ${PROJECT_AND_ACCOUNT} export_tables.py \
+   --directory ${DIRECTORY} --database cdm \
+   --tables care_site,condition_era,condition_occurrence,cost,death,device_exposure,dose_era,drug_era,drug_exposure,fact_relationship,location,measurement,observation,observation_period,payer_plan_period,person,procedure_occurrence,provider,visit_occurrence
+#popd
