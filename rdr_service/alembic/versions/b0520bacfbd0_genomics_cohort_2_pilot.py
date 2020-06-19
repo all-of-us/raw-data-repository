29ee3f7b6a75af7bed92676e0517a59b218547ae
Peggy Bertsch <65624611+pegbertsch@users.noreply.github.com>
2020-06-19 11:38:41 -0500

    Picking up another new alembic migration from devel

--- rdr_service/alembic/versions/b0520bacfbd0_genomics_cohort_2_pilot.py
+++ rdr_service/alembic/versions/b0520bacfbd0_genomics_cohort_2_pilot.py
@@ -1,7 +1,7 @@
 """genomics cohort 2 pilot
 
 Revision ID: b0520bacfbd0
-Revises: ed465f72e410
+Revises: c1198b53fdd6
 Create Date: 2020-06-17 15:27:05.910908
 
 """
@@ -22,7 +22,7 @@ from rdr_service.model.site_enums import SiteStatus, EnrollingStatus, DigitalSch
 
 # revision identifiers, used by Alembic.
 revision = 'b0520bacfbd0'
-down_revision = 'ed465f72e410'
+down_revision = 'c1198b53fdd6'
 branch_labels = None
 depends_on = None
 
