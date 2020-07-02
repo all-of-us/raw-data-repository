aabdc03914e2b0a6d9bd58416dc705eabecb64fd
pegbertsch <65624611+pegbertsch@users.noreply.github.com>
2020-07-02 15:03:18 -0500

    [ROC-547] Update cohort assignment calculations (#1820)
    * [ROC-547] Update cohort assignment calculations, revised cohort 3 start time
    
    Per HPRO, consent authored checks adjusted so cohort 3 starts at 2020-04-21T04:00:00 UTC (midnight eastern time)
    A separate backfill/correction was already performed on RDR production (see ROC-544)
    
    * Update enums / constants to use adjusted datetime value
    
    * Fix BQ references that were using date instead of datetime when extracting consent authored value
    
    * Fix the mistyped datetime calls

--- rdr_service/participant_enums.py
+++ rdr_service/participant_enums.py
@@ -39,15 +39,15 @@ TEST_HPO_ID = 19
 # Test login phone number prefix
 TEST_LOGIN_PHONE_NUMBER_PREFIX = "444"
 PARTICIPANT_COHORT_2_START_TIME = datetime(2018, 4, 24, 0, 0, 0)
-PARTICIPANT_COHORT_3_START_TIME = datetime(2020, 4, 21, 0, 0, 0)
+PARTICIPANT_COHORT_3_START_TIME = datetime(2020, 4, 21, 4, 0, 0)
 
 
 class ParticipantCohort(messages.Enum):
     """ Participant Cohort Group"""
     UNSET = 0
     COHORT_1 = 1  # Beta participants.  Consent before April 24, 2018.
-    COHORT_2 = 2  # National Launch Participants. Consent between April 24, 2018 and April 20, 2020.
-    COHORT_3 = 3  # New Participants with consent starting from April 21, 2020.
+    COHORT_2 = 2  # National Launch Participants. Consent between April 24, 2018 and April 21, 2020 (03:59:59 UTC)
+    COHORT_3 = 3  # New Participants with consent starting from April 21, 2020 04:00:00 UTC  (midnight eastern)
 
 
 # Added for DA-1622, enabling identification of Genomics pilot participants from Cohort 2
