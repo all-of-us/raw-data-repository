4685fef8f22acaaa1dffed14bd394e64d28e0bfd
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2020-08-13 17:45:24 -0500

    fixes for memory threshold code

--- rdr_service/services/gunicorn_config.py
+++ rdr_service/services/gunicorn_config.py
@@ -27,27 +27,22 @@ raw_env = [
 
 
 # GAE F4 instances allow for up to 1G of memory to be used.
-# If we're getting too close to the limit we should close the instance.
+# So each worker will need to stay under a limit to prevent the sum of their used memory to go over GAE's limit.
+# If a worker is using too much, then we'll restart it to release what it's gathered back to the OS.
 # That way we can gracefully limit our memory rather than have Google killing us forcefully
 # (and potentially in the middle of handling a request).
-def post_request(worker, *_):
-    # Sum up memory used for this process and any children (resulting in memory used by this instance)
-    self_mem_bytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    children_mem_bytes = resource.getrusage(resource.RUSAGE_CHILDREN).ru_maxrss
-    memory_threshold_bytes = 996147200  # 950 megabytes (950 x 1024 x 1024)
+def post_request(worker, request, environment, response):  # pylint: disable=unused-argument
+    # Sum up memory used for the calling worker
+    memory_used_kilobytes = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
-    # Gracefully kill the worker that just completed a request if the instance is holding on to too much memory.
-    # This unfortunate worker may not be the one using the most memory on the instance but it will help,
-    # and hopefully we'll eventually get the one that is.
-    # In the future this can be tuned to use the process id of the worker to find how much memory that worker
-    # has allocated.
-    memory_used_bytes = self_mem_bytes + children_mem_bytes
-    if memory_used_bytes > memory_threshold_bytes:
-
+    # Our instance is killed if it is using more than 1024 megabytes.
+    # To leave enough room to handle memory intensive requests, let's restart each instance if
+    # they're using more than 250 megabytes
+    memory_threshold_kilobytes = 256000  # 250 megabytes (220 x 1024)
+    if memory_used_kilobytes > memory_threshold_kilobytes:
         # This is copied from Gunicorn's code for closing out a sync worker after reaching the max_requests limit
         worker.alive = False
 
-        memory_used_megabytes = round(memory_used_bytes / 1048576, 2)
-        # Logs from the worker seem to appear beside the normal app logs, but without log levels attached to them.
-        worker.log.info(f"Auto-restarting worker after gunicorn instance {os.getpid()} was"
-                        f"found to be using {memory_used_megabytes} megabytes.")
+        memory_used_megabytes = round(memory_used_kilobytes / 1024, 2)
+        # Logs from the worker appear beside the normal app logs, but without log levels attached to them.
+        worker.log.info(f"Restarting worker found to be using {memory_used_megabytes} megabytes (pid: {os.getpid()})")
