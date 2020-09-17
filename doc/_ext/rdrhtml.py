8629fbb4e817957338ddcf5f5f20d8dd4e820dcc
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2020-09-17 16:32:32 -0500

    documentation for deceased report api

--- doc/_ext/rdrhtml.py
+++ doc/_ext/rdrhtml.py
@@ -8,27 +8,21 @@ class RdrHtml5Translator(HTML5Translator):
     def __init__(self, *args):
         super(RdrHtml5Translator, self).__init__(*args)
 
-        self._is_visiting_class_desc_signature = False
-
-    @staticmethod
-    def _get_node_text(node):
-        return node.astext().strip()
+        self._is_visiting_complex_signature = False
 
     def visit_desc_signature(self, node: Element) -> None:
-        for child in node.children:
-            child_class_name = child.__class__.__name__
-            if child_class_name == 'desc_annotation':
-                annotation_node = child.children[0]
-                if self._get_node_text(annotation_node) == 'class':
-                    self._is_visiting_class_desc_signature = True
-                    node.children = []  # Remove the signature children so we don't print those out
+        # If we're visiting a signature that has more than two children, it's most likely a
+        # class or function signature. Leaving them out to simplify the look and feel of the documentation.
+        self._is_visiting_complex_signature = len(node.children) > 2
 
-        if not self._is_visiting_class_desc_signature:
-            self.body.append(self.starttag(node, 'dt'))
+        if not self._is_visiting_complex_signature:
+            super(RdrHtml5Translator, self).visit_desc_signature(node)
+        else:
+            node.children = []  # Remove the signature children so we don't print those out
 
     def depart_desc_signature(self, node: Element) -> None:
-        if self._is_visiting_class_desc_signature:
-            self._is_visiting_class_desc_signature = False
+        if self._is_visiting_complex_signature:
+            self._is_visiting_complex_signature = False
         else:
             super(RdrHtml5Translator, self).depart_desc_signature(node)
 
