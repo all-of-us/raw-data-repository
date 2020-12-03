c5802a254ad7b82d2444f986d53bbbea1dfe9b59
wangy70 <43476466+wangy70@users.noreply.github.com>
2020-12-03 12:16:03 -0600

    update project directory api (#2120)
    * add_total_number_to_project_directory_api
    
    * add_total_number_to_project_directory_api

--- tests/api_tests/test_workspace_audit_and_research_directory_api.py
+++ tests/api_tests/test_workspace_audit_and_research_directory_api.py
@@ -709,6 +709,8 @@ class ResearchProjectsDirectoryApiTest(BaseTestCase):
         self.assertEqual(len(result['data']), 2)
         # test search by project purpose
         result = self.send_get('researchHub/projectDirectory?projectPurpose=controlSet')
+        self.assertEqual(result['total_projects'], 2)
+        self.assertEqual(result['match_projects'], 1)
         self.assertEqual(len(result['data']), 1)
         # test search by multiple project purpose
         result = self.send_get('researchHub/projectDirectory?projectPurpose=controlSet,socialBehavioral')
@@ -721,6 +723,8 @@ class ResearchProjectsDirectoryApiTest(BaseTestCase):
         self.assertEqual(len(result['data']), 1)
         # test search by generalized parameter workspaceLike
         result = self.send_get('researchHub/projectDirectory?workspaceLike=str')
+        self.assertEqual(result['total_projects'], 2)
+        self.assertEqual(result['match_projects'], 2)
         self.assertEqual(len(result['data']), 2)
         result = self.send_get('researchHub/projectDirectory?workspaceLike=string2')
         self.assertEqual(len(result['data']), 1)
@@ -741,6 +745,8 @@ class ResearchProjectsDirectoryApiTest(BaseTestCase):
         self.assertEqual(len(result['data']), 1)
         # test search by user id
         result = self.send_get('researchHub/projectDirectory?userId=1&userRole=owner')
+        self.assertEqual(result['total_projects'], 2)
+        self.assertEqual(result['match_projects'], 1)
         self.assertEqual(len(result['data']), 1)
         result = self.send_get('researchHub/projectDirectory?userId=1&userRole=member')
         self.assertEqual(len(result['data']), 1)
@@ -748,12 +754,15 @@ class ResearchProjectsDirectoryApiTest(BaseTestCase):
         self.assertEqual(len(result['data']), 2)
         # test page and page size
         result = self.send_get('researchHub/projectDirectory?page=1&pageSize=1')
+        self.assertEqual(result['total_projects'], 2)
+        self.assertEqual(result['match_projects'], 2)
         self.assertEqual(len(result['data']), 1)
         result = self.send_get('researchHub/projectDirectory?page=2&pageSize=1')
         self.assertEqual(len(result['data']), 1)
         result = self.send_get('researchHub/projectDirectory?page=3&pageSize=1')
         self.assertEqual(len(result['data']), 0)
         result = self.send_get('researchHub/projectDirectory?page=1&pageSize=2')
+        self.assertEqual(result['match_projects'], 2)
         self.assertEqual(len(result['data']), 2)
 
     def test_get_research_projects_directory_less_than_23_hours(self):
