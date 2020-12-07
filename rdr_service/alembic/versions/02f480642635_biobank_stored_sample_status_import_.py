b6fb945cd138bf265e59d9a8a232972e26eb6603
j-kanuch <joshua.d.kanuch@vumc.org>
2020-12-07 13:57:35 -0600

    Clean up imports

--- rdr_service/alembic/versions/02f480642635_biobank_stored_sample_status_import_.py
+++ rdr_service/alembic/versions/02f480642635_biobank_stored_sample_status_import_.py
@@ -9,16 +9,6 @@ from alembic import op
 import sqlalchemy as sa
 import rdr_service.model.utils
 
-
-from rdr_service.participant_enums import PhysicalMeasurementsStatus, QuestionnaireStatus, OrderStatus
-from rdr_service.participant_enums import WithdrawalStatus, WithdrawalReason, SuspensionStatus, QuestionnaireDefinitionStatus
-from rdr_service.participant_enums import EnrollmentStatus, Race, SampleStatus, OrganizationType, BiobankOrderStatus
-from rdr_service.participant_enums import OrderShipmentTrackingStatus, OrderShipmentStatus
-from rdr_service.participant_enums import MetricSetType, MetricsKey, GenderIdentity
-from rdr_service.model.base import add_table_history_table, drop_table_history_table
-from rdr_service.model.code import CodeType
-from rdr_service.model.site_enums import SiteStatus, EnrollingStatus, DigitalSchedulingStatus, ObsoleteStatus
-
 # revision identifiers, used by Alembic.
 revision = '02f480642635'
 down_revision = 'cab7fdee2895'
