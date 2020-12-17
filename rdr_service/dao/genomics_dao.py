1551860b5afe6a2600c0532bd284f196c45f0b23
j-kanuch <joshua.d.kanuch@vumc.org>
2020-12-17 15:24:12 -0600

    Implemented specific cloud functions for aw1 and aw2 resources.

--- rdr_service/dao/genomics_dao.py
+++ rdr_service/dao/genomics_dao.py
@@ -33,7 +33,7 @@ from rdr_service.participant_enums import (
 from rdr_service.model.participant import Participant
 from rdr_service.model.participant_summary import ParticipantSummary
 from rdr_service.query import FieldFilter, Operator, OrderBy, Query
-from rdr_service.resource.generators.genomics import genomic_gc_validation_metrics_update, genomic_set_member_update, \
+from rdr_service.resource.generators.genomics import genomic_set_member_update, \
     genomic_manifest_feedback_update
 
 
