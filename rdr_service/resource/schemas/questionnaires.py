799c9f16ba3ce6caad7ca5ca02adb9681327e056
Peggy Bertsch <65624611+pegbertsch@users.noreply.github.com>
2021-01-25 08:25:50 -0600

    Fixes for BQ schema issues

--- rdr_service/resource/schemas/questionnaires.py
+++ rdr_service/resource/schemas/questionnaires.py
@@ -99,6 +99,7 @@ class _QuestionnaireSchema:
                              )
                  )
              ) qq2 on qq2.code_id = c.code_id
+             where c.system = :system
              order by c.code_id;
          """
         with dao.session() as session:
