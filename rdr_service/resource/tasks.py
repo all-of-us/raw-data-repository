b5182768431f9d59ca46aaac924ce38260a74213
Robert Abram <robert.m.abram.1@vumc.org>
2021-02-03 13:58:32 -0600

    Temporarily disable rebuilding resource participant records.

--- rdr_service/resource/tasks.py
+++ rdr_service/resource/tasks.py
@@ -11,8 +11,9 @@ from rdr_service.dao.bq_pdr_participant_summary_dao import BQPDRParticipantSumma
 from rdr_service.dao.bq_questionnaire_dao import BQPDRQuestionnaireResponseGenerator
 from rdr_service.model.bq_questionnaires import BQPDRConsentPII, BQPDRTheBasics, BQPDRLifestyle, BQPDROverallHealth, \
     BQPDREHRConsentPII, BQPDRDVEHRSharing, BQPDRCOPEMay, BQPDRCOPENov, BQPDRCOPEDec, BQPDRCOPEFeb
-from rdr_service.resource.generators import ParticipantSummaryGenerator
-from rdr_service.resource.generators.participant import rebuild_participant_summary_resource
+# Temporarily comment out to improve performance when rebuilding participants.
+# from rdr_service.resource.generators import ParticipantSummaryGenerator
+# from rdr_service.resource.generators.participant import rebuild_participant_summary_resource
 
 
 def batch_rebuild_participants_task(payload, project_id=None):
@@ -24,7 +25,8 @@ def batch_rebuild_participants_task(payload, project_id=None):
     :param payload: Dict object with list of participants to work on.
     """
     ps_bqgen = BQParticipantSummaryGenerator()
-    res_gen = ParticipantSummaryGenerator()
+    # Temporarily comment out to improve performance when rebuilding participants.
+    # res_gen = ParticipantSummaryGenerator()
 
     pdr_bqgen = BQPDRParticipantSummaryGenerator()
     mod_bqgen = BQPDRQuestionnaireResponseGenerator()
@@ -40,7 +42,8 @@ def batch_rebuild_participants_task(payload, project_id=None):
         patch_data = item['patch'] if 'patch' in item else None
         count += 1
 
-        rebuild_participant_summary_resource(p_id, res_gen=res_gen, patch_data=patch_data)
+        # Temporarily comment out to improve performance when rebuilding participants.
+        # rebuild_participant_summary_resource(p_id, res_gen=res_gen, patch_data=patch_data)
 
         ps_bqr = rebuild_bq_participant(p_id, ps_bqgen=ps_bqgen, pdr_bqgen=pdr_bqgen, patch_data=patch_data,
                                         project_id=project_id)
