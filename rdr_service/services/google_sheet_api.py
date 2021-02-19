b8fe19c0d76d205f005138512e7c77947d3895ea
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-02-19 12:38:18 -0600

    adding ability to read and modify multiple tabs in spreadsheet

--- rdr_service/services/google_sheet_api.py
+++ rdr_service/services/google_sheet_api.py
@@ -6,12 +6,21 @@ from rdr_service.services.gcp_utils import gcp_get_iam_service_key_info
 
 class GoogleSheetApi:
     """
-    Allows for interacting with a spreadsheet in google drive.
-    It is assumed that a service account (with a json keyfile) is being used,
-    and that service account has the correct permissions to edit the spreadsheet.
+    Allows for interacting with a spreadsheet in google drive. This class is designed to be used as a context manager
+    and requires that:
+        - A service account (with a json keyfile) is being used
+        - and the service account has the correct permissions to edit the google spreadsheet
+
+    Please carefully verify that this works for your purpose if you re-use this. There are some things that don't
+    currently work (such as formula manipulation and making new tabs).
     """
 
     def __init__(self, spreadsheet_id, service_key_id):
+        """
+        :param spreadsheet_id: Google Drive id of the spreadsheet file.
+        :param service_key_id: Key id for the service account used.
+        """
+
         # Load credentials from service key file
         service_key_info = gcp_get_iam_service_key_info(service_key_id)
         credentials = ServiceAccountCredentials.from_json_keyfile_name(service_key_info['key_path'])
@@ -21,7 +30,8 @@ class GoogleSheetApi:
         self._spreadsheet_id = spreadsheet_id
 
         # Initialize grid for storing values
-        self._values_grid = []
+        self._default_tab_id = None
+        self._tabs = {}
         self._empty_cell_value = ''
 
     def __enter__(self):
@@ -34,40 +44,69 @@ class GoogleSheetApi:
     def download_values(self):
         """
         Retrieve the values as they currently are in google drive.
-        Note: this will overwrite any changes that have been made this instance of the document using `change_cell`.
+        Note: this will overwrite any changes that have been made this instance of the document using `update_cell`.
 
         :return: None
         """
-
         # API call documented at https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/get
         request = self._service.spreadsheets().get(spreadsheetId=self._spreadsheet_id, includeGridData=True)
         response = request.execute()
 
         # Parse the retrieved spreadsheet
-        self._values_grid = []  # re-initialize our values
-        grid_row_data = response['sheets'][0]['data'][0]['rowData']
-        # todo: start working with sheets[1] and so on...
-        for row_number, row in enumerate(grid_row_data):
-            for col_number, cell in enumerate(row['values']):
-                self.change_cell(row_number, col_number, cell.get('formattedValue', self._empty_cell_value))
+        tab_data = response['sheets']
+        for tab in tab_data:
+            tab_grid_data = tab['data'][0]['rowData']
+            tab_id = tab['properties'].get('title')
 
-    def change_cell(self, row: int, col: int, value: str):
+            # Set the default tab to the first tab
+            if self._default_tab_id is None:
+                self._default_tab_id = tab_id
+
+            # Initialize the tab and parse the values from the response
+            self._tabs[tab_id] = self._initialize_empty_tab()
+            for row_number, row in enumerate(tab_grid_data):
+                row_values = row.get('values')
+                if row_values:
+                    for col_number, cell in enumerate(row_values):
+                        cell_value = cell.get('formattedValue', self._empty_cell_value)
+                        self.update_cell(row_number, col_number, cell_value, tab_id)
+
+    @classmethod
+    def _initialize_empty_tab(cls):
+        return []
+
+    def set_current_tab(self, tab_id):
+        """
+        Change the default tab.
+
+        :param tab_id: Name of the tab to use as the default.
+        :return: None
+        """
+        self._default_tab_id = tab_id
+
+    def update_cell(self, row: int, col: int, value: str, tab_id=None):
         """
         Change the value of a cell.
         Any changes made will be stored locally until the next call to `upload_values`.
 
-        :param row: number of the row the cell is in, starting from 0
-        :param col: number of the column the cell is in, starting from 0
-        :param value: value to store in the cell
+        :param row: row number of the cell, starting from 0 at the top of the spreadsheet
+        :param col: column number of the cell, starting from 0 at the left of the spreadsheet
+        :param value: value to store
+        :param tab_id: Name of the tab to modify. Will default to the current tab
+            (set by `set_current_tab`, or the first tab if none was set) if not provided.
         :return: None
         """
 
+        if tab_id is None:
+            tab_id = self._default_tab_id
+        values_grid = self._tabs.get(tab_id)
+
         # Increase the number of rows we have if the caller is setting a cell on a
         # row farther out than what is initialized
-        while row >= len(self._values_grid):
-            self._values_grid.append([self._empty_cell_value])
+        while row >= len(values_grid):
+            values_grid.append([self._empty_cell_value])
 
-        update_row = self._values_grid[row]
+        update_row = values_grid[row]
 
         # Increase the number of columns we have in the row if the caller is setting a
         # cell on a cell father out than what is initialized in the row
@@ -76,22 +115,37 @@ class GoogleSheetApi:
 
         update_row[col] = value
 
-        # todo: return whether the value in the cell was changed
-
     def upload_values(self):
         """
-        Upload the current data to the google drive spreadsheet.
+        Upload the local data to the google drive spreadsheet.
         Note: any changes made to the target spreadsheet since the last call to `download_values` will be overwritten.
 
         :return: None
         """
-        # API call documented at https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/update
-        request = self._service.spreadsheets().values().update(
+        request = self._service.spreadsheets().values().batchUpdate(
             spreadsheetId=self._spreadsheet_id,
-            range='A1',
-            valueInputOption='RAW',
             body={
-                'values': self._values_grid
+                'valueInputOption': 'RAW',
+                'data': [{
+                    'range': f"'{tab_title}'!A1",
+                    'values': tab_data
+                } for tab_title, tab_data in self._tabs.items()]
             }
         )
         request.execute()
+
+    def get_tab_values(self, tab_id=None):
+        """
+        Returns the values of the specified tab (or the current tab if no tab was specified).
+        Empty cells are represented by empty strings.
+
+        :param tab_id: Identifier of the tab to retrieve values from.
+        :return: A two dimensional list of strings that represent the cell values, organized by
+            rows (from the top down) and then columns (from left to right).
+        """
+
+        if tab_id is None:
+            tab_id = self._default_tab_id
+
+        value_grid = self._tabs.get(tab_id)
+        return [[value for value in row] for row in value_grid]
--- tests/service_tests/test_google_sheet_api.py
+++ tests/service_tests/test_google_sheet_api.py
@@ -25,90 +25,187 @@ class GoogleSheetsApiTest(BaseTestCase):
         mock_discovery = service_patcher.start()
         self.mock_spreadsheets_return = mock_discovery.build.return_value.spreadsheets.return_value
 
+        # We can't create tabs through code, so have the spreadsheet download two empty tabs by default
+        self.default_tab_names = ['one', 'two']
+        self.mock_spreadsheets_return.get.return_value.execute.return_value = {
+            'sheets': [{
+                'properties': {'title': tab_name},
+                'data': [{'rowData': [{'values': [self._mock_cell(None)]}]}]
+            } for tab_name in self.default_tab_names]
+        }
+
     def tearDown(self):
-        # Stop the patchers (in case other tests use them in different ways)
         for patcher in self.patchers:
             patcher.stop()
 
-    def test_empty_call_structure(self):
-        """
-        Test the integrity of an empty call to the google api
-        and make sure options other than cell values are as expected
-        """
-        expected_spreadsheet_id = '123ABC'
-        with GoogleSheetApi(expected_spreadsheet_id, 'service_key_id'):
-            pass
+    def test_spreadsheet_downloads_values_from_drive(self):
+        """Check that the spreadsheet initializes with the values currently in the spreadsheet on drive"""
 
-        self.mock_spreadsheets_return.values.return_value.update.assert_called_with(
-            spreadsheetId=expected_spreadsheet_id,
-            range='A1',
-            valueInputOption='RAW',
-            body={
-                'values': []
-            }
-        )
-
-    def _get_values_sent_to_spreadsheet(self):
-        return self.mock_spreadsheets_return.values.return_value.update.call_args.kwargs.get('body').get('values')
-
-    def test_value_arrays_sent_to_api(self):
-        """
-        Test that the values sent to the API construct a matrix that will fill in the google spreadsheet as expected
-        """
-
-        # This test will expect the resulting spreadsheet update to look like the grid below.
-        # Underscores represent a cell that will be blank.
-        # _ _ 7 _ 9
-        # _ 5
-        # 3
+        # Mock a spreadsheet with three tabs on google drive
+        # The test spreadsheet looks like this:
+        # Tab title: 'first_tab'
+        # _ _ '7' _ '9'
+        # _ '5'
+        # '3'
         # _
-        # _ _ 4
-        with GoogleSheetApi('', '') as spreadsheet:
-            spreadsheet.change_cell(2, 0, '3')  # 3 on third row
-            spreadsheet.change_cell(0, 4, '9')  # 9 on first row
-            spreadsheet.change_cell(4, 2, '4')  # 4 on last row
-            spreadsheet.change_cell(0, 2, '7')  # 7 on first row
-            spreadsheet.change_cell(1, 1, '5')  # 5 on second row
+        # _ _ '4'
+        # Tab title: 'another tab'
+        # _
+        # _ _ 'another tab for testing'
+        # Tab title: 'Final tab'
+        # 'test'
 
-        self.assertEqual([
-            ['', '', '7', '', '9'],
-            ['', '5'],
-            ['3'],
-            [''],
-            ['', '', '4']
-        ], self._get_values_sent_to_spreadsheet())
+        empty_cell = self._mock_cell(None)
+        first_tab_title = 'first_tab'
+        first_tab_data = {
+            'properties': {'title': first_tab_title},
+            'data': [{
+                'rowData': [
+                    {'values': [empty_cell, empty_cell, self._mock_cell('7'), empty_cell, self._mock_cell('9')]},
+                    {'values': [empty_cell, self._mock_cell('5')]},
+                    {'values': [self._mock_cell('3')]},
+                    {'values': [empty_cell]},
+                    {'values': [empty_cell, empty_cell, self._mock_cell('4')]}
+                ]
+            }]
+        }
+        second_tab_title = 'another tab'
+        second_tab_data = {
+            'properties': {'title': second_tab_title},
+            'data': [{
+                'rowData': [
+                    {'values': [empty_cell]},
+                    {'values': [empty_cell, empty_cell, self._mock_cell('another tab for testing')]}
+                ]
+            }]
+        }
+        third_tab_title = 'Final tab'
+        third_tab_data = {
+            'properties': {'title': third_tab_title},
+            'data': [{
+                'rowData': [
+                    {'values': [self._mock_cell('test')]}
+                ]
+            }]
+        }
+        self.mock_spreadsheets_return.get.return_value.execute.return_value = {
+            'sheets': [first_tab_data, second_tab_data, third_tab_data]
+        }
 
-    @classmethod
-    def _grid_value(cls, value):
-        cell_dict = {}
-        if value:
-            cell_dict['formattedValue'] = value
-
-        return cell_dict
-
-    def test_spreadsheet_loads_values_from_drive(self):
-        """Check that the spreadsheet initializes with the values currently in the spreadsheet on drive as expected"""
-        expected_values_grid = [
+        expected_first_tab_values = [
             ['', '', '7', '', '9'],
             ['', '5'],
             ['3'],
             [''],
             ['', '', '4']
         ]
-        empty_cell = self._grid_value(None)
-        self.mock_spreadsheets_return.get.return_value.execute.return_value = {
-            'sheets': [{
-                'data': [{
-                    'rowData': [
-                        {'values': [empty_cell, empty_cell, self._grid_value('7'), empty_cell, self._grid_value('9')]},
-                        {'values': [empty_cell, self._grid_value('5')]},
-                        {'values': [self._grid_value('3')]},
-                        {'values': [empty_cell]},
-                        {'values': [empty_cell, empty_cell, self._grid_value('4')]}
-                    ]
-                }]
-            }]
-        }
-
+        expected_second_tab_values = [
+            [''],
+            ['', '', 'another tab for testing']
+        ]
+        expected_third_tab_values = [
+            ['test']
+        ]
         with GoogleSheetApi('', '') as sheet:
-            self.assertEqual(expected_values_grid, sheet._values_grid)
+            self.assertEqual(expected_first_tab_values, sheet.get_tab_values())
+            self.assertEqual(expected_second_tab_values, sheet.get_tab_values(second_tab_title))
+            self.assertEqual(expected_third_tab_values, sheet.get_tab_values(third_tab_title))
+
+    def test_modifying_sheet_values(self):
+        # Construct a sheet that appears as follows (underscores represent a cell that will be blank):
+        # Tab title: first_tab
+        # _ _ 3
+        # 4
+        # Tab title: second_tab
+        # _
+        # _ 5
+        # 7
+        with GoogleSheetApi('', '') as sheet:
+            sheet.update_cell(0, 2, '3')  # Set 3 on first tab
+            sheet.update_cell(2, 0, '7', self.default_tab_names[1])  # Set 7 on second tab
+            sheet.set_current_tab(self.default_tab_names[1])
+            sheet.update_cell(1, 1, '5')  # Set 5 on second tab
+            sheet.update_cell(1, 0, '4', self.default_tab_names[0])  # Set 4 on first tab
+
+            # Check that the tab values are set as expected
+            self.assertEqual([
+                ['', '', '3'],
+                ['4']
+            ], sheet.get_tab_values(self.default_tab_names[0]))
+            self.assertEqual([
+                [''],
+                ['', '5'],
+                ['7']
+            ], sheet.get_tab_values())  # Sheet should still be default to the second tab
+
+    def test_tab_data_uploaded_to_api(self):
+        """
+        Test that the values sent to the API construct a matrix that will fill in the google spreadsheet as expected
+        """
+
+        # Construct a sheet that appears as follows (underscores represent a cell that will be blank):
+        # Tab title: first_tab
+        # _ _ 3
+        # 4
+        # Tab title: second_tab
+        # _
+        # _ 5
+        # 7
+        with GoogleSheetApi('', '') as sheet:
+            sheet.update_cell(0, 2, '3')  # Set 3 on first tab
+            sheet.update_cell(1, 0, '4')  # Set 4 on first tab
+            sheet.set_current_tab(self.default_tab_names[1])
+            sheet.update_cell(2, 0, '7')  # Set 7 on second tab
+            sheet.update_cell(1, 1, '5')  # Set 5 on second tab
+
+        tabs_uploaded = self.mock_spreadsheets_return.values.return_value.batchUpdate\
+            .call_args.kwargs.get('body').get('data')
+
+        first_uploaded_tab_data = tabs_uploaded[0]
+        self.assertEqual(f"'{self.default_tab_names[0]}'!A1", first_uploaded_tab_data['range'])
+        self.assertEqual([
+            ['', '', '3'],
+            ['4']
+        ], first_uploaded_tab_data['values'])
+
+        second_uploaded_tab_data = tabs_uploaded[1]
+        self.assertEqual(f"'{self.default_tab_names[1]}'!A1", second_uploaded_tab_data['range'])
+        self.assertEqual([
+            [''],
+            ['', '5'],
+            ['7']
+        ], second_uploaded_tab_data['values'])
+
+    @classmethod
+    def _mock_cell(cls, value):
+        cell_dict = {}
+        if value:
+            cell_dict['formattedValue'] = value
+
+        return cell_dict
+
+    def test_values_are_only_mutable_through_object_interface(self):
+        """
+        To help keep future designs clean, we want to make sure that the internal structure of values for the sheet
+        can only be modified by the sheet (and not through the data structure given when getting values for a tab).
+        """
+
+        # Set up a sheet with some values. The resulting sheet should appear as follows
+        # 1
+        # _
+        # _ _ _ 8
+        with GoogleSheetApi('', '') as sheet:
+            sheet.update_cell(0, 0, '1')
+            sheet.update_cell(2, 3, '8')
+
+            # Get the value structure returned from the sheet and modify it
+            values = sheet.get_tab_values()
+            del values[1]  # remove the empty row
+            values[0][0] = '3'  # Change the 1 to a 3
+
+            # Check that the sheet values remain unchanged
+            self.assertEqual([
+                ['1'],
+                [''],
+                ['', '', '', '8']
+            ], sheet.get_tab_values())
