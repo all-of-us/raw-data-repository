3b60855844396a88dee7690ab0c2a9992206181a
Josh Kanuch <54945800+j-kanuch@users.noreply.github.com>
2021-03-18 08:46:41 -0500

    Remove log message when skipping file (#2309)

--- rdr_service/gcloud_functions/genomic_aw5_baylor_function/main.py
+++ rdr_service/gcloud_functions/genomic_aw5_baylor_function/main.py
@@ -34,7 +34,6 @@ class GenomicManifestGenericFunction(FunctionStoragePubSubHandler):
         """ Handle storage object created event. """
         # Verify this is a file that we want to process.
         if 'aw5' not in self.event.name.lower():
-            _logger.info(f'Skipping file {self.event.name}, name does not match Data Manifest file.')
             return
 
         _logger.info(f"file found: {self.event.name}")
