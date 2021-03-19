1a71f1cb51926e1288d861ae593b3dd3884ab4af
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-03-19 13:37:12 -0400

    init file for slack handler

---
+++ rdr_service/services/slack_utils.py
@@ -0,0 +1,11 @@
+
+class SlackMessageHandler:
+    """
+    Create Slack Messages
+    """
+    def __init__(self, *, webhook_url):
+        self.webhook_url = webhook_url
+
+    def send_message(self):
+        pass
+
