60bca9970488723b560e460faa2a6fd8a6e202eb
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-03-31 16:45:46 -0500

    trying with no-cache

--- ci/setup_python_env.sh
+++ ci/setup_python_env.sh
@@ -6,5 +6,5 @@ python3 -m venv venv
 source venv/bin/activate
 export PYTHONPATH=`pwd`
 echo "PYTHONPATH=${PYTHONPATH}"
-pip install --exists-action=w -r requirements.txt
+pip install --exists-action=w --no-cache-dir -r requirements.txt
 pip install safety
