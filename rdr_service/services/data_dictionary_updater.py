db6631d2d119dd68633370be6fed272578ec9c43
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-03-31 12:01:39 -0500

    pr cleanup

--- rdr_service/services/data_dictionary_updater.py
+++ rdr_service/services/data_dictionary_updater.py
@@ -473,6 +473,7 @@ class DataDictionaryUpdater:
         )
 
     def run_update(self):
+        """This will run the complete update, finding differences and uploading the new values to the data-dictionary"""
         with self._build_sheet() as sheet:
             self._sheet = sheet
             self._modify_sheet()
