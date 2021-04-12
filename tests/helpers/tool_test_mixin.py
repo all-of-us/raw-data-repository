27105eb98f76e89a7a1776eb1a10e081ae0acf77
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-04-12 13:56:46 -0500

    simplifying tool test code

--- tests/helpers/tool_test_mixin.py
+++ tests/helpers/tool_test_mixin.py
@@ -1,28 +1,15 @@
 import mock
-import json
-import os
 from typing import Type
 
-import rdr_service
 from rdr_service.tools.tool_libs.tool_base import ToolBase
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(rdr_service.__file__))
 
 
 class ToolTestMixin:
 
     @staticmethod
-    def _build_env(server_config):
-        if server_config is None:
-            server_config = {}
-
-        def construct_server_config_response(*_):
-            return json.dumps(server_config), 'test_server_config_file_name'
-
+    def _build_env():
         gcp_env = mock.MagicMock()
         gcp_env.project = 'localhost'
-        gcp_env.git_project = PROJECT_ROOT
-        gcp_env.get_latest_config_from_bucket = construct_server_config_response
 
         return gcp_env
 
@@ -37,11 +24,14 @@ class ToolTestMixin:
 
     @classmethod
     def run_tool(cls, tool_class: Type[ToolBase], tool_args: dict = None, server_config: dict = None):
-        gcp_env = ToolTestMixin._build_env(server_config)
+        gcp_env = ToolTestMixin._build_env()
         tool_args = ToolTestMixin._build_args(tool_args)
 
-        with mock.patch.object(ToolBase, 'initialize_process_context') as mock_init_env:
+        with mock.patch.object(ToolBase, 'initialize_process_context') as mock_init_env,\
+             mock.patch('rdr_service.services.config_client.ConfigClient.get_server_config') as mock_server_config:
+
             mock_init_env.return_value.__enter__.return_value = gcp_env
+            mock_server_config.return_value = server_config or {}
 
             tool_instance = tool_class(tool_args, gcp_env)
             return tool_instance.run_process()
