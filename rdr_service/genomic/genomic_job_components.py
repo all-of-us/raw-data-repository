339692d563552c2c2f7e8179535de54a975f7aa1
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-04-20 10:18:33 -0400

    check for result first

--- rdr_service/genomic/genomic_job_components.py
+++ rdr_service/genomic/genomic_job_components.py
@@ -207,12 +207,13 @@ class GenomicFileIngester:
                         self.file_queue[0])
                     file_ingested = self.file_queue.popleft()
                     results.append(ingestion_result == GenomicSubProcessResult.SUCCESS)
-                    ingestion_message = f'Ingestion attempt for {file_ingested.fileName}: {ingestion_result}'
 
-                    if 'invalid' in ingestion_result.name.lower():
-                        logging.warning(ingestion_message)
-                    else:
-                        logging.info(ingestion_message)
+                    if ingestion_result:
+                        ingestion_message = f'Ingestion attempt for {file_ingested.fileName}: {ingestion_result}'
+                        if 'invalid' in ingestion_result.name.lower():
+                            logging.warning(ingestion_message)
+                        else:
+                            logging.info(ingestion_message)
 
                     self.file_processed_dao.update_file_record(
                         file_ingested.id,
