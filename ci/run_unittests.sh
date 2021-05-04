36f246b658efdbc9085823f1f468df235ff1f5d1
kenny-skaggs <64044838+kenny-skaggs@users.noreply.github.com>
2021-05-04 09:27:53 -0500

    Adding code coverage reports to Codacy (#2423)
    * sending test coverage reports to codacy
    
    * fixes for coverage reporting

--- ci/run_unittests.sh
+++ ci/run_unittests.sh
@@ -3,4 +3,6 @@
 #
 source venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:`pwd`
-UNITTEST_FLAG=1 python -m unittest discover -v -s tests
+UNITTEST_FLAG=1 coverage run -m unittest discover -v -s tests
+coverage xml -o report.xml
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r report.xml
