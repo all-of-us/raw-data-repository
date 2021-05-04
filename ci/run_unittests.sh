6e557ff9afb25a97efd182a0051483dc36473281
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-05-04 08:11:53 -0500

    fixes for coverage reporting

--- ci/run_unittests.sh
+++ ci/run_unittests.sh
@@ -3,6 +3,6 @@
 #
 source venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:`pwd`
-UNITTEST_FLAG=1 coverage run -m unittest
+UNITTEST_FLAG=1 coverage run -m unittest discover -v -s tests
 coverage xml -o report.xml
-bash <(curl -Ls https://coverage.codacy.com/com.get.sh) report -r report.xml
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -r report.xml
