466aa75d0e98328020e7ce8911bc27a1fd596e47
kenny-skaggs <64044838+kenny-skaggs@users.noreply.github.com>
2021-05-05 12:44:31 -0500

    [DA-1972] Adding checks for deceased reports, patient status, and questionnaires (#2414)
    * adding checks for deceased reports, patient status, and questionnaires
    
    * fixing test

--- rdr_service/services/data_quality.py
+++ rdr_service/services/data_quality.py
@@ -1,17 +1,20 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 import logging
-from sqlalchemy import func
+from sqlalchemy import func, or_
 from typing import List, Type
 
+from rdr_service.model.deceased_report import DeceasedReport
 from rdr_service.model.participant import Participant
+from rdr_service.model.patient_status import PatientStatus
+from rdr_service.model.questionnaire import Questionnaire, QuestionnaireQuestion
 from rdr_service.model.questionnaire_response import QuestionnaireResponse, QuestionnaireResponseAnswer
 
 
-class ModelQualityChecker(ABC):
+class _ModelQualityChecker(ABC):
 
     def __init__(self, session):
-        super(ModelQualityChecker, self).__init__()
+        super(_ModelQualityChecker, self).__init__()
         self.session = session
 
     @abstractmethod
@@ -19,7 +22,88 @@ class ModelQualityChecker(ABC):
         ...
 
 
-class ResponseQualityChecker(ModelQualityChecker):
+class DeceasedReportQualityChecker(_ModelQualityChecker):
+    def run_data_quality_checks(self, for_data_since: datetime = None):
+        has_future_authored_date_expression = DeceasedReport.authored > DeceasedReport.created
+        has_future_date_of_death_expression = DeceasedReport.dateOfDeath > DeceasedReport.authored
+        has_authored_before_signup_expression = DeceasedReport.authored < Participant.signUpTime
+        query = (
+            self.session.query(
+                DeceasedReport.id,
+                has_future_authored_date_expression,
+                has_future_date_of_death_expression,
+                has_authored_before_signup_expression
+            ).join(Participant)
+            .filter(or_(
+                has_future_authored_date_expression,
+                has_future_date_of_death_expression,
+                has_authored_before_signup_expression
+            ))
+        )
+        if for_data_since is not None:
+            query = query.filter(DeceasedReport.created >= for_data_since)
+
+        for _id, has_future_authored_date, has_future_date_of_death, has_authored_before_signup in query.all():
+            issue_messages = []
+            if has_future_authored_date:
+                issue_messages.append('was authored with a future date')
+            if has_authored_before_signup:
+                issue_messages.append('was authored before participant signup')
+            if has_future_date_of_death:
+                issue_messages.append('has an effective date after the authored date')
+
+            logging.warning(f'Issues found with DeceasedReport {_id}: {", ".join(issue_messages)}')
+
+
+class PatientStatusQualityChecker(_ModelQualityChecker):
+    def run_data_quality_checks(self, for_data_since: datetime = None):
+        has_future_authored_date_expression = PatientStatus.authored > PatientStatus.created
+        has_authored_before_signup_expression = PatientStatus.authored < Participant.signUpTime
+        query = (
+            self.session.query(
+                PatientStatus.id,
+                has_future_authored_date_expression,
+                has_authored_before_signup_expression
+            ).join(Participant, Participant.participantId == PatientStatus.participantId)
+            .filter(
+                or_(
+                    has_future_authored_date_expression,
+                    has_authored_before_signup_expression
+                )
+            )
+        )
+        if for_data_since is not None:
+            query = query.filter(PatientStatus.created >= for_data_since)
+
+        for _id, has_future_authored_date, has_authored_date_before_signup in query.all():
+            if has_future_authored_date:
+                logging.warning(f'PatientStatus {_id} was authored with a future date')
+            elif has_authored_date_before_signup:
+                logging.warning(f'PatientStatus {_id} was authored before the participant signed up')
+
+
+class QuestionnaireQualityChecker(_ModelQualityChecker):
+    def run_data_quality_checks(self, for_data_since: datetime = None):
+        query = (
+            self.session.query(
+                Questionnaire.questionnaireId,
+                Questionnaire.version
+            ).select_from(Questionnaire)
+            .outerjoin(
+                QuestionnaireQuestion, (
+                    QuestionnaireQuestion.questionnaireId == Questionnaire.questionnaireId
+                    and QuestionnaireQuestion.questionnaireVersion == Questionnaire.version
+                )
+            ).filter(QuestionnaireQuestion.questionnaireQuestionId.is_(None))
+        )
+        if for_data_since is not None:
+            query = query.filter(Questionnaire.created >= for_data_since)
+
+        for _id, version in query.all():
+            logging.warning(f'Questionnaire with id {_id} and version {version} was found with no questions.')
+
+
+class ResponseQualityChecker(_ModelQualityChecker):
     def run_data_quality_checks(self, for_data_since: datetime = None):
         query = (
             self.session.query(
@@ -50,10 +134,13 @@ class ResponseQualityChecker(ModelQualityChecker):
                 logging.warning(f'Response {response_id} has no answers')
 
 
-class DataQualityChecker(ModelQualityChecker):
+class DataQualityChecker(_ModelQualityChecker):
     """Acts as a facade and runs a registered collection of data quality checks"""
 
-    _registered_checkers: List[Type[ModelQualityChecker]] = [
+    _registered_checkers: List[Type[_ModelQualityChecker]] = [
+        DeceasedReportQualityChecker,
+        PatientStatusQualityChecker,
+        QuestionnaireQualityChecker,
         ResponseQualityChecker
     ]
 
