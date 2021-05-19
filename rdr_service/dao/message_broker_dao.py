83917583648c9615ccb41cd1eb4627d0de21a926
wangy70 <yu.wang.3@vumc.org>
2021-05-19 16:26:01 -0500

    unsed variable

---
+++ rdr_service/dao/message_broker_dao.py
@@ -0,0 +1,94 @@
+from rdr_service import clock
+from dateutil.parser import parse
+from werkzeug.exceptions import BadRequest
+from rdr_service.dao.base_dao import BaseDao
+from rdr_service.dao.participant_dao import ParticipantDao
+from rdr_service.model.message_broker import MessageBrokerRecord, MessageBrokerMetadata
+
+
+class MessageBrokerDao(BaseDao):
+    def __init__(self):
+        self.message_metadata_dao = MessageBrokerMetadataDao()
+        super(MessageBrokerDao, self).__init__(MessageBrokerRecord)
+
+    def from_client_json(self, resource_json, client_id=None):
+        self._validate(resource_json)
+        participant_id = int(resource_json.get('participantId'))
+        dest_name = self._get_message_dest_name(participant_id)
+        message = MessageBrokerRecord(
+            participantId=participant_id,
+            eventType=resource_json.get('event'),
+            messageOrigin=client_id,
+            messageDest=dest_name,
+            eventAuthoredTime=parse(resource_json.get('eventAuthoredTime')),
+            requestTime=clock.CLOCK.now(),
+            requestBody=resource_json.get('messageBody'),
+            requestResource=resource_json
+        )
+
+        return message
+
+    def _validate(self, resource_json):
+        not_null_fields = ['event', 'eventAuthoredTime', 'participantId', 'messageBody']
+        for field_name in not_null_fields:
+            if resource_json.get(field_name) is None:
+                raise BadRequest(f'{field_name} can not be NULL')
+
+        if not resource_json.get('participantId').isnumeric():
+            raise BadRequest('Invalid participant ID')
+
+    def _get_message_dest_name(self, participant_id):
+        p_dao = ParticipantDao()
+        participant = p_dao.get(participant_id)
+        if not participant:
+            raise BadRequest(f'Participant not found with id {participant_id}')
+
+        return participant.participantOrigin
+
+    def _get_message_dest_url(self, event, dest):
+        dest_url = self.message_metadata_dao.get_dest_url(event, dest)
+        return dest_url
+
+    def insert_with_session(self, session, message):
+        response_code, response_body, response_error = self._send_message_to_dest(message)
+        message.responseCode = response_code
+        message.responseBody = response_body
+        message.responseError = response_error
+        message.responseTime = clock.CLOCK.now()
+        super(MessageBrokerDao, self).insert_with_session(session, message)
+
+        return message
+
+    def to_client_json(self, message):
+        response_json = {
+            "event": message.eventType,
+            "participantId": message.participantId,
+            "responseCode": message.responseCode,
+            "responseBody": message.responseBody,
+            "errorMessage": message.responseError
+        }
+        return response_json
+
+    def _send_message_to_dest(self, message):
+        dest_url = self._get_message_dest_url(message.eventType, message.messageDest)  # pylint: disable=unused-variable
+        request_body = message.requestBody  # pylint: disable=unused-variable
+
+        # PTSC test env is not ready for this implementation
+        # TODO - get access to dest endpoint and sent message to dest
+        # mock response from PTSC
+
+        response_code = '200'
+        response_body = {'result': 'mocked result'}
+        response_error = ''
+        return response_code, response_body, response_error
+
+
+class MessageBrokerMetadataDao(BaseDao):
+    def __init__(self):
+        super(MessageBrokerMetadataDao, self).__init__(MessageBrokerMetadata)
+
+    def get_dest_url(self, event, dest):
+        with self.session() as session:
+            query = session.query(MessageBrokerMetadata).filter(MessageBrokerMetadata.eventType == event,
+                                                                MessageBrokerMetadata.destination == dest)
+            return query.first()
--- rdr_service/model/message_broker.py
+++ rdr_service/model/message_broker.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, JSON, event
+from sqlalchemy import Column, ForeignKey, Integer, String, JSON, event, UniqueConstraint
 
 from rdr_service.model.base import Base, model_insert_listener, model_update_listener
 from rdr_service.model.utils import UTCDateTime6
 
 
 class MessageBrokerRecord(Base):
+    __tablename__ = "message_broker_record"
+
     id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
     """Auto increment, primary key."""
     created = Column("created", UTCDateTime6, nullable=True)
@@ -22,8 +24,8 @@ class MessageBrokerRecord(Base):
     """indicate where this message is sent from"""
     messageDest = Column("message_dest", String(80))
     """indicate where this message will be sent to"""
-    requestResource = Column("request_resource", JSON, nullable=True)
-    """Original resource value; whole payload request that was sent from the requester"""
+    requestBody = Column("request_body", JSON)
+    """http request body send to destination"""
     responseCode = Column("response_code", String(20))
     """http response code returned from the destination"""
     responseBody = Column("response_body", JSON, nullable=True)
@@ -34,9 +36,13 @@ class MessageBrokerRecord(Base):
     """The time at which RDR received the request from requester"""
     responseTime = Column("response_time", UTCDateTime6)
     """The time at which RDR received the response from the destination"""
+    requestResource = Column("request_resource", JSON, nullable=True)
+    """Original resource value; whole payload request that was sent from the requester"""
 
 
 class MessageBrokerMetadata(Base):
+    __tablename__ = "message_broker_metadata"
+
     id = Column("id", Integer, primary_key=True, autoincrement=True, nullable=False)
     """Auto increment, primary key."""
     created = Column("created", UTCDateTime6, nullable=True)
@@ -48,8 +54,10 @@ class MessageBrokerMetadata(Base):
     """message event type"""
     destination = Column("destination", String(80))
     """message destination, decided by participant origin"""
-    endpoint = Column("endpoint", String(512))
+    url = Column("url", String(512))
     """message destination endpoint"""
+
+    __table_args__ = (UniqueConstraint('event_type', 'destination', 'url', name='unique_message_target'),)
 
 
 event.listen(MessageBrokerRecord, "before_insert", model_insert_listener)
