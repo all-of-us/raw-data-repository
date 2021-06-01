e38fcab8615df52227afdef40c4ace74a732887b
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-06-01 15:28:06 -0400

    edit migration

--- rdr_service/alembic/versions/e4eedba5965f_.py
+++ rdr_service/alembic/versions/e4eedba5965f_.py
@@ -8,16 +8,7 @@ Create Date: 2021-06-01 15:17:01.968058
 from alembic import op
 import sqlalchemy as sa
 import rdr_service.model.utils
-
-
-from rdr_service.participant_enums import PhysicalMeasurementsStatus, QuestionnaireStatus, OrderStatus
-from rdr_service.participant_enums import WithdrawalStatus, WithdrawalReason, SuspensionStatus, QuestionnaireDefinitionStatus
-from rdr_service.participant_enums import EnrollmentStatus, Race, SampleStatus, OrganizationType, BiobankOrderStatus
-from rdr_service.participant_enums import OrderShipmentTrackingStatus, OrderShipmentStatus
-from rdr_service.participant_enums import MetricSetType, MetricsKey, GenderIdentity
-from rdr_service.model.base import add_table_history_table, drop_table_history_table
-from rdr_service.model.code import CodeType
-from rdr_service.model.site_enums import SiteStatus, EnrollingStatus, DigitalSchedulingStatus, ObsoleteStatus
+from rdr_service.genomic_enums import GenomicReportState
 
 # revision identifiers, used by Alembic.
 revision = 'e4eedba5965f'
@@ -32,7 +23,6 @@ def upgrade(engine_name):
 
 def downgrade(engine_name):
     globals()["downgrade_%s" % engine_name]()
-
 
 
 def upgrade_rdr():
