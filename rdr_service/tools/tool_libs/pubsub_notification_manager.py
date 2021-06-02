1566cdfcb262fd232200108f6a0a6e3667b66db5
j-kanuch <joshua.d.kanuch@vumc.org>
2021-06-02 15:12:55 -0500

    Implemented delete command

--- rdr_service/tools/tool_libs/pubsub_notification_manager.py
+++ rdr_service/tools/tool_libs/pubsub_notification_manager.py
@@ -3,7 +3,9 @@ import logging
 import os
 import sys
 
+from pprint import pprint
 from google.cloud import storage
+from google.api_core.exceptions import NotFound
 
 from rdr_service.config import PUBSUB_NOTIFICATION_BUCKETS_PROD, PUBSUB_NOTIFICATION_BUCKETS_SANDBOX
 from rdr_service.tools.tool_libs.tool_base import cli_run, ToolBase
@@ -14,12 +16,14 @@ tool_desc = 'Manage GCloud Pub/Sub Notifications'
 _logger = logging.getLogger("rdr_logger")
 
 CONFIG_ROOT = os.path.join(os.path.dirname(__file__), '../../config')
+SUPPORTED_PROJECT_CONFIGS = {
+    'all-of-us-rdr-prod': 'pub_sub_config_prod.json',
+    'all-of-us-rdr-sandbox': 'pub_sub_config_sandbox.json'
+}
 
 
 class PubSubNotificationManager(ToolBase):
     def run(self):
-        #super(PubSubNotificationManager, self).run()
-
         if self.args.command == "list":
             return self.command_list()
 
@@ -86,8 +90,7 @@ class PubSubNotificationManager(ToolBase):
         in the supplied --config-file for configurations
         where 'id' key has a value of null
         """
-
-        config_path = os.path.join(CONFIG_ROOT, self.args.config_file)
+        config_path = self.get_config_path()
 
         if not os.path.exists(config_path):
             _logger.error(f'File {config_path} was not found.')
@@ -106,17 +109,49 @@ class PubSubNotificationManager(ToolBase):
 
             new_notification['id'] = notification.notification_id
 
-        # replace existing json file with updated notification
-        with open(config_path, "w") as f:
-            f.write(json.dumps(config_data, indent=2))
+            # Output created notification config data
+            _logger.info('Notification created:')
+            pprint(new_notification)
+
+        # replace existing json file with updated notification config
+        self.update_notification_config(config_data, config_path)
 
         return 0
 
     def command_delete(self):
         """
-        Delete the Pub/Sub notification based on the JSON
-        in the supplied --config-file
+        Delete the Pub/Sub notification with --id and --bucket
         """
+        if self.gcp_env.project not in SUPPORTED_PROJECT_CONFIGS.keys():
+            _logger.error(f'Project config not supported {self.gcp_env.project}')
+            return 1
+
+        if not self.args.bucket and not self.args.id:
+            _logger.error("--bucket and --id required for delete.")
+            return 1
+
+        # Get notification
+        client = storage.Client()
+        bucket = client.get_bucket(self.args.bucket)
+        target = bucket.get_notification(self.args.id, client)
+
+        if self.gcp_env.project != target.topic_project:
+            _logger.error("Notification project and specified project do not match.")
+            return 1
+
+        # Delete the notification
+        try:
+            target.delete(client=client)
+
+        except NotFound:
+            _logger.error(f"Notification ID {self.args.id} not found.")
+            return 1
+
+        _logger.info(f"Notification id {self.args.id} has been deleted.")
+
+        _logger.info("Removing notification from config...")
+        self.delete_notification_from_config(self.args.id)
+
         return 0
 
     def create_notification(self, new_notification: dict):
@@ -146,6 +181,38 @@ class PubSubNotificationManager(ToolBase):
 
         notification.create(client=client)
         return notification
+
+    def get_config_path(self):
+        if self.args.config_file:
+            config_name = self.args.config_file
+        else:
+            config_name = SUPPORTED_PROJECT_CONFIGS[self.gcp_env.project]
+
+        return os.path.join(CONFIG_ROOT, config_name)
+
+    @staticmethod
+    def update_notification_config(data, config_path):
+        with open(config_path, "w") as f:
+            f.write(json.dumps(data, indent=2))
+
+    def delete_notification_from_config(self):
+        config_path = self.get_config_path()
+
+        with open(config_path, "r") as f:
+            config_data = json.load(f)
+
+        notification_iter = filter(
+            lambda x: x['id'] == str(self.args.id),
+            config_data['notifications']
+        )
+
+        for notification in notification_iter:
+            config_data['notifications'].remove(notification)
+
+        with open(config_path, "w") as f:
+            f.write(json.dumps(config_data, indent=2))
+
+        _logger.info("Notification removed from config.")
 
 
 def add_additional_arguments(parser):
