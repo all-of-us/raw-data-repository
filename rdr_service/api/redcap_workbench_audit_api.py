6c659e016434c751362f4e547a3bf8fc92dc1d87
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-06-07 15:01:07 -0400

    new api route for reseachers, new base class for red cap api

--- rdr_service/api/redcap_workbench_audit_api.py
+++ rdr_service/api/redcap_workbench_audit_api.py
@@ -3,40 +3,17 @@ from rdr_service.api.base_api import BaseApi
 from werkzeug.exceptions import BadRequest
 from rdr_service.api_util import REDCAP
 from rdr_service.app_util import auth_required
-from rdr_service.dao.workbench_dao import WorkbenchWorkspaceDao, WorkbenchWorkspaceAuditDao
+from rdr_service.dao.workbench_dao import WorkbenchResearcherDao, WorkbenchWorkspaceAuditDao
 
 
-class RedcapWorkbenchAuditApi(BaseApi):
-    def __init__(self):
-        super().__init__(WorkbenchWorkspaceAuditDao())
-        self.workspace_dao = WorkbenchWorkspaceDao()
+class BaseRedcapApi(BaseApi):
 
     @auth_required(REDCAP)
     def post(self):
         return super().post()
 
-    @auth_required(REDCAP)
-    def get(self):
-        params = {
-            'last_snapshot_id': request.args.get('last_snapshot_id'),
-            'snapshot_id': request.args.get('snapshot_id'),
-            'workspace_id': request.args.get('workspace_id')
-        }
-
-        filters = self.validate_params(params)
-        results = self.get_filtered_results(**filters)
-
-        return results
-
-    def get_filtered_results(self, **filters):
-        """Queries DB, returns results in format consumed by front-end
-        :param filters: query parameters for filtering the data
-        :return: Filtered results
-        """
-
-        return self.workspace_dao.get_redcap_audit_workspaces(**filters)
-
-    def validate_params(self, params):
+    @staticmethod
+    def validate_params(params):
         filters = {}
         for key in params:
             try:
@@ -45,3 +22,49 @@ class RedcapWorkbenchAuditApi(BaseApi):
                 raise BadRequest(f"Invalid parameter {key}: {params[key]}")
 
         return filters
+
+    @staticmethod
+    def get_filtered_results(dao_method, **filters):
+        """Queries DB, returns results in format consumed by front-end
+        :param filters: query parameters for filtering the data
+        :param dao_method: method on dao for returning results from query
+        :return: Filtered results
+        """
+        return dao_method(**filters)
+
+
+class RedcapWorkbenchAuditApi(BaseRedcapApi):
+    def __init__(self):
+        super().__init__(WorkbenchWorkspaceAuditDao())
+
+    @auth_required(REDCAP)
+    def get(self):
+        params = {
+            'last_snapshot_id': request.args.get('last_snapshot_id'),
+            'snapshot_id': request.args.get('snapshot_id'),
+            'workspace_id': request.args.get('workspace_id')
+        }
+        filters = self.validate_params(params)
+        method = self.dao.workspace_dao.get_redcap_audit_workspaces
+        results = self.get_filtered_results(method, **filters)
+
+        return results
+
+
+class RedcapResearcherAuditApi(BaseRedcapApi):
+    def __init__(self):
+        super().__init__(WorkbenchResearcherDao())
+
+    @auth_required(REDCAP)
+    def get(self):
+        params = {
+            'last_snapshot_id': request.args.get('last_snapshot_id'),
+            'snapshot_id': request.args.get('snapshot_id'),
+            'user_source_id': request.args.get('user_source_id')
+        }
+
+        filters = self.validate_params(params)
+        method = self.workspace_dao.get_redcap_audit_workspaces
+        results = self.get_filtered_results(method, **filters)
+
+        return results
