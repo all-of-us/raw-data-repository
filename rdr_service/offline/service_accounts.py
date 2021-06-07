be93be7077c1a001f521821510dacd6516a014b6
kenny-skaggs <64044838+kenny-skaggs@users.noreply.github.com>
2021-06-07 14:07:46 -0500

    refactoring code for expiring service account keys (#2478)
    * refactoring code for expiring service account keys
    
    * unit tests

---
+++ rdr_service/offline/service_accounts.py
@@ -0,0 +1,95 @@
+from dataclasses import dataclass
+from datetime import datetime
+from googleapiclient import discovery
+import logging
+from typing import List
+
+from rdr_service import config
+from rdr_service.config import GAE_PROJECT
+
+
+@dataclass
+class ServiceAccount:
+    email: str
+
+
+@dataclass
+class ServiceAccountKey:
+    name: str
+    start_date: datetime
+
+    def get_key_age_in_days(self):
+        return (datetime.utcnow() - self.start_date).days
+
+
+class ServiceAccountKeyManager:
+    def __init__(self):
+        self._app_id = GAE_PROJECT
+        self._google_service = discovery.build("iam", "v1", cache_discovery=False)
+        self._max_age_in_days = config.getSetting(config.DAYS_TO_DELETE_KEYS)
+        self._service_accounts_with_long_lived_keys = config.getSettingList(
+            config.SERVICE_ACCOUNTS_WITH_LONG_LIVED_KEYS, default=[]
+        )
+
+    def expire_old_keys(self):
+        """Deletes service account keys older than 3 days as required by NIH"""
+
+        if self._app_id is None:
+            raise Exception('Unable to determine current project')
+
+        self._expire_keys_for_project(
+            project_name=self._app_id,
+            ignore_service_account_func=lambda account: account.email in self._service_accounts_with_long_lived_keys
+        )
+
+    def _expire_keys_for_project(self, project_name, ignore_service_account_func):
+        for service_account in self._get_service_accounts_for_project(project_name):
+            if ignore_service_account_func(service_account):
+                logging.info("Skip key expiration check for Service Account {}".format(service_account.email))
+            else:
+                for key in self._get_keys_for_account(
+                    project_name=project_name,
+                    service_account_name=service_account.email
+                ):
+                    key_age_days = key.get_key_age_in_days()
+                    if key_age_days >= self._max_age_in_days:
+                        logging.warning(
+                            f"Deleting service account key older than {self._max_age_in_days} "
+                            f"[{key_age_days}]: {key.name}"
+                        )
+                        self._delete_key(key)
+                    else:
+                        logging.info(f'Service Account key is {key_age_days} days old: {key.name}')
+
+    def _get_service_accounts_for_project(self, project_name) -> List[ServiceAccount]:
+        account_list_request = self._google_service.projects().serviceAccounts().list(name=f'projects/{project_name}')
+        account_list_response = account_list_request.execute()
+        service_accounts = [
+            ServiceAccount(
+                email=account['email']
+            )
+            for account in account_list_response.get('accounts', [])
+        ]
+
+        if not service_accounts:
+            logging.info(f'No Service Accounts found in project "{project_name}"')
+
+        return service_accounts
+
+    def _get_keys_for_account(self, project_name, service_account_name) -> List[ServiceAccountKey]:
+        key_list_request = self._google_service.projects().serviceAccounts().keys().list(
+            name=f'projects/{project_name}/serviceAccounts/{service_account_name}',
+            keyTypes='USER_MANAGED'
+        )
+        key_list_response = key_list_request.execute()
+        return [
+            ServiceAccountKey(
+                name=key['name'],
+                start_date=datetime.strptime(key["validAfterTime"], "%Y-%m-%dT%H:%M:%SZ")
+            )
+            for key in key_list_response.get('keys', [])
+        ]
+
+    def _delete_key(self, key: ServiceAccountKey):
+        delete_request = self._google_service.projects().serviceAccounts().keys().delete(name=key.name)
+        delete_request.execute()
---
+++ tests/cron_job_tests/test_service_account_manager.py
@@ -0,0 +1,112 @@
+from datetime import datetime, timedelta
+import mock
+
+from rdr_service.offline.service_accounts import ServiceAccount, ServiceAccountKey, ServiceAccountKeyManager
+from tests.helpers.unittest_base import BaseTestCase
+
+
+class ServiceAccountManagerTest(BaseTestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.uses_database = False
+
+    def setUp(self, *args, **kwargs) -> None:
+        super(ServiceAccountManagerTest, self).setUp(*args, **kwargs)
+
+        # Mock google discover
+        patcher = mock.patch('rdr_service.offline.service_accounts.discovery')
+        self.mock_discovery = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        self.mock_project_sa_call = (self.mock_discovery.build.return_value.projects.
+                                     return_value.serviceAccounts.return_value)
+        self.mock_account_list = self.mock_project_sa_call.list
+        self.mock_key_list = self.mock_project_sa_call.keys.return_value.list
+
+        self.service_account_manager = ServiceAccountKeyManager()
+
+    def test_listing_accounts(self):
+        project_name = 'test_project'
+
+        self._mock_service_accounts([
+                {'email': 'one@test.com'},
+                {'email': 'two@test.com'}
+        ])
+
+        self.assertEqual([
+            ServiceAccount(email='one@test.com'),
+            ServiceAccount(email='two@test.com')
+        ], self.service_account_manager._get_service_accounts_for_project(project_name))
+        self.mock_account_list.assert_called_with(name=f'projects/{project_name}')
+
+    def test_listing_keys(self):
+        project_name = 'test_project'
+        service_account_name = 'test_account'
+
+        self._mock_keys([
+                {'name': 'one', 'validAfterTime': '2021-05-01T15:09:32Z'},
+                {'name': 'two', 'validAfterTime': '2020-09-17T09:10:11Z'}
+        ])
+
+        self.assertEqual([
+            ServiceAccountKey(name='one', start_date=datetime(2021, 5, 1, 15, 9, 32)),
+            ServiceAccountKey(name='two', start_date=datetime(2020, 9, 17, 9, 10, 11))
+        ], self.service_account_manager._get_keys_for_account(project_name, service_account_name))
+        self.mock_key_list.assert_called_with(
+            name=f'projects/{project_name}/serviceAccounts/{service_account_name}',
+            keyTypes='USER_MANAGED'
+        )
+
+    def test_delete_old_keys(self):
+        with mock.patch.object(self.service_account_manager, '_get_service_accounts_for_project') as mock_get_accounts,\
+                mock.patch.object(self.service_account_manager, '_get_keys_for_account') as mock_get_keys:
+            mock_get_accounts.return_value = [ServiceAccount(email='test')]
+            mock_get_keys.return_value = [
+                ServiceAccountKey(name='delete_this', start_date=datetime.now() - timedelta(days=4))
+            ]
+
+            self.service_account_manager.expire_old_keys()
+            self.assertKeyDeleted(key_name='delete_this')
+
+    def test_keep_newer_keys(self):
+        with mock.patch.object(self.service_account_manager, '_get_service_accounts_for_project') as mock_get_accounts,\
+                mock.patch.object(self.service_account_manager, '_get_keys_for_account') as mock_get_keys:
+            mock_get_accounts.return_value = [ServiceAccount(email='test')]
+            mock_get_keys.return_value = [
+                ServiceAccountKey(name='do_not_delete', start_date=datetime.now() - timedelta(days=1))
+            ]
+
+            self.service_account_manager.expire_old_keys()
+            self.assertNoKeysDeleted()
+
+    def test_ignore_long_lived_accounts(self):
+        with mock.patch.object(self.service_account_manager, '_get_service_accounts_for_project') as mock_get_accounts,\
+                mock.patch.object(self.service_account_manager, '_get_keys_for_account') as mock_get_keys:
+            long_lived_account_email = 'long@lived'
+            mock_get_accounts.return_value = [ServiceAccount(email=long_lived_account_email)]
+            mock_get_keys.return_value = [
+                ServiceAccountKey(name='do_not_delete', start_date=datetime.now() - timedelta(days=100))
+            ]
+            self.service_account_manager._service_accounts_with_long_lived_keys = [long_lived_account_email]
+
+            self.service_account_manager.expire_old_keys()
+            self.assertNoKeysDeleted()
+
+    def _mock_service_accounts(self, service_accounts):
+        self.mock_account_list.return_value.execute.return_value = {
+            'accounts': service_accounts
+        }
+
+    def _mock_keys(self, keys):
+        self.mock_key_list.return_value.execute.return_value = {
+            'keys': keys
+        }
+
+    def assertKeyDeleted(self, key_name):
+        mock_delete_request_builder = self.mock_project_sa_call.keys.return_value.delete
+        mock_delete_request_builder.assert_any_call(name=key_name)
+        mock_delete_request_builder.return_value.execute.assert_called()
+
+    def assertNoKeysDeleted(self):
+        mock_delete_request_builder = self.mock_project_sa_call.keys.return_value.delete
+        mock_delete_request_builder.assert_not_called()
