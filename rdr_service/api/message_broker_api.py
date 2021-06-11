1dab5ca8c4c74beed774c43195339b609199149e
wangy70 <yu.wang.3@vumc.org>
2021-06-11 09:29:56 -0500

    set API role to GEM

--- rdr_service/api/message_broker_api.py
+++ rdr_service/api/message_broker_api.py
@@ -1,6 +1,6 @@
 from rdr_service import app_util
 from rdr_service.api.base_api import BaseApi
-from rdr_service.api_util import RDR
+from rdr_service.api_util import GEM
 from rdr_service.dao.message_broker_dao import MessageBrokerDao
 
 
@@ -8,6 +8,6 @@ class MessageBrokerApi(BaseApi):
     def __init__(self):
         super().__init__(MessageBrokerDao())
 
-    @app_util.auth_required(RDR)
+    @app_util.auth_required(GEM)
     def post(self):
         return super(MessageBrokerApi, self).post()
