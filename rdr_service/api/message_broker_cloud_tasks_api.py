d0e9c005c815e75eea490839968e50b7dcb3098f
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-06-29 16:04:29 -0400

    more abstraction

--- rdr_service/api/message_broker_cloud_tasks_api.py
+++ rdr_service/api/message_broker_cloud_tasks_api.py
@@ -15,60 +15,71 @@ class StoreMessageBrokerEventDataTaskApi(Resource):
     """
     Cloud Task endpoint: store message broker event data.
     """
+    def __init__(self):
+        self.dao = MessageBrokenEventDataDao()
+        self.event_type = None
+
     @task_auth_required
     def post(self):
         log_task_headers()
-        dao = MessageBrokenEventDataDao()
         data = request.get_json(force=True)
+
+        self.event_type = data.get('eventType')
         message_record_id = data.get('id')
-        event_type = data.get('eventType')
         event_authored_time = parse(data.get('eventAuthoredTime'))
         participant_id = data.get('participantId')
         message_body = data.get('requestBody')
 
-        with dao.session() as session:
-            for key, value in message_body.items():
-                field_name = key
+        for key, value in message_body.items():
+            field_name = key
 
-                value_date_time = None
-                if isinstance(value, str):
-                    try:
-                        value_date_time = parse(value)
-                    except ValueError:
-                        pass
+            value_date_time = None
+            if isinstance(value, str):
+                try:
+                    value_date_time = parse(value)
+                except ValueError:
+                    pass
 
-                message_event_date = MessageBrokerEventData(
-                    messageRecordId=message_record_id,
-                    eventType=event_type,
-                    eventAuthoredTime=event_authored_time,
-                    participantId=participant_id,
-                    fieldName=field_name
-                )
-                if isinstance(value_date_time, datetime.datetime):
-                    message_event_date.valueDatetime = value
-                elif isinstance(value, bool):
-                    message_event_date.valueBool = value
-                elif isinstance(value, dict):
-                    message_event_date.valueJson = value
-                elif isinstance(value, str):
-                    message_event_date.valueString = value
-                elif isinstance(value, int):
-                    message_event_date.valueInteger = value
-                inserted_obj = dao.insert_with_session(session, message_event_date)
+            message_event_date = MessageBrokerEventData(
+                messageRecordId=message_record_id,
+                eventType=self.event_type,
+                eventAuthoredTime=event_authored_time,
+                participantId=participant_id,
+                fieldName=field_name
+            )
+            if isinstance(value_date_time, datetime.datetime):
+                message_event_date.valueDatetime = value
+            elif isinstance(value, bool):
+                message_event_date.valueBool = value
+            elif isinstance(value, dict):
+                message_event_date.valueJson = value
+            elif isinstance(value, str):
+                message_event_date.valueString = value
+            elif isinstance(value, int):
+                message_event_date.valueInteger = value
+
+            inserted_obj = self.dao.insert(message_event_date)
+
+        self.call_informing_loop_task(inserted_obj)
+
+        return '{"success": "true"}'
+
+    def call_informing_loop_task(self, obj):
 
         if GAE_PROJECT != 'localhost' \
-                and 'informing_loop'.lower() in event_type:
+                and 'informing_loop'.lower() \
+                in self.event_type:
+
             payload = {}
-            informing_records = dao.get_informing_loop(
-                inserted_obj.messageRecordId,
-                event_type
+            informing_records = self.dao.get_informing_loop(
+                obj.messageRecordId,
+                self.event_type
             )
+
             if informing_records:
-                payload['event_type'] = event_type
+                payload['event_type'] = self.event_type
                 payload['records'] = informing_records
                 _task = GCPCloudTask()
                 _task.execute('ingest_informing_loop',
                               payload=payload,
                               queue='genomics')
-
-        return '{"success": "true"}'
