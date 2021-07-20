0d27f005e4e877178b76378b3f3a74473670f696
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-07-20 11:17:45 -0400

    switch to instance vars

--- rdr_service/genomic/genomic_job_controller.py
+++ rdr_service/genomic/genomic_job_controller.py
@@ -97,6 +97,8 @@ class GenomicJobController:
         self.metrics_dao = GenomicGCValidationMetricsDao()
         self.member_dao = GenomicSetMemberDao()
         self.informing_loop_dao = GenomicInformingLoopDao()
+        self.aw1_raw_dao = GenomicAW1RawDao()
+        self.aw2_raw_dao = GenomicAW2RawDao()
         self.ingester = None
         self.file_mover = None
         self.reconciler = None
@@ -279,11 +281,10 @@ class GenomicJobController:
         if self.job_id not in [GenomicJob.AW1_MANIFEST, GenomicJob.METRICS_INGESTION]:
             raise AttributeError(f"{self.job_id.name} is invalid for this workflow")
 
-        member_dao = GenomicSetMemberDao()
-        raw_dao = GenomicAW1RawDao() if self.job_id == GenomicJob.AW1_MANIFEST else GenomicAW2RawDao()
+        raw_dao = self.aw1_raw_dao if self.job_id == GenomicJob.AW1_MANIFEST else self.aw2_raw_dao
 
         # Get member records
-        members = member_dao.get_members_from_member_ids(member_ids)
+        members = self.member_dao.get_members_from_member_ids(member_ids)
         update_recs = []
         completed_members = []
         multiples = []
@@ -313,7 +314,7 @@ class GenomicJobController:
             paths = self.get_unique_file_paths_for_raw_records([rec[1] for rec in update_recs])
             file_proc_map = self.map_file_paths_to_fp_id(paths)
             # Process records
-            with member_dao.session() as session:
+            with self.member_dao.session() as session:
                 for record_to_update in update_recs:
                     # AW1
                     if self.job_id == GenomicJob.AW1_MANIFEST:
