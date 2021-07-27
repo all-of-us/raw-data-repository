7e0c9e6251d1822d8a26f23ea09c5a2041b9ba39
Robert Abram <robert.m.abram.1@vumc.org>
2021-07-27 12:01:56 -0500

    Created generic cloud csv reader class. Update retention eligible loading code.

--- rdr_service/dao/retention_eligible_metrics_dao.py
+++ rdr_service/dao/retention_eligible_metrics_dao.py
@@ -41,12 +41,11 @@ class RetentionEligibleMetricsDao(UpdatableDao):
         else:
             task = GCPCloudTask()
             params = {'batch': pids}
-            task.execute('batch_rebuild_retention_eligible_task', queue='resource-tasks', payload=params, in_seconds=15)
+            task.execute('batch_rebuild_retention_eligible_task', queue='resource-tasks', payload=params,
+                         in_seconds=15)
 
     def upsert_all_with_session(self, session, retention_eligible_metrics_records):
-        upsert_count = 0
-        rebuild_queue = list()
-
+        update_queue = list()
         for record in retention_eligible_metrics_records:
             dup_id, need_update = self._find_dup_with_session(session, record)
             if dup_id and not need_update:
@@ -54,15 +53,10 @@ class RetentionEligibleMetricsDao(UpdatableDao):
             elif dup_id and need_update:
                 record.id = dup_id
             session.merge(record)
-            upsert_count = upsert_count + 1
             # Batch up records for resource rebuilding.
-            rebuild_queue.append(int(record.participantId))
-            if len(rebuild_queue) >= 1000:
-                session.commit()
-                self._submit_rebuild_task(rebuild_queue)
-                rebuild_queue.clear()
-        # Submit any left over records for rebuilding.
-        if rebuild_queue:
-            session.commit()
-            self._submit_rebuild_task(rebuild_queue)
-        return upsert_count
+            update_queue.append(int(record.participantId))
+
+        session.commit()
+        if update_queue:
+            self._submit_rebuild_task(update_queue)
+        return len(update_queue)
