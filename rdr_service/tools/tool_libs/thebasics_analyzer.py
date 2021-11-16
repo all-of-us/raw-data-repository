a2f4a458535a1b6ec1912a0b94141abe817578d0
Peggy Bertsch <65624611+pegbertsch@users.noreply.github.com>
2021-11-16 13:56:56 -0600

    Updates to TheBasics analyzer

--- rdr_service/tools/tool_libs/thebasics_analyzer.py
+++ rdr_service/tools/tool_libs/thebasics_analyzer.py
@@ -11,6 +11,9 @@ import logging
 import os
 import sys
 
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+
 from rdr_service.dao.bigquery_sync_dao import BigQuerySyncDao
 from rdr_service.model.bigquery_sync import BigQuerySync
 from rdr_service.model.questionnaire_response import QuestionnaireResponse
@@ -65,9 +68,14 @@ class TheBasicsAnalyzerClass(object):
         self.gcp_env = gcp_env
         self.id_list = id_list
         self.results = {}
+        self.missing_full_survey_list = []
+        self.first_response_is_partial = []
+        self.conflicting_payloads_with_same_authored = []
 
-    @staticmethod
-    def process_participant_responses(pid, response_list):
+    def process_participant_responses(self, pid, response_list):
+        if not len(response_list):
+            raise (ValueError, f'{pid} TheBasics response list was empty')
+
         result_details = list()
         for response in response_list:
             full_survey = False
@@ -77,31 +85,57 @@ class TheBasicsAnalyzerClass(object):
                 if field not in PROFILE_UPDATE_QUESTION_CODES and value:
                     full_survey = True
                     break
-            pid = response['participant_id']
-            authored = response['authored']
-            qrid = response['questionnaire_response_id']
-            # result_details.append(
-            #    { 'authored': response['authored']
-            #      'questionnaire_response_id': response['questionnaire_response_id'],
-            #      'full_survey': full_survey
-            #     }
-            # )
+
             result_details.append(
-                f'{pid}\t{authored}\t{qrid}\t{"FULL" if full_survey else "PARTIAL/PROFILE UPDATE"}'
+                {'authored': response['authored'],
+                 'questionnaire_response_id': response['questionnaire_response_id'],
+                 'full_survey': full_survey,
+                 'external_id': response['external_id']
+                 }
+             )
+
+        # Find the first full survey response location in the list of processed responses, if one exists
+        first_full_index = next((index for (index, rsp) in enumerate(result_details) if rsp['full_survey']), None)
+        # first_qrid = result_details[0]['questionnaire_response_id']
+        # first_authored = result_details[0]['authored']
+        if first_full_index is None:
+            self.missing_full_survey_list.append(pid)
+            _logger.error(
+                " ".join([f'{pid} did not have a full survey response,',
+                          f'first partial response {result_details[0]["questionnaire_response_id"]}',
+                          f'authored {result_details[0]["authored"]}'])
             )
 
-        if 'PARTIAL' in result_details[0]:
-            # Look for pattern where the next result had the same timestamp
-            if len(result_details) > 1:
-                first_authored_str =
-            _logger.error(f'Participant {pid} first TheBasics payload was not a full survey response')
+        # Process cases where the first response was preceded by a partial at index 0
+        elif first_full_index > 0:
+            first_rsp = result_details[0]
+            first_full_rsp = result_details[first_full_index]
+            first_full_ts = first_full_rsp['authored']
+            first_partial_ts = first_rsp['authored']
+            # Look for cases where the first full payload came in with same authored time (only have H:M:S)
+            # (results ordered by QuestionnaireResponse.created time, partial response was processed first by RDR)
+            if first_full_ts == first_partial_ts:
+                _logger.warning(
+                    " ".join([
+                        f'{pid} partial response {first_rsp["questionnaire_response_id"]}',
+                        f'(external id {first_rsp["external_id"]})',
+                        f'received/processed before full response {first_full_rsp["questionnaire_response_id"]}',
+                        f'(external id {first_rsp["external_id"]}, both have same authored {first_full_ts}'])
+                )
+                self.conflicting_payloads_with_same_authored.append(pid)
+            else:
+                delta = relativedelta(datetime.strptime(first_full_ts, "%Y-%m-%dT%H:%M:%S"),
+                                               datetime.strptime(first_partial_ts, "%Y-%m-%dT%H:%M:%S"))
 
-        has_full_survey = False
-        for result in result_details:
-            has_full_survey = has_full_survey or 'FULL' in result
-            print(result, flush=True)
-        if not has_full_survey:
-            _logger.error(f'Participant {pid} does not have a full TheBasics survey response')
+                delta_str = " ".join([
+                         f'offset {delta.days} days / {delta.hours} hours /',
+                         f'{delta.minutes} minutes / {delta.seconds} seconds ' ])
+
+                _logger.error(
+                    " ".join([f'{pid} Partial authored {first_partial_ts} before full {first_full_ts},',
+                              delta_str])
+                )
+                self.first_response_is_partial.append(pid)
 
     def run(self):
         """
@@ -114,8 +148,6 @@ class TheBasicsAnalyzerClass(object):
 
         self.gcp_env.activate_sql_proxy(replica=True)
 
-        last_participant = None
-        participant_responses = []
         dao = BigQuerySyncDao(backup=True)
         with dao.session() as session:
             records = session.query(
@@ -131,21 +163,33 @@ class TheBasicsAnalyzerClass(object):
                        QuestionnaireResponse.created
             ).all()
 
+            last_participant = None
+            participant_responses = []
             for record in records:
+                # Keep appending responses for the same participant to a list, until we get to a different pid in the
+                # ordered by participant_id results
                 current_participant = int(record.resource.get('participant_id', None))
-                if not last_participant:
+                if not last_participant:  # Need to initialize on the first pass through
                     last_participant = current_participant
+
                 if last_participant != current_participant:
                     if len(participant_responses):
-                        # TODO:  Add a method call to generate report rows from the responses list
                         self.process_participant_responses(last_participant, participant_responses)
-                        print('-----------------------------------------------------------------------')
                         participant_responses = []
                     last_participant = current_participant
                 participant_responses.append(record.resource)
 
+            # Process responses for the last participant in the resultset
             if len(participant_responses):
-                self.process_participant_responses(participant_responses)
+                self.process_participant_responses(current_participant, participant_responses)
+
+            if len(self.id_list) > 1:
+                print(f'{len(self.missing_full_survey_list)} participants are missing a full survey response')
+                print(f'{len(self.first_response_is_partial)} participants had partial survey as first response')
+                print(" ".join([f'{len(self.conflicting_payloads_with_same_authored)} participants',
+                                'had partial and full payloads with same timestamp, but partial received by RDR first'
+                                ])
+                )
 
 def get_id_list(fname):
     """
