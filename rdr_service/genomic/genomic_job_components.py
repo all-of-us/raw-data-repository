5064bfc86958fc545f6dbbe22439c458529385fc
Darryl Tharpe <darryl.tharpe@vumc.org>
2021-11-23 09:42:37 -0500

    updating replating copy tool

--- rdr_service/genomic/genomic_job_components.py
+++ rdr_service/genomic/genomic_job_components.py
@@ -1113,10 +1113,21 @@ class GenomicFileIngester:
 
         return GenomicSubProcessResult.SUCCESS
 
-    def copy_member_for_replating(self, member, genome_type=None, set_id=None):
+    def copy_member_for_replating(
+        self,
+        member,
+        genome_type=None,
+        set_id=None,
+        block_research_reason=None,
+        block_results_reason=None
+    ):
         """
         Inserts a new member record for replating.
         :param member: GenomicSetMember
+        :param genome_type:
+        :param set_id:
+        :param block_research_reason:
+        :param block_results_reason:
         :return:
         """
         new_member = GenomicSetMember(
@@ -1132,6 +1143,10 @@ class GenomicFileIngester:
             collectionTubeId=f'replated_{member.id}',
             genomicWorkflowState=GenomicWorkflowState.EXTRACT_REQUESTED,
             replatedMemberId=member.id,
+            blockResearch=1 if block_research_reason else 0,
+            blockResearchReason=block_research_reason if block_research_reason else None,
+            blockResults=1 if block_results_reason else 0,
+            blockResultsReason=block_results_reason if block_results_reason else None
         )
 
         self.member_dao.insert(new_member)
