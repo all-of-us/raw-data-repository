f134f7f0f29f60a726cd76bd955820d3c43bfdd0
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2021-12-13 16:27:52 -0600

    updates and tests for dao

--- rdr_service/dao/ghost_check_dao.py
+++ rdr_service/dao/ghost_check_dao.py
@@ -1,29 +1,29 @@
-from dataclasses import dataclass
-from datetime import datetime
-from typing import List
+from datetime import date, datetime
 
 from sqlalchemy.orm import Session
 
 from rdr_service.dao.base_dao import BaseDao
-
-
-@dataclass
-class LatestGhostCheck:
-    participant_id: int
-    is_ghost: bool
-    timestamp: datetime
+from rdr_service.model.participant import Participant
 
 
 class GhostCheckDao(BaseDao):
     @classmethod
-    def get_participants_needing_checked(cls, session: Session, start_date: datetime) -> List[LatestGhostCheck]:
+    def get_participants_needing_checked(cls, session: Session, earliest_signup_time: date):
         """
-        Loads the participants that should be checked for ghost status
-        (i.e. to see if they exist in Vibrent's system).
-        All participants that have not yet been checked, and all participants that haven't been checked
-        since the provided start_date, will be returned.
+        Loads the participants that should be checked for ghost status (i.e. to see if they exist in Vibrent's system).
+        :param session: Session used to interact with the database.
+        :param earliest_signup_time: Data for any Vibrent participants that have a signup
+            time later than the provided date will be returned.
+        :return: Participant objects with the participantId and isGhostId fields populated
         """
-        ...
+        return session.query(Participant.participantId, Participant.isGhostId).filter(
+            Participant.participantOrigin == 'vibrent',
+            Participant.signUpTime > datetime(
+                year=earliest_signup_time.year,
+                month=earliest_signup_time.month,
+                day=earliest_signup_time.day
+            )
+        ).all()
 
     def get_id(self, obj):
         ...
