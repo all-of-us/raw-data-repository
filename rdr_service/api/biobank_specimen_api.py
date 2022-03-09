37c5d6bf76d01b214a25366a0b0d57e39c026984
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2022-03-09 10:51:16 -0600

    moving lock so that we'll update on the second queued request (rather than try to insert again)

--- rdr_service/api/biobank_specimen_api.py
+++ rdr_service/api/biobank_specimen_api.py
@@ -29,11 +29,11 @@ class BiobankSpecimenApi(BiobankApiBase):
         if rlims_id:
             self._check_required_specimen_fields(resource)
 
-            if self.dao.exists(rlims_id):
-                rlims_id = kwargs['rlims_id']
-                return super(BiobankSpecimenApi, self).put(rlims_id, skip_etag=True)
-            else:
-                with self.dao.get_mutually_exclusive_lock(rlims_id):
+            with self.dao.get_mutually_exclusive_lock(rlims_id):
+                if self.dao.exists(rlims_id):
+                    rlims_id = kwargs['rlims_id']
+                    return super(BiobankSpecimenApi, self).put(rlims_id, skip_etag=True)
+                else:
                     return super(BiobankSpecimenApi, self).post()
         else:
             success_count = 0
@@ -201,12 +201,12 @@ class BiobankAliquotApi(BiobankApiBase):
         self.aliquot_rlims_id = kwargs['rlims_id']
         self.parent_rlims_id = kwargs['parent_rlims_id']
 
-        aliquot_id = self.dao.get_id(BiobankAliquot(rlimsId=self.aliquot_rlims_id))
-        if aliquot_id is None:
-            with self.dao.get_mutually_exclusive_lock(self.aliquot_rlims_id):
+        with self.dao.get_mutually_exclusive_lock(self.aliquot_rlims_id):
+            aliquot_id = self.dao.get_id(BiobankAliquot(rlimsId=self.aliquot_rlims_id))
+            if aliquot_id is None:
                 super(BiobankAliquotApi, self).post()
-        else:
-            super(BiobankAliquotApi, self).put(aliquot_id, skip_etag=True)
+            else:
+                super(BiobankAliquotApi, self).put(aliquot_id, skip_etag=True)
 
     def _get_model_to_update(self, resource, id_, expected_version, participant_id=None):
         return self._parse_aliquot_json(resource)
