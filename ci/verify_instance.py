f37f36d81938b8894889e556ca325b31727e7e08
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2022-05-17 11:51:04 -0500

    have ci script check for deployed instance after deploy

--- ci/verify_instance.py
+++ ci/verify_instance.py
@@ -9,7 +9,12 @@ stream_handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(stream_handler)
 logger.setLevel(logging.INFO)
 
-response = requests.get('https://ks-check-instance-running-1-dot-all-of-us-rdr-sandbox.appspot.com/')
+project_id = sys.argv[1]
+version_id = sys.argv[2]
+app_engine_url = f'https://{version_id}-dot-{project_id}.appspot.com/'
+
+logger.info(f'Checking "{app_engine_url}" for a running instance')
+response = requests.get(app_engine_url)
 
 if response.status_code != 200:
     logger.error(f'ERROR: server responded with {response.status_code} status code')
