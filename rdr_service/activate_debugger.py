314eebed141c27272275ea47a94a7547b6735153
Peggy Bertsch <65624611+pegbertsch@users.noreply.github.com>
2022-06-28 10:32:40 -0500

    [DA-2709] Remove references to GCP cloud debugger due to sunsetting

--- rdr_service/activate_debugger.py
+++
@@ -1,7 +0,0 @@
-import os
-if os.getenv('GAE_ENV', '').startswith('standard'):
-    try:
-        import googleclouddebugger
-        googleclouddebugger.enable()
-    except ImportError:
-        pass
