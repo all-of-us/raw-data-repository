59b46ca361bc622dee4aa3bf9ef14e0d9dce9add
Brian Kimmel <brian.kimmel@vumc.org>
2022-11-17 11:59:52 -0600

    Follow redirects

--- rdr_service/tools/export_organizations.sh
+++ rdr_service/tools/export_organizations.sh
@@ -2,8 +2,8 @@
 
 SHEET_EXPORT_BASE="https://docs.google.com/spreadsheets/d/1CcIGRV0Bd6BIz7PeuvrV6QDGDRQkp83CJUkWAl-fG58/export?format=csv&gid="
 
-curl "${SHEET_EXPORT_BASE}1076878570" > data/awardees.csv
-curl "${SHEET_EXPORT_BASE}1098779958" > data/organizations.csv
-curl "${SHEET_EXPORT_BASE}0" > data/sites.csv
+curl -L "${SHEET_EXPORT_BASE}1076878570" > data/awardees.csv
+curl -L "${SHEET_EXPORT_BASE}1098779958" > data/organizations.csv
+curl -L "${SHEET_EXPORT_BASE}0" > data/sites.csv
 
 echo "Done."
