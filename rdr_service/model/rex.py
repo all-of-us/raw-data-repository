d2ada6cdeef932cfcbbe926de6e32667bb9fdfdd
Joe Trujillo <jose.m.trujillo@vumc.org>
2022-12-13 15:00:20 -0600

    Added Study model to rex database

---
+++ rdr_service/model/rex.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, DateTime, BigInteger, String
+from sqlalchemy.dialects.mysql import TINYINT
+
+from rdr_service.model.base import RexBase
+
+
+class Study(RexBase):
+    __tablename__ = 'study'
+    id = Column('id', BigInteger, autoincrement=True, primary_key=True)
+    created = Column(DateTime)
+    modified = Column(DateTime)
+    ignore_flag = Column(TINYINT)
+    name = Column(String(128))
+    prefix = Column(BigInteger)
+
+
+# class Participant(RexBase):
+#     __tablename__ = 'participant'
+#     id = Column("id", BigInteger, autoincrement=True, primary_key=True)
+#     created = Column(DateTime)
+#     modified = Column(DateTime)
+#     ignore_flag = Column(TINYINT)
+#     primary_study_id = Column(BigInteger, ForeignKey("study.id"))
+#     ancillary_study_id = Column(BigInteger, ForeignKey("study.id"))
+#     primary_participant_id = Column(BigInteger, ForeignKey("rdr.participant.participant_id"))
+#     ancillary_participant_id = Column(BigInteger, ForeignKey("nph.participant.id"))
