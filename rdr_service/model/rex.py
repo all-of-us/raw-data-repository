96b7804e0fb1ebf17c42861cd9ea45f8c0953f6d
Joe Trujillo <jose.m.trujillo@vumc.org>
2022-12-13 15:26:22 -0600

    Added ParticipantMapping model to rex database

--- rdr_service/model/rex.py
+++ rdr_service/model/rex.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, ForeignKey, BigInteger, String
 from sqlalchemy.dialects.mysql import TINYINT
 
-from rdr_service.model.base import RexBase
+from rdr_service.model.base import RexBase, Base, NphBase
 from rdr_service.model.utils import UTCDateTime
 
 
 class Study(RexBase):
     __tablename__ = 'study'
+
     id = Column('id', BigInteger, autoincrement=True, primary_key=True)
     created = Column(UTCDateTime)
     modified = Column(UTCDateTime)
@@ -15,13 +16,14 @@ class Study(RexBase):
     prefix = Column(BigInteger)
 
 
-# class Participant(RexBase):
-#     __tablename__ = 'participant'
-#     id = Column("id", BigInteger, autoincrement=True, primary_key=True)
-#     created = Column(DateTime)
-#     modified = Column(DateTime)
-#     ignore_flag = Column(TINYINT)
-#     primary_study_id = Column(BigInteger, ForeignKey("study.id"))
-#     ancillary_study_id = Column(BigInteger, ForeignKey("study.id"))
-#     primary_participant_id = Column(BigInteger, ForeignKey("rdr.participant.participant_id"))
-#     ancillary_participant_id = Column(BigInteger, ForeignKey("nph.participant.id"))
+class ParticipantMapping(RexBase, Base, NphBase):
+    __tablename__ = 'participant_mapping'
+
+    id = Column("id", BigInteger, autoincrement=True, primary_key=True)
+    created = Column(UTCDateTime)
+    modified = Column(UTCDateTime)
+    ignore_flag = Column(TINYINT)
+    primary_study_id = Column(BigInteger, ForeignKey("study.id"))
+    ancillary_study_id = Column(BigInteger, ForeignKey("study.id"))
+    primary_participant_id = Column(BigInteger, ForeignKey("rdr.participant.participant_id"))
+    ancillary_participant_id = Column(BigInteger, ForeignKey("nph.participant.id"))
