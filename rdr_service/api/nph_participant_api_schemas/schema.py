29365517f372e13e3a5aa97b55fd6a8e4750eca1
Dchan <dchan4437@gmail.com>
2022-12-13 16:20:11 -0600

    Sorting functionality

--- rdr_service/api/nph_participant_api_schemas/schema.py
+++ rdr_service/api/nph_participant_api_schemas/schema.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass, field
+from typing import Optional, Dict
+
 from graphene import (
     ObjectType,
     String,
@@ -10,30 +13,99 @@ from graphene import (
     Schema
 )
 from graphene import relay
+from sqlalchemy.orm import Query, aliased
+
 from rdr_service.api.nph_participant_api_schemas import db
+from rdr_service.model.participant import Participant as DbParticipant
+from rdr_service.model.nph_sample import NphSample
+
+
+class SortableField(Field):
+    def __init__(self, *args, sort_modifier, **kwargs):
+        super(SortableField, self).__init__(*args, **kwargs)
+        self.sort_modifier = sort_modifier
+
+
+@dataclass
+class SortContext:
+    query: Query
+    order_expression: Optional = None
+    filter_expressions: List = field(default_factory=list)
+    references: Dict = field(default_factory=dict)
+    join_expressions: List = field(default_factory=list)
+    sort_table = None
+
+    def set_sort_table(self, reference):
+        self.sort_table = self.references[reference]
+
+    def add_filter(self, expr):
+        self.filter_expressions.append(expr)
+
+    def add_ref(self, table, ref_name):
+        self.references[ref_name] = aliased(table)
+        return self
+
+    def add_join(self, joined_table, join_expr):
+        self.join_expressions.append((joined_table, join_expr))
+        return self
+
+    def set_order_expression(self, expr):
+        self.order_expression = expr
+
+    def get_resulting_query(self):
+        resulting_query = self.query
+
+        for table, expr in self.join_expressions:
+            resulting_query = resulting_query.join(table, expr)
+        for expr in self.filter_expressions:
+            resulting_query = resulting_query.filter(expr)
+
+        return resulting_query.order_by(self.order_expression)
 
 
 class Event(ObjectType):
     """ NPH Participant Event Status """
 
     value = NonNull(String)
-    time = NonNull(DateTime)
+    time = SortableField(
+        NonNull(DateTime),
+        sort_modifier=lambda context: context.set_order_expression(context.sort_table.time)  # Order by time
+    )
 
 
 class EventCollection(ObjectType):
-    current = Field(Event)
+    current = SortableField(
+        Event,
+        sort_modifier=lambda _: ...  # Restrict to current
+    )
     # TODO: historical field need to sort by newest to oldest for a given aspect of a participant’s data
     historical = List(Event)
 
 
 class Sample(ObjectType):
-    parent = List(EventCollection)
-    child = List(EventCollection)
+    parent = SortableField(
+        EventCollection,
+        sort_modifier=lambda context: context.set_sort_table('sample')
+    )
+    child = SortableField(
+        EventCollection,
+        sort_modifier=lambda context: (
+            context.add_ref(
+                NphSample, 'child'
+            ).add_join(
+                context.references['child'],
+                context.references['child'].parent_id == context.references['sample'].id
+            ).set_sort_table('child')
+        )
+    )  # todo: join 'child' on parent ('sample') and sort_table = 'child'
 
 
 class SampleCollection(ObjectType):
     ordered = List(Sample)
-    stored = List(Sample)
+    stored = SortableField(
+        Sample,
+        sort_modifier=lambda _: ...  # would filter to stored samples here, but test data is only set up with stored
+    )
 
 
 class Participant(ObjectType):
@@ -114,10 +186,24 @@ class Participant(ObjectType):
     sample_4ml_edtap_1 = Field(SampleCollection)
     sample_ru_1 = Field(SampleCollection)
     sample_ru_2 = Field(SampleCollection)
-    sample_ru_3 = Field(SampleCollection)
+    sampleRU3 = SortableField(
+        SampleCollection,
+        sort_modifier=lambda context: context.add_ref(
+            NphSample, 'sample'
+        ).add_join(
+            context.references['sample'], context.references['sample'].participant_id == DbParticipant.participantId
+        ).add_filter(context.references['sample'].test == 'RU3')
+    )
     sample_tu_1 = Field(SampleCollection)
     sample_sa_1 = Field(SampleCollection)
-    sample_sa_2 = Field(SampleCollection)
+    sampleSA2 = SortableField(
+        SampleCollection,
+        sort_modifier=lambda context: context.add_ref(
+            NphSample, 'sample'
+        ).add_join(
+            context.references['sample'], context.references['sample'].participant_id == DbParticipant.participantId
+        ).add_filter(context.references['sample'].test == 'SA2')
+    )
     sample_ha_1 = Field(SampleCollection)
     sample_na_1 = Field(SampleCollection)
     sample_na_2 = Field(SampleCollection)
@@ -134,8 +220,14 @@ class Participant(ObjectType):
     city = Field(String)
     street_address = Field(String)
     enrollment_site = Field(String, description='This field will not be null')
-    participant_nph_id = Field(Int, description='NPH Participant unique identifier')
-    bio_bank_id = Field(Int, description='Participant\'s BioBank ID')
+    participantNphId = SortableField(
+        Int, description='NPH Participant unique identifier',
+        sort_modifier=lambda context: context.set_order_expression(DbParticipant.participantId)
+    )
+    biobankId = SortableField(
+        Int, description='Participant\'s BioBank ID',
+        sort_modifier=lambda context: context.set_order_expression(DbParticipant.biobankId)
+    )
     middle_name = String()
     street_address2 = String()
     phone_number = String()
@@ -149,7 +241,10 @@ class Participant(ObjectType):
     race = Field(String, description='Participant race')
     sex = Field(String, description='Participant')
     sexual_orientation = Field(String, description='Participant sexual orientation')
-    last_modified = Field(Date)
+    lastModified = SortableField(
+        Date,
+        sort_modifier=lambda context: context.set_order_expression(DbParticipant.lastModified)
+    )
     ehr_consent_expire_status = Field(String)
     withdrawal_status = Field(String)
     withdrawal_reason = Field(String)
@@ -191,18 +286,31 @@ class ParticipantQuery(ObjectType):
         interfaces = (relay.Node,)
         connection_class = ParticipantConnection
 
-    participant = relay.ConnectionField(ParticipantConnection, nph_id=Int(required=False))
+    participant = relay.ConnectionField(
+        ParticipantConnection, nph_id=Int(required=False), sort_by=String(required=False)
+    )
 
-    def resolve_participant(root, info, nph_id=None, **kwargs):
+    def resolve_participant(root, info, nph_id=None, sort_by=None, **kwargs):
         print(info, kwargs)
+        query = Query(DbParticipant)
+        current_class = Participant
+        sort_context = SortContext(query)
+
+        # sampleSA2:ordered:child:current:time
+        if sort_by:
+            sort_parts = sort_by.split(':')
+            for sort_field_name in sort_parts:
+                sort_field: SortableField = getattr(current_class, sort_field_name)
+                sort_field.sort_modifier(sort_context)
+                current_class = sort_field.type
+
         try:
             if nph_id:
                 return [x for x in db.datas if nph_id == x.get("participant_nph_id")]
             else:
-                return db.datas
+                return db.loadParticipantData(sort_context.get_resulting_query())
         except Exception as ex:
             raise ex
 
 
 NPHParticipantSchema = Schema(query=ParticipantQuery)
-
