43988008cd50dcf3ca2e18202c96f1eb5d1cbb60
Joe Trujillo <jose.m.trujillo@vumc.org>
2022-12-14 07:21:52 -0600

    Added test_insert_study test to RexStudyDaoTest

--- tests/dao_tests/test_rex_dao.py
+++ tests/dao_tests/test_rex_dao.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
 from rdr_service.dao.rex_dao import RexStudyDao
-# from rdr_service.model.rex import Study
+from rdr_service.clock import FakeClock
+from rdr_service.model.rex import Study
 from tests.helpers.unittest_base import BaseTestCase
+
+
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+TIME = datetime.strptime(datetime.now().strftime(DATETIME_FORMAT), DATETIME_FORMAT)
 
 
 class RexStudyDaoTest(BaseTestCase):
@@ -10,3 +17,24 @@ class RexStudyDaoTest(BaseTestCase):
 
     def test_get_before_insert(self):
         self.assertIsNone(self.rex_study_dao.get(1))
+
+    def test_insert_study(self):
+        rex_study_params = {
+            "ignore_flag": 0,
+            "name": "Study 1",
+            "prefix": 1E2+5E4
+        }
+        rex_study = Study(**rex_study_params)
+        with FakeClock(TIME):
+            self.rex_study_dao.insert(rex_study)
+
+        expected_rex_study = {
+            "id": 1,
+            "created": TIME,
+            "modified": TIME,
+            "ignore_flag": 0,
+            "name": "Study 1",
+            "prefix": int(1E2+5E4)
+        }
+        expected_rex_study_ = Study(**expected_rex_study)
+        self.assertEqual(expected_rex_study_.asdict(), self.rex_study_dao.get(1).asdict())
