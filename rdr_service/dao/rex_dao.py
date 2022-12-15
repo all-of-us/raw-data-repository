92d313f3c53cce4b32c37bf5133d16cbb307f188
Joe Trujillo <jose.m.trujillo@vumc.org>
2022-12-15 08:19:07 -0600

    Added dao class for rex.participant_mapping table CRUD operations

--- rdr_service/dao/rex_dao.py
+++ rdr_service/dao/rex_dao.py
@@ -1,5 +1,5 @@
 from rdr_service.dao.base_dao import BaseDao
-from rdr_service.model.rex import Study
+from rdr_service.model.rex import Study, ParticipantMapping
 
 
 class RexStudyDao(BaseDao):
@@ -8,4 +8,13 @@ class RexStudyDao(BaseDao):
         super(RexStudyDao, self).__init__(Study)
 
     def get_id(self, obj: Study):
+        return obj.id
+
+
+class RexParticipantMappingDao(BaseDao):
+
+    def __init__(self):
+        super(RexParticipantMappingDao, self).__init__(ParticipantMapping)
+
+    def get_id(self, obj: ParticipantMapping):
         return obj.id
