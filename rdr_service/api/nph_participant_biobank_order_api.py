d23ea80b6ef879de3ffee544ff89ca8cb4a0d449
Dchan <dchan4437@gmail.com>
2022-12-22 15:39:42 -0600

    Initial commit

---
+++ rdr_service/api/nph_participant_biobank_order_api.py
@@ -0,0 +1,4 @@
+
+
+class NphOrderApi:
+    pass
---
+++ tests/api_tests/test_nph_participant_biobank_order_api.py
@@ -0,0 +1,214 @@
+# Sample ID = NP124820391
+
+BLOOD_SAMPLE = {
+    "subject": "Patient/P124820391",
+    "identifier": [{
+        "system": "http://www.pmi-ops.org/order-id",
+        "value": "nph-order-id-123"
+    }, {
+        "system": "http://www.pmi-ops.org/sample-id",
+        "value": "nph-sample-id-456"
+    }],
+    "createdInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "nph-site-testa"
+        }
+    },
+    "collectedInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "nph-site-testa"
+        }
+    },
+    "finalizedInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "hnphpo-site-testa"
+        }
+    },
+    "created": "2022-11-03T09:40:21Z",
+    "module": "1",
+    "visitType": "LMT",
+    "timepoint": "15min",
+    "sample": {
+        "test": "PST8",
+        "description": "8 mL PST",
+        "collected": "2022-11-03T09:45:49Z",
+        "finalized": "2022-11-03T10:55:41Z"
+    },
+    "aliquots": [{
+        "id": "123",
+        "identifier": "LHPSTP1",
+        "container": "1.4mL Matrix Tube (500 uL)",
+        "volume": "450uL",
+        "description": "1.4 mL matrix tubes",
+        "collected": "2022-11-03T09:45:49Z"
+    }, {
+        "id": "456",
+        "identifier": "LHPSTP1",
+        "container": "1.4mL Matrix Tube (1000 uL)",
+        "volume": "970uL",
+        "description": "1.4 mL matrix tubes",
+        "collected": "2022-11-03T09:45:49Z"
+    }, {
+        "id": "789",
+        "identifier": "LHPSTP1",
+        "container": "1.4mL Matrix Tube (1000 uL)",
+        "volume": "970uL",
+        "description": "1.4 mL matrix tubes",
+        "collected": "2022-11-03T09:45:49Z"
+    }, ],
+    "notes": {
+        "collected": "Test notes 1",
+        "finalized": "Test notes 2"
+    }
+}
+
+URINE_SAMPLE = {
+    "subject": "Patient/P124820391",
+    "identifier": [{
+        "system": "http://www.pmi-ops.org/order-id",
+        "value": "nph-order-id-123"
+    }, {
+        "system": "http://www.pmi-ops.org/sample-id",
+        "value": "nph-sample-id-456"
+    }],
+    "createdInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "nph-site-testa"
+        }
+    },
+    "collectedInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "nph-site-testa"
+        }
+    },
+    "finalizedInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "hnphpo-site-testa"
+        }
+    },
+    "created": "2022-11-03T09:40:21Z",
+    "module": "1",
+    "visitType": "LMT",
+    "timepoint": "preLMT",
+    "sample": {
+        "test": "UrineS",
+        "description": "Spot Urine",
+        "collected": "2022-11-03T09:45:49Z",
+        "finalized": "2022-11-03T10:55:41Z",
+        "color": "Color 1",
+        "clarity": "Clean",
+    },
+    "aliquots": [{
+        "id": "123",
+        "identifier": "RU1",
+        "container": "1.4mL Matrix Tube (1000 uL)",
+        "volume": "970uL",
+        "description": "1.4 mL matrix tubes",
+        "collected": "2022-11-03T09:45:49Z"
+    }, {
+        "id": "456",
+        "identifier": "RU2",
+        "container": "6mL Matrix Tube (5 mL)",
+        "volume": "3mL",
+        "description": "1.4 mL matrix tubes",
+        "collected": "2022-11-03T09:45:49Z"
+    }, {
+        "id": "789",
+        "identifier": "RU2",
+        "container": "6mL Matrix Tube (5 mL)",
+        "volume": "3mL",
+        "description": "1.4 mL matrix tubes",
+        "collected": "2022-11-03T09:45:49Z"
+    }, ],
+    "notes": {
+        "collected": "Test notes 1",
+        "finalized": "Test notes 2"
+    }
+}
+
+STOOL_SAMPLE = {
+    "subject": "Patient/P124820391",
+    "identifier": [{
+        "system": "http://www.pmi-ops.org/order-id",
+        "value": "nph-order-id-kit-12345678"
+    }, {
+        "system": "http://www.pmi-ops.org/sample-id",
+        "value": "nph-sample-id-kit-12345678"
+    }],
+    "createdInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "nph-site-testa"
+        }
+    },
+    "collectedInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "nph-site-testa"
+        }
+    },
+    "finalizedInfo": {
+        "author": {
+            "system": "https://www.pmi-ops.org\/nph-username",
+            "value": "test@example.com"
+        },
+        "site": {
+            "system": "https://www.pmi-ops.org\/site-id",
+            "value": "hnphpo-site-testa"
+        }
+    },
+    "created": "2022-11-03T09:40:21Z",
+    "module": "1",
+    "visitType": "LMT",
+    "timepoint": "preLMT",
+    "sample": {
+        "test": "ST1",
+        "description": "95% Ethanol Tube 1",
+        "collected": "2022-11-03T09:45:49Z",
+        "finalized": "2022-11-03T10:55:41Z",
+        "bowelMovement": "I was constipated (had difficulty passing stool), and my stool looks like Type 1 and/or 2",
+        "bowelMovementQuality": "I tend to be constipated (have difficulty passing stool) - Type 1 and 2",
+    },
+    "notes": {
+        "collected": "Test notes 1",
+        "finalized": "Test notes 2"
+    }
+}
