6b673bf5ddb12c155ad2cf1230f2c817f4ca8fb9
Joe Trujillo <jose.m.trujillo@vumc.org>
2022-12-30 13:21:22 -0600

    Added crc32c_checksum field to BiobankFileExport model

--- rdr_service/model/study_nph.py
+++ rdr_service/model/study_nph.py
@@ -118,6 +118,7 @@ class BiobankFileExport(NphBase):
     id = Column("id", BigInteger, autoincrement=True, primary_key=True)
     created = Column(UTCDateTime)
     file_name = Column(String(256))
+    crc32c_checksum = Column(Integer, unique=True)
 
 
 event.listen(BiobankFileExport, "before_insert", model_insert_listener)
