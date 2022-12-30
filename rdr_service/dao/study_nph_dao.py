08b9c5991370488783cbdb8c83ba2c33c4bb183c
Joe Trujillo <jose.m.trujillo@vumc.org>
2022-12-30 10:00:33 -0600

    Added dao class for nph.study_category, nph.order, nph.ordered_sample, nph.sample_update, nph.biobank_file_export & nph.sample_export tables CRUD operations

--- rdr_service/dao/study_nph_dao.py
+++ rdr_service/dao/study_nph_dao.py
@@ -1,5 +1,7 @@
 from rdr_service.dao.base_dao import BaseDao
-from rdr_service.model.study_nph import Participant
+from rdr_service.model.study_nph import (
+    Participant, StudyCategory, Site, Order, OrderedSample, SampleUpdate, BiobankFileExport, SampleExport
+)
 
 
 class NphParticipantDao(BaseDao):
@@ -8,4 +10,60 @@ class NphParticipantDao(BaseDao):
         super(NphParticipantDao, self).__init__(Participant)
 
     def get_id(self, obj: Participant):
+        return obj.id
+
+
+class NphStudyCategoryDao(BaseDao):
+    def __init__(self):
+        super(NphStudyCategoryDao, self).__init__(StudyCategory)
+
+    def get_id(self, obj: StudyCategory):
+        return obj.id
+
+
+class NphSiteDao(BaseDao):
+    def __init__(self):
+        super(NphSiteDao, self).__init__(Site)
+
+    def get_id(self, obj: Site):
+        return obj.id
+
+
+class NphOrderDao(BaseDao):
+    def __init__(self):
+        super(NphOrderDao, self).__init__(Order)
+
+    def get_id(self, obj: Order):
+        return obj.id
+
+
+class NphOrderedSampleDao(BaseDao):
+    def __init__(self):
+        super(NphOrderedSampleDao, self).__init__(OrderedSample)
+
+    def get_id(self, obj: OrderedSample):
+        return obj.id
+
+
+class NphSampleUpdateDao(BaseDao):
+    def __init__(self):
+        super(NphSampleUpdateDao, self).__init__(SampleUpdate)
+
+    def get_id(self, obj: SampleUpdate):
+        return obj.id
+
+
+class NphBiobankFileExportDao(BaseDao):
+    def __init__(self):
+        super(NphBiobankFileExportDao, self).__init__(BiobankFileExport)
+
+    def get_id(self, obj: BiobankFileExport):
+        return obj.id
+
+
+class NphSampleExportDao(BaseDao):
+    def __init__(self):
+        super(NphSampleExportDao, self).__init__(SampleExport)
+
+    def get_id(self, obj: SampleExport):
         return obj.id
