f311986049d6670ce8d81a1f2d738c055f2edf12
Joe Trujillo <jose.m.trujillo@vumc.org>
2023-01-11 11:46:28 -0600

    Lots of fixes

--- rdr_service/tools/study_nph_biobank_file_export.py
+++ rdr_service/tools/study_nph_biobank_file_export.py
@@ -1,23 +1,28 @@
 # Script to create biobank file export and store the file in GCS Bucket
 from datetime import datetime, timedelta
 from collections import defaultdict
-from typing import List, Dict, Any, Iterable
+from typing import List, Dict, Any, Iterable, Optional
 from json import dump
-from random import choice as random_choice
+from re import findall
 
+from rdr_service.model.participant_summary import ParticipantSummary as RdrParticipantSummary
+from rdr_service.model.rex import ParticipantMapping as RexParticipantMapping
 from rdr_service.model.study_nph import (
+    Participant as NphParticipant,
     StudyCategory,
     Order,
     OrderedSample,
-    # SampleUpdate,
     # BiobankFileExport,
     # SampleExport
 )
+
+from rdr_service.dao.participant_summary_dao import ParticipantSummaryDao as RdrParticipantSummaryDao
+from rdr_service.dao.rex_dao import RexParticipantMappingDao
 from rdr_service.dao.study_nph_dao import (
+    NphParticipantDao,
     NphStudyCategoryDao,
     NphOrderDao,
     NphOrderedSampleDao,
-    # NphSampleUpdateDao,
     # NphBiobankFileExportDao,
     # NphSampleExportDao
 )
@@ -60,13 +65,15 @@ def _convert_ordered_samples_to_samples(order_id: int, ordered_samples: List[Ord
     for ordered_sample in ordered_samples:
         supplemental_fields = ordered_sample.supplemental_fields if ordered_sample.supplemental_fields else {}
         notes = ", ".join([f"{key}: {value}" for key, value in supplemental_fields.items()])
+        extract_volume_units = findall("[a-zA-Z]+", ordered_sample.volume)
+        volume_units = extract_volume_units[-1] if extract_volume_units else ""
         sample = {
-            "sampleID": ordered_sample.id,
-            "specimenCode": ordered_sample.aliquot_id,
-            "kitID": order_id if "stool" in ordered_sample.description else "N/A",
+            "sampleID": ordered_sample.aliquot_id,
+            "specimenCode": ordered_sample.identifier,
+            "kitID": order_id if ordered_sample.identifier.startswith("ST") else "",
             "volume": ordered_sample.volume,
             "volumeSpecified": True,
-            "volumeUOM": ordered_sample.volume.rsplit(" ", 1),
+            "volumeUOM": volume_units,
             "volumeUOMSpecified": True,
             "processingDateUTC": ordered_sample.finalized,
             "notes": notes,
@@ -75,7 +82,31 @@ def _convert_ordered_samples_to_samples(order_id: int, ordered_samples: List[Ord
     return samples
 
 
-def _convert_orders_to_collections(orders: List[Order]) -> List[Dict[str, Any]]:
+def _get_rdr_participant_summary_for_nph_partipant(nph_participant_id: int) -> Optional[RdrParticipantSummary]:
+    nph_participant_dao = NphParticipantDao()
+    with nph_participant_dao.session() as nph_participant_session:
+        nph_participant: NphParticipant = nph_participant_session.query(NphParticipant).get(nph_participant_id)
+
+    rex_participant_mapping_dao = RexParticipantMappingDao()
+    with rex_participant_mapping_dao.session() as rex_sm_session:
+        rex_participant_mapping: RexParticipantMapping = (
+            rex_sm_session.query(RexParticipantMapping).filter(
+                RexParticipantMapping.ancillary_participant_id == nph_participant.id
+            ).first()
+        )
+
+    rdr_participant_id = rex_participant_mapping.primary_participant_id
+    rdr_participant_summary_dao = RdrParticipantSummaryDao()
+    with rdr_participant_summary_dao.session() as rdr_ps_session:
+        return rdr_ps_session.query(RdrParticipantSummary).filter(
+            RdrParticipantSummary.participantId == rdr_participant_id
+        ).first()
+
+
+def _convert_orders_to_collections(
+    orders: List[Order],
+    rdr_participant_summary: RdrParticipantSummary
+) -> List[Dict[str, Any]]:
     collections = []
     for order in orders:
         samples = _convert_ordered_samples_to_samples(
@@ -87,7 +118,7 @@ def _convert_orders_to_collections(orders: List[Order]) -> List[Dict[str, Any]]:
             "visitID": parent_study_category.name if parent_study_category else "",
             "timepointID": _get_study_category(order.category_id).name,
             "orderID": order.id,
-            "nyFlag": random_choice(["N", "Y"]),
+            "nyFlag": "Y" if rdr_participant_summary.state == "NY" else "N",
             "samples": samples
         })
     return collections
@@ -96,24 +127,29 @@ def _convert_orders_to_collections(orders: List[Order]) -> List[Dict[str, Any]]:
 def main():
     orders_file_drop: List[Dict[str, Any]] = []
     orders: Iterable[Order] = get_orders_created_or_modified_in_last_n_hours(hours=48)
-    orders_by_participant: Dict[int, List[Dict[str, Any]]] = defaultdict(list)
+    grouped_orders: Dict[int, List[Dict[str, Any]]] = defaultdict(list)
     for order in orders:
+        finalized_site = order.finalized_site
+        nph_module_id = _get_parent_study_category(_get_parent_study_category(order.category_id).id)
         participant_id = order.participant_id
-        orders_by_participant[participant_id].append(order)
+        grouped_orders[(finalized_site, nph_module_id.name, participant_id)].append(order)
 
-    for participant_id, orders in orders_by_participant.items():
+    for (finalized_site, nph_module_id, participant_id), orders in grouped_orders.items():
+        rdr_participant_summary: RdrParticipantSummary = (
+            _get_rdr_participant_summary_for_nph_partipant(order.participant_id)
+        )
         json_object = {
-            "clientID": "N/A",
-            "studyID":  "N/A",
+            "clientID": finalized_site,
+            "studyID":  nph_module_id,
             "participantID": participant_id,
-            "gender": random_choice(["M", "F"]),
-            "ai_an_flag": "N/A",
-            "collections": _convert_orders_to_collections(orders)
+            "gender": rdr_participant_summary.sex or str(rdr_participant_summary.genderIdentity),
+            "ai_an_flag": "Y" if rdr_participant_summary.aian else "N",
+            "collections": _convert_orders_to_collections(orders, rdr_participant_summary)
         }
         orders_file_drop.append(json_object)
 
-    today_date = datetime.now().date().strftime("%Y_%m_%d")
-    orders_filename = f"NPH_Orders_{today_date}.json"
+    today_dt_ts = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    orders_filename = f"NPH_Orders_{today_dt_ts}.json"
     with open(orders_filename, "w") as json_fp:
         dump(orders_file_drop, json_fp, default=str)
 
