a85f6fe6891079a787ce816d9a4a8d9a5490690f
kenny-skaggs <64044838+kenny-skaggs@users.noreply.github.com>
2023-01-20 08:24:16 -0800

    [DA-3275] Implementing filter functionality on NPH Participant API (#3298)
    * implementing filter functionality on NPH Participant API
    
    * fix for unit tests

--- tests/api_tests/test_nph_participant_api.py
+++ tests/api_tests/test_nph_participant_api.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from rdr_service.dao import database_factory
 from rdr_service.data_gen.generators.data_generator import DataGenerator
 from sqlalchemy.orm import Query
+from rdr_service.model import study_nph
 from rdr_service.model.participant import Participant as aouParticipant
 from rdr_service.model.participant_summary import ParticipantSummary as ParticipantSummaryModel
 from rdr_service.model.rex import ParticipantMapping, Study
@@ -225,6 +226,32 @@ class TestQueryExecution(BaseTestCase):
             sorted_list.sort()
             self.assertTrue(deceased_list == sorted_list, msg="Resultset is not in sorting order")
 
+    def test_client_filter_parameter(self):
+        mock_load_participant_data(self.session)
+        participant_nph_id, first_name = (
+            self.session.query(study_nph.Participant.id, ParticipantSummaryModel.firstName)
+            .join(
+                ParticipantMapping,
+                ParticipantMapping.primary_participant_id == ParticipantSummaryModel.participantId
+            ).join(
+                study_nph.Participant,
+                study_nph.Participant.id == ParticipantMapping.ancillary_participant_id
+            ).first()
+        )
+
+        executed = app.test_client().post(
+            '/rdr/v1/nph_participant',
+            data='{participant (firstName: "%s") { edges { node { participantNphId firstName } } } }' % first_name
+        )
+        result = json.loads(executed.data.decode('utf-8'))
+
+        result_participant_list = result.get('participant').get('edges')
+        self.assertEqual(1, len(result_participant_list))
+
+        resulting_participant_data = result_participant_list[0].get('node')
+        self.assertEqual(first_name, resulting_participant_data.get('firstName'))
+        self.assertEqual(participant_nph_id, resulting_participant_data.get('participantNphId'))
+
     def test_graphql_syntax_error(self):
         executed = app.test_client().post('/rdr/v1/nph_participant', data=QUERY_WITH_SYNTAX_ERROR)
         result = json.loads(executed.data.decode('utf-8'))
@@ -240,6 +267,7 @@ class TestQueryExecution(BaseTestCase):
                 self.assertIn('locations', error)
 
     def tearDown(self):
+        super().tearDown()
         self.clear_table_after_test("rex.participant_mapping")
         self.clear_table_after_test("rex.study")
         self.clear_table_after_test("nph.participant")
