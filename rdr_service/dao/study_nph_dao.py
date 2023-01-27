15f7f6d98c8a00c7756284964550d76a97a9f90b
Dchan <dchan4437@gmail.com>
2023-01-27 15:09:42 -0800

    Ancillary Study Order API Fixes #2 (#3316)
    * Adding client_id column to nph.Order table as String nullable
    
    * API fetch identifier value by type (e.g. client-id, order-id)
    Store client_id in Order table
    Updated Unit test
    
    * Codacy Static Code Analysis update
    
    * Removing client ID validation.
    The order's table will not have any existing client IDs at first.

--- rdr_service/dao/study_nph_dao.py
+++ rdr_service/dao/study_nph_dao.py
@@ -288,6 +288,7 @@ class NphOrderDao(UpdatableDao):
             db_order.collected_site = collected_site
             db_order.finalized_author = self.order_cls.finalizedInfo.author.value
             db_order.finalized_site = finalized_site
+            db_order.client_id = fetch_identifier_value(self.order_cls, "client-id")
             db_order.notes = self.order_cls.notes.__dict__
         else:
             raise BadRequest("Participant ID does not match the corresponding Order ID.")
@@ -334,6 +335,7 @@ class NphOrderDao(UpdatableDao):
         order = Order()
         for order_model_field, resource_value in [("nph_order_id", fetch_identifier_value(self.order_cls, "order-id")),
                                                   ("order_created", self.order_cls.created),
+                                                  ("client_id", fetch_identifier_value(self.order_cls, "client-id")),
                                                   ("category_id", category_id),
                                                   ("participant_id", participant.id),
                                                   ("created_author", self.order_cls.createdInfo.author.value),
@@ -570,7 +572,7 @@ class NphOrderedSampleDao(UpdatableDao):
 
 def fetch_identifier_value(obj: Namespace, identifier: str) -> str:
     for each in obj.identifier:
-        if each.system == f"http://www.pmi-ops.org/{identifier}":
+        if identifier in each.system:
             return each.value
 
 
