19d239f6ff42a6d433d4e96ea577d66a2cdc4860
Joe Trujillo <jose.m.trujillo@vumc.org>
2023-02-02 15:50:09 -0600

    Fixed doc string

--- rdr_service/model/study_nph_enums.py
+++ rdr_service/model/study_nph_enums.py
@@ -2,7 +2,7 @@ from protorpc import messages
 
 
 class StoredSampleStatus(messages.Enum):
-    """Status of Genomic Set"""
+    """Status of NPH StoredSample"""
 
     RECEIVED = 0
     SHIPPED = 1
