8fe0f0f8c7d57e4ee2361e26d5ddc1c48d9f5e09
jmtrujillo75 <97454588+jmtrujillo75@users.noreply.github.com>
2023-02-02 15:57:39 -0600

    DA-3097: Implement: NPH Inventory Process (#3322)
    * Created an index on nph.participant.biobank_id field
    
    * Add stored_sample table in nph database
    
    * Added enums for nph.stored_sample table
    
    * Added script to import the NPH Inventory file
    
    * Renamed study_nph_biobank_file_export to study_nph_biobank_process
    
    * Parse csv_obj[SAMPLE_ID] to an integer
    
    * Added StoredSample model, Made biobank_id field non-nullable on
    Participant model & Created an Index on biobank_id field
    
    * Added NphStoredSampleDao for StoredSample model
    
    * Added Cron Job entry for NPH Biobank Inventory Process
    
    * Added _get_nph_inventory_samples_csv_dropped_in_last_24_hrs to filter csv files dropped in last 24 hours & only import them
    
    * Minor updates
    
    * Fixed Error log message
    
    * Added missing log messages
    
    * Added changes for StoredSample model
    
    * Added ignore_flag & sample_id fields to StoredSample model
    
    * Fixed doc string
    
    ---------
    
    Co-authored-by: Joe Trujillo <jose.m.trujillo@vumc.org>

---
+++ rdr_service/alembic/nph/versions/25f0d3a691e6_.py
@@ -0,0 +1,46 @@
+"""empty message
+
+Revision ID: 25f0d3a691e6
+Revises: 769abb4ef39a, fa9281091ce8
+Create Date: 2023-02-02 15:40:00.466612
+
+"""
+
+# pylint: disable=unused-import
+# pylint: disable=line-too-long
+from alembic import op
+import sqlalchemy as sa
+import rdr_service.model.utils
+
+
+from rdr_service.participant_enums import PhysicalMeasurementsStatus, QuestionnaireStatus, OrderStatus
+from rdr_service.participant_enums import WithdrawalStatus, WithdrawalReason, SuspensionStatus, QuestionnaireDefinitionStatus
+from rdr_service.participant_enums import EnrollmentStatus, Race, SampleStatus, OrganizationType, BiobankOrderStatus
+from rdr_service.participant_enums import OrderShipmentTrackingStatus, OrderShipmentStatus
+from rdr_service.participant_enums import MetricSetType, MetricsKey, GenderIdentity
+from rdr_service.model.base import add_table_history_table, drop_table_history_table
+from rdr_service.model.code import CodeType
+from rdr_service.model.site_enums import SiteStatus, EnrollingStatus, DigitalSchedulingStatus, ObsoleteStatus
+
+# revision identifiers, used by Alembic.
+revision = '25f0d3a691e6'
+down_revision = ('769abb4ef39a', 'fa9281091ce8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade(engine_name):
+    globals()["upgrade_%s" % engine_name]()
+
+
+def downgrade(engine_name):
+    globals()["downgrade_%s" % engine_name]()
+
+
+
+def upgrade_nph():
+    pass
+
+
+def downgrade_nph():
+    pass
