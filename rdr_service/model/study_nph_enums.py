b5164a38441ec750c7263293add9a33127fa977a
Joe Trujillo <jose.m.trujillo@vumc.org>
2023-02-28 10:23:07 -0600

    Added IncidentStatus & IncidentType Enums

--- rdr_service/model/study_nph_enums.py
+++ rdr_service/model/study_nph_enums.py
@@ -7,3 +7,14 @@ class StoredSampleStatus(messages.Enum):
     RECEIVED = 0
     SHIPPED = 1
     DISPOSED = 2
+
+
+class IncidentStatus(messages.Enum):
+    """Status of NPH"""
+    OPEN = 0
+    RESOLVED = 1
+    UNABLE_TO_RESOLVE = 2
+
+
+class IncidentType(messages.Enum):
+    UNSET = 0
