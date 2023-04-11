b56201021a77a274e148b783cbb38e1850e2ab5c
Joe Trujillo <jose.m.trujillo@vumc.org>
2023-04-11 12:14:26 -0500

    Minor fixes

--- rdr_service/offline/study_nph_ingest_awardee_data.py
+++ rdr_service/offline/study_nph_ingest_awardee_data.py
@@ -1,8 +1,9 @@
 import argparse
+import sys
 import logging
 from os.path import isabs, exists
-import sys
 from typing import Dict, Any, Iterator, Optional
+from io import StringIO
 from csv import DictReader
 
 from rdr_service.services.system_utils import setup_logging, setup_i18n
@@ -18,13 +19,21 @@ tool_cmd = "study_nph_ingest_awardee_data_from_file"
 tool_desc = "NPH Study Ingest Awardee Data"
 
 def read_csv(filepath: str) -> Iterator[Dict[str, Any]]:
-    with open(filepath, "r") as csv_fp:
-        csv_reader = DictReader(csv_fp)
-        for row in csv_reader:
+
+    def _decode_utf8_sig_content(csv_fp) -> StringIO:
+        content = csv_fp.read()
+        content = content.decode("utf-8-sig")
+        return StringIO(content)
+
+    with open(filepath, "rb") as csv_fp:
+        csv_file_content = _decode_utf8_sig_content(csv_fp)
+        csv_dict_reader = DictReader(csv_file_content)
+        for row in csv_dict_reader:
             yield row
 
 
 def _convert_csv_obj_to_site_obj(awardee_data_obj: Dict[str, Any]) -> Dict[str, Any]:
+
     return {
         "external_id": awardee_data_obj["healthpro_site_id"],
         "name": awardee_data_obj["site_name"],
@@ -36,11 +45,10 @@ def _convert_csv_obj_to_site_obj(awardee_data_obj: Dict[str, Any]) -> Dict[str, 
 def create_sites_from_csv(csv_filepath: str):
     nph_site_dao = NphSiteDao()
     for row in read_csv(csv_filepath):
-        nph_site_dao.get_site_using_params()
         site_obj = _convert_csv_obj_to_site_obj(row)
         nph_site: Optional[Site] = nph_site_dao.get_site_using_params(**site_obj)
         if nph_site:
-            _logger.info(f"An nph site already exists with {site_obj}")
+            _logger.info(f"An nph site already exists with {site_obj}. Skipping ...")
         else:
             _logger.info(f"Inserting a new nph site with {site_obj} fields")
             nph_site = Site(**site_obj)
