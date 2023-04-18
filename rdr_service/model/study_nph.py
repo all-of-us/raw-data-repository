181f254f44b6bc1d0461b775a5d93a38bb2f766a
j-kanuch <joshua.d.kanuch@vumc.org>
2023-04-18 15:10:16 -0500

    Fixes removing and creating constraints when autogenerating nph schemas

--- rdr_service/model/study_nph.py
+++ rdr_service/model/study_nph.py
@@ -310,7 +310,7 @@ class BiobankFileExport(NphBase):
     id = Column("id", BigInteger, autoincrement=True, primary_key=True)
     created = Column(UTCDateTime)
     file_name = Column(String(256))
-    crc32c_checksum = Column(String(64), nullable=False)
+    crc32c_checksum = Column(String(64))
 
 
 event.listen(BiobankFileExport, "before_insert", model_insert_listener)
