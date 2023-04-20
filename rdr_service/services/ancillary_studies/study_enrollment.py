a6553fb017a028311eb640f2ac868183d91d2bbf
Darryl Tharpe <darryl.tharpe@vumc.org>
2023-04-20 16:12:28 -0400

    add back enrollment referred event from profile update api

--- rdr_service/services/ancillary_studies/study_enrollment.py
+++ rdr_service/services/ancillary_studies/study_enrollment.py
@@ -1,14 +1,16 @@
+from rdr_service.ancillary_study_resources.nph.enums import Activity, EnrollmentEventTypes
+from rdr_service.cloud_utils.gcp_cloud_tasks import GCPCloudTask
 from rdr_service.dao.participant_dao import ParticipantDao
 from rdr_service.dao.rex_dao import RexParticipantMappingDao
 from rdr_service.dao.study_nph_dao import NphParticipantDao
-from rdr_service.config import NPH_STUDY_ID, AOU_STUDY_ID
+from rdr_service.config import NPH_STUDY_ID, AOU_STUDY_ID, GAE_PROJECT
 
 
 class EnrollmentInterface:
     def __init__(self, study_code):
         self.study_code = study_code
 
-    def create_study_participant(self, aou_pid, ancillary_pid):
+    def create_study_participant(self, aou_pid, ancillary_pid, event_authored_time):
         # We have to use the AoU research ID
         aou_participant_dao = ParticipantDao()
         aou_participant = aou_participant_dao.get(aou_pid)
@@ -20,6 +22,22 @@ class EnrollmentInterface:
         self.participant_dao.insert_participant_with_random_biobank_id(
             self.participant_dao.model_type(**insert_params))
         self.create_rex_participant_mapping(aou_pid, ancillary_pid)
+
+        enrollment_data = {
+            'study': 'nph',
+            'participant_id': ancillary_pid,
+            'activity_id': Activity.ENROLLMENT.number,
+            'event_type_id': EnrollmentEventTypes.REFERRED.number,
+            'event_authored_time': event_authored_time
+        }
+
+        if GAE_PROJECT != 'localhost':
+            cloud_task = GCPCloudTask()
+            cloud_task.execute(
+                endpoint='insert_study_event_task',
+                payload=enrollment_data,
+                queue='nph'
+            )
 
     def create_rex_participant_mapping(self, aou_pid, study_pid):
         rex_dao = RexParticipantMappingDao()
