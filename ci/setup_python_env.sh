801c4674aa1e02c38f477c32937a3a73bfe152e9
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2023-05-31 09:44:47 -0700

    setting circleci config for 3.11

--- ci/setup_python_env.sh
+++ ci/setup_python_env.sh
@@ -4,8 +4,8 @@
 # Install the python library requirements
 #
 sudo apt-get update
-sudo apt-get install python3.7-venv python3-pip libpython3.7-dev libmysqlclient-dev
-python3.7 -m venv venv
+sudo apt-get install python3.11-venv python3-pip libpython3.11-dev libmysqlclient-dev
+python3.11 -m venv venv
 source venv/bin/activate
 export PYTHONPATH=`pwd`
 echo "PYTHONPATH=${PYTHONPATH}"
