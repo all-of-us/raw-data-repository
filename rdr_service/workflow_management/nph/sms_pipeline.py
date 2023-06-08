66f7124a65c4844ebc72055267f05dfff3432ef0
j-kanuch <joshua.d.kanuch@vumc.org>
2023-06-08 10:20:13 -0500

    Fix environment bucket transfer def.

--- rdr_service/workflow_management/nph/sms_pipeline.py
+++ rdr_service/workflow_management/nph/sms_pipeline.py
@@ -6,8 +6,6 @@ from rdr_service.cloud_utils.gcp_cloud_tasks import GCPCloudTask
 from rdr_service.config import GAE_PROJECT, NPH_SMS_BUCKETS
 
 TASK_QUEUE = 'nph'
-API_ROUTE_PREFIX = '/resource/task/'
-
 
 def n1_generation():
     if GAE_PROJECT == "localhost":
@@ -16,14 +14,12 @@ def n1_generation():
         recipients = NPH_SMS_BUCKETS.get(GAE_PROJECT.split('-')[-1], 'test').keys()
 
     for destination in recipients:
-        logging.info("Pushing cloud task...")
+        logging.info(f"Pushing cloud task for {destination}...")
 
         data = {
             "file_type": "N1_MC1",
             "recipient": destination
         }
-        api_route = API_ROUTE_PREFIX + "NphSmsGenerationTaskApi"
-        logging.info(f"API ROUTE: {api_route}")
 
         _task = GCPCloudTask()
         _task.execute("nph_sms_generation_task", payload=data, queue=TASK_QUEUE)
