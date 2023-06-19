72a2e1a2abbea50b3aefdd60d4db0d05814ec9a8
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2023-06-19 09:01:30 -0700

    checking ci image version

--- ci/run_unittest_flask_server.sh
+++ ci/run_unittest_flask_server.sh
@@ -4,4 +4,8 @@
 source venv/bin/activate
 source /tmp/cloud-sdk/google-cloud-sdk/path.bash.inc
 export PYTHONPATH=$PYTHONPATH:`pwd`
+
+VERSION=$(python3 --version)
+echo "${VERSION}"
+
 python3 main.py --flask --unittests
