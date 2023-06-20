9b92ebb238b6471c2c1d304e09e679dc2399f1d3
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2023-06-20 09:16:46 -0700

    pr cleanup

--- ci/run_unittest_flask_server.sh
+++ ci/run_unittest_flask_server.sh
@@ -5,7 +5,4 @@ source venv/bin/activate
 source /tmp/cloud-sdk/google-cloud-sdk/path.bash.inc
 export PYTHONPATH=$PYTHONPATH:`pwd`
 
-VERSION=$(python3 --version)
-echo "${VERSION}"
-
 python3 main.py --flask --unittests
