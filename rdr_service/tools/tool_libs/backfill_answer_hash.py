b1830baf81f9daf49230cd2f84f86116ae90ec3d
Brian Kimmel <brian.kimmel@vumc.org>
2023-09-06 17:22:39 -0500

    Tool to test etm validation

--- rdr_service/tools/tool_libs/backfill_answer_hash.py
+++ rdr_service/tools/tool_libs/backfill_answer_hash.py
@@ -1,23 +1,369 @@
 from datetime import datetime
 import json
 import os
+from google.cloud import bigquery
+from cerberus import Validator
+import datetime
 
 from rdr_service.dao.questionnaire_response_dao import QuestionnaireResponseDao
 from rdr_service.model.questionnaire_response import QuestionnaireResponse
 from rdr_service.tools.tool_libs.tool_base import cli_run, ToolBase, logger
 
+global metadata_schema_dict
+
 tool_cmd = 'answer-hash-backfill'
 tool_desc = 'Backfill the answer digest for responses'
+
+emorecog_answer_rules = {
+    "https://research.joinallofus.org/fhir/emorecog/emotion": {
+        'type': 'string',
+        'allowed': ['a','f','h','s']
+    },
+    "https://research.joinallofus.org/fhir/emorecog/trial_id": {
+        'type': 'string'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/image": {
+        'type': 'string'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/response": {
+        'type': 'string'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/correct": {
+        'type': 'integer'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/rt": {
+        'type': 'float'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/state": {
+        'type': 'string'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/repeated": {
+        'type': 'integer'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/flagged": {
+        'type': 'integer'
+    },
+    "https://research.joinallofus.org/fhir/emorecog/timestamp": {
+        'type': 'integer'
+    },
+    "operating_system": {
+        'type': 'string'
+    },
+    "response_device": {
+        'type': 'string'
+    },
+    "screen_height": {
+        'type': 'float'
+    },
+    "screen_width": {
+        'type': 'float'
+    },
+    "test_duration": {
+        'type': 'float'
+    },
+    "test_end_date_time": {
+        'type': 'float'
+    },
+    "test_language": {
+        'type': 'string'
+    },
+    "test_name": {
+        'type': 'string'
+    },
+    "test_params": {
+        'type': 'string'
+    },
+    "test_restarted": {
+        'type': 'float'
+    },
+    "test_short_name": {
+        'type': 'string'
+    },
+    "test_start_date_time": {
+        'type': 'float'
+    },
+    "test_version": {
+        'type': 'string'
+    },
+    "touch": {
+        'type': 'float'
+    },
+    "user_agent": {
+        'type': 'string'
+    },
+    "user_utc_offset": {
+        'type': 'float'
+    },
+    "accuracy": {
+        'type': 'float'
+    },
+    "angry_accuracy": {
+        'type': 'float'
+    },
+    "angry_meanRTc": {
+        'type': 'float'
+    },
+    "angry_medianRTc": {
+        'type': 'float'
+    },
+    "angry_sdRTc": {
+        'type': 'float'
+    },
+    "any_timeouts": {
+        'type': 'float'
+    },
+    "fearful_accuracy": {
+        'type': 'float'
+    },
+    "fearful_meanRTc": {
+        'type': 'float'
+    },
+    "fearful_medianRTc": {
+        'type': 'float'
+    },
+    "fearful_sdRTc": {
+        'type': 'float'
+    },
+    "flag_medianRTc": {
+        'type': 'float'
+    }
+}
+
+emorecog_validation_schema = {
+    "group": {
+        "type": "dict",
+        "required": True,
+        "schema": {
+            "linkId": {"type": "string"},
+            "question": {
+                "type": "list",
+                "schema": {
+                    "type": "dict",
+                    "schema": {
+                        "answer": {
+                            "type": "list",
+                            "schema": {
+                                "type": "dict",
+                                "schema": {
+                                    "extension": {
+                                        "type": "list",
+                                        "schema": {
+                                            "type": "dict",
+                                            "schema": {
+                                                "url": {
+                                                    "type": "string",
+                                                    "required": True,
+                                                    "check_with": "url",
+                                                    "allowed": [
+                                                        "https://research.joinallofus.org/fhir/emorecog/trial_id",
+                                                        "https://research.joinallofus.org/fhir/emorecog/image",
+                                                        "https://research.joinallofus.org/fhir/emorecog/emotion",
+                                                        "https://research.joinallofus.org/fhir/emorecog/response",
+                                                        "https://research.joinallofus.org/fhir/emorecog/correct",
+                                                        "https://research.joinallofus.org/fhir/emorecog/rt",
+                                                        "https://research.joinallofus.org/fhir/emorecog/state",
+                                                        "https://research.joinallofus.org/fhir/emorecog/repeated",
+                                                        "https://research.joinallofus.org/fhir/emorecog/flagged",
+                                                        "https://research.joinallofus.org/fhir/emorecog/timestamp",
+                                                    ],
+                                                },
+                                                "valueString": {"type": "string"},
+                                                "valueDecimal": {"type": "float"},
+                                            },
+                                        },
+                                    }
+                                },
+                            },
+                        },
+                        "linkId": {"type": "string"},
+                    },
+                },
+            },
+        },
+    },
+    "status": {"type": "string"},
+    "subject": {"type": "dict"},
+    "authored": {"type": "string"},
+    "extension": {"type": "list",
+                  "schema": {
+                        "type": "dict",
+                        "schema": {
+                            "url": {
+                                "type": "string"
+                            },
+                            "valueString": {
+                                "type": "string",
+                                "check_with": "outcomes_metadata"
+                            }
+                        }
+                    }
+                },
+    "identifier": {"type": "dict"},
+    "questionnaire": {"type": "dict"},
+    "resourceType": {"type": "string"},
+}
+
+class MetadataValidator(Validator):
+    ...
+
+
+class EtMValidator(Validator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.url_validator = Validator(kwargs.get("metadata_schema"))
+
+    def _check_with_url(self, field, url):
+        # print(self.document)
+        if "valueString" in self.document:
+            answer_value = self.document["valueString"]
+        elif "valueDecimal" in self.document:
+            answer_value = self.document["valueDecimal"]
+        self.url_validator.validate({url: answer_value})
+        if self.url_validator.errors:
+            self._error(field, str(self.url_validator.errors))
+
+    def _check_with_outcomes_metadata(self, field, value_string):
+        self.url_validator.validate(json.loads(value_string))
+        if self.url_validator.errors:
+            self._error(field, str(self.url_validator.errors))
 
 
 class DigestBackfillTool(ToolBase):
     def run(self):
         super(DigestBackfillTool, self).run()
 
-        if self.args.id_list:
-            self.update_id_list()
-        else:
-            self.update_all()
+        self.validate_payload()
+        # self.setup_schema()
+
+    def setup_schema(self):
+        client = bigquery.Client()
+        fhir_schema = json.dumps(emorecog_validation_schema)
+        metadata_schema = json.dumps(emorecog_answer_rules)
+
+        job_options = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter("fhir_schema", "STRING", fhir_schema),
+                bigquery.ScalarQueryParameter("metadata_schema", "STRING", metadata_schema),
+                bigquery.ScalarQueryParameter("task_version", "STRING", "EmoRecog_AoU.v1.May23"),
+                bigquery.ScalarQueryParameter("questionnaire_type", "STRING", "emorecog"),
+                bigquery.ScalarQueryParameter("valid_from", "TIMESTAMP", datetime.datetime(2023,8,1))
+            ]
+        )
+        insert_schema = client.query("""
+            INSERT INTO rdr_etm_test.etm_validation_schema
+            (questionnaire_type, valid_from, created, fhir_schema, metadata_schema, task_version)
+            VALUES (@questionnaire_type, @valid_from, CURRENT_TIMESTAMP(), @fhir_schema, @metadata_schema, @task_version)
+        """, job_options)
+        insert_schema.result()
+
+    def validate_payload(self):
+        import logging
+        logging.info("Starting validation")
+        dryrun=True
+        client = bigquery.Client()
+
+        results_to_validate = client.query("SELECT * FROM rdr_etm_test.etm_questionnaire_response WHERE "
+                                           "questionnaire_type = 'emorecog'")
+        to_validate_list = results_to_validate.result()
+        eqrs_to_validate = list(to_validate_list)
+        query_job = client.query("SELECT * FROM rdr_etm_test.etm_validation_schema WHERE questionnaire_type='emorecog'")
+        result = query_job.result()
+        schemas = list(result)
+        # print(schemas[0].get('schema'))
+        fhir_schema_dict = json.loads(schemas[0].get('fhir_schema'))
+        metadata_schema_dict = json.loads(schemas[0].get('metadata_schema'))
+        v = EtMValidator(schema=fhir_schema_dict, metadata_schema=metadata_schema_dict)
+        # v.setup_metadata_validator(metadata_schema_dict)
+        print("Loaded schema dict")
+        for eqr in eqrs_to_validate:
+            resource_dict = json.loads(eqr.resource)
+            print(f"Validating {eqr.get('etm_questionnaire_response_id')}, {eqr.get('questionnaire_type')}")
+            v.validate(resource_dict)
+            validation_time = datetime.datetime.utcnow().isoformat()
+            print(str(v.errors))
+            job_config = bigquery.QueryJobConfig(
+                query_parameters=[
+                    bigquery.ScalarQueryParameter("eqrid", "INT64", eqr.etm_questionnaire_response_id),
+                    bigquery.ScalarQueryParameter("q_type", "STRING", eqr.questionnaire_type),
+                    bigquery.ScalarQueryParameter("resource", "JSON", eqr.resource.replace('\n','')),
+                    bigquery.ScalarQueryParameter("validation_time", "TIMESTAMP", validation_time),
+                    bigquery.ScalarQueryParameter("errors", "STRING", str(v.errors))
+                ]
+            )
+            if not dryrun:
+                validation_insert_query = client.query("""
+                INSERT INTO rdr_etm_test.etm_validation_result
+                    (id, created, etm_questionnaire_response_id, questionnaire_type, resource, validation_time, """
+                """validation_errors) VALUES (GENERATE_UUID(), CURRENT_TIMESTAMP(), @eqrid, @q_type, @resource, """
+                """@validation_time, @errors)""",
+                                                       job_config=job_config)
+                validation_insert_query.result()
+
+    def deliver_results(self):
+        client = bigquery.Client()
+        questionnaire_type_map = {
+            'emorecog': 'emotion_recognition'
+        }
+
+        for qt, task_name in questionnaire_type_map.items():
+            job_config = bigquery.QueryJobConfig(
+                query_parameters=[
+                    bigquery.ScalarQueryParameter("qt", "STRING", qt)
+                ]
+            )
+            valid_results_query = client.query(f"""
+            SELECT * FROM rdr_etm_test.etm_validation_result
+            WHERE questionnaire_type = @qt
+            AND errors IS NULL
+            AND id NOT IN (SELECT id FROM rdr_etm_test.{task_name})
+            """, job_config)
+            valid_results = valid_results_query.result()
+            research_id = 0
+            src_id = 'test'
+            for res in valid_results:
+                job_config = bigquery.QueryJobConfig(
+                    query_parameters=[
+                        bigquery.ScalarQueryParameter("id", "STRING", res.id),
+                        bigquery.ScalarQueryParameter("participant_id", "INT64", res.participant_id),
+                        bigquery.ScalarQueryParameter("research_id", "INT64", research_id),
+                        bigquery.ScalarQueryParameter("src_id", "STRING", src_id),
+                        bigquery.ScalarQueryParameter("resource", "JSON", res.resource)
+                    ]
+                )
+                insert_job = client.query(f"""
+                INSERT INTO rdr_etm_test.{task_name}
+                (id, participant_id, research_id, src_id, resource)
+                VALUES (@id, @participant_id, @research_id, @src_id, @resource)
+                """, job_config=job_config)
+                # wait for insert to finish
+                insert_job.result()
+
+            error_results_query = client.query(f"""
+            SELECT * FROM rdr_etm_test.etm_validation_result
+            WHERE questionnaire_type = @qt
+            AND errors IS NOT NULL
+            AND id NOT IN (SELECT id FROM rdr_etm_test.{task_name}_error)
+            """, job_config)
+            error_results = error_results_query.result()
+            research_id = 0
+            src_id = 'test'
+            for res in error_results:
+                job_config = bigquery.QueryJobConfig(
+                    query_parameters=[
+                        bigquery.ScalarQueryParameter("id", "STRING", res.id),
+                        bigquery.ScalarQueryParameter("participant_id", "INT64", res.participant_id),
+                        bigquery.ScalarQueryParameter("research_id", "INT64", research_id),
+                        bigquery.ScalarQueryParameter("src_id", "STRING", src_id),
+                        bigquery.ScalarQueryParameter("resource", "JSON", res.resource)
+                    ]
+                )
+                insert_job = client.query(f"""
+                INSERT INTO rdr_etm_test.{task_name}_error
+                (id, participant_id, research_id, src_id, resource)
+                VALUES (@id, @participant_id, @research_id, @src_id, @resource)
+                """, job_config=job_config)
+                # wait for insert to finish
+                insert_job.result()
 
     def update_id_list(self):
 
