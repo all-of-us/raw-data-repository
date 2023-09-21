e98dedd8cfa5c6b635721f10d4a34faf5f0d2d6d
Brian Kimmel <brian.kimmel@vumc.org>
2023-09-21 11:26:16 -0500

    Use correct src_id in Questionnaire Response Additional Information table

--- rdr_service/etl/bq/queries.py
+++ rdr_service/etl/bq/queries.py
@@ -969,7 +969,8 @@ queries = {
         "query": """SELECT * FROM EXTERNAL_QUERY("all-of-us-rdr-prod.us-central1.bq-rdr-preprod-curation",
                        "SELECT DISTINCT 0 AS id, """
                        """qr.questionnaire_response_id, 'NON_PARTICIPANT_AUTHOR_INDICATOR' as type, """
-                       """qr.non_participant_author as value, p.participant_origin src_id from """
+                       """qr.non_participant_author as value, CASE WHEN p.participant_origin = 'careevolution' """
+                       """THEN 'ce' ELSE p.participant_origin END src_id from """
                        """rdr.questionnaire_response qr JOIN (SELECT DISTINCT questionnaire_response_id from """
                        """cdm.src_clean) as qri ON qr.questionnaire_response_id = qri.questionnaire_response_id JOIN """
                        """rdr.participant p ON qr.participant_id = p.participant_id where qr.non_participant_author """
@@ -981,7 +982,8 @@ queries = {
         "query": """SELECT * FROM EXTERNAL_QUERY("all-of-us-rdr-prod.us-central1.bq-rdr-preprod-curation",
                    "SELECT DISTINCT 0 AS id, """
                  """qr.questionnaire_response_id, 'LANGUAGE' as type, """
-                 """qr.language as value, p.participant_origin src_id from """
+                 """qr.language as value, CASE WHEN p.participant_origin = 'careevolution' THEN 'ce' ELSE """
+                 """p.participant_origin END src_id from """
                  """rdr.questionnaire_response qr JOIN (SELECT DISTINCT questionnaire_response_id from """
                  """cdm.src_clean) as qri ON qr.questionnaire_response_id = qri.questionnaire_response_id JOIN """
                  """rdr.participant p ON qr.participant_id = p.participant_id where qr.language """
@@ -992,7 +994,8 @@ queries = {
         "append": True,
         "query": """SELECT * FROM EXTERNAL_QUERY("all-of-us-rdr-prod.us-central1.bq-rdr-preprod-curation",
                    "SELECT DISTINCT 0 AS id, """
-                   """qr.questionnaire_response_id, 'CODE' as type, c.value as value, p.participant_origin src_id """
+                   """qr.questionnaire_response_id, 'CODE' as type, c.value as value, CASE WHEN p.participant_origin """
+                   """= 'careevolution' THEN 'ce' ELSE p.participant_origin END src_id """
                    """from rdr.questionnaire_response qr JOIN rdr.questionnaire_concept qc ON qr.questionnaire_id = """
                    """qc.questionnaire_id AND qr.questionnaire_version = qc.questionnaire_version JOIN rdr.code c ON """
                    """qc.code_id = c.code_id JOIN (SELECT DISTINCT questionnaire_response_id from cdm.src_clean) as """
