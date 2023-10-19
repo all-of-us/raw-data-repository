97051b00388e63007c3ee58af0e5b20ba117cd3c
shahwaiz14 <shahwaiz14@hotmail.com>
2023-10-19 17:44:31 -0500

    Add RDR to auth list

--- rdr_service/api/site_hierarchy_api.py
+++ rdr_service/api/site_hierarchy_api.py
@@ -3,19 +3,19 @@ from flask import request
 from rdr_service import app_util
 from rdr_service.api.base_api import BaseApi
 from rdr_service.dao.site_hierarchy_dao import SiteHierarchyDao
-from rdr_service.api_util import CURATION
+from rdr_service.api_util import CURATION, RDR
 
 
 class SiteHierarchyApi(BaseApi):
     def __init__(self):
         super().__init__(SiteHierarchyDao())
 
-    @app_util.auth_required(CURATION)
-    def get(self, id_=None, p_id=None):
+    @app_util.auth_required([CURATION, RDR])
+    def get(self, id_=None, participant_id=None):
         """This will call the list method below"""
-        return super().get(id_=id_, participant_id=p_id)
+        return super().get(id_=id_, participant_id=participant_id)
 
-    @app_util.auth_required(CURATION)
+    @app_util.auth_required([CURATION, RDR])
     def list(self, participant_id=None):
         """Handle a GET request."""
         kwargs = request.args
