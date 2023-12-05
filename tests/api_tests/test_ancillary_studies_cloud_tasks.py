6cec8fb4eb536986f71e9d8c1f725b7ea8af259b
shahwaiz14 <shahwaiz14@hotmail.com>
2023-12-05 17:18:54 -0600

    Add tests for module re-consent

--- tests/api_tests/test_ancillary_studies_cloud_tasks.py
+++ tests/api_tests/test_ancillary_studies_cloud_tasks.py
@@ -75,6 +75,39 @@ class AncillaryStudiesEnrollmentCloudTaskTest(BaseTestCase):
         self.assertTrue(ps.consentForNphModule1)
         self.assertEqual(parser.parse(authored_time),ps.consentForNphModule1Authored)
 
+    @mock.patch("rdr_service.dao.rex_dao.RexParticipantMappingDao.get_from_ancillary_id")
+    def test_update_participant_summary_for_nph_reconsent(self, mock_participant_mapping):
+        aou_participant = self.data_generator.create_database_participant()
+        self.data_generator.create_database_participant_summary(
+            participant=aou_participant,
+            nphDeactivation=True,
+            nphDeactivationAuthored="2023-03-01T14:13:12",
+        )
+        mock_participant_mapping.return_value = ParticipantMapping(
+            primary_participant_id=aou_participant.participantId
+        )
+
+        data = {
+            "event_type": "consent",
+            "participant_id": aou_participant.participantId,
+            "event_authored_time": "2023-04-02T14:13:12",
+        }
+        from rdr_service.resource import main as resource_main
+
+        self.send_post(
+            local_path="UpdateParticipantSummaryForNphTaskApi",
+            request_data=data,
+            prefix="/resource/task/",
+            test_client=resource_main.app.test_client(),
+        )
+
+        ps_dao = ParticipantSummaryDao()
+        ps = ps_dao.get_by_participant_id(aou_participant.participantId)
+
+        self.assertTrue(ps.consentForNphModule1)
+        self.assertEqual(ps.nphDeactivation, False)
+        self.assertIsNone(ps.nphDeactivationAuthored)
+
     @mock.patch('rdr_service.dao.rex_dao.RexParticipantMappingDao.get_from_ancillary_id')
     def test_update_participant_summary_for_nph_withdrawal(self, mock_participant_mapping):
         aou_participant = self.data_generator.create_database_participant()
