16f36c22a61852dc80020549757596047b78aceb
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2024-01-22 10:50:37 -0800

    fix for unit tests

--- rdr_service/dao/questionnaire_response_dao.py
+++ rdr_service/dao/questionnaire_response_dao.py
@@ -1078,7 +1078,13 @@ class QuestionnaireResponseDao(BaseDao):
                         code.value, [CONSENT_FOR_ELECTRONIC_HEALTH_RECORDS_MODULE, PEDIATRIC_EHR_CONSENT]
                     ):
                         # If we got a new EHR consent, mark that it needs to be validated
-                        if ehr_consent and authored > participant_summary.consentForElectronicHealthRecordsAuthored:
+                        if (
+                            ehr_consent
+                            and (
+                                participant_summary.consentForElectronicHealthRecordsAuthored is None
+                                or authored > participant_summary.consentForElectronicHealthRecordsAuthored
+                            )
+                        ):
                             new_status = QuestionnaireStatus.SUBMITTED_NOT_VALIDATED
                         else:
                             new_status = QuestionnaireStatus.SUBMITTED_NO_CONSENT
