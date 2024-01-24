0ebae60937fa6b109345f2184107ad5d57ac8d5d
vmoniqb <54696916+vmoniqb@users.noreply.github.com>
2024-01-24 16:00:04 -0600

    Creating participant_data_to_tactis table for Tactis Data  (#3779)
    * Creating tactis table to track changes
    
    * Updating alembic migration to skip environments where the table already exists

--- rdr_service/model/database.py
+++ rdr_service/model/database.py
@@ -73,6 +73,7 @@ from rdr_service.model.curation_etl import CdrEtlRunHistory, CdrEtlSurveyHistory
 from rdr_service.model.pediatric_data_log import PediatricDataLog
 from rdr_service.model.ppi_validation_errors import PpiValidationErrors
 from rdr_service.model.profile_update import ProfileUpdate
+from rdr_service.model.participant_data_to_tactis import ParticipantDataToTactis
 
 from rdr_service.model.study_nph import Participant
 from rdr_service.model.study_nph_sms import SmsSample, SmsJobRun
