22b68e22ed7d6592e5d4ba0f8b31b6be92e5588c
kenny-skaggs <64044838+kenny-skaggs@users.noreply.github.com>
2024-03-06 08:17:14 -0800

    [DA-4141] Automatically detecting duplicate accounts (#3820)
    * cherry pick of algorithm code
    
    * duplicate account cron job

--- rdr_service/model/database.py
+++ rdr_service/model/database.py
@@ -28,6 +28,7 @@ from rdr_service.model.code import CodeBook, Code, CodeHistory
 from rdr_service.model.calendar import Calendar
 from rdr_service.model.deceased_report import DeceasedReport
 from rdr_service.model.deceased_report_import_record import DeceasedReportImportRecord
+from rdr_service.model.duplicate_account import DuplicateAccount
 from rdr_service.model.ehr import EhrReceipt, ParticipantEhrReceipt
 from rdr_service.model.enrollment_status_history import EnrollmentStatusHistory
 from rdr_service.model.ghost_api_check import GhostApiCheck
