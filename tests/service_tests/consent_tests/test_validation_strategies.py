1d6f4ee5bfc156a6bd940cbd74c751869cbf7292
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2024-04-09 08:57:05 -0700

    fix for setting participant summary ehr status after validation

--- tests/service_tests/consent_tests/test_validation_strategies.py
+++ tests/service_tests/consent_tests/test_validation_strategies.py
@@ -1,7 +1,10 @@
+from datetime import date
 import mock.mock
 
 from rdr_service.dao.consent_dao import ConsentDao
 from rdr_service.model.consent_file import ConsentFile, ConsentSyncStatus, ConsentType
+from rdr_service.model.participant_summary import ParticipantSummary
+from rdr_service.participant_enums import QuestionnaireStatus
 from rdr_service.services.consent.validation import ReplacementStoringStrategy, StoreResultStrategy
 from tests.helpers.unittest_base import BaseTestCase
 
@@ -98,4 +101,39 @@ class ValidationOutputStrategyIntegrationTest(BaseTestCase):
         self.assertEqual(
             [result_a1, result_b1, result_b2],
             stored_results
+        )
+
+    @mock.patch('rdr_service.services.consent.validation.dispatch_rebuild_consent_metrics_tasks')
+    @mock.patch('rdr_service.services.consent.validation.GCPCloudTask')
+    def test_store_updates_ehr_status_correctly(self, _, __):
+        # Initialize the data
+        participant_id = self.data_generator.create_database_participant_summary(
+            consentForElectronicHealthRecords=QuestionnaireStatus.SUBMITTED_NOT_VALIDATED
+        ).participantId
+        primary_result = ConsentFile(
+            participant_id=participant_id,
+            type=ConsentType.PRIMARY,
+            sync_status=ConsentSyncStatus.READY_FOR_SYNC,
+            file_exists=True,
+            file_path='new_primary_file',
+            consent_response=mock.MagicMock(id=1),
+            expected_sign_date=date.today()
+        )
+        ehr_result = ConsentFile(
+            participant_id=participant_id,
+            type=ConsentType.EHR,
+            sync_status=ConsentSyncStatus.NEEDS_CORRECTING,
+            file_exists=True,
+            file_path='new_ehr_file',
+            consent_response=mock.MagicMock(id=2),
+            expected_sign_date=date.today()
+        )
+
+        consent_dao = mock.MagicMock()
+        with StoreResultStrategy(self.session, consent_dao) as strategy:
+            strategy.add_all([primary_result, ehr_result])
+
+        participant_summary = self.session.query(ParticipantSummary).get(participant_id)
+        self.assertEqual(
+            QuestionnaireStatus.SUBMITTED_INVALID, participant_summary.consentForElectronicHealthRecords
         )
