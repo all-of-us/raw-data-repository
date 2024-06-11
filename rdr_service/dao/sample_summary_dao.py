3496d322b9667e528e5bec6132469136d7bcbc2d
Kenny Skaggs <kenneth.d.skaggs@vumc.org>
2024-06-11 07:56:47 -0700

    writing receipt data

--- rdr_service/dao/sample_summary_dao.py
+++ rdr_service/dao/sample_summary_dao.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from typing import List
 
 from sqlalchemy.orm import Session
 
+from rdr_service.model.biobank_stored_sample import BiobankStoredSample
 from rdr_service.model.sample_order_status import SampleOrderStatus
 from rdr_service.model.sample_receipt_status import SampleReceiptStatus
 from rdr_service.participant_enums import OrderStatus, SampleStatus
@@ -29,23 +31,64 @@ class SampleSummaryDao:
             )
 
     @classmethod
-    def upsert_receipt_data(
-        cls, participant_id: int, status: SampleStatus, test: str, time: datetime, session: Session
-    ):
+    def refresh_receipt_data(cls, participant_id: int, new_sample: BiobankStoredSample, session: Session):
         receipt_data = session.query(SampleReceiptStatus).filter(
             SampleReceiptStatus.participant_id == participant_id,
-            SampleReceiptStatus.test_code == test
+            SampleReceiptStatus.test_code == new_sample.test
         ).one_or_none()
 
         if receipt_data:
-            receipt_data.status = status
-            receipt_data.status_time = time
+            sample_list = session.query(BiobankStoredSample).filter(
+                BiobankStoredSample.biobankId == new_sample.biobankId,
+                BiobankStoredSample.test == new_sample.test
+            ).all()
+            sample_list = [*sample_list, new_sample]
+
+            status_sample = None
+            non_disposed_samples = [
+                sample for sample in sample_list if sample.status < SampleStatus.SAMPLE_NOT_RECEIVED
+            ]
+            if non_disposed_samples:
+                status_sample = cls._latest_confirmed_sample(non_disposed_samples)
+                receipt_data.status = status_sample.status
+                receipt_data.status_time = status_sample.confirmed
+            else:
+                disposed_samples = [
+                    sample for sample in sample_list if sample.status >= SampleStatus.SAMPLE_NOT_RECEIVED
+                ]
+                status_sample = cls._latest_disposed_sample(disposed_samples)
+                receipt_data.status = status_sample.status
+                receipt_data.status_time = status_sample.disposed
         else:
+            timestamp = (
+                new_sample.confirmed if new_sample.status < SampleStatus.SAMPLE_NOT_RECEIVED
+                else new_sample.disposed
+            )
             session.add(
                 SampleReceiptStatus(
                     participant_id=participant_id,
-                    test_code=test,
-                    status=status,
-                    status_time=time
+                    test_code=new_sample.test,
+                    status=new_sample.status,
+                    status_time=timestamp
                 )
             )
+
+    @classmethod
+    def _latest_confirmed_sample(cls, sample_list: List[BiobankStoredSample]) -> BiobankStoredSample:
+        timestamp = None
+        result = None
+        for sample in sample_list:
+            if timestamp is None or timestamp < sample.confirmed:
+                timestamp = sample.confirmed
+                result = sample
+        return result
+
+    @classmethod
+    def _latest_disposed_sample(cls, sample_list: List[BiobankStoredSample]) -> BiobankStoredSample:
+        timestamp = None
+        result = None
+        for sample in sample_list:
+            if timestamp is None or timestamp < sample.disposed:
+                timestamp = sample.disposed
+                result = sample
+        return result
--- tests/dao_tests/test_sample_summary_dao.py
+++ tests/dao_tests/test_sample_summary_dao.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from rdr_service.dao.sample_summary_dao import (
     OrderStatus, SampleOrderStatus, SampleReceiptStatus, SampleStatus, SampleSummaryDao
 )
+from rdr_service.model.biobank_stored_sample import BiobankStoredSample
 from tests.helpers.unittest_base import BaseTestCase
 
 
@@ -39,11 +40,23 @@ class SampleSummaryDaoTest(BaseTestCase):
     def test_insert_receipt_data(self):
         ed_time = datetime(2022, 11, 1)
         sal_time = datetime(2023, 1, 15)
-        SampleSummaryDao.upsert_receipt_data(
-            self.participant.participantId, SampleStatus.RECEIVED, '1ED10', ed_time, self.session
+        SampleSummaryDao.refresh_receipt_data(
+            self.participant.participantId,
+            BiobankStoredSample(
+                test='1ED10',
+                status=SampleStatus.RECEIVED,
+                confirmed=ed_time
+            ),
+            self.session
         )
-        SampleSummaryDao.upsert_receipt_data(
-            self.participant.participantId, SampleStatus.DISPOSED, '2SAL0', sal_time, self.session
+        SampleSummaryDao.refresh_receipt_data(
+            self.participant.participantId,
+            BiobankStoredSample(
+                test='2SAL0',
+                status=SampleStatus.SAMPLE_NOT_RECEIVED,
+                disposed=sal_time
+            ),
+            self.session
         )
         self.session.commit()
 
@@ -57,7 +70,7 @@ class SampleSummaryDaoTest(BaseTestCase):
                 self.assertEqual(SampleStatus.RECEIVED, receipt.status)
             elif receipt.test_code == '2SAL0':
                 self.assertEqual(sal_time, receipt.status_time)
-                self.assertEqual(SampleStatus.DISPOSED, receipt.status)
+                self.assertEqual(SampleStatus.SAMPLE_NOT_RECEIVED, receipt.status)
             else:
                 self.fail(f'was not expecting test code "{receipt.test}"')
 
@@ -81,20 +94,87 @@ class SampleSummaryDaoTest(BaseTestCase):
         self.assertEqual(OrderStatus.FINALIZED, order.status)
 
     def test_update_receipt_data(self):
-        SampleSummaryDao.upsert_receipt_data(
-            self.participant.participantId, SampleStatus.RECEIVED, '1ED10',
-            datetime(2021, 10, 30), self.session
+        SampleSummaryDao.refresh_receipt_data(
+            self.participant.participantId,
+            BiobankStoredSample(
+                status=SampleStatus.RECEIVED,
+                test='1ED10',
+                confirmed=datetime(2021, 10, 30)
+            ),
+            self.session
         )
         self.session.commit()
 
         updated_timestamp = datetime(2022, 11, 1)
-        SampleSummaryDao.upsert_receipt_data(
-            self.participant.participantId, SampleStatus.DISPOSED, '1ED10', updated_timestamp, self.session
+        SampleSummaryDao.refresh_receipt_data(
+            self.participant.participantId,
+            BiobankStoredSample(
+                status=SampleStatus.DISPOSED,
+                test='1ED10',
+                confirmed=updated_timestamp
+            ),
+            self.session
         )
         self.session.commit()
 
-        order = self.session.query(SampleReceiptStatus).filter(
+        data = self.session.query(SampleReceiptStatus).filter(
             SampleReceiptStatus.participant_id == self.participant.participantId
         ).one()
-        self.assertEqual(updated_timestamp, order.status_time)
-        self.assertEqual(SampleStatus.DISPOSED, order.status)
+        self.assertEqual(updated_timestamp, data.status_time)
+        self.assertEqual(SampleStatus.DISPOSED, data.status)
+
+    def test_ignore_bad_receipt_update(self):
+        """A new sample with a "bad" status should not replace existing data from a "good" status"""
+        original_timestamp = datetime(2021, 10, 30)
+        sample = self.data_generator.create_database_biobank_stored_sample(
+            status=SampleStatus.RECEIVED,
+            test='1ED10',
+            confirmed=original_timestamp
+        )
+        SampleSummaryDao.refresh_receipt_data(self.participant.participantId, sample, self.session)
+        self.session.commit()
+
+        SampleSummaryDao.refresh_receipt_data(
+            self.participant.participantId,
+            BiobankStoredSample(
+                status=SampleStatus.SAMPLE_NOT_RECEIVED,
+                test='1ED10',
+                disposed=datetime(2022, 11, 1)
+            ),
+            self.session
+        )
+        self.session.commit()
+
+        data = self.session.query(SampleReceiptStatus).filter(
+            SampleReceiptStatus.participant_id == self.participant.participantId
+        ).one()
+        self.assertEqual(original_timestamp, data.status_time)
+        self.assertEqual(SampleStatus.RECEIVED, data.status)
+
+    def test_prioritize_good_receipt_update(self):
+        """A new sample with a "good" status should replace existing data from a "bad" status"""
+        sample = self.data_generator.create_database_biobank_stored_sample(
+            status=SampleStatus.SAMPLE_NOT_RECEIVED,
+            test='1ED10',
+            disposed=datetime(2021, 10, 30)
+        )
+        SampleSummaryDao.refresh_receipt_data(self.participant.participantId, sample, self.session)
+        self.session.commit()
+
+        new_receipt_time = datetime(2022, 11, 1)
+        SampleSummaryDao.refresh_receipt_data(
+            self.participant.participantId,
+            BiobankStoredSample(
+                status=SampleStatus.DISPOSED,
+                test='1ED10',
+                confirmed=new_receipt_time
+            ),
+            self.session
+        )
+        self.session.commit()
+
+        data = self.session.query(SampleReceiptStatus).filter(
+            SampleReceiptStatus.participant_id == self.participant.participantId
+        ).one()
+        self.assertEqual(new_receipt_time, data.status_time)
+        self.assertEqual(SampleStatus.DISPOSED, data.status)
