ae88770042d277d3133488b0f1014c2343653301
davek14 <david.konopka@vumc.org>
2024-07-29 11:33:05 -0400

    Added default to finalized value, testing support.

--- tests/workflow_tests/test_nph_biobank_workflows.py
+++ tests/workflow_tests/test_nph_biobank_workflows.py
@@ -2,7 +2,8 @@ import os
 
 from rdr_service.api_util import open_cloud_file
 from rdr_service.dao.study_nph_dao import NphStoredSampleDao
-from rdr_service.data_gen.generators.nph import NphDataGenerator
+from rdr_service.data_gen.generators.nph import NphDataGenerator, NphSmsDataGenerator
+from rdr_service.offline.study_nph_biobank_file_export import get_processing_timestamp
 from rdr_service.offline.study_nph_biobank_import_inventory_file import import_biobank_inventory_into_stored_samples
 from tests.helpers.unittest_base import BaseTestCase
 from tests.test_data import data_path
@@ -56,3 +57,18 @@ class NphBiobankWorkflowsTest(BaseTestCase):
         stored_samples = ss_dao.get_all()
         self.assertEqual(stored_samples[0].sample_id, "00005")
         self.assertEqual(stored_samples[1].sample_id, "00006")
+
+    def test_no_error_on_timestamp(self):
+        sms_datagen = NphSmsDataGenerator()
+        sample = sms_datagen.create_database_ordered_sample(
+            nph_sample_id=10006,
+            test="ST01",
+            supplemental_fields={
+                "bowelMovement": "I had normal formed stool, and my stool looks like Type 3 and/or 4",
+                "bowelMovementQuality": "I tend to have normal formed stool - Type 3 and 4"
+            },
+            collected="2024-04-08T15:11:00",
+            finalized="2024-05-09T15:11:00",
+        )
+        timestamp = get_processing_timestamp(sample)
+        self.assertEqual(timestamp, sample.finalized)
