1df7a9975a15f9966b7c0533c9366fedba1c449f
davek14 <david.konopka@vumc.org>
2024-08-21 11:37:06 -0400

    removed unused imports in migration file

--- rdr_service/alembic/nph/versions/e8e1582e569c_new_fields_for_nph_storedsamples.py
+++ rdr_service/alembic/nph/versions/e8e1582e569c_new_fields_for_nph_storedsamples.py
@@ -7,17 +7,6 @@ Create Date: 2024-08-21 11:12:36.170010
 """
 from alembic import op
 import sqlalchemy as sa
-import rdr_service.model.utils
-
-
-from rdr_service.participant_enums import PhysicalMeasurementsStatus, QuestionnaireStatus, OrderStatus
-from rdr_service.participant_enums import WithdrawalStatus, WithdrawalReason, SuspensionStatus, QuestionnaireDefinitionStatus
-from rdr_service.participant_enums import EnrollmentStatus, Race, SampleStatus, OrganizationType, BiobankOrderStatus
-from rdr_service.participant_enums import OrderShipmentTrackingStatus, OrderShipmentStatus
-from rdr_service.participant_enums import MetricSetType, MetricsKey, GenderIdentity
-from rdr_service.model.base import add_table_history_table, drop_table_history_table
-from rdr_service.model.code import CodeType
-from rdr_service.model.site_enums import SiteStatus, EnrollingStatus, DigitalSchedulingStatus, ObsoleteStatus
 
 # revision identifiers, used by Alembic.
 revision = 'e8e1582e569c'
