44597fe78140e4d9554b59dfda5ea058f1a87554
DaveK-14 <131796727+DaveK-14@users.noreply.github.com>
2024-11-07 15:43:45 -0500

    Remove DuplicationData fields from PS queries. (#4000)
    * removed duplicationData field from PSApi query. Added hardcoded null value.
    
    * removed unused imports
    
    * removed test for duplicateaccounts function
    
    ---------
    
    Co-authored-by: davek14 <david.konopka@vumc.org>

--- rdr_service/dao/participant_summary_dao.py
+++ rdr_service/dao/participant_summary_dao.py
@@ -448,8 +448,7 @@ class ParticipantSummaryDao(UpdatableDao):
                 ParticipantSummary.participantId,
                 ParticipantSummary.firstName,
                 ParticipantSummary.lastName
-            ),
-            joinedload(ParticipantSummary.duplicationData)
+            )
         ]
 
     def get_by_hpo(self, hpo, session, yield_batch_size=1000):
@@ -1548,17 +1547,7 @@ class ParticipantSummaryDao(UpdatableDao):
             result['questionnaireOnEnvironmentalExposuresTime'] = env_exposures_data.created
             result['questionnaireOnEnvironmentalExposuresAuthored'] = env_exposures_data.value
 
-        duplicate_data = []
-        for duplicate in obj.duplicationData:
-            primary_account_id = duplicate.get_primary_id()
-            duplicate_data.append({
-                'duplicateId': to_client_participant_id(duplicate.get_other_participant_id(obj.participantId)),
-                'primaryAccount': to_client_participant_id(primary_account_id) if primary_account_id else 'UNSET',
-                'status': duplicate.status.name,
-                'timestamp': duplicate.authored.isoformat(),
-                'origin': duplicate.source.name
-            })
-        result['duplicationInfo'] = duplicate_data
+        result['duplicationInfo'] = []
 
         # Format other responses to default to UNSET when none
         field_names = [
