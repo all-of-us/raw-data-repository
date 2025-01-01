079c86ca79e0725a023dc2e01068257ec7d3982b
shahwaiz14 <shahwaiz14@hotmail.com>
2025-01-01 17:30:40 -0600

    Add logging for debugging

--- rdr_service/workflow_management/ppsc/ppsc_data_transfer_input_feed.py
+++ rdr_service/workflow_management/ppsc/ppsc_data_transfer_input_feed.py
@@ -329,9 +329,9 @@ class AwardeeInSiteFeed(PPSCBigQueryDatafeedBase):
         streaming_data_rows = self.make_datafeed_job(datafeed_def["streaming_data_sql"])
 
         dao = AwardeeInSiteDao()
-        print("total_rows", streaming_data_rows.total_rows)
         if streaming_data_rows:
             for row in streaming_data_rows:
+                print("total_rows", streaming_data_rows.total_rows)
                 print(row)
                 awardee_insite_dict = AwardeeInSiteFeed.row_to_dict(row)
                 camel_case_awardee_insite_dict = {
