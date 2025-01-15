6e28d1f21a25f2ac29c0e2a6667d903d9c38a1dd
Shahwaiz Punjwani <shahwaiz.punjwani@vumc.org>
2025-01-15 10:33:04 -0600

    [DA-4616] Update query for ehr and primary consents for Awardee InSite (#4044)
    * Update query for ehr and primary consents for awardee insite
    
    * Convert to bytes for patient_status
    
    * Convert to bytes for patient_status
    
    * Add tobase64 for patient_status

--- rdr_service/model/awardee_insite.py
+++ rdr_service/model/awardee_insite.py
@@ -324,7 +324,7 @@ class AwardeeInSite(PPSCBase):
                 (
                     f"COALESCE(CAST({key} AS STRING), ''), '|' "
                     if key != "patient_status"
-                    else "COALESCE(TO_JSON_STRING(patient_status), '[]'), '|' "
+                    else "TO_BASE64(CAST(TO_JSON_STRING(patient_status) AS BYTES)), '|' "
                 )
                 for key in keys
             ).rstrip(", '|' ")
