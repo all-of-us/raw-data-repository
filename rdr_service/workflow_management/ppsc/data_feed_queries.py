9cee88da0e9cf609b2779e0d8b8713f2d8a154f5
lancealopez <lance.a.lopez@vumc.org>
2025-04-09 10:21:11 -0500

    remove extra coalesce

--- rdr_service/workflow_management/ppsc/data_feed_queries.py
+++ rdr_service/workflow_management/ppsc/data_feed_queries.py
@@ -408,17 +408,17 @@ def insert_awardee_insite_data(
           ),
           profile_pivot AS (
             SELECT participant_id
-              , coalesce(piiname_first,'') AS first_name
-              , coalesce(piiname_middle,'')  AS middle_name
-              , coalesce(piiname_last,'')  AS last_name
-              , coalesce(streetaddress_piizip,'')  AS zip_code
-              , coalesce(streetaddress_piistate,'')  AS state
-              , coalesce(streetaddress_piicity,'')  AS city
-              , coalesce(piiaddress_streetaddress,'')  AS street_address
-              , coalesce(piiaddress_streetaddress2,'')  AS street_address2
-              , coalesce(piicontactinformation_phone,'')  AS phone_number
-              , coalesce(piicontactinformation_email ,'') AS email
-              , coalesce(piibirthinformation_birthdate,'')  AS date_of_birth
+              , piiname_first AS first_name
+              , piiname_middle AS middle_name
+              , piiname_last AS last_name
+              , streetaddress_piizip AS zip_code
+              , streetaddress_piistate AS state
+              , streetaddress_piicity AS city
+              , piiaddress_streetaddress AS street_address
+              , piiaddress_streetaddress2 AS street_address2
+              , piicontactinformation_phone AS phone_number
+              , piicontactinformation_email AS email
+              , piibirthinformation_birthdate AS date_of_birth
             FROM
               (
                 SELECT participant_id
@@ -429,9 +429,9 @@ def insert_awardee_insite_data(
               )
             PIVOT(ANY_VALUE(data_element_value)
                 FOR data_element_name IN
-                    ('piiname_first'
+                    (, coalesce(piiname_first,'') AS first_name
                       , 'piiname_middle'
-                      , 'piiname_last'
+                      , coalesce(piiname_last,'')  AS last_name
                       , 'streetaddress_piizip'
                       , 'streetaddress_piistate'
                       , 'streetaddress_piicity'
