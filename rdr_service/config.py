e5630fb5fd65f8fc944caf23b79925edf6e2df7b
Shahwaiz Punjwani <shahwaiz.punjwani@vumc.org>
2025-12-16 08:41:14 -0600

    [DA-5080] Job to drop EHR, Health Data Sharing Stream, and Core Data Report to AWS Bucket (#4147)
    * [DA-5080] Copy file from GCS to AWS
    
    * [DA-5080] Update

--- rdr_service/config.py
+++ rdr_service/config.py
@@ -318,6 +318,8 @@ PUBLIC_METRICS_GENDER_ANSWERS_TABLE = "public_metrics_gender_answers_table"
 PUBLIC_METRICS_RACE_ANSWERS_TABLE = "public_metrics_race_answers_table"
 PUBLIC_METRICS_PARTICIPANT_STATUS_EVENT_TABLE = "public_metrics_participant_status_event_table"
 
+PPSC_AWS_CREDS = "ppsc_aws_creds"
+
 # Overrides for testing scenarios
 CONFIG_OVERRIDES = {}
 
