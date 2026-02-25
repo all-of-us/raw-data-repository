1f594f7fbaf0febe26428d6af9ad05ed19f5ee86
shahwaiz14 <shahwaiz14@hotmail.com>
2026-02-25 14:08:24 -0600

    [DA-5146] Remove print

--- tests/api_tests/test_awardee_insite_api.py
+++ tests/api_tests/test_awardee_insite_api.py
@@ -277,8 +277,6 @@ class AwardeeInSiteApiTest(BaseTestCase):
         response = self.send_get("AwardeeInSite")
         result = response.get("entry")[0]["resource"]
 
-        print(result)
-
         self.assertEqual(result, expected_result)
 
 
