fbbde19fde6a4a57f962922cdbd4c69513f43945
brianmkimmel <brian.kimmel@vumc.org>
2026-03-05 11:56:07 -0600

    Update participant research_id job

--- rdr_service/offline/create_research_ids.py
+++ rdr_service/offline/create_research_ids.py
@@ -6,4 +6,4 @@ def create_participant_research_ids():
     research_id_dao = ParticipantResearchIdsDao()
     participants = research_id_dao.get_new_participants()
     research_id_dao.insert_new_participants(participants)
-
+    return '{ "success": "true" }'
